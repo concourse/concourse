d8bda62b83ff572d8c6b4327d00a11614309cb73
Alex Suraci <suraci.alex@gmail.com>
2014-04-26 22:16:35 -0700

    hack together redgreen api use

--- smith.go
+++ smith.go
@@ -3,35 +3,45 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/mgutz/ansi"
 	"github.com/pivotal-golang/archiver/compressor"
 )
 
-type Source struct {
-	Type string `json:"type"`
-	URI  string `json:"uri"`
-	Ref  string `json:"ref"`
+type Build struct {
+	Guid   string `json:"guid"`
+	Image  string `json:"image"`
+	Script string `json:"script"`
 }
 
-type Build struct {
-	Guid     string `json:"guid"`
-	Image    string `json:"image"`
-	Script   string `json:"script"`
-	Callback string `json:"callback"`
-	Source   Source `json:"source"`
+type BuildResult struct {
+	Status string `json:"status"`
 }
+
+var redgreenURL = flag.String(
+	"redgreenAddr",
+	"http://127.0.0.1:5637",
+	"address denoting the redgreen service",
+)
 
 func main() {
+	flag.Parse()
+
 	compressor := compressor.NewTgz()
+
 	src, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Couldn't get current directory...")
 		os.Exit(1)
 	}
+
 	dest, err := ioutil.TempFile("", "smith")
 	if err != nil {
 		fmt.Println("Couldn't create temporary file...")
@@ -45,55 +55,82 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Created archive of current directory: %s", dest.Name())
-
-	http.HandleFunc("/file", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, dest.Name())
-	})
-
-	http.HandleFunc("/die", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		os.Exit(0)
-	})
-
-	// RAAAAACE CONDITION
-	go http.ListenAndServe(":8080", nil)
-
 	build := Build{
-		Guid:     "abc",
-		Image:    "ubuntu",
-		Script:   "ls -l",
-		Callback: "http://localhost:8080/die",
-		Source: Source{
-			Type: "raw",
-			URI:  "http://localhost:8080/file",
-			Ref:  "",
-		},
+		Image:  "mischief/docker-golang",
+		Script: "find .",
 	}
 
-	client := &http.Client{}
 	buffer := &bytes.Buffer{}
-	encoder := json.NewEncoder(buffer)
-	encoder.Encode(build)
 
-	req, err := http.NewRequest("POST", "http://localhost:4637/builds", buffer)
+	err = json.NewEncoder(buffer).Encode(build)
 	if err != nil {
-		fmt.Println("Couldn't create request... %s", err)
-		os.Exit(1)
-	}
-	req.Header.Add("Content-Type", "application/json")
-	response, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Request failed... %s", err)
-		os.Exit(1)
+		log.Fatalln("encoding build failed:", err)
 	}
 
-	body, err := ioutil.ReadAll(response.Body)
+	response, err := http.Post(
+		*redgreenURL+"/builds",
+		"application/json",
+		buffer,
+	)
 	if err != nil {
-		fmt.Println("Body failed... %s", err)
-		os.Exit(1)
+		log.Fatalln("request failed:", err)
 	}
-	fmt.Println("%s", string(body))
 
-	select {}
+	if response.StatusCode != http.StatusCreated {
+		log.Fatalln("bad response:", response)
+	}
+
+	err = json.NewDecoder(response.Body).Decode(&build)
+	if err != nil {
+		log.Fatalln("response decoding failed:", err)
+	}
+
+	archive, err := os.Open(dest.Name())
+	if err != nil {
+		log.Fatalln("could not open archive")
+	}
+
+	response, err = http.Post(
+		*redgreenURL+"/builds/"+build.Guid+"/bits",
+		"application/octet-stream",
+		archive,
+	)
+	if err != nil {
+		log.Fatalln("request failed:", err)
+	}
+
+	if response.StatusCode != http.StatusCreated {
+		log.Fatalln("bad response:", response)
+	}
+
+	for {
+		var result BuildResult
+
+		response, err := http.Get(*redgreenURL + "/builds/" + build.Guid + "/result")
+		if err != nil {
+			log.Fatalln("error polling for result:", err)
+		}
+
+		err = json.NewDecoder(response.Body).Decode(&result)
+		if err != nil {
+			log.Fatalln("error decoding result:", err)
+		}
+
+		if result.Status != "" {
+			exitCode := 1
+			if result.Status == "succeeded" {
+				exitCode = 0
+			}
+
+			if exitCode == 0 {
+				fmt.Println(ansi.Color(result.Status, "green"))
+			} else {
+				fmt.Println(ansi.Color(result.Status, "red"))
+			}
+
+			os.Exit(exitCode)
+		}
+
+		time.Sleep(time.Second)
+	}
 }
