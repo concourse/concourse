ca897fb906741b286f832eb4b96f037875081b21
Alex Suraci <suraci.alex@gmail.com>
2014-05-01 21:36:08 -0700

    report upload progress

--- smith.go
+++ smith.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/cheggaaa/pb"
 	"github.com/fraenkel/candiedyaml"
 	"github.com/gorilla/websocket"
 	"github.com/mgutz/ansi"
@@ -183,10 +184,21 @@ func upload(build Build) {
 		log.Fatalln("could not open archive:", err)
 	}
 
+	info, err := archive.Stat()
+	if err != nil {
+		log.Fatalln("could not stat archive:", err)
+	}
+
+	progress := pb.New64(info.Size())
+	progress.SetUnits(pb.U_BYTES)
+
+	progress.Start()
+	defer progress.Finish()
+
 	response, err := http.Post(
 		"http://"+*redgreenAddr+"/builds/"+build.Guid+"/bits",
 		"application/octet-stream",
-		archive,
+		progress.NewProxyReader(archive),
 	)
 	if err != nil {
 		log.Fatalln("request failed:", err)
