6eef360e167d7fbc24a36164a1e16e57d39c3cc3
Alex Suraci <suraci.alex@gmail.com>
2014-07-13 16:31:54 -0700

    fly test uses helper rootfs

--- flying_test.go
+++ flying_test.go
@@ -1,6 +1,9 @@
 package testflight_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
@@ -12,9 +15,40 @@ import (
 )
 
 var _ = Describe("Flying", func() {
+	var tmpdir string
+
+	BeforeEach(func() {
+		var err error
+
+		tmpdir, err = ioutil.TempDir("", "fly-test")
+		Ω(err).ShouldNot(HaveOccurred())
+
+		err = ioutil.WriteFile(filepath.Join(tmpdir, "build.yml"), []byte(fmt.Sprintf(`---
+---
+image: %s
+
+params:
+  FOO: 1
+
+run:
+  path: bash
+  args:
+    - -c
+    - |
+      echo some output
+      echo FOO is $FOO
+      exit 0
+`, helperRootfs)), 0644)
+		Ω(err).ShouldNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		os.RemoveAll(tmpdir)
+	})
+
 	It("works", func() {
 		fly := exec.Command(builtComponents["fly"])
-		fly.Dir = filepath.Join(fixturesDir, "trivial-build")
+		fly.Dir = tmpdir
 
 		session, err := gexec.Start(fly, GinkgoWriter, GinkgoWriter)
 		Ω(err).ShouldNot(HaveOccurred())
