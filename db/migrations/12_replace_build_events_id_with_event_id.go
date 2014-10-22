ab78200145485b3e07476f7e9fe8fe96a8e71948
Alex Suraci <suraci.alex@gmail.com>
2014-10-21 23:52:44 -0700

    make migration work on datasets larger than zero
    single query = not webscale

--- db/migrations/12_replace_build_events_id_with_event_id.go
+++ db/migrations/12_replace_build_events_id_with_event_id.go
@@ -8,18 +8,41 @@ func ReplaceBuildEventsIDWithEventID(tx migration.LimitedTx) error {
 		return err
 	}
 
-	_, err = tx.Exec(`
-		UPDATE build_events AS a
-		SET event_id = a.id - (
-			SELECT b.id
-			FROM build_events b
-			WHERE b.build_id = a.build_id
-			ORDER BY b.id ASC
-			LIMIT 1
-		)
+	startIDs := map[int]int{}
+
+	rows, err := tx.Query(`
+		SELECT build_id, min(id)
+		FROM build_events
+		GROUP BY build_id
 	`)
 	if err != nil {
 		return err
+	}
+
+	for rows.Next() {
+		var buildID, id int
+		err := rows.Scan(&buildID, &id)
+		if err != nil {
+			return err
+		}
+
+		startIDs[buildID] = id
+	}
+
+	err = rows.Close()
+	if err != nil {
+		return err
+	}
+
+	for buildID, id := range startIDs {
+		_, err := tx.Exec(`
+			UPDATE build_events
+			SET event_id = id - $2
+			WHERE build_id = $1
+		`, buildID, id)
+		if err != nil {
+			return err
+		}
 	}
 
 	_, err = tx.Exec(`ALTER TABLE build_events DROP COLUMN id`)
