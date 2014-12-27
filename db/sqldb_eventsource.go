6c38f97fa857ed251dbd2cf5d6f5e0c111efeefc
Alex Suraci <suraci.alex@gmail.com>
2014-12-27 13:13:02 -0800

    fix db events stopping after batch size events

--- db/sqldb_eventsource.go
+++ db/sqldb_eventsource.go
@@ -81,85 +81,93 @@ func (source *sqldbBuildEventSource) collectEvents(cursor uint) {
 			source.err = ErrBuildEventStreamClosed
 			close(source.events)
 			return
+		default:
+		}
 
+		rows, err := source.conn.Query(`
+			SELECT type, version, payload
+			FROM build_events
+			WHERE build_id = $1
+			ORDER BY event_id ASC
+			OFFSET $2
+			LIMIT $3
+		`, source.buildID, cursor, batchSize)
+		if err != nil {
+			source.err = err
+			close(source.events)
+			return
+		}
+
+		rowsReturned := 0
+
+		for rows.Next() {
+			rowsReturned++
+
+			cursor++
+
+			var t, v, p string
+			err := rows.Scan(&t, &v, &p)
+			if err != nil {
+				rows.Close()
+
+				source.err = err
+				close(source.events)
+				return
+			}
+
+			ev, err := event.ParseEvent(atc.EventVersion(v), atc.EventType(t), []byte(p))
+			if err != nil {
+				rows.Close()
+
+				source.err = err
+				close(source.events)
+				return
+			}
+
+			select {
+			case source.events <- ev:
+			case <-source.stop:
+				rows.Close()
+
+				source.err = ErrBuildEventStreamClosed
+				close(source.events)
+				return
+			}
+		}
+
+		if rowsReturned == batchSize {
+			// still more events
+			continue
+		}
+
+		var completed bool
+		var lastEventID uint
+		err = source.conn.QueryRow(`
+			SELECT builds.completed, coalesce(max(build_events.event_id), 0)
+			FROM builds
+			LEFT JOIN build_events
+			ON build_events.build_id = builds.id
+			WHERE builds.id = $1
+			GROUP BY builds.id
+		`, source.buildID).Scan(&completed, &lastEventID)
+		if err != nil {
+			source.err = err
+			close(source.events)
+			return
+		}
+
+		if completed && cursor > lastEventID {
+			source.err = ErrEndOfBuildEventStream
+			close(source.events)
+			return
+		}
+
+		select {
 		case <-source.notifier.Notify():
-			rows, err := source.conn.Query(`
-				SELECT type, version, payload
-				FROM build_events
-				WHERE build_id = $1
-				ORDER BY event_id ASC
-				OFFSET $2
-				LIMIT $3
-			`, source.buildID, cursor, batchSize)
-			if err != nil {
-				source.err = err
-				close(source.events)
-				return
-			}
-
-			rowsReturned := 0
-
-			for rows.Next() {
-				rowsReturned++
-
-				cursor++
-
-				var t, v, p string
-				err := rows.Scan(&t, &v, &p)
-				if err != nil {
-					rows.Close()
-
-					source.err = err
-					close(source.events)
-					return
-				}
-
-				ev, err := event.ParseEvent(atc.EventVersion(v), atc.EventType(t), []byte(p))
-				if err != nil {
-					rows.Close()
-
-					source.err = err
-					close(source.events)
-					return
-				}
-
-				select {
-				case source.events <- ev:
-				case <-source.stop:
-					rows.Close()
-
-					source.err = ErrBuildEventStreamClosed
-					close(source.events)
-					return
-				}
-			}
-
-			if rowsReturned == batchSize {
-				// still potentially more events; keep going
-				continue
-			}
-
-			var completed bool
-			var lastEventID uint
-			err = source.conn.QueryRow(`
-				SELECT builds.completed, coalesce(max(build_events.event_id), 0)
-				FROM builds
-				LEFT JOIN build_events
-				ON build_events.build_id = builds.id
-				WHERE builds.id = $1
-				GROUP BY builds.id
-			`, source.buildID).Scan(&completed, &lastEventID)
-			if err != nil {
-				source.err = err
-				close(source.events)
-				return
-			}
-
-			if completed && cursor > lastEventID {
-				source.err = ErrEndOfBuildEventStream
-				close(source.events)
-				return
-			}
+		case <-source.stop:
+			source.err = ErrBuildEventStreamClosed
+			close(source.events)
+			return
 		}
 	}
 }
