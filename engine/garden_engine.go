44a1ac6504d4c5bf87768908a993523e770db08b
Alex Suraci <suraci.alex@gmail.com>
2015-01-10 10:44:49 -0800

    initial refactoring of garden engine

--- engine/garden_engine.go
+++ engine/garden_engine.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sync"
-	"time"
 	"unicode/utf8"
 
 	garden "github.com/cloudfoundry-incubator/garden/api"
@@ -93,91 +91,15 @@ func (build *gardenBuild) Abort() error {
 	return nil
 }
 
-type implicitOutput struct {
-	plan atc.InputPlan
-	info exec.VersionInfo
-}
-
-type implicitOutputTracker struct {
-	implicitOutputs  map[string]implicitOutput
-	implicitOutputsL sync.Mutex
-}
-
-func (t *implicitOutputTracker) register(name string, output implicitOutput) {
-	t.implicitOutputsL.Lock()
-	t.implicitOutputs[name] = output
-	t.implicitOutputsL.Unlock()
-}
-
-func (t *implicitOutputTracker) unregister(name string) {
-	t.implicitOutputsL.Lock()
-	delete(t.implicitOutputs, name)
-	t.implicitOutputsL.Unlock()
-}
-
-// Compose(
-//   Compose(
-//     Aggregate{ // inputs
-//       "A": Get()
-//       "B": Get()
-//     },
-//     Execute( // build
-//       BuildConfigSource,
-//     ),
-//   ),
-//   Aggregate{ // outputs
-//     "C": Conditional{Put()},
-//     "D": Conditional{Put()},
-//   },
-// )
 func (build *gardenBuild) Resume(lager.Logger) {
 	plan := build.metadata.Plan
 
-	implicitOutputsT := &implicitOutputTracker{
-		implicitOutputs: map[string]implicitOutput{},
-	}
+	delegate := newDelegate(build.db, build.buildID)
 
 	var step exec.Step
 
 	if len(plan.Inputs) > 0 {
-		inputs := exec.Aggregate{}
-
-		for _, input := range plan.Inputs {
-			origin := event.Origin{
-				Type: event.OriginTypeInput,
-				Name: input.Name,
-			}
-
-			ioConfig := exec.IOConfig{
-				Stdout: build.ioWriter(origin),
-				Stderr: build.ioWriter(origin),
-			}
-
-			resourceConfig := atc.ResourceConfig{
-				Name:   input.Resource,
-				Type:   input.Type,
-				Source: input.Source,
-			}
-
-			plan := input
-			inputs[input.Name] = exec.OnComplete(
-				build.factory.Get(build.inputSessionID(input.Name), ioConfig, resourceConfig, input.Params, input.Version),
-				func(err error, source exec.ArtifactSource) {
-					if err != nil {
-						build.saveErr(err, origin)
-					} else {
-						var info exec.VersionInfo
-						if source.Result(&info) {
-							build.saveInput(plan, info)
-						}
-
-						implicitOutputsT.register(plan.Resource, implicitOutput{plan, info})
-					}
-				},
-			)
-		}
-
-		step = inputs
+		step = build.aggregateInputsStep(delegate)
 	}
 
 	var configSource exec.BuildConfigSource
@@ -192,120 +114,34 @@ func (build *gardenBuild) Resume(lager.Logger) {
 		configSource = exec.FileConfigSource{plan.ConfigPath}
 	}
 
-	successReporter := exec.NewSuccessReporter()
-
 	if configSource != nil {
-		ioConfig := exec.IOConfig{
-			Stdout: build.ioWriter(event.Origin{
-				Type: event.OriginTypeRun,
-				Name: "stdout",
-			}),
-			Stderr: build.ioWriter(event.Origin{
-				Type: event.OriginTypeRun,
-				Name: "stderr",
-			}),
-		}
-
-		configSource = initializeEmitter{
-			buildID:           build.buildID,
-			db:                build.db,
-			BuildConfigSource: configSource,
-		}
-
-		step = exec.Compose(step, exec.OnComplete(
-			successReporter.Subject(build.factory.Execute(build.executeSessionID(), ioConfig, configSource)),
-			func(err error, source exec.ArtifactSource) {
-				if err != nil {
-					build.saveErr(err, event.Origin{})
-				} else {
-					var status exec.ExitStatus
-					if source.Result(&status) {
-						build.saveFinish(status)
-					}
-				}
-			},
-		))
+		step = exec.Compose(step, build.executeStep(delegate, configSource))
 	}
 
 	if len(plan.Outputs) > 0 {
-		outputs := exec.Aggregate{}
-
-		for _, output := range plan.Outputs {
-			origin := event.Origin{
-				Type: event.OriginTypeOutput,
-				Name: output.Name,
-			}
-
-			ioConfig := exec.IOConfig{
-				Stdout: build.ioWriter(origin),
-				Stderr: build.ioWriter(origin),
-			}
-
-			resourceConfig := atc.ResourceConfig{
-				Name:   output.Name,
-				Type:   output.Type,
-				Source: output.Source,
-			}
-
-			plan := output
-
-			outputs[output.Name] = exec.Conditional{
-				Conditions: output.On,
-				Step: exec.OnComplete(
-					build.factory.Put(build.outputSessionID(output.Name), ioConfig, resourceConfig, output.Params),
-					func(err error, source exec.ArtifactSource) {
-						if err != nil {
-							build.saveErr(err, origin)
-						} else {
-							implicitOutputsT.unregister(plan.Name)
-
-							var info exec.VersionInfo
-							if source.Result(&info) {
-								build.saveOutput(plan, info)
-							}
-						}
-					},
-				),
-			}
-		}
-
-		step = exec.Compose(step, outputs)
+		step = exec.Compose(step, build.aggregateOutputsStep(delegate))
 	}
+
+	step = exec.OnComplete(step, delegate.Finish())
 
 	source := step.Using(&exec.NoopArtifactSource{})
 
-	build.saveStart()
+	delegate.Start()
 
 	process := ifrit.Background(source)
 
 	exited := process.Wait()
 
-	var aborted bool
-
 	for {
 		select {
-		case err := <-exited:
-			if aborted {
-				build.saveStatus(atc.StatusAborted)
-			} else if err != nil {
-				build.saveStatus(atc.StatusErrored)
-			} else if successReporter.Successful() {
-				build.saveStatus(atc.StatusSucceeded)
-
-				for _, o := range implicitOutputsT.implicitOutputs {
-					build.saveImplicitOutput(o.plan, o.info)
-				}
-			} else {
-				build.saveStatus(atc.StatusFailed)
-			}
-
+		case <-exited:
 			return
 
 		case sig := <-build.signals:
 			process.Signal(sig)
 
 			if sig == os.Kill {
-				aborted = true
+				delegate.Aborted()
 			}
 		}
 	}
@@ -313,6 +149,91 @@ func (build *gardenBuild) Resume(lager.Logger) {
 
 func (build *gardenBuild) Hijack(spec garden.ProcessSpec, io garden.ProcessIO) (garden.Process, error) {
 	return build.factory.Hijack(build.executeSessionID(), spec, io)
+}
+
+func (build *gardenBuild) aggregateInputsStep(delegate Delegate) exec.Step {
+	inputs := exec.Aggregate{}
+
+	for _, input := range build.metadata.Plan.Inputs {
+		origin := event.Origin{
+			Type: event.OriginTypeInput,
+			Name: input.Name,
+		}
+
+		ioConfig := exec.IOConfig{
+			Stdout: build.ioWriter(origin),
+			Stderr: build.ioWriter(origin),
+		}
+
+		resourceConfig := atc.ResourceConfig{
+			Name:   input.Resource,
+			Type:   input.Type,
+			Source: input.Source,
+		}
+
+		inputs[input.Name] = exec.OnComplete(
+			build.factory.Get(build.inputSessionID(input.Name), ioConfig, resourceConfig, input.Params, input.Version),
+			delegate.InputCompleted(input),
+		)
+	}
+
+	return inputs
+}
+
+func (build *gardenBuild) executeStep(delegate Delegate, configSource exec.BuildConfigSource) exec.Step {
+	ioConfig := exec.IOConfig{
+		Stdout: build.ioWriter(event.Origin{
+			Type: event.OriginTypeRun,
+			Name: "stdout",
+		}),
+		Stderr: build.ioWriter(event.Origin{
+			Type: event.OriginTypeRun,
+			Name: "stderr",
+		}),
+	}
+
+	configSource = initializeEmitter{
+		buildID:           build.buildID,
+		db:                build.db,
+		BuildConfigSource: configSource,
+	}
+
+	return exec.OnComplete(
+		build.factory.Execute(build.executeSessionID(), ioConfig, configSource),
+		delegate.ExecutionCompleted(),
+	)
+}
+
+func (build *gardenBuild) aggregateOutputsStep(delegate Delegate) exec.Step {
+	outputs := exec.Aggregate{}
+
+	for _, output := range build.metadata.Plan.Outputs {
+		origin := event.Origin{
+			Type: event.OriginTypeOutput,
+			Name: output.Name,
+		}
+
+		ioConfig := exec.IOConfig{
+			Stdout: build.ioWriter(origin),
+			Stderr: build.ioWriter(origin),
+		}
+
+		resourceConfig := atc.ResourceConfig{
+			Name:   output.Name,
+			Type:   output.Type,
+			Source: output.Source,
+		}
+
+		outputs[output.Name] = exec.Conditional{
+			Conditions: output.On,
+			Step: exec.OnComplete(
+				build.factory.Put(build.outputSessionID(output.Name), ioConfig, resourceConfig, output.Params),
+				delegate.OutputCompleted(output),
+			),
+		}
+	}
+
+	return outputs
 }
 
 func (build *gardenBuild) executeSessionID() exec.SessionID {
@@ -325,103 +246,6 @@ func (build *gardenBuild) inputSessionID(inputName string) exec.SessionID {
 
 func (build *gardenBuild) outputSessionID(outputName string) exec.SessionID {
 	return exec.SessionID(fmt.Sprintf("build-%d-output-%s", build.buildID, outputName))
-}
-
-func (build *gardenBuild) saveStart() {
-	// TODO handle errs
-
-	time := time.Now()
-
-	build.db.SaveBuildStartTime(build.buildID, time)
-
-	build.db.SaveBuildStatus(build.buildID, db.StatusStarted)
-
-	build.db.SaveBuildEvent(build.buildID, event.Start{
-		Time: time.Unix(),
-	})
-
-	build.db.SaveBuildEvent(build.buildID, event.Status{
-		Status: atc.StatusStarted,
-		Time:   time.Unix(),
-	})
-}
-
-func (build *gardenBuild) saveFinish(status exec.ExitStatus) {
-	build.db.SaveBuildEvent(build.buildID, event.Finish{
-		ExitStatus: int(status),
-		Time:       time.Now().Unix(),
-	})
-}
-
-func (build *gardenBuild) saveStatus(status atc.BuildStatus) {
-	// TODO handle errs
-
-	time := time.Now()
-
-	build.db.SaveBuildEndTime(build.buildID, time)
-
-	build.db.SaveBuildStatus(build.buildID, db.Status(status))
-
-	build.db.SaveBuildEvent(build.buildID, event.Status{
-		Status: status,
-		Time:   time.Unix(),
-	})
-
-	build.db.CompleteBuild(build.buildID)
-}
-
-func (build *gardenBuild) saveErr(err error, origin event.Origin) {
-	// TODO handle errs
-
-	build.db.SaveBuildEvent(build.buildID, event.Error{
-		Message: err.Error(),
-		Origin:  origin,
-	})
-}
-
-func (build *gardenBuild) saveInput(plan atc.InputPlan, info exec.VersionInfo) {
-	ev := event.Input{
-		Plan:            plan,
-		FetchedVersion:  info.Version,
-		FetchedMetadata: info.Metadata,
-	}
-
-	build.db.SaveBuildEvent(build.buildID, ev)
-
-	build.db.SaveBuildInput(build.buildID, db.BuildInput{
-		Name:              plan.Name,
-		VersionedResource: vrFromInput(ev),
-	})
-}
-
-func (build *gardenBuild) saveOutput(plan atc.OutputPlan, info exec.VersionInfo) {
-	ev := event.Output{
-		Plan:            plan,
-		CreatedVersion:  info.Version,
-		CreatedMetadata: info.Metadata,
-	}
-
-	build.db.SaveBuildEvent(build.buildID, ev)
-
-	build.db.SaveBuildOutput(build.buildID, vrFromOutput(ev))
-}
-
-func (build *gardenBuild) saveImplicitOutput(plan atc.InputPlan, info exec.VersionInfo) {
-	metadata := make([]db.MetadataField, len(info.Metadata))
-	for i, md := range info.Metadata {
-		metadata[i] = db.MetadataField{
-			Name:  md.Name,
-			Value: md.Value,
-		}
-	}
-
-	build.db.SaveBuildOutput(build.buildID, db.VersionedResource{
-		Resource: plan.Resource,
-		Type:     plan.Type,
-		Source:   db.Source(plan.Source),
-		Version:  db.Version(info.Version),
-		Metadata: metadata,
-	})
 }
 
 func (build *gardenBuild) ioWriter(origin event.Origin) io.Writer {
---
+++ engine/garden_engine_build_delegate.go
@@ -0,0 +1,242 @@
+package engine
+
+import (
+	"sync"
+	"time"
+
+	"github.com/concourse/atc"
+	"github.com/concourse/atc/db"
+	"github.com/concourse/atc/event"
+	"github.com/concourse/atc/exec"
+)
+
+type implicitOutput struct {
+	plan atc.InputPlan
+	info exec.VersionInfo
+}
+
+type Delegate interface {
+	InputCompleted(atc.InputPlan) exec.CompleteCallback
+	ExecutionCompleted() exec.CompleteCallback
+	OutputCompleted(atc.OutputPlan) exec.CompleteCallback
+
+	Start()
+	Finish() exec.CompleteCallback
+	Aborted()
+}
+
+type delegate struct {
+	db EngineDB
+
+	buildID int
+
+	successful bool
+	aborted    bool
+
+	implicitOutputs map[string]implicitOutput
+
+	lock sync.Mutex
+}
+
+func newDelegate(db EngineDB, buildID int) Delegate {
+	return &delegate{
+		db: db,
+
+		buildID: buildID,
+
+		successful: true,
+		aborted:    false,
+
+		implicitOutputs: make(map[string]implicitOutput),
+	}
+}
+
+func (delegate *delegate) InputCompleted(plan atc.InputPlan) exec.CompleteCallback {
+	return func(err error, source exec.ArtifactSource) {
+		if err != nil {
+			delegate.saveErr(err, event.Origin{
+				Type: event.OriginTypeInput,
+				Name: plan.Name,
+			})
+		} else {
+			var info exec.VersionInfo
+			if source.Result(&info) {
+				delegate.saveInput(plan, info)
+				delegate.registerImplicitOutput(plan.Resource, implicitOutput{plan, info})
+			}
+		}
+	}
+}
+
+func (delegate *delegate) ExecutionCompleted() exec.CompleteCallback {
+	return func(err error, source exec.ArtifactSource) {
+		if err != nil {
+			delegate.saveErr(err, event.Origin{})
+		} else {
+			var status exec.ExitStatus
+			if source.Result(&status) {
+				delegate.saveFinish(status)
+			}
+
+			var success exec.Success
+			if source.Result(&success) {
+				if success == false {
+					delegate.successful = false
+				}
+			}
+		}
+	}
+}
+
+func (delegate *delegate) OutputCompleted(plan atc.OutputPlan) exec.CompleteCallback {
+	return func(err error, source exec.ArtifactSource) {
+		if err != nil {
+			delegate.saveErr(err, event.Origin{
+				Type: event.OriginTypeOutput,
+				Name: plan.Name,
+			})
+		} else {
+			delegate.unregisterImplicitOutput(plan.Name)
+
+			var info exec.VersionInfo
+			if source.Result(&info) {
+				delegate.saveOutput(plan, info)
+			}
+		}
+	}
+}
+
+func (delegate *delegate) Start() {
+	delegate.saveStart()
+}
+
+func (delegate *delegate) Finish() exec.CompleteCallback {
+	return func(err error, source exec.ArtifactSource) {
+		if delegate.aborted {
+			delegate.saveStatus(atc.StatusAborted)
+		} else if err != nil {
+			delegate.saveStatus(atc.StatusErrored)
+		} else if delegate.successful {
+			delegate.saveStatus(atc.StatusSucceeded)
+
+			for _, o := range delegate.implicitOutputs {
+				delegate.saveImplicitOutput(o.plan, o.info)
+			}
+		} else {
+			delegate.saveStatus(atc.StatusFailed)
+		}
+	}
+}
+
+func (delegate *delegate) Aborted() {
+	delegate.aborted = true
+}
+
+func (delegate *delegate) registerImplicitOutput(resource string, output implicitOutput) {
+	delegate.lock.Lock()
+	delegate.implicitOutputs[resource] = output
+	delegate.lock.Unlock()
+}
+
+func (delegate *delegate) unregisterImplicitOutput(resource string) {
+	delegate.lock.Lock()
+	delete(delegate.implicitOutputs, resource)
+	delegate.lock.Unlock()
+}
+
+func (delegate *delegate) saveStart() {
+	// TODO handle errs
+
+	time := time.Now()
+
+	delegate.db.SaveBuildStartTime(delegate.buildID, time)
+
+	delegate.db.SaveBuildStatus(delegate.buildID, db.StatusStarted)
+
+	delegate.db.SaveBuildEvent(delegate.buildID, event.Start{
+		Time: time.Unix(),
+	})
+
+	delegate.db.SaveBuildEvent(delegate.buildID, event.Status{
+		Status: atc.StatusStarted,
+		Time:   time.Unix(),
+	})
+}
+
+func (delegate *delegate) saveFinish(status exec.ExitStatus) {
+	delegate.db.SaveBuildEvent(delegate.buildID, event.Finish{
+		ExitStatus: int(status),
+		Time:       time.Now().Unix(),
+	})
+}
+
+func (delegate *delegate) saveStatus(status atc.BuildStatus) {
+	// TODO handle errs
+
+	time := time.Now()
+
+	delegate.db.SaveBuildEndTime(delegate.buildID, time)
+
+	delegate.db.SaveBuildStatus(delegate.buildID, db.Status(status))
+
+	delegate.db.SaveBuildEvent(delegate.buildID, event.Status{
+		Status: status,
+		Time:   time.Unix(),
+	})
+
+	delegate.db.CompleteBuild(delegate.buildID)
+}
+
+func (delegate *delegate) saveErr(err error, origin event.Origin) {
+	// TODO handle errs
+
+	delegate.db.SaveBuildEvent(delegate.buildID, event.Error{
+		Message: err.Error(),
+		Origin:  origin,
+	})
+}
+
+func (delegate *delegate) saveInput(plan atc.InputPlan, info exec.VersionInfo) {
+	ev := event.Input{
+		Plan:            plan,
+		FetchedVersion:  info.Version,
+		FetchedMetadata: info.Metadata,
+	}
+
+	delegate.db.SaveBuildEvent(delegate.buildID, ev)
+
+	delegate.db.SaveBuildInput(delegate.buildID, db.BuildInput{
+		Name:              plan.Name,
+		VersionedResource: vrFromInput(ev),
+	})
+}
+
+func (delegate *delegate) saveOutput(plan atc.OutputPlan, info exec.VersionInfo) {
+	ev := event.Output{
+		Plan:            plan,
+		CreatedVersion:  info.Version,
+		CreatedMetadata: info.Metadata,
+	}
+
+	delegate.db.SaveBuildEvent(delegate.buildID, ev)
+
+	delegate.db.SaveBuildOutput(delegate.buildID, vrFromOutput(ev))
+}
+
+func (delegate *delegate) saveImplicitOutput(plan atc.InputPlan, info exec.VersionInfo) {
+	metadata := make([]db.MetadataField, len(info.Metadata))
+	for i, md := range info.Metadata {
+		metadata[i] = db.MetadataField{
+			Name:  md.Name,
+			Value: md.Value,
+		}
+	}
+
+	delegate.db.SaveBuildOutput(delegate.buildID, db.VersionedResource{
+		Resource: plan.Resource,
+		Type:     plan.Type,
+		Source:   db.Source(plan.Source),
+		Version:  db.Version(info.Version),
+		Metadata: metadata,
+	})
+}
