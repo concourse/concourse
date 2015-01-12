f384dd4d9027ab93b081188afddebd0dfd765e57
Chris Brown <xoebus@xoeb.us>
2015-01-12 00:20:24 +0000

    make version argument to docs optional

--- docs/common.rkt
+++ docs/common.rkt
@@ -2,4 +2,8 @@
 
 (provide version)
 
-(define version (vector-ref (current-command-line-arguments) 0))
+(define version
+  (let ([version-arg-provided (positive? (vector-length (current-command-line-arguments)))])
+    (if version-arg-provided
+      (vector-ref (current-command-line-arguments) 0)   
+      "x.x.x")))
