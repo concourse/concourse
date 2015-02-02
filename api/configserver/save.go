ecbf7e3a65cce0c09ffa7f619168625b6fe0085f
Alex Suraci <suraci.alex@gmail.com>
2015-02-02 09:57:17 -0800

    log when saving config

--- api/configserver/save.go
+++ api/configserver/save.go
@@ -6,28 +6,38 @@ import (
 	"net/http"
 
 	"github.com/concourse/atc"
+	"github.com/pivotal-golang/lager"
 )
 
 func (s *Server) SaveConfig(w http.ResponseWriter, r *http.Request) {
+	session := s.logger.Session("set-config")
+
 	var config atc.Config
 	err := json.NewDecoder(r.Body).Decode(&config)
 	if err != nil {
+		session.Error("malformed-json", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	err = s.validate(config)
 	if err != nil {
+		session.Error("ignoring-invalid-config", err)
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "%s", err)
 		return
 	}
 
+	session.Info("saving", lager.Data{"config": config})
+
 	err = s.db.SaveConfig(config)
 	if err != nil {
+		session.Error("failed-to-save-config", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	session.Info("saved")
 
 	w.WriteHeader(http.StatusOK)
 }
