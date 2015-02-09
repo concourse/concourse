95198c9ce28fab7ee691439fe71cd014f1ef2bc1
Alex Suraci <suraci.alex@gmail.com>
2015-02-08 19:29:13 -0800

    switch to node-ansiparser; fixes some jankiness

--- javascript/stores.js
+++ javascript/stores.js
@@ -2,6 +2,8 @@ var Fluxxor = require('fluxxor');
 var Immutable = require('immutable');
 var Cursor = require('immutable/contrib/cursor');
 
+var AnsiParser = require('node-ansiparser');
+
 var BATCH_SIZE = 300;
 var EMIT_INTERVAL = 300;
 
@@ -15,50 +17,100 @@ var constants = {
 }
 
 function LogsModel() {
-  this.carriageReturned = false;
   this.lines = Immutable.fromJS([[[]]]);
   this.batchCursor = Cursor.from(this.lines, function(newLines) {
     this.lines = newLines;
   }.bind(this));
   this.linesCursor = this.batchCursor.last();
-  this.carriage = 0;
+  this.seqsSinceCR = 0;
+
+  this.state = {}
+
+  this.inst_p = function(s) {
+    var textLen = s.length;
+    var seqsSinceCR = this.seqsSinceCR;
+
+    var cursor = this.cursor.update(function(line) {
+      return line.slice(seqsSinceCR);
+    });
+
+    this.cursor = this.cursor.update(function(line) {
+      return line.slice(0, seqsSinceCR);
+    });
+
+    this.pushSequence({
+      text: s,
+      foreground: this.state.foreground,
+      background: this.state.background,
+      bold: this.state.bold,
+      italic: this.state.italic,
+      underline: this.state.underline
+    });
+
+    this.seqsSinceCR++;
+
+    cursor.forEach(function(e, i) {
+      if(e.text.length >= textLen) {
+        e.text = e.text.substr(textLen);
+        textLen -= e.text.length;
+        this.pushSequence(e);
+        return false;
+      }
+    }, this);
+
+    this.refreshLineCursor();
+    this.changed = true;
+  }
+
+  this.inst_x = function(flag) {
+    switch(flag.charCodeAt(0)) {
+    case 10: // LN
+      this.pushLine(Immutable.List.of({text: "\n", linebreak: true}))
+      this.refreshCursor();
+      this.changed = true;
+      this.seqsSinceCR = 0;
+      break;
+    case 13: // CR
+      this.seqsSinceCR = 0;
+      break;
+    default:
+      this.inst_p(flag);
+      break;
+    }
+  }
+
+  this.inst_c = function(collected, params, flag) {
+    for (var p in params) {
+      var ansiCode = params[p];
+
+      if (foregroundColors[ansiCode]) {
+        this.state.foreground = foregroundColors[ansiCode];
+      } else if (brightForegroundColors[ansiCode]) {
+        this.state.foreground = brightForegroundColors[ansiCode];
+      } else if (backgroundColors[ansiCode]) {
+        this.state.background = backgroundColors[ansiCode];
+      } else if (ansiCode == 39) {
+        delete this.state.foreground;
+      } else if (ansiCode == 49) {
+        delete this.state.background;
+      } else if (styles[ansiCode]) {
+        this.state[styles[ansiCode]] = true;
+      } else if (ansiCode == 22) {
+        this.state.bold = false;
+      } else if (ansiCode == 23) {
+        this.state.italic = false;
+      } else if (ansiCode == 24) {
+        this.state.underline = false;
+      } else if (ansiCode == 0) {
+        this.state = {};
+      }
+    }
+  }
+
+  var ansiParser = new AnsiParser(this);
 
   this.addLog = function(line) {
-    var sequence = ansiparse(line);
-
-    for (var i = 0; i < sequence.length; i++) {
-      if (sequence[i].cr) {
-        this.carriage = 0;
-      } else if (sequence[i].linebreak) {
-        this.pushLine(Immutable.List.of(sequence[i]))
-        this.refreshCursor();
-        this.carriage = 0;
-        this.changed = true;
-      } else if (sequence[i].text) {
-        var textLen = sequence[i].text.length;
-        var carriage = this.carriage;
-        var cursor = this.cursor.update(function(line) {
-          return line.slice(carriage);
-        });
-        this.cursor = this.cursor.update(function(line) {
-          return line.slice(0, carriage);
-        });
-        this.pushSequence(sequence[i]);
-        this.carriage++;
-
-        cursor.forEach(function(e, i) {
-          if(e.text.length >= textLen) {
-            e.text = e.text.substr(textLen);
-            textLen -= e.text.length;
-            this.pushSequence(e);
-            return false;
-          }
-        }, this);
-
-        this.refreshLineCursor();
-        this.changed = true;
-      }
-    };
+    ansiParser.parse(line);
   };
 
   this.addError = function(message) {
@@ -69,7 +121,6 @@ function LogsModel() {
       error: true,
     });
     this.refreshLineCursor();
-    this.carriageReturned = false;
     this.changed = true;
   };
 
@@ -249,6 +300,45 @@ var actions = {
 var stores = {
   LogStore: new LogStore(),
   ResourceStore: new ResourceStore(),
+};
+
+var foregroundColors = {
+  30: 'black',
+  31: 'red',
+  32: 'green',
+  33: 'yellow',
+  34: 'blue',
+  35: 'magenta',
+  36: 'cyan',
+  37: 'white',
+};
+
+var brightForegroundColors = {
+  90: 'bright-black',
+  91: 'bright-red',
+  92: 'bright-green',
+  93: 'bright-yellow',
+  94: 'bright-blue',
+  95: 'bright-magenta',
+  96: 'bright-cyan',
+  97: 'bright-white',
+};
+
+var backgroundColors = {
+  40: 'black',
+  41: 'red',
+  42: 'green',
+  43: 'yellow',
+  44: 'blue',
+  45: 'magenta',
+  46: 'cyan',
+  47: 'white'
+};
+
+var styles = {
+  1: 'bold',
+  3: 'italic',
+  4: 'underline'
 };
 
 module.exports = new Fluxxor.Flux(stores, actions);
