fdab9a6b4934eab51f006112b6b697670e398e56
Alex Suraci <suraci.alex@gmail.com>
2015-03-08 14:25:52 -0700

    add support for gauges analytics
    improve command-line option parsing along the way

--- docs/common.rkt
+++ docs/common.rkt
@@ -1,16 +1,23 @@
 #lang scheme/base
 
-(require scribble/core
+(require racket/cmdline
+         scribble/core
          scribble/html-properties
          scriblib/render-cond)
 
 (provide version)
 
-(define version
-  (let ([version-arg-provided (> (vector-length (current-command-line-arguments)) 0)])
-    (if version-arg-provided
-      (vector-ref (current-command-line-arguments) 0)
-      "x.x.x")))
+(define version-param (make-parameter "x.x.x"))
+(define analytics-id (make-parameter ""))
+(define gauges-id (make-parameter ""))
+
+(command-line
+   #:once-each
+   ["--version" v "Version of Concourse the docs are for" (version-param v)]
+   ["--analytics-id" i "Google Analytics site ID" (analytics-id i)]
+   ["--gauges-id" i "Gauges site ID" (gauges-id i)])
+
+(define version (version-param))
 
 (provide strike)
 
@@ -20,25 +27,45 @@
 (provide inject-analytics)
 
 (define (inject-analytics)
-  (let ([analytics-token-provided (> (vector-length (current-command-line-arguments)) 1)])
-    (if analytics-token-provided
-      (cond-element
-        [latex ""]
-        [html (make-element
-                (make-style #f
-                    (list (make-script-property
-                            "text/javascript"
-                            (analytics-script (vector-ref (current-command-line-arguments) 1)))))
-                '())]
-        [text ""])
-      "")))
+  (cond-element
+    [latex ""]
+    [html (make-element
+            (make-style #f
+                (list
+                  (if (not (equal? "" (analytics-id)))
+                    (make-script-property
+                      "text/javascript"
+                      (analytics-script (analytics-id)))
+                    "")
+                  (if (not (equal? "" (gauges-id)))
+                    (make-script-property
+                      "text/javascript"
+                      (gauges-script (gauges-id)))
+                    "")))
+            '())]
+    [text ""]))
 
-(define (analytics-script token)
+(define (analytics-script site-id)
   (list
     "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n"
     "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n"
     "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n"
     "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n"
     "\n"
-    "ga('create', '" token "', 'auto');\n"
+    "ga('create', '" site-id "', 'auto');\n"
     "ga('send', 'pageview');\n"))
+
+(define (gauges-script site-id)
+  (list
+    "var _gauges = _gauges || [];\n"
+    "(function() {\n"
+    "  var t   = document.createElement('script');\n"
+    "  t.type  = 'text/javascript';\n"
+    "  t.async = true;\n"
+    "  t.id    = 'gauges-tracker';\n"
+    "  t.setAttribute('data-site-id', '" site-id "');\n"
+    "  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');\n"
+    "  t.src = 'https://track.gaug.es/track.js';\n"
+    "  var s = document.getElementsByTagName('script')[0];\n"
+    "  s.parentNode.insertBefore(t, s);\n"
+    "})();\n"))
