8c194f1dd47ba553d952f4b8351355ae816d12af
Alex Suraci <asuraci@pivotal.io>
2015-03-16 14:11:10 -0700

    massively worthwhile refactor

--- scripts/takeoff.sh
+++ scripts/takeoff.sh
@@ -4,34 +4,23 @@ set -e
 
 JOB_NAME=$1
 
-if [ -z $JOB_NAME ]; then
+if [ -z "$JOB_NAME" ]; then
   echo "usage: takeoff.sh job"
   exit 1
 fi
 
-if [ -z $ATC_URL ]; then
+if [ -z "$ATC_URL" ]; then
   echo "ATC_URL must be set"
   exit 1
 fi
 
-USERNAME=$(
-  echo $ATC_URL |
-  python -c "import sys, urlparse; print urlparse.urlparse(sys.stdin.read()).username"
-)
-
-PASSWORD=$(
-  echo $ATC_URL |
-  python -c "import sys, urlparse; print urlparse.urlparse(sys.stdin.read()).password"
-)
-
-HOST=$(
-  echo $ATC_URL |
-  python -c "import sys, urlparse; print urlparse.urlparse(sys.stdin.read()).hostname"
-)
-
-PORT=$(
-  echo $ATC_URL |
-  python -c "import sys, urlparse; print urlparse.urlparse(sys.stdin.read()).port"
+read USERNAME PASSWORD HOST PORT < <(
+  echo -n "$ATC_URL" | python -c "
+import sys, urlparse
+url = urlparse.urlparse(sys.stdin.read())
+port = (url.port or (url.scheme == 'https' and 443 or 80))
+print url.username, url.password, url.hostname, port
+"
 )
 
 curl \
