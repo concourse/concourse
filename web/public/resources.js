0f1606fe857a341b30aa88355ef467d2606b2654
Toby Rumans <trumans@pivotal.io>
2015-04-21 17:52:40 -0700

    Make the version history pretty
    [finishes #92884182]

--- web/public/resources.js
+++ web/public/resources.js
@@ -16,13 +16,24 @@ $(document).ready(function() {
     }).done(function() {
       if ($(that).data("action") == "enable") {
         $(that).data("action", "disable");
-        $(that).closest("tr").removeClass("disabled").addClass("enabled");
+        $(that).closest("li").removeClass("disabled").addClass("enabled");
       } else {
         $(that).data("action", "enable");
-        $(that).closest("tr").removeClass("enabled").addClass("disabled");
+        $(that).closest("li").removeClass("enabled").addClass("disabled");
       }
     });
 
     return false;
   });
+
+
+  $(".js-expandable").on("click", function() {
+
+    if($(this).parent().hasClass("expanded")) {
+      $(this).parent().removeClass("expanded")
+    } else {
+      $(this).parent().addClass("expanded")
+    }
+  });
+
 });
