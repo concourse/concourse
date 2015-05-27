8f1086495379dc6219ee5bd222bd2dc1fe5dc341
Alex Suraci <suraci.alex@gmail.com>
2015-05-27 10:52:38 -0700

    push more styling into concourse/docs

--- common.rkt
+++ common.rkt
@@ -9,25 +9,13 @@
 
 (define version-param (make-parameter "x.x.x"))
 (define analytics-id (make-parameter ""))
-(define gauges-id (make-parameter ""))
 
 (command-line
    #:once-each
    ["--version" v "Version of Concourse the docs are for" (version-param v)]
-   ["--analytics-id" i "Google Analytics site ID" (analytics-id i)]
-   ["--gauges-id" i "Gauges site ID" (gauges-id i)])
+   ["--analytics-id" i "Google Analytics site ID" (analytics-id i)])
 
 (define version (version-param))
-
-(provide strike)
-
-(define strike (make-style "strike"
-  (list (make-css-addition "concourse.css"))))
-
-(provide break-word)
-
-(define break-word (make-style "break-word"
-  (list (make-css-addition "concourse.css"))))
 
 (provide inject-analytics)
 
@@ -39,9 +27,7 @@
               (list
                 (make-script-property
                   "text/javascript"
-                  (append
-                    (analytics-script (analytics-id))
-                    (gauges-script (gauges-id))))))
+                  (analytics-script (analytics-id)))))
             '())]
     [text ""]))
 
@@ -56,20 +42,3 @@
       "\n"
       "ga('create', '" site-id "', 'auto');\n"
       "ga('send', 'pageview');\n")))
-
-(define (gauges-script site-id)
-  (if (equal? "" site-id)
-    (list)
-    (list
-      "var _gauges = _gauges || [];\n"
-      "(function() {\n"
-      "  var t   = document.createElement('script');\n"
-      "  t.type  = 'text/javascript';\n"
-      "  t.async = true;\n"
-      "  t.id    = 'gauges-tracker';\n"
-      "  t.setAttribute('data-site-id', '" site-id "');\n"
-      "  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');\n"
-      "  t.src = 'https://track.gaug.es/track.js';\n"
-      "  var s = document.getElementsByTagName('script')[0];\n"
-      "  s.parentNode.insertBefore(t, s);\n"
-      "})();\n")))
