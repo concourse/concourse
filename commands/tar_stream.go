9d41fff8c5e99aac68ac3d76965dccdb952f02f8
Andrew Thorburn <athorburn@pivotal.io>
2015-06-03 16:59:29 +0100

    Fix issue where tar command would fail if there were too many command line arguments
    Signed-off-by: Dan Weinberg <danielw@cloudcredo.com>

--- commands/tar_stream.go
+++ commands/tar_stream.go
@@ -11,9 +11,10 @@ import (
 
 func tarStreamFrom(workDir string, paths []string) (io.ReadCloser, error) {
 	var archive io.ReadCloser
+	var writer io.WriteCloser
 
 	if tarPath, err := exec.LookPath("tar"); err == nil {
-		tarCmd := exec.Command(tarPath, append([]string{"-czf", "-"}, paths...)...)
+		tarCmd := exec.Command(tarPath, []string{"-czf", "-", "-T", "-"}...)
 		tarCmd.Dir = workDir
 		tarCmd.Stderr = os.Stderr
 
@@ -22,10 +23,22 @@ func tarStreamFrom(workDir string, paths []string) (io.ReadCloser, error) {
 			log.Fatalln("could not create tar pipe:", err)
 		}
 
+		writer, err = tarCmd.StdinPipe()
+		if err != nil {
+			log.Fatalln("could not create tar stdin pipe:", err)
+		}
+
 		err = tarCmd.Start()
 		if err != nil {
 			log.Fatalln("could not run tar:", err)
 		}
+
+		go func() {
+			for _, s := range paths {
+				io.WriteString(writer, s+"\n")
+			}
+			writer.Close()
+		}()
 	} else {
 		return nativeTarGZStreamFrom(workDir, paths)
 	}
