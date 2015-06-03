ebb637dd3aed7e33408cc45201e826da0b51d47c
Andrew Thorburn <athorburn@pivotal.io>
2015-06-03 18:06:40 +0100

    [#15] Tweak fix for tar argument list

--- commands/tar_stream.go
+++ commands/tar_stream.go
@@ -3,42 +3,33 @@
 package commands
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func tarStreamFrom(workDir string, paths []string) (io.ReadCloser, error) {
 	var archive io.ReadCloser
-	var writer io.WriteCloser
 
 	if tarPath, err := exec.LookPath("tar"); err == nil {
-		tarCmd := exec.Command(tarPath, []string{"-czf", "-", "-T", "-"}...)
+		tarCmd := exec.Command(tarPath, []string{"-czf", "-", "--null", "-T", "-"}...)
 		tarCmd.Dir = workDir
 		tarCmd.Stderr = os.Stderr
+
+		tarCmd.Stdin = bytes.NewBufferString(strings.Join(paths, "\x00"))
 
 		archive, err = tarCmd.StdoutPipe()
 		if err != nil {
 			log.Fatalln("could not create tar pipe:", err)
 		}
 
-		writer, err = tarCmd.StdinPipe()
-		if err != nil {
-			log.Fatalln("could not create tar stdin pipe:", err)
-		}
-
 		err = tarCmd.Start()
 		if err != nil {
 			log.Fatalln("could not run tar:", err)
 		}
-
-		go func() {
-			for _, s := range paths {
-				io.WriteString(writer, s+"\n")
-			}
-			writer.Close()
-		}()
 	} else {
 		return nativeTarGZStreamFrom(workDir, paths)
 	}
