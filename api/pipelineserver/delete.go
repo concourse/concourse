ca64eaca594cc8585cdf85e4b8bb11fdb8f0e4df
Chris Brown <cbrown@pivotal.io>
2015-06-03 11:21:02 -0700

    add logging to happy path of deleting a pipeline

--- api/pipelineserver/delete.go
+++ api/pipelineserver/delete.go
@@ -9,15 +9,21 @@ import (
 
 func (s *Server) DeletePipeline(pipelineDB db.PipelineDB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := s.logger.Session("destroying-pipeline", lager.Data{
+			"name": pipelineDB.GetPipelineName(),
+		})
+
+		logger.Info("start")
+
 		err := pipelineDB.Destroy()
 		if err != nil {
-			s.logger.Error("destroying-pipeline", err, lager.Data{
-				"pipeline-name": pipelineDB.GetPipelineName(),
-			})
+			s.logger.Error("failed", err)
 
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		logger.Info("done")
 
 		w.WriteHeader(http.StatusNoContent)
 	})
