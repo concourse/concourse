7b1995d62c92bc662ba927eed2717a4773e09b9d
Chris Walter <cwalter@pivotal.io>
2015-08-17 10:53:21 -0700

    Added tests for location passing into delegates.
    [#100342916]

--- engine/exec_engine_test.go
+++ engine/exec_engine_test.go
@@ -318,6 +318,15 @@ var _ = Describe("ExecEngine", func() {
 				}
 
 				plan := atc.Plan{
+					Location: &atc.Location{
+						ID:       145,
+						ParentID: 1,
+
+						ParallelGroup: 1234,
+						SerialGroup:   5678,
+
+						Hook: "boring input hook",
+					},
 					Get: getPlan,
 				}
 
@@ -332,22 +341,27 @@ var _ = Describe("ExecEngine", func() {
 					sourceName, workerID, delegate, resourceConfig, params, tags, version := fakeFactory.GetArgsForCall(0)
 					Ω(sourceName).Should(Equal(exec.SourceName("some-input")))
 					Ω(workerID).Should(Equal(worker.Identifier{
-						BuildID: 42,
-						Type:    worker.ContainerTypeGet,
-						Name:    "some-input",
+						BuildID:      42,
+						Type:         worker.ContainerTypeGet,
+						Name:         "some-input",
+						StepLocation: 145,
 					}))
 					Ω(tags).Should(ConsistOf("some", "get", "tags"))
-
-					Ω(delegate).Should(Equal(fakeInputDelegate))
-					_, plan, location := fakeDelegate.InputDelegateArgsForCall(0)
-					Ω(plan).Should(Equal(*inputPlan))
-					Ω(location).ShouldNot(BeNil())
-
 					Ω(resourceConfig.Name).Should(Equal("some-input-resource"))
 					Ω(resourceConfig.Type).Should(Equal("some-type"))
 					Ω(resourceConfig.Source).Should(Equal(atc.Source{"some": "source"}))
 					Ω(params).Should(Equal(atc.Params{"some": "params"}))
 					Ω(version).Should(Equal(atc.Version{"some": "version"}))
+
+					Ω(delegate).Should(Equal(fakeInputDelegate))
+					_, plan, location := fakeDelegate.InputDelegateArgsForCall(0)
+					Ω(plan).Should(Equal(*inputPlan))
+					Ω(location).ShouldNot(BeNil())
+					Ω(location.ID).Should(Equal(uint(145)))
+					Ω(location.ParentID).Should(Equal(uint(1)))
+					Ω(location.ParallelGroup).Should(Equal(uint(1234)))
+					Ω(location.SerialGroup).Should(Equal(uint(5678)))
+					Ω(location.Hook).Should(Equal("boring input hook"))
 
 				})
 
@@ -391,6 +405,15 @@ var _ = Describe("ExecEngine", func() {
 					Privileged: privileged,
 				}
 				plan := atc.Plan{
+					Location: &atc.Location{
+						ID:       123,
+						ParentID: 41,
+
+						ParallelGroup: 123498,
+						SerialGroup:   69,
+
+						Hook: "look at me I'm a hook",
+					},
 					Task: taskPlan,
 				}
 				It("constructs tasks correctly", func() {
@@ -404,15 +427,26 @@ var _ = Describe("ExecEngine", func() {
 					sourceName, workerID, delegate, privileged, tags, configSource := fakeFactory.TaskArgsForCall(0)
 					Ω(sourceName).Should(Equal(exec.SourceName("some-task")))
 					Ω(workerID).Should(Equal(worker.Identifier{
-						BuildID: 42,
-						Type:    worker.ContainerTypeTask,
-						Name:    "some-task",
+						BuildID:      42,
+						Type:         worker.ContainerTypeTask,
+						Name:         "some-task",
+						StepLocation: 123,
 					}))
-					Ω(delegate).Should(Equal(fakeExecutionDelegate))
 					Ω(privileged).Should(Equal(exec.Privileged(false)))
 					Ω(tags).Should(BeEmpty())
 					Ω(configSource).ShouldNot(BeNil())
+
+					Ω(delegate).Should(Equal(fakeExecutionDelegate))
+					_, _, location := fakeDelegate.ExecutionDelegateArgsForCall(0)
+
+					Ω(location).ShouldNot(BeNil())
+					Ω(location.ID).Should(Equal(uint(123)))
+					Ω(location.ParentID).Should(Equal(uint(41)))
+					Ω(location.ParallelGroup).Should(Equal(uint(123498)))
+					Ω(location.SerialGroup).Should(Equal(uint(69)))
+					Ω(location.Hook).Should(Equal("look at me I'm a hook"))
 				})
+
 				It("releases the tasks correctly", func() {
 					taskStep.RunStub = func(signals <-chan os.Signal, ready chan<- struct{}) error {
 						defer GinkgoRecover()
@@ -450,10 +484,29 @@ var _ = Describe("ExecEngine", func() {
 
 			Context("that contains outputs", func() {
 				plan := atc.Plan{
-					Location: &atc.Location{},
+					Location: &atc.Location{
+
+						ID:       50,
+						ParentID: 25,
+
+						ParallelGroup: 1020,
+						SerialGroup:   150,
+
+						Hook: "hook",
+					},
+
 					OnSuccess: &atc.OnSuccessPlan{
 						Step: atc.Plan{
-							Location: &atc.Location{},
+							Location: &atc.Location{
+
+								ID:       51,
+								ParentID: 26,
+
+								ParallelGroup: 1021,
+								SerialGroup:   151,
+
+								Hook: "special hook",
+							},
 							Put: &atc.PutPlan{
 								Name:     "some-put",
 								Resource: "some-output-resource",
@@ -464,7 +517,16 @@ var _ = Describe("ExecEngine", func() {
 							},
 						},
 						Next: atc.Plan{
-							Location: &atc.Location{},
+							Location: &atc.Location{
+
+								ID:       512,
+								ParentID: 2134,
+
+								ParallelGroup: 12,
+								SerialGroup:   121243,
+
+								Hook: "more special hook",
+							},
 							DependentGet: &atc.DependentGetPlan{
 								Name:     "some-put",
 								Resource: "some-output-resource",
@@ -487,16 +549,26 @@ var _ = Describe("ExecEngine", func() {
 
 					workerID, delegate, resourceConfig, tags, params := fakeFactory.PutArgsForCall(0)
 					Ω(workerID).Should(Equal(worker.Identifier{
-						BuildID: 42,
-						Type:    worker.ContainerTypePut,
-						Name:    "some-put",
+						BuildID:      42,
+						Type:         worker.ContainerTypePut,
+						Name:         "some-put",
+						StepLocation: 51,
 					}))
-					Ω(delegate).Should(Equal(fakeOutputDelegate))
 					Ω(resourceConfig.Name).Should(Equal("some-output-resource"))
 					Ω(resourceConfig.Type).Should(Equal("some-type"))
 					Ω(resourceConfig.Source).Should(Equal(atc.Source{"some": "source"}))
 					Ω(tags).Should(ConsistOf("some", "putget", "tags"))
 					Ω(params).Should(Equal(atc.Params{"some": "params"}))
+
+					Ω(delegate).Should(Equal(fakeOutputDelegate))
+					_, _, location := fakeDelegate.OutputDelegateArgsForCall(0)
+
+					Ω(location).ShouldNot(BeNil())
+					Ω(location.ID).Should(Equal(uint(51)))
+					Ω(location.ParentID).Should(Equal(uint(26)))
+					Ω(location.ParallelGroup).Should(Equal(uint(1021)))
+					Ω(location.SerialGroup).Should(Equal(uint(151)))
+					Ω(location.Hook).Should(Equal("special hook"))
 				})
 
 				It("constructs the dependent get correctly", func() {
@@ -509,9 +581,10 @@ var _ = Describe("ExecEngine", func() {
 
 					sourceName, workerID, delegate, resourceConfig, tags, params := fakeFactory.DependentGetArgsForCall(0)
 					Ω(workerID).Should(Equal(worker.Identifier{
-						BuildID: 42,
-						Type:    worker.ContainerTypeGet,
-						Name:    "some-put",
+						BuildID:      42,
+						Type:         worker.ContainerTypeGet,
+						Name:         "some-put",
+						StepLocation: 512,
 					}))
 					Ω(tags).Should(ConsistOf("some", "putget", "tags"))
 					Ω(sourceName).Should(Equal(exec.SourceName("some-put")))
@@ -523,6 +596,11 @@ var _ = Describe("ExecEngine", func() {
 					Ω(delegate).Should(Equal(fakeInputDelegate))
 					_, _, location := fakeDelegate.InputDelegateArgsForCall(0)
 					Ω(location).ShouldNot(BeNil())
+					Ω(location.ID).Should(Equal(uint(512)))
+					Ω(location.ParentID).Should(Equal(uint(2134)))
+					Ω(location.ParallelGroup).Should(Equal(uint(12)))
+					Ω(location.SerialGroup).Should(Equal(uint(121243)))
+					Ω(location.Hook).Should(Equal("more special hook"))
 				})
 				It("releases all sources", func() {
 					var err error
