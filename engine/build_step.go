2dc46be08919e41da9f8f2edf593e499151e751c
Rob Dimsdale <rdimsdale@pivotal.io>
2015-08-17 11:45:23 -0700

    Refactor exec engine build step.
    - use double-dispatch pattern to separate concerns.
    
    [#100342916]
    
    Signed-off-by: Chris Walter <cwalter@pivotal.io>

---
+++ engine/build_step.go
@@ -0,0 +1,154 @@
+package engine
+
+import (
+	"github.com/concourse/atc"
+	"github.com/concourse/atc/event"
+	"github.com/concourse/atc/exec"
+	"github.com/pivotal-golang/lager"
+)
+
+func (build *execBuild) buildAggregateStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	logger = logger.Session("aggregate")
+
+	step := exec.Aggregate{}
+
+	for _, innerPlan := range *plan.Aggregate {
+		stepFactory := build.buildStepFactory(logger, innerPlan)
+		step = append(step, stepFactory)
+	}
+
+	return step
+}
+func (build *execBuild) buildTimeoutStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	step := build.buildStepFactory(logger, plan.Timeout.Step)
+	return exec.Timeout(step, plan.Timeout.Duration)
+}
+
+func (build *execBuild) buildTryStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	step := build.buildStepFactory(logger, plan.Try.Step)
+	return exec.Try(step)
+}
+
+func (build *execBuild) buildOnSuccessStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	step := build.buildStepFactory(logger, plan.OnSuccess.Step)
+	next := build.buildStepFactory(logger, plan.OnSuccess.Next)
+	return exec.OnSuccess(step, next)
+}
+
+func (build *execBuild) buildOnFailureStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	step := build.buildStepFactory(logger, plan.OnFailure.Step)
+	next := build.buildStepFactory(logger, plan.OnFailure.Next)
+	return exec.OnFailure(step, next)
+}
+
+func (build *execBuild) buildEnsureStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	step := build.buildStepFactory(logger, plan.Ensure.Step)
+	next := build.buildStepFactory(logger, plan.Ensure.Next)
+	return exec.Ensure(step, next)
+}
+
+func (build *execBuild) buildTaskStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	logger = logger.Session("task")
+
+	var configSource exec.TaskConfigSource
+	if plan.Task.Config != nil && plan.Task.ConfigPath != "" {
+		configSource = exec.MergedConfigSource{
+			A: exec.FileConfigSource{plan.Task.ConfigPath},
+			B: exec.StaticConfigSource{*plan.Task.Config},
+		}
+	} else if plan.Task.Config != nil {
+		configSource = exec.StaticConfigSource{*plan.Task.Config}
+	} else if plan.Task.ConfigPath != "" {
+		configSource = exec.FileConfigSource{plan.Task.ConfigPath}
+	} else {
+		return exec.Identity{}
+	}
+
+	var location event.OriginLocation
+	if plan.Location != nil {
+		location = event.OriginLocationFrom(*plan.Location)
+	}
+
+	return build.factory.Task(
+		exec.SourceName(plan.Task.Name),
+		build.taskIdentifier(plan.Task.Name, location),
+		build.delegate.ExecutionDelegate(logger, *plan.Task, location),
+		exec.Privileged(plan.Task.Privileged),
+		plan.Task.Tags,
+		configSource,
+	)
+}
+
+func (build *execBuild) buildGetStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	logger = logger.Session("get", lager.Data{
+		"name": plan.Get.Name,
+	})
+
+	var location event.OriginLocation
+	if plan.Location != nil {
+		location = event.OriginLocationFrom(*plan.Location)
+	}
+
+	return build.factory.Get(
+		exec.SourceName(plan.Get.Name),
+		build.getIdentifier(plan.Get.Name, location),
+		build.delegate.InputDelegate(logger, *plan.Get, location),
+		atc.ResourceConfig{
+			Name:   plan.Get.Resource,
+			Type:   plan.Get.Type,
+			Source: plan.Get.Source,
+		},
+		plan.Get.Params,
+		plan.Get.Tags,
+		plan.Get.Version,
+	)
+
+}
+
+func (build *execBuild) buildPutStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	logger = logger.Session("put", lager.Data{
+		"name": plan.Put.Name,
+	})
+
+	var location event.OriginLocation
+	if plan.Location != nil {
+		location = event.OriginLocationFrom(*plan.Location)
+	}
+
+	return build.factory.Put(
+		build.putIdentifier(plan.Put.Name, location),
+		build.delegate.OutputDelegate(logger, *plan.Put, location),
+		atc.ResourceConfig{
+			Name:   plan.Put.Resource,
+			Type:   plan.Put.Type,
+			Source: plan.Put.Source,
+		},
+		plan.Put.Tags,
+		plan.Put.Params,
+	)
+}
+
+func (build *execBuild) buildDependentGetStep(logger lager.Logger, plan atc.Plan) exec.StepFactory {
+	logger = logger.Session("get", lager.Data{
+		"name": plan.DependentGet.Name,
+	})
+
+	var location event.OriginLocation
+	if plan.Location != nil {
+		location = event.OriginLocationFrom(*plan.Location)
+	}
+
+	getPlan := plan.DependentGet.GetPlan()
+	return build.factory.DependentGet(
+		exec.SourceName(getPlan.Name),
+		build.getIdentifier(getPlan.Name, location),
+		build.delegate.InputDelegate(logger, getPlan, location),
+		atc.ResourceConfig{
+			Name:   getPlan.Resource,
+			Type:   getPlan.Type,
+			Source: getPlan.Source,
+		},
+		getPlan.Tags,
+		getPlan.Params,
+	)
+}
--- engine/exec_engine.go
+++ engine/exec_engine.go
@@ -136,151 +136,43 @@ func (build *execBuild) Resume(logger lager.Logger) {
 
 func (build *execBuild) buildStepFactory(logger lager.Logger, plan atc.Plan) exec.StepFactory {
 	if plan.Aggregate != nil {
-
-		logger = logger.Session("aggregate")
-
-		step := exec.Aggregate{}
-
-		for _, innerPlan := range *plan.Aggregate {
-			stepFactory := build.buildStepFactory(logger, innerPlan)
-
-			step = append(step, stepFactory)
-		}
-
-		return step
+		return build.buildAggregateStep(logger, plan)
 	}
 
 	if plan.Timeout != nil {
-		step := build.buildStepFactory(logger, plan.Timeout.Step)
-		return exec.Timeout(step, plan.Timeout.Duration)
+		return build.buildTimeoutStep(logger, plan)
 	}
 
 	if plan.Try != nil {
-		step := build.buildStepFactory(logger, plan.Try.Step)
-		return exec.Try(step)
+		return build.buildTryStep(logger, plan)
 	}
 
 	if plan.OnSuccess != nil {
-		step := build.buildStepFactory(logger, plan.OnSuccess.Step)
-		next := build.buildStepFactory(logger, plan.OnSuccess.Next)
-		return exec.OnSuccess(step, next)
+		return build.buildOnSuccessStep(logger, plan)
 	}
 
 	if plan.OnFailure != nil {
-		step := build.buildStepFactory(logger, plan.OnFailure.Step)
-		next := build.buildStepFactory(logger, plan.OnFailure.Next)
-		return exec.OnFailure(step, next)
+		return build.buildOnFailureStep(logger, plan)
 	}
 
 	if plan.Ensure != nil {
-		step := build.buildStepFactory(logger, plan.Ensure.Step)
-		next := build.buildStepFactory(logger, plan.Ensure.Next)
-		return exec.Ensure(step, next)
+		return build.buildEnsureStep(logger, plan)
 	}
 
 	if plan.Task != nil {
-		logger = logger.Session("task")
-
-		var configSource exec.TaskConfigSource
-		if plan.Task.Config != nil && plan.Task.ConfigPath != "" {
-			configSource = exec.MergedConfigSource{
-				A: exec.FileConfigSource{plan.Task.ConfigPath},
-				B: exec.StaticConfigSource{*plan.Task.Config},
-			}
-		} else if plan.Task.Config != nil {
-			configSource = exec.StaticConfigSource{*plan.Task.Config}
-		} else if plan.Task.ConfigPath != "" {
-			configSource = exec.FileConfigSource{plan.Task.ConfigPath}
-		} else {
-			return exec.Identity{}
-		}
-
-		var location event.OriginLocation
-		if plan.Location != nil {
-			location = event.OriginLocationFrom(*plan.Location)
-		}
-
-		return build.factory.Task(
-			exec.SourceName(plan.Task.Name),
-			build.taskIdentifier(plan.Task.Name, location),
-			build.delegate.ExecutionDelegate(logger, *plan.Task, location),
-			exec.Privileged(plan.Task.Privileged),
-			plan.Task.Tags,
-			configSource,
-		)
+		return build.buildTaskStep(logger, plan)
 	}
 
 	if plan.Get != nil {
-		logger = logger.Session("get", lager.Data{
-			"name": plan.Get.Name,
-		})
-
-		var location event.OriginLocation
-		if plan.Location != nil {
-			location = event.OriginLocationFrom(*plan.Location)
-		}
-
-		return build.factory.Get(
-			exec.SourceName(plan.Get.Name),
-			build.getIdentifier(plan.Get.Name, location),
-			build.delegate.InputDelegate(logger, *plan.Get, location),
-			atc.ResourceConfig{
-				Name:   plan.Get.Resource,
-				Type:   plan.Get.Type,
-				Source: plan.Get.Source,
-			},
-			plan.Get.Params,
-			plan.Get.Tags,
-			plan.Get.Version,
-		)
+		return build.buildGetStep(logger, plan)
 	}
 
 	if plan.Put != nil {
-		logger = logger.Session("put", lager.Data{
-			"name": plan.Put.Name,
-		})
-
-		var location event.OriginLocation
-		if plan.Location != nil {
-			location = event.OriginLocationFrom(*plan.Location)
-		}
-
-		return build.factory.Put(
-			build.putIdentifier(plan.Put.Name, location),
-			build.delegate.OutputDelegate(logger, *plan.Put, location),
-			atc.ResourceConfig{
-				Name:   plan.Put.Resource,
-				Type:   plan.Put.Type,
-				Source: plan.Put.Source,
-			},
-			plan.Put.Tags,
-			plan.Put.Params,
-		)
+		return build.buildPutStep(logger, plan)
 	}
 
 	if plan.DependentGet != nil {
-		logger = logger.Session("get", lager.Data{
-			"name": plan.DependentGet.Name,
-		})
-
-		var location event.OriginLocation
-		if plan.Location != nil {
-			location = event.OriginLocationFrom(*plan.Location)
-		}
-
-		getPlan := plan.DependentGet.GetPlan()
-		return build.factory.DependentGet(
-			exec.SourceName(getPlan.Name),
-			build.getIdentifier(getPlan.Name, location),
-			build.delegate.InputDelegate(logger, getPlan, location),
-			atc.ResourceConfig{
-				Name:   getPlan.Resource,
-				Type:   getPlan.Type,
-				Source: getPlan.Source,
-			},
-			getPlan.Tags,
-			getPlan.Params,
-		)
+		return build.buildDependentGetStep(logger, plan)
 	}
 
 	return exec.Identity{}
