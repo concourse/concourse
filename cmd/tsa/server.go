68dbe6545c86cff1098aa509868e649a826aaeb5
Alex Suraci <asuraci@pivotal.io>
2015-08-24 14:49:18 -0700

    use tcp keepalives
    [#101732194]

--- cmd/tsa/server.go
+++ cmd/tsa/server.go
@@ -14,11 +14,41 @@ import (
 	gconn "github.com/cloudfoundry-incubator/garden/client/connection"
 	"github.com/concourse/atc"
 	"github.com/concourse/tsa"
+	"github.com/felixge/tcpkeepalive"
 	"github.com/pivotal-golang/lager"
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/rata"
 	"golang.org/x/crypto/ssh"
 )
+
+func keepaliveDialer(network string, address string) (net.Conn, error) {
+	conn, err := net.DialTimeout(network, address, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	kac, err := tcpkeepalive.EnableKeepAlive(conn)
+	if err != nil {
+		println("failed to enable connection keepalive: " + err.Error())
+	}
+
+	err = kac.SetKeepAliveIdle(10 * time.Second)
+	if err != nil {
+		println("failed to set keepalive idle threshold: " + err.Error())
+	}
+
+	err = kac.SetKeepAliveCount(3)
+	if err != nil {
+		println("failed to set keepalive count: " + err.Error())
+	}
+
+	err = kac.SetKeepAliveInterval(5 * time.Second)
+	if err != nil {
+		println("failed to set keepalive interval: " + err.Error())
+	}
+
+	return conn, nil
+}
 
 type registrarSSHServer struct {
 	logger            lager.Logger
@@ -217,7 +247,7 @@ func (server *registrarSSHServer) heartbeatWorker(logger lager.Logger, worker at
 	return ifrit.Background(tsa.NewHeartbeater(
 		logger,
 		server.heartbeatInterval,
-		gclient.New(gconn.New("tcp", worker.Addr)),
+		gclient.New(gconn.NewWithDialerAndLogger("tcp", worker.Addr, keepaliveDialer, logger.Session("garden-connection"))),
 		server.atcEndpoint,
 		worker,
 		channel,
