5c1a434c79b16b83ffb9947334fce0e7f1ae6ead
Alex Suraci <suraci.alex@gmail.com>
2015-09-12 15:25:19 -0700

    woops that wasn't dead code

--- cmd/atc/main.go
+++ cmd/atc/main.go
@@ -551,3 +551,17 @@ func keepaliveDialerFactory(network string, address string) gconn.DialerFunc {
 		return conn, nil
 	}
 }
+
+func keepaliveDialer(network string, address string) (net.Conn, error) {
+	conn, err := net.DialTimeout(network, address, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tcpkeepalive.SetKeepAlive(conn, 10*time.Second, 3, 5*time.Second)
+	if err != nil {
+		println("failed to enable connection keepalive: " + err.Error())
+	}
+
+	return conn, nil
+}
