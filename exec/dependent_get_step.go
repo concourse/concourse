48bec9c6e6b31cd49dcafae26d00731703781fab
Alex Suraci <suraci.alex@gmail.com>
2015-09-26 12:29:18 -0700

    dependent get steps just construct get steps

--- exec/dependent_get_step.go
+++ exec/dependent_get_step.go
@@ -1,10 +1,6 @@
 package exec
 
 import (
-	"archive/tar"
-	"io"
-	"os"
-
 	"github.com/concourse/atc"
 	"github.com/concourse/atc/resource"
 	"github.com/concourse/atc/worker"
@@ -22,15 +18,6 @@ type dependentGetStep struct {
 	tags           atc.Tags
 	delegate       ResourceDelegate
 	trackerFactory TrackerFactory
-
-	previousStep Step
-	repository   *SourceRepository
-
-	resource resource.Resource
-
-	versionedSource resource.VersionedSource
-
-	exitStatus int
 }
 
 func newDependentGetStep(
@@ -60,124 +47,20 @@ func newDependentGetStep(
 }
 
 func (step dependentGetStep) Using(prev Step, repo *SourceRepository) Step {
-	step.previousStep = prev
-	step.repository = repo
+	var info VersionInfo
+	prev.Result(&info)
 
-	return failureReporter{
-		Step:          &step,
-		ReportFailure: step.delegate.Failed,
-	}
-}
-
-func (step *dependentGetStep) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
-	resourceSpec := worker.WorkerSpec{
-		ResourceType: step.resourceConfig.Type,
-		Tags:         step.tags,
-	}
-
-	chosenWorker, err := step.workerPool.Satisfying(resourceSpec)
-	if err != nil {
-		return err
-	}
-
-	tracker := step.trackerFactory.TrackerFor(chosenWorker)
-
-	trackedResource, err := tracker.Init(
+	return newGetStep(
+		step.logger,
+		step.sourceName,
+		step.workerPool,
+		step.resourceConfig,
+		info.Version,
+		step.params,
 		step.stepMetadata,
 		step.session,
-		resource.ResourceType(step.resourceConfig.Type),
 		step.tags,
-		resource.VolumeMount{},
-	)
-	if err != nil {
-		return err
-	}
-
-	step.resource = trackedResource
-
-	var versionInfo VersionInfo
-	step.previousStep.Result(&versionInfo)
-
-	step.versionedSource = step.resource.Get(
-		resource.IOConfig{
-			Stdout: step.delegate.Stdout(),
-			Stderr: step.delegate.Stderr(),
-		},
-		step.resourceConfig.Source,
-		step.params,
-		versionInfo.Version,
-	)
-
-	err = step.versionedSource.Run(signals, ready)
-
-	if err, ok := err.(resource.ErrResourceScriptFailed); ok {
-		step.exitStatus = err.ExitStatus
-		step.delegate.Completed(ExitStatus(err.ExitStatus), nil)
-		return nil
-	}
-
-	if err != nil {
-		return err
-	}
-
-	step.repository.RegisterSource(step.sourceName, step)
-
-	step.exitStatus = 0
-	step.delegate.Completed(ExitStatus(0), &VersionInfo{
-		Version:  step.versionedSource.Version(),
-		Metadata: step.versionedSource.Metadata(),
-	})
-
-	return nil
-}
-
-func (step *dependentGetStep) Release() {
-	if step.resource != nil {
-		step.resource.Release()
-	}
-}
-
-func (step *dependentGetStep) Result(x interface{}) bool {
-	switch v := x.(type) {
-	case *Success:
-		*v = step.exitStatus == 0
-		return true
-	case *VersionInfo:
-		*v = VersionInfo{
-			Version:  step.versionedSource.Version(),
-			Metadata: step.versionedSource.Metadata(),
-		}
-		return true
-
-	default:
-		return false
-	}
-}
-
-func (step *dependentGetStep) StreamTo(destination ArtifactDestination) error {
-	out, err := step.versionedSource.StreamOut(".")
-	if err != nil {
-		return err
-	}
-
-	return destination.StreamIn(".", out)
-}
-
-func (step *dependentGetStep) StreamFile(path string) (io.ReadCloser, error) {
-	out, err := step.versionedSource.StreamOut(path)
-	if err != nil {
-		return nil, err
-	}
-
-	tarReader := tar.NewReader(out)
-
-	_, err = tarReader.Next()
-	if err != nil {
-		return nil, FileNotFoundError{Path: path}
-	}
-
-	return fileReadCloser{
-		Reader: tarReader,
-		Closer: out,
-	}, nil
+		step.delegate,
+		step.trackerFactory,
+	).Using(prev, repo)
 }
