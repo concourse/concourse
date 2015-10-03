72b1b3b19a1385775c46b032b28f99dd22936c10
Alex Suraci <suraci.alex@gmail.com>
2015-10-03 14:42:35 -0700

    woops, fix compile error

--- web/paths/for_build_test.go
+++ web/paths/for_build_test.go
@@ -2,7 +2,7 @@ package paths_test
 
 import (
 	"github.com/concourse/atc/db"
-	"github.com/concourse/atc/web"
+	"github.com/concourse/atc/web/paths"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -17,7 +17,7 @@ var _ = Describe("Routes", func() {
 				PipelineName: "a-pipeline",
 			}
 
-			path := web.PathForBuild(joblessBuild)
+			path := paths.PathForBuild(joblessBuild)
 			Expect(path).To(Equal("/builds/1"))
 		})
 
@@ -29,7 +29,7 @@ var _ = Describe("Routes", func() {
 				PipelineName: "a-pipeline",
 			}
 
-			path := web.PathForBuild(build)
+			path := paths.PathForBuild(build)
 			Expect(path).To(Equal("/pipelines/a-pipeline/jobs/hello/builds/23"))
 		})
 	})
