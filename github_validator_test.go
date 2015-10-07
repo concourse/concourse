79080a64eb22856bfe8248911c8d9841d68cfc95
Noah Denton <ndenton@pivotal.io>
2015-10-07 16:21:12 -0700

    add github token authentication for the api
    [#105045306]
    
    Signed-off-by: Toby Rumans <trumans@pivotal.io>

---
+++ github_validator_test.go
@@ -0,0 +1,222 @@
+package auth_test
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-golang/lager/lagertest"
+
+	"github.com/concourse/atc/auth"
+	"github.com/concourse/atc/auth/fakes"
+)
+
+var _ = Describe("GitHubAuthHandler", func() {
+	var server *httptest.Server
+	var client *http.Client
+	var fakeGitHubClient *fakes.FakeGitHubClient
+
+	simpleHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buffer := bytes.NewBufferString("github ")
+
+		io.Copy(w, buffer)
+		io.Copy(w, r.Body)
+	})
+
+	BeforeEach(func() {
+		fakeGitHubClient = new(fakes.FakeGitHubClient)
+		authHandler := auth.Handler{
+			Handler: simpleHandler,
+			Validator: auth.GitHubOrganizationValidator{
+				Organization: "testOrg",
+				Client:       fakeGitHubClient,
+			},
+		}
+
+		server = httptest.NewServer(authHandler)
+
+		client = &http.Client{
+			Transport: &http.Transport{},
+		}
+	})
+
+	AfterEach(func() {
+		server.Close()
+	})
+
+	It("strips out the Token keyword before calling to github", func() {
+		requestBody := bytes.NewBufferString("hello")
+		request, err := http.NewRequest("GET", server.URL, requestBody)
+		Expect(err).NotTo(HaveOccurred())
+		request.Header.Add("Authorization", "Token abcd")
+
+		_, err = client.Do(request)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(fakeGitHubClient.GetOrganizationsCallCount()).To(Equal(1))
+
+		Expect(fakeGitHubClient.GetOrganizationsArgsForCall(0)).To(Equal("abcd"))
+	})
+
+	It("returns a 401 when the Token is not passed in the header properly", func() {
+		requestBody := bytes.NewBufferString("hello")
+		request, err := http.NewRequest("GET", server.URL, requestBody)
+		Expect(err).NotTo(HaveOccurred())
+
+		response, err := client.Do(request)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+
+		request, err = http.NewRequest("GET", server.URL, requestBody)
+		request.Header.Add("Authorization", "abcd")
+
+		response, err = client.Do(request)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+
+		Expect(fakeGitHubClient.GetOrganizationsCallCount()).To(Equal(0))
+	})
+
+	Context("when authentication fails", func() {
+		Context("because the github client errors", func() {
+			BeforeEach(func() {
+				fakeGitHubClient.GetOrganizationsReturns(nil, errors.New("disaster"))
+			})
+
+			It("returns 401", func() {
+				requestBody := bytes.NewBufferString("hello")
+				request, err := http.NewRequest("GET", server.URL, requestBody)
+				Expect(err).NotTo(HaveOccurred())
+				request.Header.Add("Authorization", "Token besttoken")
+
+				response, err := client.Do(request)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+
+				responseBody, err := ioutil.ReadAll(response.Body)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(responseBody)).To(Equal("not authorized"))
+
+				Expect(fakeGitHubClient.GetOrganizationsCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("because the given token is not in the org", func() {
+			BeforeEach(func() {
+				fakeGitHubClient.GetOrganizationsReturns([]string{"nope"}, nil)
+			})
+
+			It("returns 401", func() {
+				requestBody := bytes.NewBufferString("hello")
+				request, err := http.NewRequest("GET", server.URL, requestBody)
+				request.Header.Add("Authorization", "Token besttoken")
+				Expect(err).NotTo(HaveOccurred())
+
+				response, err := client.Do(request)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+
+				responseBody, err := ioutil.ReadAll(response.Body)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(responseBody)).To(Equal("not authorized"))
+
+				Expect(fakeGitHubClient.GetOrganizationsCallCount()).To(Equal(1))
+			})
+		})
+
+	})
+
+	Context("with the correct credentials", func() {
+		BeforeEach(func() {
+			fakeGitHubClient.GetOrganizationsReturns([]string{"testOrg"}, nil)
+		})
+
+		It("It athenticates with GitHub", func() {
+			requestBody := bytes.NewBufferString("hello")
+			request, err := http.NewRequest("GET", server.URL, requestBody)
+			Expect(err).NotTo(HaveOccurred())
+			request.Header.Add("Authorization", "Token abcd")
+
+			response, err := client.Do(request)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+			responseBody, err := ioutil.ReadAll(response.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(responseBody)).To(Equal("github hello"))
+		})
+	})
+})
+
+var _ = Describe("actual auth with github", func() {
+	var server *httptest.Server
+	var client *http.Client
+
+	simpleHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buffer := bytes.NewBufferString("github ")
+
+		io.Copy(w, buffer)
+		io.Copy(w, r.Body)
+	})
+
+	BeforeEach(func() {
+		testLogger := lagertest.NewTestLogger("github-auth-test")
+		gitHubClient := auth.NewGitHubClient(testLogger)
+		authHandler := auth.Handler{
+			Handler: simpleHandler,
+			Validator: auth.GitHubOrganizationValidator{
+				Organization: "ConcourseGitHubAuthTestOrg",
+				Client:       gitHubClient,
+			},
+		}
+
+		server = httptest.NewServer(authHandler)
+
+		client = &http.Client{
+			Transport: &http.Transport{},
+		}
+	})
+
+	AfterEach(func() {
+		server.Close()
+	})
+
+	Context("with the correct credentials", func() {
+		It("It athenticates with GitHub", func() {
+			requestBody := bytes.NewBufferString("hello")
+			request, err := http.NewRequest("GET", server.URL, requestBody)
+			Expect(err).NotTo(HaveOccurred())
+			request.Header.Add("Authorization", "Token 35e79fab5ec51a039bd3afabb335dfe16a6daf64")
+
+			response, err := client.Do(request)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+			responseBody, err := ioutil.ReadAll(response.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(responseBody)).To(Equal("github hello"))
+		})
+	})
+
+	Context("with an invalid token", func() {
+		It("It returns a 401", func() {
+			requestBody := bytes.NewBufferString("hello")
+			request, err := http.NewRequest("GET", server.URL, requestBody)
+			Expect(err).NotTo(HaveOccurred())
+			request.Header.Add("Authorization", "Token bogus-token")
+
+			response, err := client.Do(request)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+
+			responseBody, err := ioutil.ReadAll(response.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(responseBody)).To(Equal("not authorized"))
+		})
+	})
+})
