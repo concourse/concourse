ad3e7ad824b234f0ef96f1b6bc5aa92a2b6b12d6
Anand Gaitonde <agaitonde@pivotal.io>
2015-10-08 10:53:27 -0700

    move access token to an environment variable
    [#105045306]
    
    Signed-off-by: Toby Rumans <trumans@pivotal.io>

--- github/client_test.go
+++ github/client_test.go
@@ -1,6 +1,8 @@
 package github_test
 
 import (
+	"os"
+
 	"github.com/concourse/atc/github"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -15,17 +17,28 @@ var _ = Describe("github", func() {
 		client = github.NewClient(lagertest.NewTestLogger("github-client"))
 	})
 
-	Context("GetOrganizations", func() {
-		It("returns a list of organizations given a valid access token", func() {
-			orgs, err := client.GetOrganizations("37f6d08a745cd5b4a2a72733654bb16c3b3c1b24")
+	Context("with a valid token", func() {
+		var token string
+		BeforeEach(func() {
+			token = os.Getenv("GITHUB_ACCESS_TOKEN")
+			if token == "" {
+				Skip("Set a GITHUB_ACCESS_TOKEN envrionment variable to run these tests")
+			}
+		})
+
+		It(" can return a list of organizations the user belongs to", func() {
+			orgs, err := client.GetOrganizations(token)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(orgs).To(HaveLen(1))
 			Expect(orgs[0]).To(Equal("ConcourseGitHubAuthTestOrg"))
 		})
+	})
 
-		It("returns an error given an invalid token", func() {
+	Context("without a valid token", func() {
+		It("returns an error when trying to fetch organizations given an invalid token", func() {
 			_, err := client.GetOrganizations("nope")
 			Expect(err).To(HaveOccurred())
 		})
 	})
+
 })
