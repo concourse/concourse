ade2c00a93c488c3228edc5259a305dca73908c8
Alex Suraci <suraci.alex@gmail.com>
2015-10-14 13:41:00 -0700

    fix validator fake

--- fakes/fake_validator.go
+++ fakes/fake_validator.go
@@ -17,6 +17,12 @@ type FakeValidator struct {
 	isAuthenticatedReturns struct {
 		result1 bool
 	}
+	UnauthorizedStub        func(http.ResponseWriter, *http.Request)
+	unauthorizedMutex       sync.RWMutex
+	unauthorizedArgsForCall []struct {
+		arg1 http.ResponseWriter
+		arg2 *http.Request
+	}
 }
 
 func (fake *FakeValidator) IsAuthenticated(arg1 *http.Request) bool {
@@ -49,6 +55,30 @@ func (fake *FakeValidator) IsAuthenticatedReturns(result1 bool) {
 	fake.isAuthenticatedReturns = struct {
 		result1 bool
 	}{result1}
+}
+
+func (fake *FakeValidator) Unauthorized(arg1 http.ResponseWriter, arg2 *http.Request) {
+	fake.unauthorizedMutex.Lock()
+	fake.unauthorizedArgsForCall = append(fake.unauthorizedArgsForCall, struct {
+		arg1 http.ResponseWriter
+		arg2 *http.Request
+	}{arg1, arg2})
+	fake.unauthorizedMutex.Unlock()
+	if fake.UnauthorizedStub != nil {
+		fake.UnauthorizedStub(arg1, arg2)
+	}
+}
+
+func (fake *FakeValidator) UnauthorizedCallCount() int {
+	fake.unauthorizedMutex.RLock()
+	defer fake.unauthorizedMutex.RUnlock()
+	return len(fake.unauthorizedArgsForCall)
+}
+
+func (fake *FakeValidator) UnauthorizedArgsForCall(i int) (http.ResponseWriter, *http.Request) {
+	fake.unauthorizedMutex.RLock()
+	defer fake.unauthorizedMutex.RUnlock()
+	return fake.unauthorizedArgsForCall[i].arg1, fake.unauthorizedArgsForCall[i].arg2
 }
 
 var _ auth.Validator = new(FakeValidator)
