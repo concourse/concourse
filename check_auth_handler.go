f36c722f9bebff94725711674d61ae27d86f10fa
Alex Suraci <asuraci@pivotal.io>
2015-10-16 15:22:59 -0700

    extract api auth + metrics wrapping logic
    * remove validator usage from individual handlers; use
      auth.IsAuthenticated instead
    
    * test wrapping logic for api auth
    
    * split request rejecting out from api.WrapHandler so that individual
      handlers can use legitimate auth info without rejecting upfront
    
    [#105796608]

---
+++ check_auth_handler.go
@@ -0,0 +1,26 @@
+package auth
+
+import "net/http"
+
+type checkAuthHandler struct {
+	handler  http.Handler
+	rejector Rejector
+}
+
+func CheckAuthHandler(
+	handler http.Handler,
+	rejector Rejector,
+) http.Handler {
+	return checkAuthHandler{
+		handler:  handler,
+		rejector: rejector,
+	}
+}
+
+func (h checkAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if IsAuthenticated(r) {
+		h.handler.ServeHTTP(w, r)
+	} else {
+		h.rejector.Unauthorized(w, r)
+	}
+}
