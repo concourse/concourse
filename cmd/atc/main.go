1aa7396e9fa472dc3f328983e69b5ba6fcb6f025
Alex Suraci <suraci.alex@gmail.com>
2015-10-16 21:53:31 -0700

    kill comment

--- cmd/atc/main.go
+++ cmd/atc/main.go
@@ -10,22 +10,6 @@ import (
 	"github.com/tedsuo/ifrit"
 )
 
-// var resourceTypes = flag.String(
-// 	"resourceTypes",
-// 	`[
-// 		{"type": "archive", "image": "docker:///concourse/archive-resource" },
-// 		{"type": "docker-image", "image": "docker:///concourse/docker-image-resource" },
-// 		{"type": "git", "image": "docker:///concourse/git-resource" },
-// 		{"type": "github-release", "image": "docker:///concourse/github-release-resource" },
-// 		{"type": "s3", "image": "docker:///concourse/s3-resource" },
-// 		{"type": "semver", "image": "docker:///concourse/semver-resource" },
-// 		{"type": "time", "image": "docker:///concourse/time-resource" },
-// 		{"type": "tracker", "image": "docker:///concourse/tracker-resource" },
-// 		{"type": "pool", "image": "docker:///concourse/pool-resource" }
-// 	]`,
-// 	"map of resource type to its rootfs",
-// )
-
 func main() {
 	cmd := &ATCCommand{}
 
