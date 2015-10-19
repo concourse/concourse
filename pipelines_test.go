4c6714d1433319ecdc863984b7c5b11c35250336
Anand Gaitonde <agaitonde@pivotal.io>
2015-10-18 17:10:55 -0700

    Pipelines command now uses AtcClient to make requests.
    [#105155444]

--- pipelines_test.go
+++ pipelines_test.go
@@ -3,12 +3,45 @@ package atcclient_test
 import (
 	"net/http"
 
+	"github.com/concourse/atc"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 )
 
 var _ = Describe("ATC Handler Pipelines", func() {
+	Describe("ListPipelines", func() {
+		var expectedPipelines []atc.Pipeline
+
+		BeforeEach(func() {
+			expectedURL := "/api/v1/pipelines"
+
+			expectedPipelines = []atc.Pipeline{
+				{
+					Name:   "mypipeline-1",
+					Paused: true,
+				},
+				{
+					Name:   "mypipeline-2",
+					Paused: false,
+				},
+			}
+
+			atcServer.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("GET", expectedURL),
+					ghttp.RespondWithJSONEncoded(http.StatusOK, expectedPipelines),
+				),
+			)
+		})
+
+		It("returns all the pipelines", func() {
+			pipelines, err := handler.ListPipelines()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pipelines).To(Equal(expectedPipelines))
+		})
+	})
+
 	Describe("DeletePipeline", func() {
 		expectedURL := "/api/v1/pipelines/mypipeline"
 
