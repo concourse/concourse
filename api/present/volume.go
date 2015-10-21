2d42df8b45a01e95b4c36dc4b1b4ec483bfc53fe
Alex Suraci <asuraci@pivotal.io>
2015-10-21 13:42:10 -0700

    use atc.Version, not *json.RawMessage

--- api/present/volume.go
+++ api/present/volume.go
@@ -1,19 +1,15 @@
 package present
 
 import (
-	"encoding/json"
-
 	"github.com/concourse/atc"
 	"github.com/concourse/atc/db"
 )
 
 func Volume(volume db.VolumeData) atc.Volume {
-	version, _ := json.Marshal(volume.ResourceVersion)
-
 	return atc.Volume{
 		ID:              volume.Handle,
 		TTLInSeconds:    int64(volume.TTL.Seconds()),
-		ResourceVersion: (*json.RawMessage)(&version),
+		ResourceVersion: volume.ResourceVersion,
 		WorkerName:      volume.WorkerName,
 	}
 }
--- volume.go
+++ volume.go
@@ -1,10 +1,8 @@
 package atc
 
-import "encoding/json"
-
 type Volume struct {
-	ID              string           `json:"id"`
-	TTLInSeconds    int64            `json:"ttl_in_seconds"`
-	ResourceVersion *json.RawMessage `json:"resource_version"`
-	WorkerName      string           `json:"worker_name"`
+	ID              string  `json:"id"`
+	TTLInSeconds    int64   `json:"ttl_in_seconds"`
+	ResourceVersion Version `json:"resource_version"`
+	WorkerName      string  `json:"worker_name"`
 }
