dd8d9f8967b0e6ff30021e36faabdd4b0034a0c3
Chris Brown <cbrown@pivotal.io>
2015-11-24 17:38:38 -0800

    move header, abort button, trigger button into elm

--- web/elm/src/Build.elm
+++ web/elm/src/Build.elm
@@ -5,9 +5,10 @@ import Debug
 import Effects exposing (Effects)
 import EventSource exposing (EventSource)
 import Html exposing (Html)
-import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (action, class, href, id, method)
 import Http
-import Json.Decode
+import Json.Decode exposing ((:=))
 import Task
 import Time exposing (Time)
 
@@ -21,14 +22,25 @@ type alias Model =
   { actions : Signal.Address Action
   , buildId : Int
   , stepRoot : Maybe StepTree.Root
+  , build : Maybe Build
   , eventSource : Maybe EventSource
   , eventsLoaded : Bool
   , autoScroll : Bool
+  , status : Maybe BuildEvent.BuildStatus
+  }
+
+type alias Build =
+  { id : Int
+  , name : String
+  , status : String
+  , jobName : String
+  , pipelineName : String
   }
 
 type Action
   = Noop
   | PlanFetched (Result Http.Error BuildPlan)
+  | BuildFetched (Result Http.Error Build)
   | Listening EventSource
   | Opened
   | Errored
@@ -38,6 +50,7 @@ type Action
   | ScrollTick
   | ScrollFromBottom Int
   | StepTreeAction StepTree.Action
+  | AbortBuild
 
 init : Signal.Address Action -> Int -> (Model, Effects Action)
 init actions buildId =
@@ -46,12 +59,18 @@ init actions buildId =
       { actions = actions
       , buildId = buildId
       , stepRoot = Nothing
+      , build = Nothing
       , eventSource = Nothing
       , eventsLoaded = False
       , autoScroll = True
+      , status = Nothing
       }
   in
-    (model, Effects.batch [keepScrolling, fetchBuildPlan 0 buildId])
+    (model, Effects.batch [ keepScrolling
+                          , fetchBuildPlan 0 buildId
+                          , fetchBuild buildId
+                          ]
+    )
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
@@ -71,6 +90,9 @@ update action model =
       else
         ({ model | autoScroll = False }, Effects.none)
 
+    AbortBuild ->
+      (model, abortBuild model.buildId)
+
     PlanFetched (Err (Http.BadResponse 404 _)) ->
       (model, fetchBuildPlan Time.second model.buildId)
 
@@ -81,6 +103,15 @@ update action model =
     PlanFetched (Ok plan) ->
       ( { model | stepRoot = Just (StepTree.init plan) }
       , subscribeToEvents model.buildId model.actions
+      )
+
+    BuildFetched (Err err) ->
+      Debug.log ("failed to fetch build: " ++ toString err) <|
+        (model, Effects.none)
+
+    BuildFetched (Ok build) ->
+      ( { model | build = Just build }
+      , Effects.none
       )
 
     Listening es ->
@@ -127,8 +158,8 @@ update action model =
       , Effects.none
       )
 
-    Event (Ok (BuildEvent.BuildStatus _)) ->
-      (model, Effects.none)
+    Event (Ok (BuildEvent.BuildStatus status)) ->
+      ( { model | status = Just status} , Effects.none)
 
     Event (Err e) ->
       (model, Debug.log e Effects.none)
@@ -148,6 +179,18 @@ update action model =
 
     Closed ->
       ({ model | eventSource = Nothing }, Effects.none)
+
+abortBuild : Int -> Effects.Effects Action
+abortBuild buildId =
+  Http.send Http.defaultSettings
+    { verb = "POST"
+    , headers = []
+    , url = "/api/v1/builds/" ++ toString buildId ++ "/abort"
+    , body = Http.empty
+    }
+    |> Task.toMaybe
+    |> Task.map (always Noop)
+    |> Effects.task
 
 keepScrolling : Effects Action
 keepScrolling = Effects.tick (always ScrollTick)
@@ -187,13 +230,68 @@ setStepState state tree =
 
 view : Signal.Address Action -> Model -> Html
 view actions model =
-  case model.stepRoot of
-    Nothing ->
+  case (model.build, model.stepRoot) of
+    (Just build, Just root) ->
+      Html.div []
+        [ viewBuildHeader actions build model.status
+        , Html.div [id "build-body"]
+            [ Html.div [class "steps"]
+                [ StepTree.view (Signal.forwardTo actions StepTreeAction) root.tree ]
+            ]
+        ]
+
+    (Just build, Nothing) ->
+      Html.div []
+        [ viewBuildHeader actions build model.status
+        , Html.div [id "build-body"] []
+        ]
+
+    _ ->
       Html.text "loading..."
 
-    Just root ->
-      Html.div [class "steps"]
-        [ StepTree.view (Signal.forwardTo actions StepTreeAction) root.tree ]
+statusClass : BuildEvent.BuildStatus -> String
+statusClass status =
+  case status of
+    BuildEvent.BuildStatusPending ->
+      "pending"
+
+    BuildEvent.BuildStatusStarted ->
+      "started"
+
+    BuildEvent.BuildStatusSucceeded ->
+      "succeeded"
+
+    BuildEvent.BuildStatusFailed ->
+      "failed"
+
+    BuildEvent.BuildStatusErrored ->
+      "errored"
+
+    BuildEvent.BuildStatusAborted ->
+      "aborted"
+
+viewBuildHeader : Signal.Address Action -> Build -> Maybe BuildEvent.BuildStatus -> Html
+viewBuildHeader actions build status =
+  Html.div [id "page-header", class (statusClass (Maybe.withDefault BuildEvent.BuildStatusPending status))]
+    [ Html.div [class "build-header"]
+        [ Html.div [class "build-actions fr"]
+            [ Html.form [class "trigger-build", method "post", action ("/pipelines/" ++ build.pipelineName ++ "/jobs/" ++ build.jobName ++ "/builds") ]
+                [ Html.button [ class "build-action fr" ] [ Html.i [class "fa fa-plus-circle" ] [] ] ]
+            , Html.span [class "build-action build-action-abort fr", onClick actions AbortBuild] [ Html.i [class "fa fa-times-circle"] [] ] -- TODO: only show if started and pending
+            ]
+        , Html.h1 []
+            [ Html.a [href ""] [ Html.text (build.jobName ++ " #" ++ build.name) ] ] -- TODO: url
+        , Html.dl [class "build-times"] []
+        ]
+    , Html.ul [id "builds"] -- TODO: populate
+        [ Html.li [class "succeeded current"]
+            [ Html.a [href ""] [ Html.text "#3" ] ]
+        , Html.li [class "failed"]
+            [ Html.a [href ""] [ Html.text "#2" ] ]
+        , Html.li [class "succeeded"]
+            [ Html.a [href ""] [ Html.text "#1" ] ]
+        ]
+    ]
 
 fetchBuildPlan : Time -> Int -> Effects.Effects Action
 fetchBuildPlan delay buildId =
@@ -204,6 +302,22 @@ fetchBuildPlan delay buildId =
         |> Task.map PlanFetched
   in
     Effects.task (Task.sleep delay `Task.andThen` \_ -> fetchPlan)
+
+fetchBuild : Int -> Effects.Effects Action
+fetchBuild buildId =
+  Http.get decode ("/api/v1/builds/" ++ toString buildId)
+    |> Task.toResult
+    |> Task.map BuildFetched
+    |> Effects.task
+
+decode : Json.Decode.Decoder Build
+decode =
+  Json.Decode.object5 Build
+    ("id" := Json.Decode.int)
+    ("name" := Json.Decode.string)
+    ("status" := Json.Decode.string)
+    ("job_name" := Json.Decode.string)
+    ("pipeline_name" := Json.Decode.string)
 
 subscribeToEvents : Int -> Signal.Address Action -> Effects.Effects Action
 subscribeToEvents build actions =
