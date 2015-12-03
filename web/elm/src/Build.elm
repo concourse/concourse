ec83d0fb754f9464afb4602e48ef27a9a9e5a59d
Alex Suraci <asuraci@pivotal.io>
2015-12-03 10:49:55 -0800

    fix spacing on loading indicator

--- web/elm/src/Build.elm
+++ web/elm/src/Build.elm
@@ -419,11 +419,13 @@ view actions model =
 
 loadingIndicator : Html
 loadingIndicator =
-  Html.div [class "build-step"]
-    [ Html.div [class "header"]
-        [ Html.i [class "left fa fa-fw fa-spin fa-circle-o-notch"] []
-        , Html.h3 [] [Html.text "loading"]
-        ]
+  Html.div [class "steps"]
+    [ Html.div [class "build-step"]
+      [ Html.div [class "header"]
+          [ Html.i [class "left fa fa-fw fa-spin fa-circle-o-notch"] []
+          , Html.h3 [] [Html.text "loading"]
+          ]
+      ]
     ]
 
 viewSteps : Signal.Address Action -> Maybe Ansi.Log.Model -> Build -> StepTree.Root -> Html
