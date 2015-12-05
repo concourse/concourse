b44f33f8a4431bf0e5e7baf0ce7d02836698d141
Evan Short <eshort@pivotal.io>
2015-12-04 18:17:50 -0800

    Fix broken migration again
    [#92884224]

--- db/migrations/57_make_container_working_directory_not_null.go
+++ db/migrations/57_make_container_working_directory_not_null.go
@@ -4,17 +4,21 @@ import "github.com/BurntSushi/migration"
 
 func MakeContainerWorkingDirectoryNotNull(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
-		BEGIN WORK;
-		LOCK TABLE containers IN SHARE ROW EXCLUSIVE MODE;
-
-		UPDATE containers SET working_directory = '' WHERE working_directory IS NULL;
-
-		ALTER TABLE containers
-		ALTER COLUMN working_directory SET DEFAULT '',
-		ALTER COLUMN working_directory SET NOT NULL;
-
-		COMMIT WORK;
+		ALTER TABLE containers ALTER COLUMN working_directory SET DEFAULT '';
 	`)
+	if err != nil {
+		return err
+	}
 
+	_, err = tx.Exec(`
+	  UPDATE containers SET working_directory = '' WHERE working_directory IS null;
+  `)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(`
+	  ALTER TABLE containers ALTER COLUMN working_directory SET NOT NULL;
+  `)
 	return err
 }
