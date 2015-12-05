8dbdf65d6d595a8fecf6bf24da2a52ff8e98b36b
Evan Short <eshort@pivotallabs.com>
2015-12-04 16:50:50 -0800

    Fix previous migration
    - It causes sql error when there are existing containers
    
    [#92884224]
    
    Signed-off-by: Liz Dahlstrom <edahlstrom@pivotallabs.com>

--- db/migrations/57_make_container_working_directory_not_null.go
+++ db/migrations/57_make_container_working_directory_not_null.go
@@ -4,9 +4,16 @@ import "github.com/BurntSushi/migration"
 
 func MakeContainerWorkingDirectoryNotNull(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
+		BEGIN WORK;
+		LOCK TABLE containers IN SHARE ROW EXCLUSIVE MODE;
+
+		UPDATE containers SET working_directory = '' WHERE working_directory IS NULL;
+
 		ALTER TABLE containers
-		ALTER COLUMN working_directory SET NOT NULL,
-		ALTER COLUMN working_directory SET DEFAULT ''
+		ALTER COLUMN working_directory SET DEFAULT '',
+		ALTER COLUMN working_directory SET NOT NULL;
+
+		COMMIT WORK;
 	`)
 
 	return err
