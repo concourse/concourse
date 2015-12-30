17a5b52eb0f049404b6bab83a5f087f23f39bc56
Alex Suraci <suraci.alex@gmail.com>
2015-12-30 16:44:36 -0500

    support more TSA flags

--- cmd/concourse/web.go
+++ cmd/concourse/web.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"sync"
+	"time"
 
 	"github.com/concourse/atc/atccmd"
 	"github.com/concourse/tsa/tsacmd"
@@ -13,15 +14,25 @@ type WebCommand struct {
 	atccmd.ATCCommand
 
 	TSA struct {
+		BindIP   tsacmd.IPFlag `long:"bind-ip"   default:"0.0.0.0" description:"IP address on which to listen for SSH."`
+		BindPort uint16        `long:"bind-port" default:"2222"    description:"Port on which to listen for SSH."`
+
 		HostKeyPath        tsacmd.FileFlag `long:"host-key"               required:"true" description:"Key to use for the TSA's ssh server."`
 		AuthorizedKeysPath tsacmd.FileFlag `long:"authorized-keys" required:"true" description:"Path to a file containing public keys to authorize for SSH access."`
+
+		HeartbeatInterval time.Duration `long:"heartbeat-interval" default:"30s" description:"interval on which to heartbeat workers to the ATC"`
 	} `group:"TSA Configuration" namespace:"tsa"`
 }
 
 func (cmd *WebCommand) Execute(args []string) error {
 	tsa := &tsacmd.TSACommand{
+		BindIP:   cmd.TSA.BindIP,
+		BindPort: cmd.TSA.BindPort,
+
 		HostKeyPath:        cmd.TSA.HostKeyPath,
 		AuthorizedKeysPath: cmd.TSA.AuthorizedKeysPath,
+
+		HeartbeatInterval: cmd.TSA.HeartbeatInterval,
 	}
 
 	cmd.populateTSAFlagsFromATCFlags(tsa)
