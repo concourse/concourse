c52e107634bf8133f4b207b056ee2b122e9e385a
Chris Brown <cbrown@pivotal.io>
2016-01-04 14:08:30 -0800

    don't panic with invalid job flags
    [#109399574]

--- commands/internal/flaghelpers/job_flag.go
+++ commands/internal/flaghelpers/job_flag.go
@@ -1,6 +1,7 @@
 package flaghelpers
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/concourse/go-concourse/concourse"
@@ -13,9 +14,15 @@ type JobFlag struct {
 
 func (job *JobFlag) UnmarshalFlag(value string) error {
 	vs := strings.SplitN(value, "/", 2)
+
+	if len(vs) != 2 {
+		return errors.New("argument format should be <pipeline>/<job>")
+	}
+
 	if vs[0] == "" {
 		return concourse.NameRequiredError("pipeline")
 	}
+
 	if vs[1] == "" {
 		return concourse.NameRequiredError("job")
 	}
