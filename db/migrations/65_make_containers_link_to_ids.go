9b108fdd7f8e1b7cefd5231cdb2c9eefe03dc122
Corbin Halliwill <challiwill@pivotal.io>
2016-01-05 13:37:42 -0800

    can't delete from db with foreign keys present
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- db/migrations/65_make_containers_link_to_ids.go
+++ db/migrations/65_make_containers_link_to_ids.go
@@ -5,7 +5,7 @@ import "github.com/BurntSushi/migration"
 func MakeContainersLinkToIds(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
 		ALTER TABLE containers
-		ADD COLUMN pipeline_id INT REFERENCES pipelines(id) NULL;
+		ADD COLUMN pipeline_id INT NULL;
 
 		UPDATE containers c SET pipeline_id =
 		(SELECT id FROM pipelines p where p.name = c.pipeline_name);
@@ -14,7 +14,7 @@ func MakeContainersLinkToIds(tx migration.LimitedTx) error {
 		DROP COLUMN pipeline_name;
 
 		ALTER TABLE containers
-		ADD COLUMN resource_id INT REFERENCES resources(id) NULL;
+		ADD COLUMN resource_id INT NULL;
 
 		UPDATE containers c SET resource_id =
 		(SELECT id from resources r where r.name = c.name);
@@ -27,10 +27,6 @@ func MakeContainersLinkToIds(tx migration.LimitedTx) error {
 		WHERE resource_id IS NOT NULL;
 
 		ALTER TABLE containers
-		ADD CONSTRAINT containers_build_id_fk FOREIGN KEY (build_id)
-		               REFERENCES builds(id);
-
-		ALTER TABLE containers
 		ALTER COLUMN build_id DROP NOT NULL;
 
 		UPDATE containers SET build_id = NULL
@@ -40,7 +36,7 @@ func MakeContainersLinkToIds(tx migration.LimitedTx) error {
 		ADD COLUMN id BIGSERIAL PRIMARY KEY;
 
 		ALTER TABLE containers
-		ADD COLUMN worker_id INT REFERENCES workers(id) NULL;
+		ADD COLUMN worker_id INT NULL;
 
 		UPDATE containers c SET worker_id =
 		(select id from workers w where w.name = c.worker_name);
