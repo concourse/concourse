780a49a3e149e5056e7e330b40d6905e9a12e0b5
Corbin Halliwill <challiwill@pivotal.io>
2016-01-13 09:44:13 -0800

    fix migration to handle dropped workers
    [#111477168]
    
    Signed-off-by: Evan Short <eshort@pivotal.io>

--- db/migrations/71_remove_worker_ids.go
+++ db/migrations/71_remove_worker_ids.go
@@ -19,7 +19,11 @@ func RemoveWorkerIds(tx migration.LimitedTx) error {
 	}
 
 	_, err = tx.Exec(`
+		BEGIN WORK;
+		LOCK TABLE volumes IN ACCESS EXCLUSIVE MODE;
+		DELETE FROM volumes WHERE worker_name IS NULL;
 		ALTER TABLE volumes ALTER COLUMN worker_name SET NOT NULL;
+		COMMIT WORK;
 	`)
 	if err != nil {
 		return err
