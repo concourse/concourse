06ad89d34b4812b54750865496fd6af32852d424
Alex Suraci <suraci.alex@gmail.com>
2016-01-13 14:04:09 -0800

    hack in color picker to aid in testing

---
+++ web/assets/css/gen-colors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -x
+
+cd $(dirname $0)
+
+builder=/tmp/base16-builder
+
+if ! [ -d $builder ]; then
+  git clone --depth 1 https://github.com/chriskempson/base16-builder $builder
+fi
+
+for scheme in ${builder}/schemes/*.yml; do
+  grep '^base[0-9A-F]\{2\}' $scheme | \
+    sed -e 's/^base\([0-9A-F]\{2\}\): "\(.*\)".*/@base\1: #\2;/' > \
+    _vars.less
+
+  name=$(echo $(basename $scheme) | sed -e 's/.yml//')
+  lessc main.less ../../public/main.${name}.css
+done
