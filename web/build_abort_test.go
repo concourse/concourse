cc00a0341b9f2e2619b486b1dde41bc5aafb7c7e
Alex Suraci <suraci.alex@gmail.com>
2016-01-14 17:39:51 -0800

    comment out hanging web tests
    phantomjs issues seem to cause these to get stuck. will re-enable once
    we've switched to chromedriver.

--- web/build_abort_test.go
+++ web/build_abort_test.go
@@ -1,90 +1,90 @@
 package web_test
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	. "github.com/sclevine/agouti/matchers"
+// 	. "github.com/onsi/ginkgo"
+// 	. "github.com/onsi/gomega"
+// 	. "github.com/sclevine/agouti/matchers"
 
-	"github.com/concourse/atc"
-)
+// 	"github.com/concourse/atc"
+// )
 
-var _ = Describe("Aborting a build", func() {
-	Context("with a build in the configuration", func() {
-		var build atc.Build
+// var _ = Describe("Aborting a build", func() {
+// 	Context("with a build in the configuration", func() {
+// 		var build atc.Build
 
-		BeforeEach(func() {
-			_, _, err := client.CreateOrUpdatePipelineConfig(pipelineName, "0", atc.Config{
-				Jobs: []atc.JobConfig{
-					{
-						Name: "some-job",
-						Plan: atc.PlanSequence{
-							{
-								Task: "some-task",
-								TaskConfig: &atc.TaskConfig{
-									Run: atc.TaskRunConfig{
-										Path: "sleep",
-										Args: []string{"1000"},
-									},
-								},
-							},
-						},
-					},
-				},
-			})
-			Expect(err).NotTo(HaveOccurred())
+// 		BeforeEach(func() {
+// 			_, _, err := client.CreateOrUpdatePipelineConfig(pipelineName, "0", atc.Config{
+// 				Jobs: []atc.JobConfig{
+// 					{
+// 						Name: "some-job",
+// 						Plan: atc.PlanSequence{
+// 							{
+// 								Task: "some-task",
+// 								TaskConfig: &atc.TaskConfig{
+// 									Run: atc.TaskRunConfig{
+// 										Path: "sleep",
+// 										Args: []string{"1000"},
+// 									},
+// 								},
+// 							},
+// 						},
+// 					},
+// 				},
+// 			})
+// 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = client.UnpausePipeline(pipelineName)
-			Expect(err).NotTo(HaveOccurred())
+// 			_, err = client.UnpausePipeline(pipelineName)
+// 			Expect(err).NotTo(HaveOccurred())
 
-			build, err = client.CreateJobBuild(pipelineName, "some-job")
-			Expect(err).NotTo(HaveOccurred())
-		})
+// 			build, err = client.CreateJobBuild(pipelineName, "some-job")
+// 			Expect(err).NotTo(HaveOccurred())
+// 		})
 
-		It("can abort the build", func() {
-			Expect(page.Navigate(atcRoute(build.URL))).To(Succeed())
-			Eventually(page).Should(HaveURL(atcRoute(fmt.Sprintf("pipelines/%s/jobs/some-job/builds/%s", pipelineName, build.Name))))
-			Eventually(page.Find("h1")).Should(HaveText(fmt.Sprintf("some-job #%s", build.Name)))
+// 		It("can abort the build", func() {
+// 			Expect(page.Navigate(atcRoute(build.URL))).To(Succeed())
+// 			Eventually(page).Should(HaveURL(atcRoute(fmt.Sprintf("pipelines/%s/jobs/some-job/builds/%s", pipelineName, build.Name))))
+// 			Eventually(page.Find("h1")).Should(HaveText(fmt.Sprintf("some-job #%s", build.Name)))
 
-			Eventually(page.Find(".build-action-abort")).Should(BeFound())
-			Expect(page.Find(".build-action-abort").Click()).To(Succeed())
+// 			Eventually(page.Find(".build-action-abort")).Should(BeFound())
+// 			Expect(page.Find(".build-action-abort").Click()).To(Succeed())
 
-			Eventually(page.Find("#page-header.aborted")).Should(BeFound())
-			Eventually(page.Find(".build-action-abort")).ShouldNot(BeFound())
-		})
-	})
+// 			Eventually(page.Find("#page-header.aborted")).Should(BeFound())
+// 			Eventually(page.Find(".build-action-abort")).ShouldNot(BeFound())
+// 		})
+// 	})
 
-	Context("with a one-off build", func() {
-		var build atc.Build
+// 	Context("with a one-off build", func() {
+// 		var build atc.Build
 
-		BeforeEach(func() {
-			var err error
+// 		BeforeEach(func() {
+// 			var err error
 
-			pf := atc.NewPlanFactory(0)
+// 			pf := atc.NewPlanFactory(0)
 
-			build, err = client.CreateBuild(pf.NewPlan(atc.TaskPlan{
-				Name: "some-task",
-				Config: &atc.TaskConfig{
-					Run: atc.TaskRunConfig{
-						Path: "sleep",
-						Args: []string{"1000"},
-					},
-				},
-			}))
-			Expect(err).NotTo(HaveOccurred())
-		})
+// 			build, err = client.CreateBuild(pf.NewPlan(atc.TaskPlan{
+// 				Name: "some-task",
+// 				Config: &atc.TaskConfig{
+// 					Run: atc.TaskRunConfig{
+// 						Path: "sleep",
+// 						Args: []string{"1000"},
+// 					},
+// 				},
+// 			}))
+// 			Expect(err).NotTo(HaveOccurred())
+// 		})
 
-		It("can abort the build", func() {
-			Expect(page.Navigate(atcRoute(build.URL))).To(Succeed())
-			Eventually(page).Should(HaveURL(atcRoute(fmt.Sprintf("builds/%d", build.ID))))
-			Eventually(page.Find("h1")).Should(HaveText(fmt.Sprintf("build #%d", build.ID)))
+// 		It("can abort the build", func() {
+// 			Expect(page.Navigate(atcRoute(build.URL))).To(Succeed())
+// 			Eventually(page).Should(HaveURL(atcRoute(fmt.Sprintf("builds/%d", build.ID))))
+// 			Eventually(page.Find("h1")).Should(HaveText(fmt.Sprintf("build #%d", build.ID)))
 
-			Eventually(page.Find(".build-action-abort")).Should(BeFound())
-			Expect(page.Find(".build-action-abort").Click()).To(Succeed())
+// 			Eventually(page.Find(".build-action-abort")).Should(BeFound())
+// 			Expect(page.Find(".build-action-abort").Click()).To(Succeed())
 
-			Eventually(page.Find("#page-header.aborted")).Should(BeFound())
-			Eventually(page.Find(".build-action-abort")).ShouldNot(BeFound())
-		})
-	})
-})
+// 			Eventually(page.Find("#page-header.aborted")).Should(BeFound())
+// 			Eventually(page.Find(".build-action-abort")).ShouldNot(BeFound())
+// 		})
+// 	})
+// })
