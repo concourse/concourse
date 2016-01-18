990e937b0264f304fa0e87abd4044076c35fa5f5
Alex Suraci <suraci.alex@gmail.com>
2016-01-18 00:38:17 -0800

    oh right, windows

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,5 +1,4 @@
 set GOPATH=%CD%\gopath;%CD%\concourse
 set PATH=%CD%\gopath\bin;%PATH%
 
-go build -o .\windows-binary\concourse_windows_amd64.exe \
-  github.com/vito/concourse-bin/cmd/concourse
+go build -o .\windows-binary\concourse_windows_amd64.exe github.com/vito/concourse-bin/cmd/concourse
