51e2982db37b7834f742f75e7ac4566ccc9bff61
Corbin Halliwill <challiwill@pivotal.io>
2016-01-29 11:11:55 -0800

    remove dependencies on implicit task outputs
    [#112645581]
    
    Signed-off-by: Evan Short <eshort@pivotal.io>

---
+++ pipelines/task_missing_outputs_test.go
@@ -0,0 +1,36 @@
+package pipelines_test
+
+import (
+	"github.com/concourse/testflight/gitserver"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("A task with no outputs declared", func() {
+	var originGitServer *gitserver.Server
+
+	BeforeEach(func() {
+		originGitServer = gitserver.Start(gitServerRootfs, gardenClient)
+
+		configurePipeline(
+			"-c", "fixtures/task-missing-outputs.yml",
+			"-v", "testflight-helper-image="+guidServerRootfs,
+			"-v", "origin-git-server="+originGitServer.URI(),
+		)
+
+		originGitServer.Commit()
+	})
+
+	AfterEach(func() {
+		originGitServer.Stop()
+	})
+
+	It("doesn't mount its file system into the next task", func() {
+		watch := flyWatch("missing-outputs-job")
+		Expect(watch).To(gexec.Exit(2))
+
+		Expect(watch).To(gbytes.Say("missing inputs: missing-outputs"))
+	})
+})
