a9a177733220f910f35c306852332f73c810336e
Alex Suraci <suraci.alex@gmail.com>
2016-01-29 10:40:25 -0800

    Revert "never use selenium ever"
    This reverts commit 2b17348c5eb516d3d5b340c8fd28cc065594d42a.

--- helpers/agouti_driver.go
+++ helpers/agouti_driver.go
@@ -1,7 +1,19 @@
 package helpers
 
-import "github.com/sclevine/agouti"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/onsi/ginkgo"
+	"github.com/sclevine/agouti"
+)
 
 func AgoutiDriver() *agouti.WebDriver {
-	return agouti.PhantomJS()
+	if _, err := exec.LookPath("phantomjs"); err == nil && os.Getenv("FORCE_SELENIUM") != "true" {
+		fmt.Fprintln(ginkgo.GinkgoWriter, "WARNING: using phantomjs, which is flaky in CI, but is more convenient during development")
+		return agouti.PhantomJS()
+	} else {
+		return agouti.Selenium(agouti.Browser("firefox"))
+	}
 }
