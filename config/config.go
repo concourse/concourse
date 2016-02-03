c04f3bde68bbe4aef3b9f58ea20394c54435f744
Alex Suraci <asuraci@pivotal.io>
2016-02-03 10:38:15 -0800

    use atc.LoadTaskConfig
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- config/config.go
+++ config/config.go
@@ -6,7 +6,6 @@ import (
 	"syscall"
 
 	"github.com/concourse/atc"
-	"gopkg.in/yaml.v2"
 )
 
 func LoadTaskConfig(configPath string, args []string) (atc.TaskConfig, error) {
@@ -15,11 +14,9 @@ func LoadTaskConfig(configPath string, args []string) (atc.TaskConfig, error) {
 		return atc.TaskConfig{}, fmt.Errorf("failed to read task config: %s", err)
 	}
 
-	var config atc.TaskConfig
-
-	err = yaml.Unmarshal(configFile, &config)
+	config, err := atc.LoadTaskConfig(configFile)
 	if err != nil {
-		return atc.TaskConfig{}, fmt.Errorf("task config is malformed: %s", err)
+		return atc.TaskConfig{}, err
 	}
 
 	config.Run.Args = append(config.Run.Args, args...)
