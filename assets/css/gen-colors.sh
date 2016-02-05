851507e8c2109d41fbeb82fcb4114c8a06404617
Anand Gaitonde <agaitonde@pivotal.io>
2016-02-04 17:20:34 -0800

    add custom concourse theme based off of flat
    [#113039669]

--- assets/css/gen-colors.sh
+++ assets/css/gen-colors.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 builder=/tmp/base16-builder
 
 if ! [ -d $builder ]; then
-  git clone --depth 1 https://github.com/chriskempson/base16-builder $builder
+  git clone --depth 1 https://github.com/concourse/base16-builder $builder
 fi
 
 for scheme in ${builder}/schemes/*.yml; do
