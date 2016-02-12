62ce895c24f5d616108caff2497a3db51a693e4e
Alex Suraci <suraci.alex@gmail.com>
2016-02-12 11:07:39 -0800

    bend windows to my will

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,8 +1,10 @@
 set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\Godeps_windows\_workspace
 set PATH=%CD%\gopath\bin;%PATH%
 
-go get github.com/jteeuwen/go-bindata/go-bindata
+go get github.com/jteeuwen/go-bindata
 
-go-bindata -pkg bindata -o bindata/bindata.go cli-artifacts/...
+go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
+
+.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
 
 go build -o .\windows-binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
