0d2784899907cfcefcff96cd814fa2a326b32232
Alex Suraci <suraci.alex@gmail.com>
2016-02-20 09:41:00 -0800

    nuke nonfunctioning validation
    originally intended to validate alphanumeric + [_-] targets to catch URI
    usage, but the regexp wasn't anchored, and we hadn't finished the
    message yet, and I don't want to be at fault for someone's
    interesting-but-morally-valid target name.

--- rc/target_name.go
+++ rc/target_name.go
@@ -1,20 +1,8 @@
 package rc
 
-import (
-	"fmt"
-	"regexp"
-)
-
 type TargetName string
 
-var validTargetName = regexp.MustCompile(`[[:alnum:]\-_]+`)
-
 func (name *TargetName) UnmarshalFlag(value string) error {
-	if !validTargetName.MatchString(value) {
-		return fmt.Errorf("dfskjhdkgjhfdkjgh")
-	}
-
 	*name = TargetName(value)
-
 	return nil
 }
