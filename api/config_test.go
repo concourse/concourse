7642d03f6bc9bd4cd2481a58d97cc970be10ac7d
Anand Gaitonde <agaitonde@pivotal.io>
2016-02-29 15:43:22 -0800

    params should always have string value
    [#114162767]
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- api/config_test.go
+++ api/config_test.go
@@ -78,12 +78,6 @@ var _ = Describe("Config API", func() {
 							Passed:   []string{"job-1", "job-2"},
 							Params: atc.Params{
 								"some-param": "some-value",
-								"nested": map[string]interface{}{
-									"key": "value",
-									"nested": map[string]interface{}{
-										"key": "value",
-									},
-								},
 							},
 						},
 						{
@@ -99,12 +93,6 @@ var _ = Describe("Config API", func() {
 							Resource: "some-resource",
 							Params: atc.Params{
 								"some-param": "some-value",
-								"nested": map[string]interface{}{
-									"key": "value",
-									"nested": map[string]interface{}{
-										"key": "value",
-									},
-								},
 							},
 						},
 					},
--- resource_types.go
+++ resource_types.go
@@ -2,6 +2,6 @@ package atc
 
 type Source map[string]interface{}
 
-type Params map[string]interface{}
+type Params map[string]string
 
 type Version map[string]string
