a0605aad471fda3e2498498f56f6b7c117db3054
Johan Bloemberg <github@ijohan.nl>
2016-03-04 00:08:15 +0100

    Test client uses override url for API calls.

--- github/client_test.go
+++ github/client_test.go
@@ -149,6 +149,75 @@ var _ = Describe("Client", func() {
 			})
 		})
 	})
+
+	Describe("Github Enterprise", func() {
+		Context("when getting the current user succeeds", func() {
+			BeforeEach(func() {
+				githubServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/user"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, gogithub.User{
+							Login: gogithub.String("some-user"),
+						}),
+					),
+				)
+			})
+
+			It("returns the user's login", func() {
+				user, err := client.CurrentUser(proxiedClient, "https://github.example.com/api/v3/")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(user).To(Equal("some-user"))
+			})
+		})
+
+		Context("when listing teams succeeds", func() {
+			BeforeEach(func() {
+				githubServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/user/teams"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, []gogithub.Team{
+							{Name: gogithub.String("Team 1"), Slug: gogithub.String("team-1"),
+								Organization: &gogithub.Organization{Login: gogithub.String("org-1")}},
+							{Name: gogithub.String("Team 2"), Slug: gogithub.String("team-2"),
+								Organization: &gogithub.Organization{Login: gogithub.String("org-1")}},
+							{Name: gogithub.String("Team 3"), Slug: gogithub.String("team-3"),
+								Organization: &gogithub.Organization{Login: gogithub.String("org-2")}},
+						}),
+					),
+				)
+			})
+
+			It("returns the map of organization to team names", func() {
+				teams, err := client.Teams(proxiedClient, "https://github.example.com/api/v3/")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(teams).To(HaveLen(2))
+				Expect(teams["org-1"]).To(ConsistOf([]string{"Team 1", "Team 2"}))
+				Expect(teams["org-2"]).To(ConsistOf([]string{"Team 3"}))
+			})
+		})
+		Context("when listing organization succeeds", func() {
+			BeforeEach(func() {
+				githubServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/user/orgs"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, []gogithub.Organization{
+							{Login: gogithub.String("org-1")},
+							{Login: gogithub.String("org-2")},
+							{Login: gogithub.String("org-3")},
+						}),
+					),
+				)
+			})
+
+			It("returns the list of organization names", func() {
+				orgs, err := client.Organizations(proxiedClient, "https://github.example.com/api/v3/")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(orgs).To(Equal([]string{"org-1", "org-2", "org-3"}))
+			})
+		})
+
+	})
+
 })
 
 type proxiedTransport struct {
