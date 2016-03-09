c13f00d3d9a8248285ccd9e7966f47e26bcb0214
Alex Suraci <suraci.alex@gmail.com>
2016-03-08 19:07:35 -0800

    make ttl not-nullable; default to 0

--- db/migrations/89_add_ttl_to_containers.go
+++ db/migrations/89_add_ttl_to_containers.go
@@ -4,7 +4,7 @@ import "github.com/BurntSushi/migration"
 
 func AddTTLToContainers(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
-		ALTER TABLE containers ADD COLUMN ttl text;
+		ALTER TABLE containers ADD COLUMN ttl text NOT NULL DEFAULT 0;
 	`)
 	if err != nil {
 		return err
