95bab0a63e5b1fdd3cb6234ddc44da10b551dbaf
Maria Shaldibina <mshaldibina@pivotal.io>
2016-03-21 09:53:45 -0700

    fix rename pipeline integration tests
    Signed-off-by: Evan Short <eshort@pivotal.io>

--- integration/rename_pipeline_test.go
+++ integration/rename_pipeline_test.go
@@ -79,7 +79,7 @@ var _ = Describe("RenamePipeline", func() {
 
 				Eventually(sess).Should(gexec.Exit(1))
 				Expect(atcServer.ReceivedRequests()).To(HaveLen(4))
-				Expect(sess.Err).To(gbytes.Say("failed to find pipeline"))
+				Expect(sess.Err).To(gbytes.Say("pipeline 'some-pipeline' not found"))
 			})
 		})
 
@@ -96,7 +96,7 @@ var _ = Describe("RenamePipeline", func() {
 
 				Eventually(sess).Should(gexec.Exit(1))
 				Expect(atcServer.ReceivedRequests()).To(HaveLen(4))
-				Expect(sess.Err).To(gbytes.Say("client failed with error: "))
+				Expect(sess.Err).To(gbytes.Say("418 I'm a teapot"))
 			})
 		})
 	})
