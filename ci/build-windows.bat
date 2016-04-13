499465d3a2bc5539f97233d7b5b57447bd3b321e
Alex Suraci <suraci.alex@gmail.com>
2016-04-13 08:03:00 -0700

    use /version, not /number

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,7 +1,7 @@
 set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\Godeps_windows\_workspace
 set PATH=%CD%\gopath\bin;%PATH%
 
-set /p FinalVersion=<final-version\number
+set /p FinalVersion=<final-version\version
 
 go get github.com/jteeuwen/go-bindata
 
