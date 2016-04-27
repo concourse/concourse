b7b827472c2cbe9b3fb71d8728de8c319c60447e
Kris Hicks <khicks@pivotal.io>
2016-04-26 17:19:48 -0700

    add watch option to trigger-job
    [#118010767]
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- integration/trigger_job_test.go
+++ integration/trigger_job_test.go
@@ -1,13 +1,17 @@
 package integration_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/vito/go-sse/sse"
 
 	"github.com/concourse/atc"
+	"github.com/concourse/atc/event"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	"github.com/onsi/gomega/ghttp"
@@ -31,7 +35,7 @@ var _ = Describe("Fly CLI", func() {
 					atcServer.AppendHandlers(
 						ghttp.CombineHandlers(
 							ghttp.VerifyRequest("POST", path),
-							ghttp.RespondWithJSONEncoded(http.StatusOK, atc.Build{}),
+							ghttp.RespondWithJSONEncoded(http.StatusOK, atc.Build{ID: 57, Name: "42"}),
 						),
 					)
 				})
@@ -43,13 +47,85 @@ var _ = Describe("Fly CLI", func() {
 						sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
 						Expect(err).NotTo(HaveOccurred())
 
-						Eventually(sess).Should(gbytes.Say(`started 'awesome-pipeline/awesome-job'`))
+						Eventually(sess).Should(gbytes.Say(`started awesome-pipeline/awesome-job #42`))
 
 						<-sess.Exited
 						Expect(sess.ExitCode()).To(Equal(0))
 					}).To(Change(func() int {
 						return len(atcServer.ReceivedRequests())
 					}).By(2))
+				})
+
+				Context("when -w option is provided", func() {
+					var streaming chan struct{}
+					var events chan atc.Event
+
+					BeforeEach(func() {
+						streaming = make(chan struct{})
+						events = make(chan atc.Event)
+						atcServer.AppendHandlers(
+							ghttp.CombineHandlers(
+								ghttp.VerifyRequest("GET", "/api/v1/builds/57/events"),
+								func(w http.ResponseWriter, r *http.Request) {
+									flusher := w.(http.Flusher)
+
+									w.Header().Add("Content-Type", "text/event-stream; charset=utf-8")
+									w.Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
+									w.Header().Add("Connection", "keep-alive")
+
+									w.WriteHeader(http.StatusOK)
+
+									flusher.Flush()
+
+									close(streaming)
+
+									id := 0
+
+									for e := range events {
+										payload, err := json.Marshal(event.Message{Event: e})
+										Expect(err).NotTo(HaveOccurred())
+
+										event := sse.Event{
+											ID:   fmt.Sprintf("%d", id),
+											Name: "event",
+											Data: payload,
+										}
+
+										err = event.Write(w)
+										Expect(err).NotTo(HaveOccurred())
+
+										flusher.Flush()
+
+										id++
+									}
+
+									err := sse.Event{
+										Name: "end",
+									}.Write(w)
+									Expect(err).NotTo(HaveOccurred())
+								},
+							),
+						)
+					})
+
+					It("watches the build", func() {
+						flyCmd := exec.Command(flyPath, "-t", targetName, "trigger-job", "-j", "awesome-pipeline/awesome-job", "-w")
+
+						sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
+						Expect(err).NotTo(HaveOccurred())
+
+						Eventually(sess).Should(gbytes.Say(`started awesome-pipeline/awesome-job #42`))
+						Eventually(streaming).Should(BeClosed())
+
+						events <- event.Log{Payload: "sup"}
+
+						Eventually(sess.Out).Should(gbytes.Say("sup"))
+
+						close(events)
+
+						<-sess.Exited
+						Expect(sess.ExitCode()).To(Equal(0))
+					})
 				})
 			})
 
