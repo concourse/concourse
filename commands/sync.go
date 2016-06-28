b7514c8b7a0f294701652f6385667c3b2ea791da
Chris Dutra <cdutra@pivotal.io>
2016-06-28 16:23:19 -0700

    adds progress bar to fly sync
    [#119297789]
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- commands/sync.go
+++ commands/sync.go
@@ -3,6 +3,9 @@ package commands
 import (
 	"fmt"
 	"runtime"
+	"strconv"
+
+	"gopkg.in/cheggaaa/pb.v1"
 
 	"github.com/inconshreveable/go-update"
 
@@ -18,18 +21,25 @@ func (command *SyncCommand) Execute(args []string) error {
 		return err
 	}
 
-	body, err := client.GetCLIReader(runtime.GOARCH, runtime.GOOS)
+	body, headers, err := client.GetCLIReader(runtime.GOARCH, runtime.GOOS)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("downloading fly from %s... ", client.URL())
+	fmt.Printf("downloading fly from %s... \n", client.URL())
 
-	err = update.Apply(body, update.Options{})
+	filesSize, _ := strconv.ParseInt(headers.Get("Content-Length"), 10, 64)
+	progressBar := pb.New64(filesSize).SetUnits(pb.U_BYTES)
+	progressBar.SetWidth(100)
+	progressBar.Start()
+	r := body
+	reader := progressBar.NewProxyReader(r)
+
+	err = update.Apply(reader, update.Options{})
 	if err != nil {
 		displayhelpers.Failf("update failed: %s", err)
 	}
 
-	fmt.Println("update successful!")
+	progressBar.FinishPrint("update successful!")
 	return nil
 }
