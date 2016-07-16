5bbb8f05079ed254041158d208c5aba8eb68c758
Alex Suraci <suraci.alex@gmail.com>
2016-07-16 13:13:12 -0700

    Revert "use houdini.NewServer to fix vendor issue"
    This reverts commit 49e7a5674a853c64b5d98d12c664531524ff84fe.

--- cmd/concourse/houdini.go
+++ cmd/concourse/houdini.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/cloudfoundry-incubator/garden/server"
 	"github.com/concourse/atc"
 	"github.com/pivotal-golang/lager"
 	"github.com/tedsuo/ifrit"
@@ -25,7 +26,7 @@ func (cmd *WorkerCommand) houdiniRunner(logger lager.Logger, platform string) (a
 
 	backend := houdini.NewBackend(depotDir)
 
-	server := houdini.NewServer(
+	server := server.New(
 		"tcp",
 		cmd.bindAddr(),
 		0,
