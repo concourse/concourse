c4e45293f4a6594bc7e7b08d165fd21161852065
Alex Suraci <suraci.alex@gmail.com>
2016-08-09 12:59:01 -0700

    more windows flailing

--- ci/scripts/install-go-windows.ps1
+++ ci/scripts/install-go-windows.ps1
@@ -1,10 +1,8 @@
 Set-PSDebug -trace 2 -strict
 
-cd .\golang-windows
-
 Remove-Item -Recurse -Force $env:USERPROFILE\go
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-$zipfile = Get-Item "go*.zip"
+$zipfile = Get-Item ".\golang-windows\go*.zip"
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $env:USERPROFILE)
