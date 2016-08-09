cf6496fea9a005bb4500be875dd51c014048b89b
Alex Suraci <suraci.alex@gmail.com>
2016-08-09 13:09:12 -0700

    ugh

--- ci/scripts/install-go-windows.ps1
+++ ci/scripts/install-go-windows.ps1
@@ -6,12 +6,23 @@ trap {
 Set-PSDebug -trace 2 -strict
 
 echo what
+Set-PSDebug -trace 2 -strict
+
 echo the
+Set-PSDebug -trace 2 -strict
+
 echo deuce
+Set-PSDebug -trace 2 -strict
 
 Remove-Item -Recurse -Force $env:USERPROFILE\go
 
+Set-PSDebug -trace 2 -strict
+
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+Set-PSDebug -trace 2 -strict
+
 $zipfile = Get-Item ".\golang-windows\go*.zip"
+Set-PSDebug -trace 2 -strict
+
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $env:USERPROFILE)
