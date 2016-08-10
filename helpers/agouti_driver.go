7c8b15f937596a746adf065342ee8f9582f622a3
Josh Zarrabi <jzarrabi@pivotal.io>
2016-08-10 16:07:55 -0700

    use ChromeDriver for local development
    phantomJS is flaky
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- helpers/agouti_driver.go
+++ helpers/agouti_driver.go
@@ -1,19 +1,15 @@
 package helpers
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 
-	"github.com/onsi/ginkgo"
 	"github.com/sclevine/agouti"
 )
 
 func AgoutiDriver() *agouti.WebDriver {
-	if _, err := exec.LookPath("phantomjs"); err == nil && os.Getenv("FORCE_SELENIUM") != "true" {
-		fmt.Fprintln(ginkgo.GinkgoWriter, "WARNING: using phantomjs, which is flaky in CI, but is more convenient during development")
-		return agouti.PhantomJS()
-	} else {
+	if os.Getenv("FORCE_SELENIUM") == "true" {
 		return agouti.Selenium(agouti.Browser("firefox"))
+	} else {
+		return agouti.ChromeDriver()
 	}
 }
