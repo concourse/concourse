f48e20fbbccb80e4a70277ea8f784b575d5d72f4
Josh Zarrabi <jzarrabi@pivotal.io>
2016-08-12 17:24:21 -0700

    rewrite pipelines nav sidebar in elm
    [#126871129]

--- web/assets/javascript/concourse/concourse.pipelinesNav.js
+++ web/assets/javascript/concourse/concourse.pipelinesNav.js
@@ -1,9 +1,7 @@
-(function(sortable){
+(function(){
   concourse.PipelinesNav = function ($el) {
     this.$el = $($el);
     this.$toggle = $el.find($('.js-sidebar-toggle'));
-    this.$list = $el.find($('.js-pipelines-list'));
-    this.pipelinesEndpoint = '/api/v1/pipelines';
   };
 
   concourse.PipelinesNav.prototype.bindEvents = function () {
@@ -11,83 +9,12 @@
     _this.$toggle.on("click", function() {
         _this.toggle();
     });
-
-    sortable.create(_this.$list[0], {
-      "onUpdate": function() {
-        _this.onSort();
-      }
-    });
-
-    _this.loadPipelines();
-  };
-
-  concourse.PipelinesNav.prototype.onSort = function() {
-    var _this = this;
-
-    var pipelineNames = _this.$list.find('a')
-      .toArray()
-      .map(function(e) {
-        return e.innerHTML;
-      });
-
-    var teamName = $(_this.$list[0]).find('.js-pauseUnpause').parent().data('teamName');
-
-    $.ajax({
-      method: 'PUT',
-      url: '/api/v1/teams/' + teamName + '/pipelines/ordering',
-      contentType: "application/json",
-      data: JSON.stringify(pipelineNames)
-    });
   };
 
   concourse.PipelinesNav.prototype.toggle = function() {
     $('.js-sidebar').toggleClass('visible');
   };
-
-  concourse.PipelinesNav.prototype.loadPipelines = function() {
-    var _this = this;
-    $.ajax({
-      method: 'GET',
-      url: _this.pipelinesEndpoint
-    }).done(function(resp, jqxhr){
-      $(resp).each( function(index, pipeline){
-        var $pipelineListItem = $("<li>");
-
-        var ed = pipeline.paused ? 'enabled' : 'disabled';
-        var icon = pipeline.paused ? 'play' : 'pause';
-
-        $pipelineListItem.html('<span class="btn-pause fl ' + ed + ' js-pauseUnpause"><i class="fa fa-fw fa-' + icon +  '"></i></span><a href="' + pipeline.url + '">' + pipeline.name + '</a>');
-        $pipelineListItem.data('endpoint', '/api/v1/teams/' +  pipeline.team_name + '/pipelines/' + pipeline.name);
-        $pipelineListItem.data('pipelineName', pipeline.name);
-        $pipelineListItem.data('teamName', pipeline.team_name);
-        $pipelineListItem.addClass('clearfix');
-
-
-        _this.$list.append($pipelineListItem);
-
-        _this.newPauseUnpause($pipelineListItem);
-
-        if(concourse.pipelineName === pipeline.name && pipeline.paused) {
-          _this.$el.find('.js-top-bar').addClass('paused');
-        }
-      });
-    });
-  };
-
-  concourse.PipelinesNav.prototype.newPauseUnpause = function($el) {
-    var _this = this;
-    var pauseUnpause = new concourse.PauseUnpause($el, function() {
-      if($el.data('pipelineName') === concourse.pipelineName) {
-        _this.$el.find('.js-top-bar').addClass('paused');
-      }
-    }, function() {
-      if($el.data('pipelineName') === concourse.pipelineName) {
-        _this.$el.find('.js-top-bar').removeClass('paused');
-      }
-    });
-    pauseUnpause.bindEvents();
-  };
-})(Sortable);
+})();
 
 $(function () {
   if ($('.js-with-pipeline').length) {
---
+++ web/elm/src/PipelinesNav.elm
@@ -0,0 +1,517 @@
+module PipelinesNav exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes exposing (class, href, id, disabled, attribute, style)
+import Html.Events as Events
+import Http
+import Json.Decode
+import List
+import Mouse exposing (Position)
+import Task
+
+import Concourse.Pipeline exposing (Pipeline, fetchPipelines)
+
+type alias Model =
+  { teams : Maybe (List (String, List UIPipeline))
+  , dragInfo : Maybe DragInfo
+  }
+
+purposefulThreshold : Int
+purposefulThreshold = 10 -- in pixels
+
+type alias DragInfo =
+  { startPos : Position
+  , pos : Position
+  , teamName : String
+  , pipelineName : String
+  , purposeful : Bool
+  , hover : Maybe (ListHover String)
+  }
+
+type alias UIPipeline =
+  { pipeline : Pipeline
+  , pausedChanging : Bool
+  , pauseErrored : Bool
+  }
+
+type ListHover a
+  = BeforeAll
+  | AfterElement a
+
+type Action
+  = Noop
+  | PausePipeline String String
+  | UnpausePipeline String String
+  | PipelinesFetched (Result Http.Error (List Pipeline))
+  | PipelinePaused String String (Result Http.Error ())
+  | PipelineUnpaused String String (Result Http.Error ())
+  | StartDragging String String Position
+  | StopDragging Position
+  | Drag Position
+  | Hover String (ListHover String)
+  | Unhover String (ListHover String)
+  | PipelinesReordered (Result Http.Error ())
+
+init : (Model, Cmd Action)
+init =
+  ( { teams = Nothing
+    , dragInfo = Nothing
+    }
+  , fetchPipelines
+  )
+
+update : Action -> Model -> (Model, Cmd Action)
+update action model =
+  case action of
+    Noop -> (model, Cmd.none)
+    PausePipeline teamName pipelineName ->
+      ( mapModelPipelines updatePausedChanging teamName pipelineName model
+      , pausePipeline teamName pipelineName
+      )
+    UnpausePipeline teamName pipelineName ->
+      ( mapModelPipelines updatePausedChanging teamName pipelineName model
+      , unpausePipeline teamName pipelineName
+      )
+    PipelinesFetched (Ok pipelines)->
+      ({ model | teams = Just <| groupPipelinesByTeam pipelines }, Cmd.none)
+    PipelinesFetched (Err err) ->
+      Debug.log ("failed to fetch pipelines: " ++ toString err) (model, Cmd.none)
+    PipelinePaused teamName pipelineName (Ok ()) ->
+      ( mapModelPipelines (setPaused True) teamName pipelineName model
+      , Cmd.none
+      )
+    PipelinePaused teamName pipelineName (Err err) ->
+      Debug.log
+        ("failed to pause pipeline: " ++ toString err)
+        ( mapModelPipelines updatePauseErrored teamName pipelineName model
+        , Cmd.none
+        )
+    PipelineUnpaused teamName pipelineName (Ok ()) ->
+      ( mapModelPipelines (setPaused False) teamName pipelineName model
+      , Cmd.none
+      )
+    PipelineUnpaused teamName pipelineName (Err err) ->
+      Debug.log
+        ("failed to unpause pipeline: " ++ toString err)
+        ( mapModelPipelines updatePauseErrored teamName pipelineName model
+        , Cmd.none
+        )
+    Drag pos ->
+      case model.dragInfo of
+        Just dragInfo ->
+          ( { model
+            | dragInfo =
+                Just <|
+                  let newDragInfo = { dragInfo | pos = pos } in
+                    { newDragInfo
+                    | purposeful =
+                        dragInfo.purposeful ||
+                          abs (dragX newDragInfo) >= purposefulThreshold ||
+                          abs (dragY newDragInfo) >= purposefulThreshold
+                    }
+            }
+          , Cmd.none
+          )
+        Nothing -> (model, Cmd.none)
+    StartDragging teamName pipelineName pos ->
+      always
+        ( { model
+          | dragInfo =
+              Just
+                { startPos = pos
+                , pos = pos
+                , teamName = teamName
+                , pipelineName = pipelineName
+                , purposeful = False
+                , hover = Nothing
+                }
+          }
+        , Cmd.none
+        ) <|
+        Debug.log "start" pos
+    StopDragging pos ->
+      case model.dragInfo of
+        Just dragInfo ->
+          case dragInfo.hover of
+            Just hover ->
+              case getPipelinesByTeamName dragInfo.teamName model.teams of
+                Just pipelines ->
+                  case popPipelineByName dragInfo.pipelineName pipelines of
+                    (tmpPipelines, Just draggedPipeline) ->
+                      let
+                        updatedPipelines =
+                          insertPipelineAt hover draggedPipeline tmpPipelines
+                      in
+                        ( { model
+                          | dragInfo = Nothing
+                          , teams =
+                              setPipelinesByTeamName
+                                dragInfo.teamName
+                                updatedPipelines
+                                model.teams
+                          }
+                        , orderPipelines dragInfo.teamName <|
+                          List.map (.pipeline >> .name) updatedPipelines
+                        )
+                    (_, Nothing) -> ({ model | dragInfo = Nothing }, Cmd.none)
+                Nothing -> ({ model | dragInfo = Nothing }, Cmd.none)
+            Nothing -> ({ model | dragInfo = Nothing }, Cmd.none)
+        Nothing -> (model, Cmd.none)
+    Hover teamName listHover ->
+      case model.dragInfo of
+        Just dragInfo ->
+          if dragInfo.teamName == teamName &&
+            listHover /= AfterElement dragInfo.pipelineName &&
+            (getPrevHover model) /= Just listHover then
+            ( { model
+              | dragInfo = Just { dragInfo | hover = Just listHover }
+              }
+            , Cmd.none
+            )
+          else (model, Cmd.none)
+        Nothing -> (model, Cmd.none)
+    Unhover teamName listHover ->
+      case model.dragInfo of
+        Just dragInfo ->
+          if dragInfo.hover == Just listHover then
+            ( { model
+              | dragInfo = Just { dragInfo | hover = Nothing }
+              }
+            , Cmd.none
+            )
+          else (model, Cmd.none)
+        Nothing -> (model, Cmd.none)
+    PipelinesReordered (Ok ()) ->
+      (model, Cmd.none)
+    PipelinesReordered (Err err) ->
+      Debug.log ("failed to reorder pipelines: " ++ toString err) (model, Cmd.none)
+
+getPrevHover : Model -> Maybe (ListHover String)
+getPrevHover model =
+  case model.dragInfo of
+    Just dragInfo ->
+      Maybe.andThen
+        (getPipelinesByTeamName dragInfo.teamName model.teams) <|
+        getPrevHoverForPipelines dragInfo.pipelineName
+    Nothing -> Nothing
+
+getPrevHoverForPipelines : String -> List UIPipeline -> Maybe (ListHover String)
+getPrevHoverForPipelines pipelineName pipelines =
+  case pipelines of
+    [] ->
+      Nothing
+    [ first ] ->
+      Just BeforeAll
+    first :: second :: rest ->
+      if second.pipeline.name == pipelineName then
+        Just <| AfterElement first.pipeline.name
+      else
+        getPrevHoverForPipelines pipelineName <| second :: rest
+
+getPipelinesByTeamName :
+  String -> Maybe (List (String, List UIPipeline)) -> Maybe (List UIPipeline)
+getPipelinesByTeamName teamName teams =
+  Maybe.map
+    snd <|
+    List.head <|
+      List.filter
+        (\team -> fst team == teamName) <|
+        Maybe.withDefault [] teams
+
+setPipelinesByTeamName :
+  String -> List UIPipeline -> Maybe (List (String, List UIPipeline)) ->
+    Maybe (List (String, List UIPipeline))
+setPipelinesByTeamName teamName newPipelines teams =
+  Maybe.map (setPipelinesByTeamNameHelper teamName newPipelines) teams
+
+setPipelinesByTeamNameHelper :
+  String -> List UIPipeline -> List (String, List UIPipeline) ->
+    List (String, List UIPipeline)
+setPipelinesByTeamNameHelper teamName newPipelines teams =
+  case teams of
+    [] -> []
+    team :: rest ->
+      if fst team == teamName then (fst team, newPipelines) :: rest
+      else team :: setPipelinesByTeamNameHelper teamName newPipelines rest
+
+popPipelineByName : String -> List UIPipeline -> (List UIPipeline, Maybe UIPipeline)
+popPipelineByName name pipelines =
+  case pipelines of
+    [] -> ([], Nothing)
+    first :: rest ->
+      if first.pipeline.name == name then (rest, Just first)
+      else
+        let (updatedRest, poppedPipeline) = popPipelineByName name rest in
+          (first :: updatedRest, poppedPipeline)
+
+insertPipelineAt : ListHover String -> UIPipeline -> List UIPipeline -> List UIPipeline
+insertPipelineAt location pipeline pipelines =
+  case location of
+    BeforeAll -> pipeline :: pipelines
+    AfterElement name ->
+      case pipelines of
+        [] -> []
+        first :: rest ->
+          if first.pipeline.name == name then first :: pipeline :: rest
+          else first :: (insertPipelineAt location pipeline rest)
+
+dragX : DragInfo -> Int
+dragX dragInfo = dragInfo.pos.x - dragInfo.startPos.x
+
+dragY : DragInfo -> Int
+dragY dragInfo = dragInfo.pos.y - dragInfo.startPos.y
+
+isDragging : Model -> Bool
+isDragging model = model.dragInfo /= Nothing
+
+isPurposeful : Maybe DragInfo -> Bool
+isPurposeful = Maybe.withDefault False << Maybe.map .purposeful
+
+setPaused : Bool -> UIPipeline -> UIPipeline
+setPaused paused uip =
+  -- arbitrary elm restriction: record update syntax only works on local variables
+  let pipeline = uip.pipeline in
+    { uip
+    | pipeline = { pipeline | paused = paused }
+    , pausedChanging = False
+    , pauseErrored = False
+    }
+
+updatePausedChanging : UIPipeline -> UIPipeline
+updatePausedChanging uip = {uip | pausedChanging = True}
+
+updatePauseErrored : UIPipeline -> UIPipeline
+updatePauseErrored uip = {uip | pauseErrored = True, pausedChanging = False}
+
+mapModelPipelines : (UIPipeline -> UIPipeline) -> String -> String -> Model -> Model
+mapModelPipelines f teamName pipelineName model =
+  let maybePipelines = getPipelinesByTeamName teamName model.teams in
+    case maybePipelines of
+      Nothing -> model
+      Just pipelines ->
+        let newPipelines = List.map (mapPipeline f pipelineName) pipelines in
+          { model
+          | teams = setPipelinesByTeamName teamName newPipelines model.teams
+          }
+
+mapPipeline : (UIPipeline -> UIPipeline) -> String -> UIPipeline -> UIPipeline
+mapPipeline f pipelineName uip =
+  if uip.pipeline.name == pipelineName then f uip
+  else uip
+
+view : Model -> Html Action
+view model =
+  case model.teams of
+    Nothing -> Html.text "loading"
+    Just teams ->
+      Html.ul [] <| List.map (viewTeam model.dragInfo) teams
+
+viewTeam : Maybe DragInfo -> (String, List UIPipeline) -> Html Action
+viewTeam maybeDragInfo (teamName, pipelines) =
+  Html.li [class "team"]
+    [ Html.text <| "team "
+      , Html.span
+        [ class "bright-text" ]
+        [ Html.text teamName ]
+
+    , Html.ul [] <|
+      let firstElem = List.head pipelines
+        in
+          case firstElem of
+            Nothing -> []
+            Just firstElem ->
+              let
+                firstElemView = viewFirstPipeline maybeDragInfo firstElem
+              in let
+                restView =
+                  List.map
+                    (viewPipeline maybeDragInfo) <|
+                      Maybe.withDefault [] <| List.tail pipelines
+              in
+                firstElemView :: restView
+    ]
+
+viewFirstPipeline : Maybe DragInfo -> UIPipeline -> Html Action
+viewFirstPipeline maybeDragInfo uip =
+  Html.li
+    ( case maybeDragInfo of
+        Just dragInfo ->
+          if dragInfo.teamName == uip.pipeline.teamName then
+            case dragInfo.hover of
+              Just hover ->
+                if hover == BeforeAll then
+                  [ class "space-before" ]
+                else if hover == AfterElement uip.pipeline.name then
+                  [ class "space-after" ]
+                else
+                  []
+              Nothing -> []
+          else []
+        Nothing -> []
+    ) <|
+    ( if isPurposeful maybeDragInfo then
+        [ viewFirstDropArea uip.pipeline.teamName
+        , viewDropArea uip.pipeline.teamName uip.pipeline.name
+        ]
+      else []
+    ) ++
+      [ viewDraggable maybeDragInfo uip ]
+
+viewPipeline : Maybe DragInfo -> UIPipeline -> Html Action
+viewPipeline maybeDragInfo uip =
+  Html.li
+    ( case maybeDragInfo of
+        Just dragInfo ->
+          if dragInfo.teamName == uip.pipeline.teamName then
+            case dragInfo.hover of
+              Just hover ->
+                if hover == AfterElement uip.pipeline.name then
+                  [ class "space-after" ]
+                else
+                  []
+              Nothing -> []
+          else []
+        Nothing -> []
+    )<|
+    ( if isPurposeful maybeDragInfo then
+        [ viewDropArea uip.pipeline.teamName uip.pipeline.name ]
+      else []
+    ) ++
+      [ viewDraggable maybeDragInfo uip ]
+
+viewDraggable : Maybe DragInfo -> UIPipeline -> Html Action
+viewDraggable maybeDragInfo uip =
+  Html.div
+    ( let
+        dragging =
+          case maybeDragInfo of
+            Just dragInfo ->
+              dragInfo.teamName == uip.pipeline.teamName &&
+                dragInfo.pipelineName == uip.pipeline.name
+            Nothing -> False
+      in
+        [ class <|
+            if dragging then
+              if isPurposeful maybeDragInfo then "draggable dragging purposeful"
+              else "draggable dragging"
+            else "draggable"
+        , Events.onWithOptions
+            "mousedown"
+            { stopPropagation = False, preventDefault = True } <|
+            Json.Decode.map (StartDragging uip.pipeline.teamName uip.pipeline.name) Mouse.position
+        ] ++
+          case (maybeDragInfo, dragging) of
+            (Just dragInfo, True) -> [ dragStyle dragInfo ]
+            _ -> []
+    )
+    [ Html.div []
+        [ viewPauseButton uip
+        , Html.a
+            ( [ href uip.pipeline.url ] ++
+              if isPurposeful maybeDragInfo then
+                [ ignoreClicks ]
+              else
+                []
+            )
+            [ Html.text uip.pipeline.name ]
+        ]
+    ]
+
+dragStyle : DragInfo -> Html.Attribute action
+dragStyle dragInfo =
+  style
+    [ ("left", toString (dragX dragInfo) ++ "px")
+    , ("top", toString (dragY dragInfo) ++ "px")
+    ]
+
+viewFirstDropArea : String -> Html Action
+viewFirstDropArea teamName =
+  Html.div
+    [ class "drop-area first"
+    , Events.onMouseEnter <| Hover teamName BeforeAll
+    , Events.onMouseLeave <| Unhover teamName BeforeAll
+    ]
+    []
+
+viewDropArea : String -> String -> Html Action
+viewDropArea teamName pipelineName =
+  Html.div
+    [ class "drop-area"
+    , Events.onMouseEnter <| Hover teamName <| AfterElement pipelineName
+    , Events.onMouseLeave <| Unhover teamName <| AfterElement pipelineName
+    ]
+    []
+
+ignoreClicks : Html.Attribute Action
+ignoreClicks =
+  Events.onWithOptions
+    "click"
+    { stopPropagation = False, preventDefault = True } <|
+    Json.Decode.succeed Noop
+
+viewPauseButton : UIPipeline -> Html Action
+viewPauseButton uip =
+  if uip.pipeline.paused then
+    Html.span
+      [ Events.onClick <| UnpausePipeline uip.pipeline.teamName uip.pipeline.name
+      , class <|
+          if uip.pauseErrored then "btn-pause errored"
+          else "btn-pause enabled"
+      ] <|
+      if uip.pausedChanging then
+        [ Html.i [class "fa fa-fw fa-circle-o-notch fa-spin"] [] ]
+      else
+        [ Html.i [class "fa fa-fw fa-play"] [] ]
+  else
+    Html.span
+      [ Events.onClick <| PausePipeline uip.pipeline.teamName uip.pipeline.name
+      , class <|
+          if uip.pauseErrored then "btn-pause errored"
+          else "btn-pause disabled"
+      ] <|
+      if uip.pausedChanging then
+        [ Html.i [class "fa fa-fw fa-circle-o-notch fa-spin"] [] ]
+      else
+        [ Html.i [class "fa fa-fw fa-pause"] [] ]
+
+fetchPipelines : Cmd Action
+fetchPipelines =
+  Cmd.map PipelinesFetched <|
+    Task.perform Err Ok Concourse.Pipeline.fetchPipelines
+
+unpausePipeline : String -> String -> Cmd Action
+unpausePipeline teamName pipelineName =
+  Cmd.map (PipelineUnpaused teamName pipelineName) <|
+    Task.perform Err Ok <| Concourse.Pipeline.unpause teamName pipelineName
+
+pausePipeline : String -> String -> Cmd Action
+pausePipeline teamName pipelineName =
+  Cmd.map (PipelinePaused teamName pipelineName) <|
+    Task.perform Err Ok <| Concourse.Pipeline.pause teamName pipelineName
+
+orderPipelines : String -> List String -> Cmd Action
+orderPipelines teamName pipelineNames =
+  Cmd.map PipelinesReordered <|
+    Task.perform Err Ok <| Concourse.Pipeline.order teamName pipelineNames
+
+groupPipelinesByTeam : List Pipeline -> List (String, List UIPipeline)
+groupPipelinesByTeam pipelines =
+  let
+    firstPipeline = List.head pipelines
+  in
+    case firstPipeline of
+      Nothing -> []
+      Just firstPipeline ->
+        let
+          (teamGroup, rest) = List.partition (\p -> p.teamName == firstPipeline.teamName) pipelines
+        in let
+          team = List.map toUIPipeline teamGroup
+        in
+          (firstPipeline.teamName, team) :: (groupPipelinesByTeam rest)
+
+toUIPipeline : Pipeline -> UIPipeline
+toUIPipeline pipeline =
+  { pipeline = pipeline
+  , pausedChanging = False
+  , pauseErrored = False
+  }
