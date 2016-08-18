06e406320ef4d036e3773c5f265eb5e5699dfb75
Chris Dutra <cdutra@pivotal.io>
2016-08-18 10:30:34 -0700

    add logout endpoint
    [#128520085]
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

---
+++ logout_handler_test.go
@@ -0,0 +1,71 @@
+package auth_test
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"net/http"
+	"net/http/httptest"
+
+	"code.cloudfoundry.org/lager/lagertest"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/concourse/atc/auth"
+	"github.com/concourse/atc/auth/authfakes"
+	"github.com/concourse/atc/db/dbfakes"
+)
+
+var _ = Describe("LogOutHandler", func() {
+	Describe("GET /auth/logout", func() {
+		var (
+			fakeProviderFactory *authfakes.FakeProviderFactory
+			signingKey          *rsa.PrivateKey
+			server              *httptest.Server
+			client              *http.Client
+			request             *http.Request
+			response            *http.Response
+			err                 error
+		)
+
+		BeforeEach(func() {
+			fakeProviderFactory = new(authfakes.FakeProviderFactory)
+			fakeTeamDBFactory := new(dbfakes.FakeTeamDBFactory)
+			signingKey, err = rsa.GenerateKey(rand.Reader, 1024)
+			Expect(err).ToNot(HaveOccurred())
+
+			handler, err := auth.NewOAuthHandler(
+				lagertest.NewTestLogger("test"),
+				fakeProviderFactory,
+				fakeTeamDBFactory,
+				signingKey,
+			)
+			Expect(err).ToNot(HaveOccurred())
+
+			mux := http.NewServeMux()
+			mux.Handle("/auth/", handler)
+
+			server = httptest.NewServer(mux)
+
+			client = &http.Client{
+				Transport: &http.Transport{},
+			}
+
+			request, err = http.NewRequest("GET", server.URL+"/auth/logout", nil)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		JustBeforeEach(func() {
+			response, err = client.Do(request)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("deletes ATC-Authorization cookie", func() {
+			cookies := response.Cookies()
+			Expect(len(cookies)).To(Equal(1))
+
+			deletedCookie := cookies[0]
+			Expect(deletedCookie.Name).To(Equal(auth.CookieName))
+			Expect(deletedCookie.MaxAge).To(Equal(-1))
+		})
+	})
+})
--- oauth_routes.go
+++ oauth_routes.go
@@ -5,9 +5,11 @@ import "github.com/tedsuo/rata"
 const (
 	OAuthBegin    = "OAuthBegin"
 	OAuthCallback = "OAuthCallback"
+	LogOut        = "LogOut"
 )
 
 var OAuthRoutes = rata.Routes{
+	{Path: "/auth/logout", Method: "GET", Name: LogOut},
 	{Path: "/auth/:provider", Method: "GET", Name: OAuthBegin},
 	{Path: "/auth/:provider/callback", Method: "GET", Name: OAuthCallback},
 }
