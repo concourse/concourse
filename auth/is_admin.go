9c68d148ba7ce660b7f93db61d2f516a251d9df4
Alex Suraci <suraci.alex@gmail.com>
2016-08-26 09:19:36 -0700

    kill debug logging

--- auth/is_admin.go
+++ auth/is_admin.go
@@ -1,12 +1,8 @@
 package auth
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
 func IsAdmin(r *http.Request) bool {
 	isAdmin, present := r.Context().Value(isAdminKey).(bool)
-	fmt.Printf("isAdmin %t and preset %t", isAdmin, present)
 	return present && isAdmin
 }
