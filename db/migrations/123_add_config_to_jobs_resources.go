a3fe5c1385d197a27cf1a0920e6ac89af2336127
Clara Fu <cfu@pivotal.io>
2016-09-02 14:33:47 -0700

    fix migration to populate old records with config
    [#127510245]
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- db/migrations/123_add_config_to_jobs_resources.go
+++ db/migrations/123_add_config_to_jobs_resources.go
@@ -4,15 +4,14 @@ import (
 	"encoding/json"
 
 	"github.com/BurntSushi/migration"
-
 	"github.com/concourse/atc"
 )
 
 func AddConfigToJobsResources(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
-				ALTER TABLE resources
-					ADD COLUMN config text NOT NULL,
-					ADD COLUMN active bool NOT NULL DEFAULT false;
+		ALTER TABLE resources
+		ADD COLUMN config json NOT NULL DEFAULT '{}',
+		ADD COLUMN active bool NOT NULL DEFAULT false;
  	`)
 	if err != nil {
 		return err
@@ -20,7 +19,7 @@ func AddConfigToJobsResources(tx migration.LimitedTx) error {
 
 	_, err = tx.Exec(`
 		ALTER TABLE resource_types
-		ADD COLUMN config text NOT NULL,
+		ADD COLUMN config json NOT NULL DEFAULT '{}',
 		ADD COLUMN active bool NOT NULL DEFAULT false;
 	`)
 	if err != nil {
@@ -29,7 +28,7 @@ func AddConfigToJobsResources(tx migration.LimitedTx) error {
 
 	_, err = tx.Exec(`
 		ALTER TABLE jobs
-		ADD COLUMN config text NOT NULL,
+		ADD COLUMN config json NOT NULL DEFAULT '{}',
 		ADD COLUMN active bool NOT NULL DEFAULT false;
 	`)
 	if err != nil {
@@ -37,14 +36,16 @@ func AddConfigToJobsResources(tx migration.LimitedTx) error {
 	}
 
 	rows, err := tx.Query(`
-          SELECT id, config
-        	FROM pipelines
-        `)
+    SELECT id, config
+  	FROM pipelines
+  `)
 	if err != nil {
 		return err
 	}
 
 	defer rows.Close()
+
+	pipelineConfigs := map[int]atc.Config{}
 
 	for rows.Next() {
 		var pipelineID int
@@ -60,6 +61,10 @@ func AddConfigToJobsResources(tx migration.LimitedTx) error {
 			return err
 		}
 
+		pipelineConfigs[pipelineID] = pipelineConfig
+	}
+
+	for pipelineID, pipelineConfig := range pipelineConfigs {
 		for _, jobConfig := range pipelineConfig.Jobs {
 			jobConfigPayload, err := json.Marshal(jobConfig)
 			if err != nil {
@@ -70,43 +75,64 @@ func AddConfigToJobsResources(tx migration.LimitedTx) error {
 				UPDATE jobs
 				SET config = $1, active = true
 				WHERE name = $2 AND pipeline_id = $3
-			  `, jobConfigPayload, jobConfig.Name, pipelineID)
+			`, jobConfigPayload, jobConfig.Name, pipelineID)
 			if err != nil {
 				return err
+			}
+
+			for _, resourceConfig := range pipelineConfig.Resources {
+				resourceConfigPayload, err := json.Marshal(resourceConfig)
+				if err != nil {
+					return err
+				}
+
+				_, err = tx.Exec(`
+					UPDATE resources
+					SET config = $1, active = true
+					WHERE name = $2 AND pipeline_id = $3
+			      `, resourceConfigPayload, resourceConfig.Name, pipelineID)
+				if err != nil {
+					return err
+				}
+			}
+
+			for _, resourceTypeConfig := range pipelineConfig.ResourceTypes {
+				resourceTypeConfigPayload, err := json.Marshal(resourceTypeConfig)
+				if err != nil {
+					return err
+				}
+
+				_, err = tx.Exec(`
+					UPDATE resource_types
+					SET config = $1, active = true
+					WHERE name = $2 AND pipeline_id = $3
+				 `, resourceTypeConfigPayload, resourceTypeConfig.Name, pipelineID)
+				if err != nil {
+					return err
+				}
 			}
 		}
+	}
 
-		for _, resourceConfig := range pipelineConfig.Resources {
-			resourceConfigPayload, err := json.Marshal(resourceConfig)
-			if err != nil {
-				return err
-			}
+	_, err = tx.Exec(`
+		ALTER TABLE resources ALTER COLUMN config DROP DEFAULT;
+	`)
+	if err != nil {
+		return err
+	}
 
-			_, err = tx.Exec(`
-				UPDATE resources
-				SET config = $1, active = true
-				WHERE name = $2 AND pipeline_id = $3
-		      `, resourceConfigPayload, resourceConfig.Name, pipelineID)
-			if err != nil {
-				return err
-			}
-		}
+	_, err = tx.Exec(`
+		ALTER TABLE resource_types ALTER COLUMN config DROP DEFAULT;
+	`)
+	if err != nil {
+		return err
+	}
 
-		for _, resourceTypeConfig := range pipelineConfig.ResourceTypes {
-			resourceTypeConfigPayload, err := json.Marshal(resourceTypeConfig)
-			if err != nil {
-				return err
-			}
-
-			_, err = tx.Exec(`
-				UPDATE resource_types
-				SET config = $1, active = true
-				WHERE name = $2 AND pipeline_id = $3
-			 `, resourceTypeConfigPayload, resourceTypeConfig.Name, pipelineID)
-			if err != nil {
-				return err
-			}
-		}
+	_, err = tx.Exec(`
+		ALTER TABLE jobs ALTER COLUMN config DROP DEFAULT;
+	`)
+	if err != nil {
+		return err
 	}
 
 	return nil
