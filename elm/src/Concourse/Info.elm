14ad36d992ba45f4edc749229c5fc364b978dec9
Maria Shaldibina <mshaldibina@pivotal.io>
2016-09-12 16:19:16 -0700

    get version header case-insensetively
    [#128881443]
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- elm/src/Concourse/Info.elm
+++ elm/src/Concourse/Info.elm
@@ -2,9 +2,10 @@ module Concourse.Info exposing
   ( fetchVersion
   )
 
-import Dict
+import Dict exposing (Dict)
 import Http
 import Task exposing (Task)
+import String
 
 fetchVersion : Task Http.Error String
 fetchVersion =
@@ -23,7 +24,7 @@ fetchVersion =
 handleResponse : Http.Response -> Task Http.Error String
 handleResponse response =
   if 200 <= response.status && response.status < 300 then
-    case Dict.get "X-Concourse-Version" response.headers of
+    case Dict.get "x-concourse-version" <| keysToLower response.headers of
       Nothing ->
         Task.fail (Http.UnexpectedPayload "response headers should have 'X-Concourse-Version' field")
 
@@ -37,3 +38,9 @@ promoteHttpError rawError =
   case rawError of
     Http.RawTimeout -> Http.Timeout
     Http.RawNetworkError -> Http.NetworkError
+
+keysToLower : Dict String a -> Dict String a
+keysToLower = Dict.fromList << List.map fstToLower << Dict.toList
+
+fstToLower : (String, a) -> (String, a)
+fstToLower (x, y) = (String.toLower x, y)
