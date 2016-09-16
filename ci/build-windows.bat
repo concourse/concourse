02f6f7f2a52595c2b3404dfd979f05632a30aa2b
Chris Dutra <cdutra@pivotal.io>
2016-09-16 15:48:53 -0700

    use fly-rc instead fly-binaries
    [#129925039]
    
    Signed-off-by: James Quintana <jquintana@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -9,6 +9,6 @@ go get github.com/jteeuwen/go-bindata
 
 go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
-.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
+.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go fly-rc/...
 
 go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
