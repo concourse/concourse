b5a7e5f751dc8e068b5b0dd5d8e7c0778fea4608
Alex Suraci <suraci.alex@gmail.com>
2016-09-22 16:13:18 -0700

    rework css for fixed headers and autoscrolling
    [#130488387]

--- elm/src/Native/Scroll.js
+++ elm/src/Native/Scroll.js
@@ -7,6 +7,13 @@ var _concourse$atc$Native_Scroll = function() {
     });
   }
 
+  function toWindowBottom() {
+    return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+      window.scrollTo(0, document.body.scrollHeight);
+      callback(_elm_lang$core$Native_Scheduler.succeed(_elm_lang$core$Native_Utils.Tuple0));
+    });
+  }
+
   function scrollElement(id, delta) {
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
       document.getElementById(id).scrollLeft -= delta;
@@ -23,6 +30,7 @@ var _concourse$atc$Native_Scroll = function() {
 
   return {
     toBottom: toBottom,
+    toWindowBottom: toWindowBottom,
     scrollElement: F2(scrollElement),
     scrollIntoView: scrollIntoView
   };
