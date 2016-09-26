1f08eebd25333c0e715ac7cb1fa7488efda22b68
James Quintana <q@world.oberlin.edu>
2016-09-26 16:52:05 -0700

    WIP build page, job page

--- elm/src/SubPage.elm
+++ elm/src/SubPage.elm
@@ -5,6 +5,11 @@ import Json.Encode
 import Html exposing (Html)
 import Html.App
 import Login
+import String
+
+import Autoscroll
+import Job
+import Build
 import Routes
 import Pipeline
 import TeamSelection
@@ -13,14 +18,19 @@ import TeamSelection
 
 port renderPipeline : (Json.Encode.Value, Json.Encode.Value) -> Cmd msg
 port renderFinished : (Bool -> msg) -> Sub msg
+-- port setTitle : String -> Cmd msg
 
 type Model
-  = LoginModel Login.Model
+  = BuildModel (Autoscroll.Model Build.Model)
+  | JobModel Job.Model
+  | LoginModel Login.Model
   | PipelineModel Pipeline.Model
   | SelectTeamModel TeamSelection.Model
 
 type Msg
-  = LoginMsg Login.Msg
+  = BuildMsg (Autoscroll.Msg Build.Msg)
+  | JobMsg Job.Msg
+  | LoginMsg Login.Msg
   | PipelineMsg Pipeline.Msg
   | SelectTeamMsg TeamSelection.Msg
 
@@ -31,6 +41,47 @@ superDupleWrap (modelFunc, msgFunc) (model, msg) =
 init : Routes.ConcourseRoute -> (Model, Cmd Msg)
 init route =
   case route.logical of
+    Routes.Build teamName pipelineName jobName buildName ->
+      superDupleWrap (BuildModel, BuildMsg) <|
+        Autoscroll.init
+          Build.getScrollBehavior <<
+            Build.init <|
+              Build.JobBuildPage
+                { teamName = teamName
+                , pipelineName = pipelineName
+                , jobName = jobName
+                , buildName = buildName
+                }
+    Routes.OneOffBuild buildId ->
+      superDupleWrap (BuildModel, BuildMsg) <|
+        Autoscroll.init
+          Build.getScrollBehavior <<
+            Build.init <|
+              Build.BuildPage <|
+                Result.withDefault 0 (String.toInt buildId)
+    Routes.Job teamName pipelineName jobName ->
+      let
+        pageSince =
+          case Dict.get "since" route.parsed.query of
+            Nothing ->
+              0
+            Just since ->
+              Result.withDefault 0 (String.toInt since)
+        pageUntil =
+          case Dict.get "until" route.parsed.query of
+            Nothing ->
+              0
+            Just until ->
+              Result.withDefault 0 (String.toInt until)
+      in
+        superDupleWrap (JobModel, JobMsg) <|
+          Job.init
+            { jobName = jobName
+            , teamName = teamName
+            , pipelineName = pipelineName
+            , pageSince = pageSince
+            , pageUntil = pageUntil
+            }
     Routes.SelectTeam ->
       let
         redirect =
@@ -65,6 +116,10 @@ init route =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg mdl =
   case (msg, mdl) of
+    (BuildMsg message, BuildModel scrollModel) ->
+      superDupleWrap (BuildModel, BuildMsg) <| Autoscroll.update Build.update message scrollModel
+    (JobMsg message, JobModel model) ->
+      superDupleWrap (JobModel, JobMsg) <| Job.update message model
     (LoginMsg message, LoginModel model) ->
       superDupleWrap (LoginModel, LoginMsg) <| Login.update message model
     (PipelineMsg message, PipelineModel model) ->
@@ -82,6 +137,10 @@ urlUpdate route model =
 view : Model -> Html Msg
 view mdl =
   case mdl of
+    BuildModel model ->
+      Html.App.map BuildMsg <| Autoscroll.view Build.view model
+    JobModel model ->
+      Html.App.map JobMsg <| Job.view model
     LoginModel model ->
       Html.App.map LoginMsg <| Login.view model
     PipelineModel model ->
@@ -92,6 +151,10 @@ view mdl =
 subscriptions : Model -> Sub Msg
 subscriptions mdl =
   case mdl of
+    BuildModel model ->
+      Sub.map BuildMsg <| Autoscroll.subscriptions Build.subscriptions model
+    JobModel model ->
+      Sub.map JobMsg <| Job.subscriptions model
     LoginModel model ->
       Sub.map LoginMsg <| Login.subscriptions model
     PipelineModel model ->
