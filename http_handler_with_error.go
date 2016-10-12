1a8204425d3d9c50fdb40f52b0a0654e3007e405
Topher Bullock <cbullock@pivotal.io>
2016-10-12 15:21:49 -0400

    kill dead code under web/

--- http_handler_with_error.go
+++
@@ -1,9 +0,0 @@
-package web
-
-import "net/http"
-
-//go:generate counterfeiter . HTTPHandlerWithError
-
-type HTTPHandlerWithError interface {
-	ServeHTTP(w http.ResponseWriter, r *http.Request) error
-}
