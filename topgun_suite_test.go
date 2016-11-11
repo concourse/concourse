b0776438d46349eea8f8975216672473c9d7711d
Alex Suraci <suraci.alex@gmail.com>
2016-11-10 17:02:04 -0800

    init

---
+++ topgun_suite_test.go
@@ -0,0 +1,104 @@
+package topgun_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"code.cloudfoundry.org/lager/lagertest"
+
+	"github.com/concourse/go-concourse/concourse"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"testing"
+)
+
+var (
+	boshEnv string
+
+	deploymentName, flyTarget string
+
+	atcIP, atcExternalURL string
+
+	pipelineName string
+
+	tmpHome string
+	flyBin  string
+
+	client concourse.Client
+	team   concourse.Team
+
+	logger *lagertest.TestLogger
+)
+
+func TestTOPGUN(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "TOPGUN Suite")
+}
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	flyBinPath, err := gexec.Build("github.com/concourse/fly")
+	Expect(err).ToNot(HaveOccurred())
+
+	return []byte(flyBinPath)
+}, func(data []byte) {
+	flyBin = string(data)
+})
+
+var _ = SynchronizedAfterSuite(func() {
+	gexec.CleanupBuildArtifacts()
+}, func() {})
+
+var _ = BeforeEach(func() {
+	logger = lagertest.NewTestLogger("test")
+
+	boshEnv = os.Getenv("BOSH_ENV")
+	if boshEnv == "" {
+		Fail("must specify $BOSH_ENV")
+	}
+
+	deploymentName = fmt.Sprintf("concourse-topgun-%d", GinkgoParallelNode())
+	flyTarget = deploymentName
+
+	bosh("delete-deployment")
+
+	atcIP = fmt.Sprintf("10.234.%d.2", GinkgoParallelNode())
+	atcExternalURL = fmt.Sprintf("http://%s:8080", atcIP)
+
+	client = concourse.NewClient(atcExternalURL, nil)
+	team = client.Team("main")
+})
+
+var _ = AfterEach(func() {
+	bosh("delete-deployment")
+})
+
+func Deploy(manifest string) {
+	bosh(
+		"deploy", manifest,
+		"-v", "deployment-name="+deploymentName,
+		"-v", "atc-ip="+atcIP,
+		"-v", "atc-external-url="+atcExternalURL,
+	)
+
+	fly("login", "-c", atcExternalURL)
+}
+
+func bosh(argv ...string) {
+	run("bosh", append([]string{"-n", "-e", boshEnv, "-d", deploymentName}, argv...)...)
+}
+
+func fly(argv ...string) {
+	run("fly", append([]string{"-t", flyTarget}, argv...)...)
+}
+
+func run(argc string, argv ...string) {
+	cmd := exec.Command(argc, argv...)
+	cmd.Stdout = GinkgoWriter
+	cmd.Stderr = GinkgoWriter
+
+	err := cmd.Run()
+	Expect(err).ToNot(HaveOccurred())
+}
---
+++ volume_gc_test.go
@@ -0,0 +1,124 @@
+package topgun_test
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	gclient "code.cloudfoundry.org/garden/client"
+	gconn "code.cloudfoundry.org/garden/client/connection"
+	sq "github.com/Masterminds/squirrel"
+	bgclient "github.com/concourse/baggageclaim/client"
+	_ "github.com/lib/pq"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var psql = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+
+var _ = Describe("A volume that belonged to a container that is now gone", func() {
+	var dbConn *sql.DB
+
+	var _ = Expect
+
+	BeforeEach(func() {
+		var err error
+		dbConn, err = sql.Open("postgres", fmt.Sprintf("postgres://atc:dummy-password@%s:5432/atc?sslmode=disable", atcIP))
+		Expect(err).ToNot(HaveOccurred())
+
+		Deploy("deployments/single-vm.yml")
+	})
+
+	It("is removed from the database and worker [#129726011]", func() {
+		By("running a task with container having a rootfs, input, and output volume")
+		fly("execute", "-c", "tasks/input-output.yml", "-i", "some-input=./tasks")
+
+		By("collecting the build containers")
+		rows, err := psql.Select("id, handle").From("containers").Where(sq.Eq{"build_id": 1}).RunWith(dbConn).Query()
+		Expect(err).ToNot(HaveOccurred())
+
+		buildContainers := map[int]string{}
+		containerIDs := []int{}
+		for rows.Next() {
+			var id int
+			var handle string
+			err := rows.Scan(&id, &handle)
+			Expect(err).ToNot(HaveOccurred())
+
+			buildContainers[id] = handle
+			containerIDs = append(containerIDs, id)
+		}
+
+		By("collecting the container volumes")
+		rows, err = psql.Select("id, handle").From("volumes").Where(sq.Eq{"container_id": containerIDs}).RunWith(dbConn).Query()
+		Expect(err).ToNot(HaveOccurred())
+
+		containerVolumes := map[int]string{}
+		volumeIDs := []int{}
+		for rows.Next() {
+			var id int
+			var handle string
+			err := rows.Scan(&id, &handle)
+			Expect(err).ToNot(HaveOccurred())
+
+			containerVolumes[id] = handle
+			volumeIDs = append(volumeIDs, id)
+		}
+
+		By(fmt.Sprintf("eventually expiring the build containers: %v", containerIDs))
+		Eventually(func() int {
+			var volNum int
+			err := psql.Select("COUNT(id)").From("volumes").Where(sq.Eq{"id": volumeIDs}).RunWith(dbConn).QueryRow().Scan(&volNum)
+			Expect(err).ToNot(HaveOccurred())
+
+			var containerNum int
+			err = psql.Select("COUNT(id)").From("containers").Where(sq.Eq{"build_id": 1}).RunWith(dbConn).QueryRow().Scan(&containerNum)
+			Expect(err).ToNot(HaveOccurred())
+
+			if containerNum > 0 {
+				By(fmt.Sprintf("not expiring volumes so long as their containers are there (%d remaining)", containerNum))
+				Expect(volNum).To(Equal(len(volumeIDs)))
+			}
+
+			return containerNum
+		}, 10*time.Minute, time.Second).Should(BeZero())
+
+		By("having removed the containers from the worker")
+		gClient := gclient.New(gconn.New("tcp", fmt.Sprintf("%s:7777", atcIP)))
+
+		containers, err := gClient.Containers(nil)
+		Expect(err).ToNot(HaveOccurred())
+
+		existingHandles := []string{}
+		for _, c := range containers {
+			existingHandles = append(existingHandles, c.Handle())
+		}
+
+		for _, handle := range buildContainers {
+			Expect(existingHandles).ToNot(ContainElement(handle))
+		}
+
+		By(fmt.Sprintf("eventually expiring the container volumes: %v", volumeIDs))
+		Eventually(func() int {
+			var num int
+			err := psql.Select("COUNT(id)").From("volumes").Where(sq.Eq{"id": volumeIDs}).RunWith(dbConn).QueryRow().Scan(&num)
+			Expect(err).ToNot(HaveOccurred())
+			return num
+		}, 10*time.Minute, time.Second).Should(BeZero())
+
+		By("having removed the volumes from the worker")
+		bcClient := bgclient.New(fmt.Sprintf("http://%s:7788", atcIP))
+
+		volumes, err := bcClient.ListVolumes(logger, nil)
+		Expect(err).ToNot(HaveOccurred())
+
+		existingHandles = []string{}
+		for _, v := range volumes {
+			existingHandles = append(existingHandles, v.Handle())
+		}
+
+		for _, handle := range containerVolumes {
+			Expect(existingHandles).ToNot(ContainElement(handle))
+		}
+	})
+})
