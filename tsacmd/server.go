7de4c81d756e7b9782d2d2931d75c4eb4115b7bc
Maria Shaldibina <mshaldibina@pivotal.io>
2016-12-13 16:30:18 -0800

    stop processing requests if heartbeat process exits
    When heartbeating returns 404 the worker is being drained and we should
    stop processing any more requests and close ssh channel.
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- tsacmd/server.go
+++ tsacmd/server.go
@@ -101,7 +101,14 @@ func (server *registrarSSHServer) handshake(logger lager.Logger, netConn net.Con
 	chansGroup.Wait()
 }
 
-func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID string, forwardedTCPIPs <-chan forwardedTCPIP, chansGroup *sync.WaitGroup, channel ssh.Channel, requests <-chan *ssh.Request) {
+func (server *registrarSSHServer) handleChannel(
+	logger lager.Logger,
+	sessionID string,
+	forwardedTCPIPs <-chan forwardedTCPIP,
+	chansGroup *sync.WaitGroup,
+	channel ssh.Channel,
+	requests <-chan *ssh.Request,
+) {
 	var processes []ifrit.Process
 
 	// ensure processes get cleaned up
@@ -164,8 +171,10 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 			if err != nil {
 				logger.Error("failed-to-land-worker", err)
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{1}))
+				channel.Close()
 			} else {
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{0}))
+				channel.Close()
 			}
 
 		case retireWorkerRequest:
@@ -178,8 +187,10 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 			if err != nil {
 				logger.Error("failed-to-retire-worker", err)
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{1}))
+				channel.Close()
 			} else {
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{0}))
+				channel.Close()
 			}
 
 		case deleteWorkerRequest:
@@ -192,8 +203,10 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 			if err != nil {
 				logger.Error("failed-to-delete-worker", err)
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{1}))
+				channel.Close()
 			} else {
 				channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{0}))
+				channel.Close()
 			}
 
 		case registerWorkerRequest:
@@ -206,7 +219,7 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 				logger.Error("failed-to-register", err)
 				return
 			}
-
+			watchForProcessToExit(logger, process, channel)
 			processes = append(processes, process)
 
 		case forwardWorkerRequest:
@@ -250,7 +263,7 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 						logger.Error("failed-to-register", err)
 						return
 					}
-
+					watchForProcessToExit(logger, process, channel)
 					processes = append(processes, process)
 
 					break
@@ -280,7 +293,7 @@ func (server *registrarSSHServer) handleChannel(logger lager.Logger, sessionID s
 					logger.Error("failed-to-register", err)
 					return
 				}
-
+				watchForProcessToExit(logger, process, channel)
 				processes = append(processes, process)
 			}
 
@@ -623,4 +636,17 @@ func keepaliveDialerFactory(network string, address string) gconn.DialerFunc {
 	return func(string, string) (net.Conn, error) {
 		return keepaliveDialer(network, address)
 	}
+}
+
+func watchForProcessToExit(logger lager.Logger, process ifrit.Process, channel ssh.Channel) {
+	logger = logger.Session("wait-for-process-or-connection-to-finish")
+
+	go func() {
+		err := <-process.Wait()
+		if err == nil {
+			channel.SendRequest("exit-status", false, ssh.Marshal(exitStatusRequest{0}))
+		}
+
+		channel.Close()
+	}()
 }
