b150da0af8157afb01081f25d73616a5f69da945
Chris Hendrix <chendrix@pivotal.io>
2017-01-26 16:58:33 -0800

    add hijacked container for old build test
    [#129726125 #138350035]
    
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

---
+++ hijack_container_gc_test.go
@@ -0,0 +1,95 @@
+package topgun_test
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	gclient "code.cloudfoundry.org/garden/client"
+	gconn "code.cloudfoundry.org/garden/client/connection"
+	_ "github.com/lib/pq"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("[#129726125] Hijacked containers", func() {
+	var (
+		gClient gclient.Client
+
+		containerHandle string
+	)
+
+	BeforeEach(func() {
+		Deploy("deployments/single-vm.yml")
+
+		gClient = gclient.New(gconn.New("tcp", fmt.Sprintf("%s:7777", atcIP)))
+	})
+
+	getContainer := func() (h hijackedContainerResult) {
+		containers := flyTable("containers")
+
+		for _, c := range containers {
+			if c["build #"] == "1" {
+				containerHandle = c["handle"]
+				h.flyContainerExists = true
+
+				break
+			}
+		}
+
+		_, err := gClient.Lookup(containerHandle)
+		if err == nil {
+			h.gardenContainerExists = true
+		}
+
+		return
+	}
+
+	It("does not delete hijacked containers from the database, and sets a 5 minute TTL on the container in garden", func() {
+		By("setting the pipeline that has a build")
+		fly("set-pipeline", "-n", "-c", "pipelines/task-waiting.yml", "-p", "hijacked-containers-test")
+
+		By("triggering the build")
+		fly("unpause-pipeline", "-p", "hijacked-containers-test")
+		buildSession := spawnFly("trigger-job", "-w", "-j", "hijacked-containers-test/simple-job")
+		Eventually(buildSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
+
+		By("hijacking into the build container")
+		hijackSession := spawnFlyInteractive(
+			bytes.NewBufferString("3\n"),
+			"hijack",
+			"-j", "hijacked-containers-test/simple-job",
+			"-b", "1",
+			"-s", "simple-task",
+			"touch", "/tmp/stop-waiting",
+		)
+
+		By("triggering a new build")
+		<-buildSession.Exited
+		buildSession = spawnFly("trigger-job", "-w", "-j", "hijacked-containers-test/simple-job")
+		Eventually(buildSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
+		<-spawnFlyInteractive(
+			bytes.NewBufferString("3\n"),
+			"hijack",
+			"-j", "hijacked-containers-test/simple-job",
+			"-b", "2",
+			"-s", "simple-task",
+			"touch", "/tmp/stop-waiting",
+		).Exited
+
+		By("verifying the hijacked container exists via fly and Garden")
+		Consistently(getContainer, 2*time.Minute, 30*time.Second).Should(Equal(hijackedContainerResult{true, true}))
+
+		By("unhijacking and seeing the container removed via fly/Garden after 5 minutes")
+		hijackSession.Terminate()
+		<-hijackSession.Exited
+
+		Eventually(getContainer, 10*time.Minute, 30*time.Second).Should(Equal(hijackedContainerResult{false, false}))
+	})
+})
+
+type hijackedContainerResult struct {
+	flyContainerExists    bool
+	gardenContainerExists bool
+}
