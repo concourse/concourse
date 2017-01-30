1ba4193b9c495c12262a06c4bc1c9b5a62c2a093
Josh Zarrabi <jzarrabi@pivotal.io>
2017-01-30 12:20:34 -0800

    add scoped container test [#138350035 #136140165]
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

---
+++ containers_scoped_to_team_test.go
@@ -0,0 +1,74 @@
+package topgun_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"bytes"
+
+	"strconv"
+
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe(":life [#136140165] Container scope", func() {
+	Context("when the container is scoped to a team", func() {
+		BeforeEach(func() {
+			Deploy("deployments/single-vm.yml")
+		})
+
+		It("is only hijackable by someone in that team", func() {
+			By("setting a pipeline for team `main`")
+			fly("set-pipeline", "-n", "-c", "pipelines/get-task-put-waiting.yml", "-p", "container-scope-test")
+
+			By("triggering the build")
+			fly("unpause-pipeline", "-p", "container-scope-test")
+			buildSession := spawnFly("trigger-job", "-w", "-j", "container-scope-test/simple-job")
+			Eventually(buildSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
+
+			By("demonstrating we can hijack into all of the containers")
+			var (
+				hijackSession *gexec.Session
+			)
+			for i := 1; i <= 4; i++ {
+				hijackSession = spawnFlyInteractive(
+					bytes.NewBufferString(strconv.Itoa(i)+"\n"),
+					"hijack",
+					"-b", "1",
+				)
+
+				<-hijackSession.Exited
+				Expect(hijackSession.ExitCode()).To(Equal(0))
+			}
+
+			By("creating a separate team")
+			setTeamSession := spawnFlyInteractive(
+				bytes.NewBufferString("y\n"),
+				"set-team", "--team-name", "no-access", "--no-really-i-dont-want-any-auth")
+
+			<-setTeamSession.Exited
+			Expect(setTeamSession.ExitCode()).To(Equal(0))
+
+			By("logging into other team")
+			fly("login", "-n", "no-access")
+
+			By("not allowing hijacking into any containers")
+			failedFly := spawnFly("hijack", "-b", "1")
+			<-failedFly.Exited
+			Expect(failedFly.ExitCode()).NotTo(Equal(0))
+			Expect(failedFly.Err).To(gbytes.Say("no containers matched your search parameters!"))
+
+			By("logging back into the other team")
+			fly("login", "-n", "main")
+
+			By("stopping the build")
+			hijackSession = spawnFlyInteractive(
+				bytes.NewBufferString("3\n"),
+				"hijack",
+				"-b", "1",
+				"touch", "/tmp/stop-waiting",
+			)
+		})
+	})
+})
