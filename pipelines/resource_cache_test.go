777993cca5e3eed2095a7157fff1ac0d19b5cc43
Chris Hendrix <chendrix@pivotal.io>
2017-02-15 14:01:57 -0800

    Add test for resource caches with params [#139960779]
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

---
+++ pipelines/resource_cache_test.go
@@ -0,0 +1,42 @@
+package pipelines_test
+
+import (
+	"github.com/concourse/testflight/gitserver"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("[#139960779] resource caching", func() {
+	var (
+		originGitServer *gitserver.Server
+	)
+
+	BeforeEach(func() {
+		originGitServer = gitserver.Start(client)
+	})
+
+	AfterEach(func() {
+		originGitServer.Stop()
+	})
+
+	It("gets the resource from the cache based on given params", func() {
+		configurePipeline(
+			"-c", "fixtures/resource-with-params.yml",
+			"-v", "origin-git-server="+originGitServer.URI(),
+		)
+
+		originGitServer.Commit()
+
+		watch := triggerJob("without-params")
+		<-watch.Exited
+
+		watch = triggerJob("without-params")
+		<-watch.Exited
+		Expect(watch).To(gbytes.Say("using version of resource found in cache"))
+
+		watch = triggerJob("with-params")
+		<-watch.Exited
+		Expect(watch).ToNot(gbytes.Say("using version of resource found in cache"))
+	})
+})
