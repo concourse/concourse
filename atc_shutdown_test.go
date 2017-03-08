9472c99a5c950239e295a07cc342835ec304843f
Maria Shaldibina <mshaldibina@pivotal.io>
2017-03-07 17:33:19 -0800

    fix topgun test for atc shutdown
    [#137641079]
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- atc_shutdown_test.go
+++ atc_shutdown_test.go
@@ -3,8 +3,8 @@ package topgun_test
 import (
 	"database/sql"
 	"fmt"
-	"os"
 	"regexp"
+	"time"
 
 	_ "github.com/lib/pq"
 	. "github.com/onsi/ginkgo"
@@ -31,7 +31,6 @@ var _ = Describe("[#137641079] ATC Shutting down", func() {
 
 		Describe("tracking builds previously tracked by shutdown ATC", func() {
 			var stopSession *gexec.Session
-			var landSession *gexec.Session
 
 			BeforeEach(func() {
 				By("stopping one of the web instances")
@@ -44,13 +43,11 @@ var _ = Describe("[#137641079] ATC Shutting down", func() {
 			})
 
 			Context("with a build in-flight", func() {
-				var buildSession *gexec.Session
-				var startSession *gexec.Session
 				var buildID string
 
 				BeforeEach(func() {
 					By("executing a task")
-					buildSession = spawnFly("execute", "-c", "tasks/wait.yml")
+					buildSession := spawnFly("execute", "-c", "tasks/wait.yml")
 					Eventually(buildSession).Should(gbytes.Say("executing build"))
 
 					buildRegex := regexp.MustCompile(`executing build (\d+)`)
@@ -60,48 +57,46 @@ var _ = Describe("[#137641079] ATC Shutting down", func() {
 					Eventually(buildSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
 
 					By("starting the stopped web instance")
-					startSession = spawnBosh("start", "web/1")
-					Eventually(stopSession).Should(gexec.Exit(0))
-					spawnFly("login", "-c", atcExternalURL2)
+					startSession := spawnBosh("start", "web/1")
+					<-startSession.Exited
+					Eventually(startSession).Should(gexec.Exit(0))
+					<-spawnFly("login", "-c", atcExternalURL2).Exited
 				})
 
 				AfterEach(func() {
-					buildSession.Signal(os.Interrupt)
-					<-buildSession.Exited
-					<-startSession.Exited
 					restartSession := spawnBosh("start", "web/0")
+					<-restartSession.Exited
 					Eventually(restartSession).Should(gexec.Exit(0))
 				})
 
 				Context("when the atc tracking the build shuts down", func() {
 					JustBeforeEach(func() {
 						By("stopping the first web instance")
-						landSession = spawnBosh("stop", "web/0")
-						Eventually(stopSession).Should(gexec.Exit(0))
-					})
-
-					AfterEach(func() {
-						<-stopSession.Exited
+						landSession := spawnBosh("stop", "web/0")
+						<-landSession.Exited
+						Eventually(landSession).Should(gexec.Exit(0))
 					})
 
 					It("continues tracking the build progress", func() {
-						By("hijacking the build to tell it to finish")
-						Eventually(func() int {
-							session := spawnFly(
-								"hijack",
-								"-b", buildID,
-								"-s", "one-off",
-								"touch", "/tmp/stop-waiting",
-							)
+						By("waiting for another atc to attach to process")
+						watchSession := spawnFly("watch", "-b", buildID)
+						Eventually(watchSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
+						time.Sleep(10 * time.Second)
 
-							<-session.Exited
-							return session.ExitCode()
-						}).Should(Equal(0))
+						By("hijacking the build to tell it to finish")
+						hijackSession := spawnFly(
+							"hijack",
+							"-b", buildID,
+							"-s", "one-off",
+							"touch", "/tmp/stop-waiting",
+						)
+						<-hijackSession.Exited
+						Expect(hijackSession.ExitCode()).To(Equal(0))
 
 						By("waiting for the build to exit")
-						Eventually(buildSession).Should(gbytes.Say("done"))
-						<-buildSession.Exited
-						Expect(buildSession.ExitCode()).To(Equal(0))
+						Eventually(watchSession, 1*time.Minute).Should(gbytes.Say("done"))
+						<-watchSession.Exited
+						Expect(watchSession.ExitCode()).To(Equal(0))
 					})
 				})
 			})
