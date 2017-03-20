914de9e43b018d0582babdad9adf82fb31ae0770
Chris Hendrix <chendrix@pivotal.io>
2017-03-20 16:09:43 -0400

    Add X- headers for security
    X-XSS, X-Content-Type-Options, X-Download-Options have sane defaults,
    but X-Frame-Options would break existing functionality of including
    concourse on iframes if we added this header by default.
    
    Instead, we expose what to put in this header in the ATC flags,
    defaulting it to no header.
    
    [#142094977]
    
    Signed-off-by: Anisha Hirji <ahirji@pivotal.io>

--- cookie_set_handler.go
+++ cookie_set_handler.go
@@ -14,6 +14,7 @@ func (handler CookieSetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	cookie, err := r.Cookie(CookieName)
 	if err == nil && r.Header.Get("Authorization") == "" {
 		r.Header.Set("Authorization", cookie.Value)
+
 	}
 
 	handler.Handler.ServeHTTP(w, r)
