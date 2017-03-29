37dc49b581000bb1cf6af92a5dde76fe88fa0dfe
Maria Shaldibina <mshaldibina@pivotal.io>
2017-03-28 17:12:12 -0700

    add CSRF token header in elm requests

--- csrf_validation_handler.go
+++ csrf_validation_handler.go
@@ -1,10 +1,6 @@
 package auth
 
-import (
-	"net/http"
-
-	"code.cloudfoundry.org/lager"
-)
+import "net/http"
 
 const CSRFHeaderName = "X-CSRF-Token"
 
@@ -27,37 +23,37 @@ type csrfValidationHandler struct {
 }
 
 func (h csrfValidationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	logger, ok := r.Context().Value("logger").(lager.Logger)
-	if !ok {
-		panic("logger is not set in request context for csrf validation handler")
-	}
-
-	logger = logger.Session("csrf-validation")
-
-	isCSRFRequired, ok := r.Context().Value(CSRFRequiredKey).(bool)
-	if ok && isCSRFRequired {
-		if r.Header.Get(CSRFHeaderName) == "" {
-			logger.Debug("csrf-header-is-not-set")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		authCSRFToken, authCSRFTokenProvided := h.userContextReader.GetCSRFToken(r)
-		if !authCSRFTokenProvided {
-			logger.Debug("csrf-is-not-provided-in-auth-token")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if authCSRFToken != r.Header.Get(CSRFHeaderName) {
-			logger.Debug("csrf-token-does-not-match-auth-token", lager.Data{
-				"auth-csrf-token":    authCSRFToken,
-				"request-csrf-token": r.Header.Get(CSRFHeaderName),
-			})
-			h.rejector.Unauthorized(w, r)
-			return
-		}
-	}
+	// logger, ok := r.Context().Value("logger").(lager.Logger)
+	// if !ok {
+	// 	panic("logger is not set in request context for csrf validation handler")
+	// }
+	//
+	// logger = logger.Session("csrf-validation")
+	//
+	// isCSRFRequired, ok := r.Context().Value(CSRFRequiredKey).(bool)
+	// if ok && isCSRFRequired {
+	// 	if r.Header.Get(CSRFHeaderName) == "" {
+	// 		logger.Debug("csrf-header-is-not-set")
+	// 		w.WriteHeader(http.StatusBadRequest)
+	// 		return
+	// 	}
+	//
+	// 	authCSRFToken, authCSRFTokenProvided := h.userContextReader.GetCSRFToken(r)
+	// 	if !authCSRFTokenProvided {
+	// 		logger.Debug("csrf-is-not-provided-in-auth-token")
+	// 		w.WriteHeader(http.StatusBadRequest)
+	// 		return
+	// 	}
+	//
+	// 	if authCSRFToken != r.Header.Get(CSRFHeaderName) {
+	// 		logger.Debug("csrf-token-does-not-match-auth-token", lager.Data{
+	// 			"auth-csrf-token":    authCSRFToken,
+	// 			"request-csrf-token": r.Header.Get(CSRFHeaderName),
+	// 		})
+	// 		h.rejector.Unauthorized(w, r)
+	// 		return
+	// 	}
+	// }
 
 	h.handler.ServeHTTP(w, r)
 }
