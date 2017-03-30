daca8e74979f7bddbc192f35a920dd033694e26f
Chris Hendrix <chendrix@pivotal.io>
2017-03-30 16:09:27 -0400

    add test for tagged resource checking
    moved this from testflight so it won't have to deal with polluting the
    containers table from previous tests
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

---
+++ resource_check_test.go
@@ -0,0 +1,44 @@
+package topgun_test
+
+import (
+	_ "github.com/lib/pq"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = FDescribe("Resource checking", func() {
+	BeforeEach(func() {
+		Deploy("deployments/two-workers-one-tagged.yml")
+	})
+
+	Context("with tags on the resource", func() {
+		BeforeEach(func() {
+			By("setting a pipeline that has a tagged resource")
+			fly("set-pipeline", "-n", "-c", "pipelines/tagged-resource.yml", "-p", "tagged-resource")
+
+			By("unpausing the pipeline pipeline")
+			fly("unpause-pipeline", "-p", "tagged-resource")
+		})
+
+		It("places the checking container on the tagged worker", func() {
+			By("running the check")
+			fly("check-resource", "-r", "tagged-resource/some-resource")
+
+			By("getting the worker name")
+			workersTable := flyTable("workers")
+			var taggedWorkerName string
+			for _, w := range workersTable {
+				if w["tags"] == "tagged" {
+					taggedWorkerName = w["name"]
+				}
+			}
+			Expect(taggedWorkerName).ToNot(BeEmpty())
+
+			By("checking that the container is on the tagged worker")
+			containerTable := flyTable("containers")
+			Expect(containerTable).To(HaveLen(1))
+			Expect(containerTable[0]["type"]).To(Equal("check"))
+			Expect(containerTable[0]["worker"]).To(Equal(taggedWorkerName))
+		})
+	})
+})
