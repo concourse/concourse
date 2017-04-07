d4f25bc43cd80e8dd6991971b842f69da8764ae1
Topher Bullock <cbullock@pivotal.io>
2017-04-07 17:27:53 -0400

    dial to garden addr, not "api"
    garden client has some wonky behavior with its injected dialer
    
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- gcng/dialer_unix.go
+++ gcng/dialer_unix.go
@@ -9,16 +9,18 @@ import (
 	"github.com/felixge/tcpkeepalive"
 )
 
-func keepaliveDialer(network string, address string) (net.Conn, error) {
-	conn, err := net.DialTimeout(network, address, 5*time.Second)
-	if err != nil {
-		return nil, err
-	}
+func keepaliveDialer(addr string) func(string, string) (net.Conn, error) {
+	return func(string, string) (net.Conn, error) {
+		conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
+		if err != nil {
+			return nil, err
+		}
 
-	err = tcpkeepalive.SetKeepAlive(conn, 10*time.Second, 3, 5*time.Second)
-	if err != nil {
-		println("failed to enable connection keepalive: " + err.Error())
-	}
+		err = tcpkeepalive.SetKeepAlive(conn, 10*time.Second, 3, 5*time.Second)
+		if err != nil {
+			println("failed to enable connection keepalive: " + err.Error())
+		}
 
-	return conn, nil
+		return conn, nil
+	}
 }
