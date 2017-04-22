0ce52eb2339cc82404512ffefdae6ff903ec218c
Jens Deppe <jdeppe@pivotal.io>
2017-04-21 20:28:19 -0700

    Sync up Solaris code with mainline changes

--- cmd/concourse/worker_solaris.go
+++ cmd/concourse/worker_solaris.go
@@ -1,22 +1,19 @@
 package main
 
 import (
+	"code.cloudfoundry.org/lager"
 	"github.com/concourse/atc"
 	"github.com/jessevdk/go-flags"
-	"github.com/pivotal-golang/lager"
 	"github.com/tedsuo/ifrit"
 )
 
 type GardenBackend struct{}
 
-func (cmd WorkerCommand) lessenRequirements(command *flags.Command) {}
+func (cmd WorkerCommand) lessenRequirements(command *flags.Command) {
+	command.FindOptionByLongName("baggageclaim-volumes").Required = false
+}
 
 func (cmd *WorkerCommand) gardenRunner(logger lager.Logger, args []string) (atc.Worker, ifrit.Runner, error) {
-	err := cmd.checkRoot()
-	if err != nil {
-		return atc.Worker{}, nil, err
-	}
-
 	return cmd.houdiniRunner(logger, "solaris")
 }
 
