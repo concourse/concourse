7d189e9f6739cc537079c9a767a4af7f8c716108
Maria Shaldibina <mshaldibina@pivotal.io>
2017-04-26 10:56:00 -0700

    migrate bin to concourse release
    instead of using vendor put bin into concourse release which behaves as
    a GOPATH and includes all dependencies
    
    Signed-off-by: Clara Fu <cfu@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -12,6 +12,6 @@ go get github.com/jteeuwen/go-bindata
 
 go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
-.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
+.\go-bindata.exe -pkg bindata -o concourse\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
 
 go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
