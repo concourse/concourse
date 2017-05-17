f607782e76b6b9b7f84eabe9893a86284c43f8d8
Andrew Edstrom <aedstrom@pivotal.io>
2017-05-17 15:46:12 -0700

    add tests for resource cache metadata
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

---
+++ web/resource_metadata_test.go
@@ -0,0 +1,125 @@
+package web_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/concourse/testflight/gitserver"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/sclevine/agouti/matchers"
+)
+
+var _ = Describe("resource metadata", func() {
+	var (
+		originGitServer *gitserver.Server
+	)
+
+	BeforeEach(func() {
+		originGitServer = gitserver.Start(client)
+	})
+
+	AfterEach(func() {
+		originGitServer.Stop()
+	})
+
+	Context("when running build again on the same job", func() {
+		It("prints resource metadata", func() {
+			By("configuring pipeline")
+			flyHelper.ConfigurePipeline(
+				pipelineName,
+				"-c", "fixtures/resource.yml",
+				"-v", "origin-git-server="+originGitServer.URI(),
+			)
+
+			sha := originGitServer.Commit()
+
+			By("triggering job")
+			watch := flyHelper.TriggerJob(pipelineName, "simple")
+			<-watch.Exited
+
+			By("navigating to the build's page")
+			Expect(page.Navigate(atcRoute(fmt.Sprintf("teams/%s/pipelines/%s/jobs/simple/builds/%d", teamName, pipelineName, 1)))).To(Succeed())
+			Eventually(page.Find("h3"), 10*time.Second).Should(HaveText("some-git-resource"))
+
+			By("clicking the header")
+			Expect(page.Find(".header").Click()).To(Succeed()) // first resource
+
+			By("seeing the metadata")
+			Eventually(page.Find(".step-body"), 30*time.Second, 1*time.Second).Should(MatchText("commit.*" + sha))
+
+			By("triggering the job again")
+			watch = flyHelper.TriggerJob(pipelineName, "simple")
+			<-watch.Exited
+
+			By("navigating to the page")
+			Expect(page.Navigate(atcRoute(fmt.Sprintf("teams/%s/pipelines/%s/jobs/simple/builds/%d", teamName, pipelineName, 2)))).To(Succeed())
+			Eventually(page.Find("h3"), 10*time.Second).Should(HaveText("some-git-resource"))
+
+			By("clicking the header")
+			Expect(page.Find(".header").Click()).To(Succeed()) // first resource
+
+			By("seeing the metadata")
+			Eventually(page.Find(".step-body"), 30*time.Second, 1*time.Second).Should(MatchText("commit.*" + sha))
+		})
+	})
+
+	Context("when running build on a another pipeline with the same resource config", func() {
+		var (
+			commitSHA           string
+			anotherPipelineName string
+		)
+
+		BeforeEach(func() {
+			anotherPipelineName = "another-pipeline"
+
+			flyHelper.ConfigurePipeline(
+				pipelineName,
+				"-c", "fixtures/resource.yml",
+				"-v", "origin-git-server="+originGitServer.URI(),
+			)
+
+			flyHelper.ConfigurePipeline(
+				anotherPipelineName,
+				"-c", "fixtures/resource.yml",
+				"-v", "origin-git-server="+originGitServer.URI(),
+			)
+
+			commitSHA = originGitServer.Commit()
+		})
+
+		AfterEach(func() {
+			flyHelper.DestroyPipeline(anotherPipelineName)
+		})
+
+		It("prints resource metadata", func() {
+			By("triggering job")
+			watch := flyHelper.TriggerJob(pipelineName, "simple")
+			<-watch.Exited
+
+			By("navigating to the build's page")
+			Expect(page.Navigate(atcRoute(fmt.Sprintf("teams/%s/pipelines/%s/jobs/simple/builds/%d", teamName, pipelineName, 1)))).To(Succeed())
+			Eventually(page.Find("h3"), 30*time.Second).Should(HaveText("some-git-resource"))
+
+			By("clicking the header")
+			Expect(page.Find(".header").Click()).To(Succeed()) // first resource
+
+			By("seeing the metadata")
+			Eventually(page.Find(".step-body"), 30*time.Second, 1*time.Second).Should(MatchText("commit.*" + commitSHA))
+
+			By("triggering the job in another pipeline")
+			watch = flyHelper.TriggerJob(anotherPipelineName, "simple")
+			<-watch.Exited
+
+			By("navigating to the other pipeline's build's page")
+			Expect(page.Navigate(atcRoute(fmt.Sprintf("teams/%s/pipelines/%s/jobs/simple/builds/%d", teamName, anotherPipelineName, 1)))).To(Succeed())
+			Eventually(page.Find("h3"), 30*time.Second).Should(HaveText("some-git-resource"))
+
+			By("clicking the header")
+			Expect(page.Find(".header").Click()).To(Succeed()) // first resource
+
+			By("seeing the metadata")
+			Eventually(page.Find(".step-body"), 30*time.Second, 1*time.Second).Should(MatchText("commit.*" + commitSHA))
+		})
+	})
+})
--- web/web_suite_test.go
+++ web/web_suite_test.go
@@ -2,6 +2,7 @@ package web_test
 
 import (
 	"fmt"
+	"os"
 
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagertest"
@@ -16,14 +17,17 @@ import (
 	"testing"
 )
 
-var atcURL = helpers.AtcURL()
+var (
+	atcURL       = helpers.AtcURL()
+	pipelineName string
+	teamName     string
+	client       concourse.Client
+	team         concourse.Team
+	logger       lager.Logger
 
-var pipelineName string
-var teamName string
-
-var client concourse.Client
-var team concourse.Team
-var logger lager.Logger
+	flyHelper *helpers.FlyHelper
+	tmpHome   string
+)
 
 func TestWeb(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -36,25 +40,33 @@ var page *agouti.Page
 var _ = SynchronizedBeforeSuite(func() []byte {
 	Eventually(helpers.ErrorPolling(atcURL)).ShouldNot(HaveOccurred())
 
-	data, err := helpers.FirstNodeClientSetup(atcURL)
+	data, err := helpers.FirstNodeFlySetup(atcURL, helpers.TargetedConcourse)
 	Expect(err).NotTo(HaveOccurred())
 
 	return data
 }, func(data []byte) {
+	agoutiDriver = helpers.AgoutiDriver()
+	Expect(agoutiDriver.Start()).To(Succeed())
+
+	var flyBinPath string
 	var err error
+	flyBinPath, tmpHome, err = helpers.AllNodeFlySetup(data)
+	Expect(err).NotTo(HaveOccurred())
+
+	flyHelper = &helpers.FlyHelper{Path: flyBinPath}
+
 	client, err = helpers.AllNodeClientSetup(data)
 	Expect(err).NotTo(HaveOccurred())
 
 	pipelineName = fmt.Sprintf("test-pipeline-%d", GinkgoParallelNode())
 	teamName = "main"
 	team = client.Team(teamName)
-
-	agoutiDriver = helpers.AgoutiDriver()
-	Expect(agoutiDriver.Start()).To(Succeed())
 })
 
-var _ = AfterSuite(func() {
+var _ = SynchronizedAfterSuite(func() {
+}, func() {
 	Expect(agoutiDriver.Stop()).To(Succeed())
+	os.RemoveAll(tmpHome)
 })
 
 var _ = BeforeEach(func() {
@@ -71,8 +83,7 @@ var _ = BeforeEach(func() {
 var _ = AfterEach(func() {
 	Expect(page.Destroy()).To(Succeed())
 
-	_, err := team.DeletePipeline(pipelineName)
-	Expect(err).ToNot(HaveOccurred())
+	flyHelper.DestroyPipeline(pipelineName)
 })
 
 func atcRoute(path string) string {
