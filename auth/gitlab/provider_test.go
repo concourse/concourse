8297b3861191690296be2a478758e9eee9e34d6f
Mark St.Godard <markstgodard@gmail.com>
2017-06-02 10:12:27 -0500

    Add GitLab auth provider validate unit tests

--- auth/gitlab/provider_test.go
+++ auth/gitlab/provider_test.go
@@ -8,13 +8,17 @@ import (
 )
 
 var _ = Describe("GitLab Provider", func() {
+	var (
+		authMethod atc.AuthMethod
+		authConfig *gitlab.GitLabAuthConfig
+	)
+
+	BeforeEach(func() {
+		authConfig = &gitlab.GitLabAuthConfig{}
+	})
+
 	Describe("AuthMethod", func() {
-		var (
-			authMethod atc.AuthMethod
-			authConfig *gitlab.GitLabAuthConfig
-		)
 		BeforeEach(func() {
-			authConfig = &gitlab.GitLabAuthConfig{}
 			authMethod = authConfig.AuthMethod("http://bum-bum-bum.com", "dudududum")
 		})
 
@@ -24,6 +28,55 @@ var _ = Describe("GitLab Provider", func() {
 				DisplayName: "GitLab",
 				AuthURL:     "http://bum-bum-bum.com/auth/gitlab?team_name=dudududum",
 			}))
+		})
+	})
+
+	Describe("Validate", func() {
+		BeforeEach(func() {
+			authConfig.ClientID = "foo"
+			authConfig.ClientSecret = "bar"
+			authConfig.Groups = []string{"group1", "group2"}
+		})
+
+		Context("when client id/secret and groups are specified", func() {
+			It("succeeds", func() {
+				err := authConfig.Validate()
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when client id is not specified", func() {
+			It("returns an error", func() {
+				authConfig.ClientID = ""
+				err := authConfig.Validate()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(ContainSubstring("must specify --gitlab-auth-client-id and --gitlab-auth-client-secret to use GitLab OAuth.")))
+			})
+		})
+
+		Context("when client secret is not specified", func() {
+			It("returns an error", func() {
+				authConfig.ClientSecret = ""
+				err := authConfig.Validate()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(ContainSubstring("must specify --gitlab-auth-client-id and --gitlab-auth-client-secret to use GitLab OAuth.")))
+			})
+		})
+
+		Context("when group is not specified", func() {
+			It("returns an error", func() {
+				authConfig.Groups = nil
+				err := authConfig.Validate()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(ContainSubstring("the following is required for gitlab-auth: groups")))
+			})
+		})
+
+		Context("when client id/secret and groups specified", func() {
+			It("succeeds", func() {
+				err := authConfig.Validate()
+				Expect(err).NotTo(HaveOccurred())
+			})
 		})
 	})
 
