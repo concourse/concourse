kurtmc/atc

wip resource refactor
Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>
Signed-off-by: Clara Fu <cfu@pivotal.io>

exec/execfakes/fake_factory.go
@@ -13,22 +13,16 @@ import (
 )
 
 type FakeFactory struct {
-	GetStub        func(lager.Logger, int, int, atc.PlanID, exec.StepMetadata, worker.ArtifactName, dbng.ContainerMetadata, exec.GetDelegate, atc.ResourceConfig, atc.Tags, atc.Params, atc.Version, atc.VersionedResourceTypes) exec.StepFactory
+	GetStub        func(logger lager.Logger, teamID int, buildID int, plan atc.Plan, stepMetadata exec.StepMetadata, workerMetadata dbng.ContainerMetadata, delegate exec.GetDelegate) exec.StepFactory
 	getMutex       sync.RWMutex
 	getArgsForCall []struct {
-		arg1  lager.Logger
-		arg2  int
-		arg3  int
-		arg4  atc.PlanID
-		arg5  exec.StepMetadata
-		arg6  worker.ArtifactName
-		arg7  dbng.ContainerMetadata
-		arg8  exec.GetDelegate
-		arg9  atc.ResourceConfig
-		arg10 atc.Tags
-		arg11 atc.Params
-		arg12 atc.Version
-		arg13 atc.VersionedResourceTypes
+		logger         lager.Logger
+		teamID         int
+		buildID        int
+		plan           atc.Plan
+		stepMetadata   exec.StepMetadata
+		workerMetadata dbng.ContainerMetadata
+		delegate       exec.GetDelegate
 	}
 	getReturns struct {
 		result1 exec.StepFactory
@@ -108,28 +102,22 @@ type FakeFactory struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeFactory) Get(arg1 lager.Logger, arg2 int, arg3 int, arg4 atc.PlanID, arg5 exec.StepMetadata, arg6 worker.ArtifactName, arg7 dbng.ContainerMetadata, arg8 exec.GetDelegate, arg9 atc.ResourceConfig, arg10 atc.Tags, arg11 atc.Params, arg12 atc.Version, arg13 atc.VersionedResourceTypes) exec.StepFactory {
+func (fake *FakeFactory) Get(logger lager.Logger, teamID int, buildID int, plan atc.Plan, stepMetadata exec.StepMetadata, workerMetadata dbng.ContainerMetadata, delegate exec.GetDelegate) exec.StepFactory {
 	fake.getMutex.Lock()
 	ret, specificReturn := fake.getReturnsOnCall[len(fake.getArgsForCall)]
 	fake.getArgsForCall = append(fake.getArgsForCall, struct {
-		arg1  lager.Logger
-		arg2  int
-		arg3  int
-		arg4  atc.PlanID
-		arg5  exec.StepMetadata
-		arg6  worker.ArtifactName
-		arg7  dbng.ContainerMetadata
-		arg8  exec.GetDelegate
-		arg9  atc.ResourceConfig
-		arg10 atc.Tags
-		arg11 atc.Params
-		arg12 atc.Version
-		arg13 atc.VersionedResourceTypes
-	}{arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13})
-	fake.recordInvocation("Get", []interface{}{arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13})
+		logger         lager.Logger
+		teamID         int
+		buildID        int
+		plan           atc.Plan
+		stepMetadata   exec.StepMetadata
+		workerMetadata dbng.ContainerMetadata
+		delegate       exec.GetDelegate
+	}{logger, teamID, buildID, plan, stepMetadata, workerMetadata, delegate})
+	fake.recordInvocation("Get", []interface{}{logger, teamID, buildID, plan, stepMetadata, workerMetadata, delegate})
 	fake.getMutex.Unlock()
 	if fake.GetStub != nil {
-		return fake.GetStub(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13)
+		return fake.GetStub(logger, teamID, buildID, plan, stepMetadata, workerMetadata, delegate)
 	}
 	if specificReturn {
 		return ret.result1
@@ -143,10 +131,10 @@ func (fake *FakeFactory) GetCallCount() int {
 	return len(fake.getArgsForCall)
 }
 
-func (fake *FakeFactory) GetArgsForCall(i int) (lager.Logger, int, int, atc.PlanID, exec.StepMetadata, worker.ArtifactName, dbng.ContainerMetadata, exec.GetDelegate, atc.ResourceConfig, atc.Tags, atc.Params, atc.Version, atc.VersionedResourceTypes) {
+func (fake *FakeFactory) GetArgsForCall(i int) (lager.Logger, int, int, atc.Plan, exec.StepMetadata, dbng.ContainerMetadata, exec.GetDelegate) {
 	fake.getMutex.RLock()
 	defer fake.getMutex.RUnlock()
-	return fake.getArgsForCall[i].arg1, fake.getArgsForCall[i].arg2, fake.getArgsForCall[i].arg3, fake.getArgsForCall[i].arg4, fake.getArgsForCall[i].arg5, fake.getArgsForCall[i].arg6, fake.getArgsForCall[i].arg7, fake.getArgsForCall[i].arg8, fake.getArgsForCall[i].arg9, fake.getArgsForCall[i].arg10, fake.getArgsForCall[i].arg11, fake.getArgsForCall[i].arg12, fake.getArgsForCall[i].arg13
+	return fake.getArgsForCall[i].logger, fake.getArgsForCall[i].teamID, fake.getArgsForCall[i].buildID, fake.getArgsForCall[i].plan, fake.getArgsForCall[i].stepMetadata, fake.getArgsForCall[i].workerMetadata, fake.getArgsForCall[i].delegate
 }
 
 func (fake *FakeFactory) GetReturns(result1 exec.StepFactory) {

exec/garden_factory.go
@@ -72,43 +72,46 @@ func (factory *gardenFactory) Get(
 	logger lager.Logger,
 	teamID int,
 	buildID int,
-	planID atc.PlanID,
+	plan atc.Plan,
 	stepMetadata StepMetadata,
-	sourceName worker.ArtifactName,
 	workerMetadata dbng.ContainerMetadata,
 	delegate GetDelegate,
-	resourceConfig atc.ResourceConfig,
-	tags atc.Tags,
-	params atc.Params,
-	version atc.Version,
-	resourceTypes atc.VersionedResourceTypes,
 ) StepFactory {
 	workerMetadata.WorkingDirectory = resource.ResourcesDir("get")
-	return newGetStep(
-		logger,
-		sourceName,
-		resourceConfig,
-		version,
-		params,
-		resource.NewResourceInstance(
-			resource.ResourceType(resourceConfig.Type),
-			version,
-			resourceConfig.Source,
-			params,
-			dbng.ForBuild(buildID),
-			resourceTypes,
-			factory.dbResourceCacheFactory,
-		),
-		stepMetadata,
-		resource.Session{
-			Metadata: workerMetadata,
+
+	actions := []Action{
+		&GetAction{
+			Type:     plan.Get.Type,
+			Name:     plan.Get.Name,
+			Resource: plan.Get.Resource,
+			Source:   plan.Get.Source,
+			Params:   plan.Get.Params,
+			Version:  plan.Get.Version,
+			Tags:     plan.Get.Tags,
+			Outputs:  []string{plan.Get.Name},
+
+			// TODO: can we remove these dependencies?
+			delegate:          delegate,
+			resourceFetcher:   factory.resourceFetcher,
+			teamID:            teamID,
+			containerMetadata: workerMetadata,
+			resourceInstance: resource.NewResourceInstance(
+				resource.ResourceType(plan.Get.Type),
+				plan.Get.Version,
+				plan.Get.Source,
+				plan.Get.Params,
+				dbng.ForBuild(buildID),
+				plan.Get.VersionedResourceTypes,
+				factory.dbResourceCacheFactory,
+			),
+			stepMetadata: stepMetadata,
+
+			// TODO: remove after all actions are introduced
+			resourceTypes: plan.Get.VersionedResourceTypes,
 		},
-		tags,
-		teamID,
-		delegate,
-		factory.resourceFetcher,
-		resourceTypes,
-	)
+	}
+
+	return newActionsStep(actions, logger)
 }
 
 func (factory *gardenFactory) Put(

concourse/concourse

exec/execfakes/fake_factory.go
@@ -13,22 +13,16 @@ import (
 )
 
 type FakeFactory struct {
-	GetStub        func(lager.Logger, int, int, atc.PlanID, exec.StepMetadata, worker.ArtifactName, db.ContainerMetadata, exec.GetDelegate, atc.ResourceConfig, atc.Tags, atc.Params, atc.Version, atc.VersionedResourceTypes) exec.StepFactory
+	GetStub        func(lager.Logger, int, int, atc.Plan, exec.StepMetadata, db.ContainerMetadata, exec.GetDelegate) exec.StepFactory
 	getMutex       sync.RWMutex
 	getArgsForCall []struct {
-		arg1  lager.Logger
-		arg2  int
-		arg3  int
-		arg4  atc.PlanID
-		arg5  exec.StepMetadata
-		arg6  worker.ArtifactName
-		arg7  db.ContainerMetadata
-		arg8  exec.GetDelegate
-		arg9  atc.ResourceConfig
-		arg10 atc.Tags
-		arg11 atc.Params
-		arg12 atc.Version
-		arg13 atc.VersionedResourceTypes
+		arg1 lager.Logger
+		arg2 int
+		arg3 int
+		arg4 atc.Plan
+		arg5 exec.StepMetadata
+		arg6 db.ContainerMetadata
+		arg7 exec.GetDelegate
 	}
 	getReturns struct {
 		result1 exec.StepFactory
@@ -108,28 +102,22 @@ type FakeFactory struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeFactory) Get(arg1 lager.Logger, arg2 int, arg3 int, arg4 atc.PlanID, arg5 exec.StepMetadata, arg6 worker.ArtifactName, arg7 db.ContainerMetadata, arg8 exec.GetDelegate, arg9 atc.ResourceConfig, arg10 atc.Tags, arg11 atc.Params, arg12 atc.Version, arg13 atc.VersionedResourceTypes) exec.StepFactory {
+func (fake *FakeFactory) Get(arg1 lager.Logger, arg2 int, arg3 int, arg4 atc.Plan, arg5 exec.StepMetadata, arg6 db.ContainerMetadata, arg7 exec.GetDelegate) exec.StepFactory {
 	fake.getMutex.Lock()
 	ret, specificReturn := fake.getReturnsOnCall[len(fake.getArgsForCall)]
 	fake.getArgsForCall = append(fake.getArgsForCall, struct {
-		arg1  lager.Logger
-		arg2  int
-		arg3  int
-		arg4  atc.PlanID
-		arg5  exec.StepMetadata
-		arg6  worker.ArtifactName
-		arg7  db.ContainerMetadata
-		arg8  exec.GetDelegate
-		arg9  atc.ResourceConfig
-		arg10 atc.Tags
-		arg11 atc.Params
-		arg12 atc.Version
-		arg13 atc.VersionedResourceTypes
-	}{arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13})
-	fake.recordInvocation("Get", []interface{}{arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13})
+		arg1 lager.Logger
+		arg2 int
+		arg3 int
+		arg4 atc.Plan
+		arg5 exec.StepMetadata
+		arg6 db.ContainerMetadata
+		arg7 exec.GetDelegate
+	}{arg1, arg2, arg3, arg4, arg5, arg6, arg7})
+	fake.recordInvocation("Get", []interface{}{arg1, arg2, arg3, arg4, arg5, arg6, arg7})
 	fake.getMutex.Unlock()
 	if fake.GetStub != nil {
-		return fake.GetStub(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13)
+		return fake.GetStub(arg1, arg2, arg3, arg4, arg5, arg6, arg7)
 	}
 	if specificReturn {
 		return ret.result1
@@ -143,10 +131,10 @@ func (fake *FakeFactory) GetCallCount() int {
 	return len(fake.getArgsForCall)
 }
 
-func (fake *FakeFactory) GetArgsForCall(i int) (lager.Logger, int, int, atc.PlanID, exec.StepMetadata, worker.ArtifactName, db.ContainerMetadata, exec.GetDelegate, atc.ResourceConfig, atc.Tags, atc.Params, atc.Version, atc.VersionedResourceTypes) {
+func (fake *FakeFactory) GetArgsForCall(i int) (lager.Logger, int, int, atc.Plan, exec.StepMetadata, db.ContainerMetadata, exec.GetDelegate) {
 	fake.getMutex.RLock()
 	defer fake.getMutex.RUnlock()
-	return fake.getArgsForCall[i].arg1, fake.getArgsForCall[i].arg2, fake.getArgsForCall[i].arg3, fake.getArgsForCall[i].arg4, fake.getArgsForCall[i].arg5, fake.getArgsForCall[i].arg6, fake.getArgsForCall[i].arg7, fake.getArgsForCall[i].arg8, fake.getArgsForCall[i].arg9, fake.getArgsForCall[i].arg10, fake.getArgsForCall[i].arg11, fake.getArgsForCall[i].arg12, fake.getArgsForCall[i].arg13
+	return fake.getArgsForCall[i].arg1, fake.getArgsForCall[i].arg2, fake.getArgsForCall[i].arg3, fake.getArgsForCall[i].arg4, fake.getArgsForCall[i].arg5, fake.getArgsForCall[i].arg6, fake.getArgsForCall[i].arg7
 }
 
 func (fake *FakeFactory) GetReturns(result1 exec.StepFactory) {

exec/garden_factory.go
@@ -73,44 +73,47 @@ func (factory *gardenFactory) Get(
 	logger lager.Logger,
 	teamID int,
 	buildID int,
-	planID atc.PlanID,
+	plan atc.Plan,
 	stepMetadata StepMetadata,
-	sourceName worker.ArtifactName,
 	workerMetadata db.ContainerMetadata,
 	delegate GetDelegate,
-	resourceConfig atc.ResourceConfig,
-	tags atc.Tags,
-	params atc.Params,
-	version atc.Version,
-	resourceTypes atc.VersionedResourceTypes,
 ) StepFactory {
 	workerMetadata.WorkingDirectory = resource.ResourcesDir("get")
-	return newGetStep(
-		logger,
-		sourceName,
-		resourceConfig,
-		version,
-		params,
-		resource.NewResourceInstance(
-			resource.ResourceType(resourceConfig.Type),
-			version,
-			resourceConfig.Source,
-			params,
-			db.ForBuild(buildID),
-			db.NewBuildStepContainerOwner(buildID, planID),
-			resourceTypes,
-			factory.dbResourceCacheFactory,
-		),
-		stepMetadata,
-		resource.Session{
-			Metadata: workerMetadata,
+
+	actions := []Action{
+		&GetAction{
+			Type:     plan.Get.Type,
+			Name:     plan.Get.Name,
+			Resource: plan.Get.Resource,
+			Source:   plan.Get.Source,
+			Params:   plan.Get.Params,
+			Version:  plan.Get.Version,
+			Tags:     plan.Get.Tags,
+			Outputs:  []string{plan.Get.Name},
+
+			// TODO: can we remove these dependencies?
+			delegate:          delegate,
+			resourceFetcher:   factory.resourceFetcher,
+			teamID:            teamID,
+			containerMetadata: workerMetadata,
+			resourceInstance: resource.NewResourceInstance(
+				resource.ResourceType(plan.Get.Type),
+				plan.Get.Version,
+				plan.Get.Source,
+				plan.Get.Params,
+				db.ForBuild(buildID),
+				db.NewBuildStepContainerOwner(buildID, planID),
+				plan.Get.VersionedResourceTypes,
+				factory.dbResourceCacheFactory,
+			),
+			stepMetadata: stepMetadata,
+
+			// TODO: remove after all actions are introduced
+			resourceTypes: plan.Get.VersionedResourceTypes,
 		},
-		tags,
-		teamID,
-		delegate,
-		factory.resourceFetcher,
-		resourceTypes,
-	)
+	}
+
+	return newActionsStep(actions, logger)
 }
 
 func (factory *gardenFactory) Put(