a00dcd2c7ff0f4c3fcbb5100cf77371ef93e88c8
Alex Suraci <asuraci@pivotal.io>
2017-06-16 09:43:11 -0400

    clear duplicates before creating unique index

--- db/migrations/161_add_unique_worker_resource_cache_id_to_volumes.go
+++ db/migrations/161_add_unique_worker_resource_cache_id_to_volumes.go
@@ -4,7 +4,22 @@ import "github.com/concourse/atc/db/migration"
 
 func AddUniqueWorkerResourceCacheIDToVolumes(tx migration.LimitedTx) error {
 	_, err := tx.Exec(`
-		CREATE UNIQUE INDEX volumes_worker_resource_cache_unique ON volumes (worker_resource_cache_id)
+		WITH distinct_vols AS (
+			SELECT DISTINCT ON (worker_resource_cache_id) id
+			FROM volumes
+			WHERE worker_resource_cache_id IS NOT NULL
+		)
+		DELETE FROM volumes
+		WHERE worker_resource_cache_id IS NOT NULL
+		AND id NOT IN (SELECT id FROM distinct_vols)
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(`
+		CREATE UNIQUE INDEX volumes_worker_resource_cache_unique
+		ON volumes (worker_resource_cache_id)
 	`)
 	if err != nil {
 		return err
