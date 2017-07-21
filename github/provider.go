a8bbf3357656bc0d3b11816f755f10017ee996b2
Brian Keyser <brian.keyser@digitalglobe.com>
2017-07-21 16:41:16 -0600

    Utilize a proxy when configured for OAuth exchange

--- github/provider.go
+++ github/provider.go
@@ -163,6 +163,7 @@ func (GitHubTeamProvider) ProviderConstructor(
 func (GitHubProvider) PreTokenClient() (*http.Client, error) {
 	return &http.Client{
 		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
 			DisableKeepAlives: true,
 		},
 	}, nil
