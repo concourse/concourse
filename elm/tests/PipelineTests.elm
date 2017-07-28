35ee1ce60e869f67b00370f97ed24159b61b2038
JT Archie <jtarchie@gmail.com>
2017-07-28 17:41:20 -0400

    Backfill Pipeline elm tests
    concourse/concourse#1389
    concourse/concourse#1388
    
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

---
+++ elm/tests/PipelineTests.elm
@@ -0,0 +1,77 @@
+module PipelineTests exposing (..)
+
+import Pipeline exposing (update, Msg(..))
+import Test exposing (..)
+import Expect exposing (..)
+import Time exposing (Time)
+import RemoteData exposing (WebData)
+import Keyboard
+import Char
+
+
+all : Test
+all =
+    describe "Pipeline"
+        [ describe "update" <|
+            let
+                resetFocus =
+                    (\_ -> Cmd.map (\_ -> Noop) Cmd.none)
+
+                defaultModel : Pipeline.Model
+                defaultModel =
+                    { ports = { render = (\( _, _ ) -> Cmd.none), title = (\_ -> Cmd.none), resetFocus = resetFocus }
+                    , pipelineLocator = { teamName = "some-team", pipelineName = "some-pipeline" }
+                    , pipeline = RemoteData.NotAsked
+                    , fetchedJobs = Nothing
+                    , fetchedResources = Nothing
+                    , renderedJobs = Nothing
+                    , renderedResources = Nothing
+                    , concourseVersion = "some-version"
+                    , turbulenceImgSrc = "some-turbulence-img-src"
+                    , experiencingTurbulence = False
+                    , selectedGroups = []
+                    , hideLegend = False
+                    , hideLegendCounter = 0
+                    }
+            in
+                [ test "HideLegendTimerTicked" <|
+                    \_ ->
+                        Expect.equal
+                            (1 * Time.second)
+                        <|
+                            .hideLegendCounter <|
+                                Tuple.first <|
+                                    update (HideLegendTimerTicked 0) defaultModel
+                , test "HideLegendTimeTicked reaches timeout" <|
+                    \_ ->
+                        Expect.equal
+                            True
+                        <|
+                            .hideLegend <|
+                                Tuple.first <|
+                                    update (HideLegendTimerTicked 0) { defaultModel | hideLegendCounter = 10 * Time.second }
+                , test "ShowLegend" <|
+                    \_ ->
+                        let
+                            updatedModel =
+                                Tuple.first <|
+                                    update ShowLegend { defaultModel | hideLegend = True, hideLegendCounter = 3 * Time.second }
+                        in
+                            Expect.equal
+                                ( False, 0 )
+                            <|
+                                ( .hideLegend updatedModel, .hideLegendCounter updatedModel )
+                , test "KeyPressed" <|
+                    \_ ->
+                        Expect.equal
+                            ( defaultModel, Cmd.none )
+                        <|
+                            update (KeyPressed (Char.toCode 'a')) defaultModel
+                , test "KeyPressed f" <|
+                    \_ ->
+                        Expect.notEqual
+                            ( defaultModel, Cmd.none )
+                        <|
+                            update (KeyPressed (Char.toCode 'f')) defaultModel
+                ]
+        ]
