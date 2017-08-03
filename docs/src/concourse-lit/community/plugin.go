a8baefa32fd4607e183ebb749397f36a80c35996
Alex Suraci <suraci.alex@gmail.com>
2017-08-03 15:03:51 -0400

    use styled content with partials
    moving away from booklit.Block and booklit.Element in favor of templates

--- docs/src/concourse-lit/community/plugin.go
+++ docs/src/concourse-lit/community/plugin.go
@@ -17,68 +17,47 @@ func NewPlugin(section *booklit.Section) booklit.Plugin {
 }
 
 func (p Plugin) CommunityImage(path string) booklit.Content {
-	return booklit.Block{
-		Class: "community-image",
-		Content: booklit.Image{
-			Path: path,
+	return booklit.Styled{
+		Style: "community-image",
+
+		Content: booklit.Paragraph{},
+
+		Partials: booklit.Partials{
+			"Image": booklit.Image{
+				Path: path,
+			},
 		},
 	}
 }
 
 func (p Plugin) Talk(title, author booklit.Content, thumb string, videoURL string, slidesURL string, description booklit.Content) booklit.Content {
-	var slidesLink booklit.Content = booklit.Empty
+	var slidesLink booklit.Content
 	if slidesURL != "" {
-		slidesLink = booklit.Element{
-			Class: "talk-slides-link",
-			Content: booklit.Link{
-				Target:  slidesURL,
-				Content: booklit.String("slides"),
-			},
+		slidesLink = booklit.Link{
+			Target:  slidesURL,
+			Content: booklit.String("slides"),
 		}
 	}
 
-	return booklit.Block{
-		Class: "talk",
-		Content: booklit.Sequence{
-			booklit.Block{
-				Class: "talk-info",
-				Content: booklit.Sequence{
-					booklit.Block{
-						Class: "h2 talk-header",
-						Content: booklit.Link{
-							Target:  videoURL,
-							Content: title,
-						},
-					},
-					booklit.Block{
-						Class: "talk-author",
-						Content: booklit.Paragraph{
-							booklit.String("by "),
-							author,
-						},
-					},
-					booklit.Block{
-						Class:   "talk-description",
-						Content: description,
-					},
+	return booklit.Styled{
+		Style: "talk",
+
+		Content: description,
+
+		Partials: booklit.Partials{
+			"Link": booklit.Link{
+				Target:  videoURL,
+				Content: title,
+			},
+			"Thumb": booklit.Link{
+				Target: videoURL,
+				Content: booklit.Image{
+					Path:        thumb,
+					Description: "talk thumbnail",
 				},
 			},
-			booklit.Block{
-				Class: "talk-links",
-				Content: booklit.Block{
-					Class: "talk-thumbnail",
-					Content: booklit.Sequence{
-						booklit.Link{
-							Target: videoURL,
-							Content: booklit.Image{
-								Path:        thumb,
-								Description: "talk thumbnail",
-							},
-						},
-						slidesLink,
-					},
-				},
-			},
+			"Author":     author,
+			"SlidesLink": slidesLink,
 		},
 	}
 }
