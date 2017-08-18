309754a23004e02158918725f7a076ab3036519f
Mark Huang <mhuang@pivotal.io>
2017-08-18 10:35:11 -0400

    switch from poltergeist to headless-chrome
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- web/acceptance/spec/spec_helper.rb
+++ web/acceptance/spec/spec_helper.rb
@@ -1,5 +1,19 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require "selenium/webdriver"
 
-Capybara.default_driver = :poltergeist
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu no-sandbox) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.default_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
