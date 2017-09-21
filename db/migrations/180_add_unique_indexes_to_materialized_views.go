7d7affad600e1678cd768008b6af96b3de1e9e61
Mark Huang <mhuang@pivotal.io>
2017-09-21 11:31:53 -0400

    fix migration 180
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- db/migrations/180_add_unique_indexes_to_materialized_views.go
+++ db/migrations/180_add_unique_indexes_to_materialized_views.go
@@ -11,14 +11,14 @@ func AddUniqueIndexesToMaterializedViews(tx migration.LimitedTx) error {
 	}
 
 	_, err = tx.Exec(`
-		CREATE UNIQUE INDEX next_builds_per_job _id ON next_builds_per_job (id)
+		CREATE UNIQUE INDEX next_builds_per_job_id ON next_builds_per_job (id)
 	`)
 	if err != nil {
 		return err
 	}
 
 	_, err = tx.Exec(`
-		CREATE UNIQUE INDEX transition_builds_per_job _id ON transition_builds_per_job (id)
+		CREATE UNIQUE INDEX transition_builds_per_job_id ON transition_builds_per_job (id)
 	`)
 	if err != nil {
 		return err
