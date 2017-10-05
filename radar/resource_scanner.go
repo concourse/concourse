b5b3545f5d60fdfb3ec2386cb4fb5f407798f547
Tanzeeb Khalili <tkhalili@pivotal.io>
2017-10-05 17:37:09 -0400

    unify resource scanner's Run, ScanFromVersion, and Scan methods
    Signed-off-by: Joris Melchior <jmelchior@pivotal.io>

--- radar/resource_scanner.go
+++ radar/resource_scanner.go
@@ -47,6 +47,28 @@ func NewResourceScanner(
 var ErrFailedToAcquireLock = errors.New("failed-to-acquire-lock")
 
 func (scanner *resourceScanner) Run(logger lager.Logger, resourceName string) (time.Duration, error) {
+	interval, err := scanner.scan(logger.Session("tick"), resourceName, nil, false)
+
+	err = swallowErrResourceScriptFailed(err)
+
+	return interval, err
+}
+
+func (scanner *resourceScanner) ScanFromVersion(logger lager.Logger, resourceName string, fromVersion atc.Version) error {
+	_, err := scanner.scan(logger, resourceName, fromVersion, true)
+
+	return err
+}
+
+func (scanner *resourceScanner) Scan(logger lager.Logger, resourceName string) error {
+	_, err := scanner.scan(logger, resourceName, nil, true)
+
+	err = swallowErrResourceScriptFailed(err)
+
+	return err
+}
+
+func (scanner *resourceScanner) scan(logger lager.Logger, resourceName string, fromVersion atc.Version, mustComplete bool) (time.Duration, error) {
 	lockLogger := logger.Session("lock", lager.Data{
 		"resource": resourceName,
 	})
@@ -106,126 +128,29 @@ func (scanner *resourceScanner) Run(logger lager.Logger, resourceName string) (t
 		return 0, err
 	}
 
-	lock, acquired, err := scanner.dbPipeline.AcquireResourceCheckingLockWithIntervalCheck(
-		logger,
-		savedResource.Name(),
-		resourceConfigCheckSession.ResourceConfig(),
-		interval,
-		false,
-	)
-	if err != nil {
-		lockLogger.Error("failed-to-get-lock", err, lager.Data{
-			"resource": resourceName,
-		})
-		return interval, ErrFailedToAcquireLock
-	}
-
-	if !acquired {
-		lockLogger.Debug("did-not-get-lock")
-		return interval, ErrFailedToAcquireLock
-	}
-
-	defer lock.Release()
-
-	vr, _, err := scanner.dbPipeline.GetLatestVersionedResource(resourceName)
-	if err != nil {
-		logger.Error("failed-to-get-current-version", err)
-		return interval, err
-	}
-	fromVersion := atc.Version(vr.Version)
-
-	err = scanner.scan(
-		logger.Session("tick"),
-		savedResource,
-		resourceConfigCheckSession,
-		fromVersion,
-		versionedResourceTypes,
-		source,
-	)
-	err = swallowErrResourceScriptFailed(err)
-	return interval, err
-}
-
-func (scanner *resourceScanner) ScanFromVersion(logger lager.Logger, resourceName string, fromVersion atc.Version) error {
-	lockLogger := logger.Session("lock", lager.Data{
-		"resource": resourceName,
-	})
-
-	savedResource, found, err := scanner.dbPipeline.Resource(resourceName)
-	if err != nil {
-		return err
-	}
-
-	if !found {
-		logger.Debug("resource-not-found")
-		return db.ResourceNotFoundError{Name: resourceName}
-	}
-
-	interval, err := scanner.checkInterval(savedResource.CheckEvery())
-	if err != nil {
-		scanner.setResourceCheckError(logger, savedResource, err)
-
-		return err
-	}
-
-	resourceTypes, err := scanner.dbPipeline.ResourceTypes()
-	if err != nil {
-		logger.Error("failed-to-get-resource-types", err)
-		return err
-	}
-
-	versionedResourceTypes := creds.NewVersionedResourceTypes(
-		scanner.variables,
-		resourceTypes.Deserialize(),
-	)
-
-	source, err := creds.NewSource(scanner.variables, savedResource.Source()).Evaluate()
-	if err != nil {
-		logger.Error("failed-to-evaluate-resource-source", err)
-		scanner.setResourceCheckError(logger, savedResource, err)
-		return err
-	}
-
-	resourceConfigCheckSession, err := scanner.resourceConfigCheckSessionFactory.FindOrCreateResourceConfigCheckSession(
-		logger,
-		savedResource.Type(),
-		source,
-		versionedResourceTypes,
-		ContainerExpiries,
-	)
-	if err != nil {
-		logger.Error("failed-to-find-or-create-resource-config-check-session", err)
-		scanner.setResourceCheckError(logger, savedResource, err)
-		return err
-	}
-
-	err = savedResource.SetResourceConfig(resourceConfigCheckSession.ResourceConfig().ID)
-	if err != nil {
-		logger.Error("failed-to-set-resource-config-id-on-resource", err)
-		scanner.setResourceCheckError(logger, savedResource, err)
-		return err
-	}
-
-	for {
+	for breaker := true; breaker == true; breaker = mustComplete {
 		lock, acquired, err := scanner.dbPipeline.AcquireResourceCheckingLockWithIntervalCheck(
 			logger,
 			savedResource.Name(),
 			resourceConfigCheckSession.ResourceConfig(),
 			interval,
-			true,
+			mustComplete,
 		)
 		if err != nil {
 			lockLogger.Error("failed-to-get-lock", err, lager.Data{
 				"resource": resourceName,
 			})
-
-			return err
+			return interval, ErrFailedToAcquireLock
 		}
 
 		if !acquired {
 			lockLogger.Debug("did-not-get-lock")
-			scanner.clock.Sleep(time.Second)
-			continue
+			if mustComplete {
+				scanner.clock.Sleep(time.Second)
+				continue
+			} else {
+				return interval, ErrFailedToAcquireLock
+			}
 		}
 
 		defer lock.Release()
@@ -233,7 +158,16 @@ func (scanner *resourceScanner) ScanFromVersion(logger lager.Logger, resourceNam
 		break
 	}
 
-	err = scanner.scan(
+	if fromVersion == nil {
+		vr, _, err := scanner.dbPipeline.GetLatestVersionedResource(resourceName)
+		if err != nil {
+			logger.Error("failed-to-get-current-version", err)
+			return interval, err
+		}
+		fromVersion = atc.Version(vr.Version)
+	}
+
+	return interval, scanner.check(
 		logger,
 		savedResource,
 		resourceConfigCheckSession,
@@ -241,22 +175,9 @@ func (scanner *resourceScanner) ScanFromVersion(logger lager.Logger, resourceNam
 		versionedResourceTypes,
 		source,
 	)
-	return err
 }
 
-func (scanner *resourceScanner) Scan(logger lager.Logger, resourceName string) error {
-	vr, _, err := scanner.dbPipeline.GetLatestVersionedResource(resourceName)
-	if err != nil {
-		logger.Error("failed-to-get-current-version", err)
-		return err
-	}
-
-	return swallowErrResourceScriptFailed(
-		scanner.ScanFromVersion(logger, resourceName, atc.Version(vr.Version)),
-	)
-}
-
-func (scanner *resourceScanner) scan(
+func (scanner *resourceScanner) check(
 	logger lager.Logger,
 	savedResource db.Resource,
 	resourceConfigCheckSession db.ResourceConfigCheckSession,
