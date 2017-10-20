a77acad828c3c93c4ad0002a04e63175fd0c3cec
Alex Suraci <suraci.alex@gmail.com>
2017-10-20 16:40:18 -0400

    add test for build gg shortcut

--- wats/dom_helper.js
+++ wats/dom_helper.js
@@ -10,6 +10,16 @@ class Dom extends Helper {
       }, el, attr);
     });
   }
+
+  grabScrollPosition() {
+    let browser = this.helpers['Nightmare'].browser;
+
+    return browser.findElement({ css: 'body' }).then((el) => {
+      return browser.evaluate(() => {
+        return window.scrollY;
+      });
+    });
+  }
 }
 
 module.exports = Dom;
