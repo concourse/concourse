aaed7c88b436c7d9fe2ab87f3a65a5c852254ebd
Alex Suraci <suraci.alex@gmail.com>
2017-11-07 13:44:49 -0500

    squash all migrations into one file
    we have too many files now and it's making the build fail on windows
    
    this is a mess in general; see #1793 for the long-term path forward

--- db/migrations/29_add_paused_to_jobs.go
+++
@@ -1,9 +0,0 @@
-package migrations
-
-import "github.com/concourse/atc/db/migration"
-
-func AddPausedToJobs(tx migration.LimitedTx) error {
-	_, err := tx.Exec(`ALTER TABLE jobs ADD COLUMN paused boolean DEFAULT(false)`)
-
-	return err
-}
