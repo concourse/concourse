4011d238aa618d00489830791b5a10cc7e3c51b8
Josh Ghiloni <jghiloni@pivotal.io>
2017-11-10 14:28:29 -0700

    Add tests for resource types with params

--- pipelines/resource_type_test.go
+++ pipelines/resource_type_test.go
@@ -124,6 +124,107 @@ var _ = Describe("Configuring a resource type in a pipeline config", func() {
 		})
 	})
 
+	Context("with custom resource types that have params", func() {
+		BeforeEach(func() {
+			flyHelper.ConfigurePipeline(
+				pipelineName,
+				"-c", "fixtures/resource-types-with-params.yml",
+				"-v", "origin-git-server="+originGitServer.URI(),
+				"-y", "privileged=true",
+			)
+		})
+
+		It("can use custom resource types for 'get', 'put', and task 'image_resource's", func() {
+			watch := flyHelper.Watch(pipelineName, "resource-getter")
+			<-watch.Exited
+			Expect(watch.ExitCode()).To(Equal(0))
+
+			watch = flyHelper.Watch(pipelineName, "resource-putter")
+			Expect(watch).To(gbytes.Say("pushing using custom resource"))
+			Expect(watch).To(gbytes.Say("some-output/some-file"))
+
+			watch = flyHelper.Watch(pipelineName, "resource-imgur")
+			Expect(watch).To(gbytes.Say("fetched from custom resource"))
+			Expect(watch).To(gbytes.Say("SOME_ENV=yep"))
+		})
+
+		It("should be able to run privileged operations in 'check', 'get' and 'put' steps", func() {
+			watch := flyHelper.TriggerJob(pipelineName, "failing-task")
+			<-watch.Exited
+			// time.Sleep(time.Hour)
+			By("hijacking into get container for my-resource")
+			hijackS, stdin := flyHelper.HijackInteractive(
+				"-j", pipelineName+"/failing-task",
+				"-s", "my-resource",
+				"--", "sh", "-c",
+				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp && exit $?")
+			Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: get"))
+			fmt.Fprintln(stdin, "1")
+			Eventually(hijackS).Should(gexec.Exit(0))
+
+			By("hijacking into put container for my-resource")
+			hijackS, stdin = flyHelper.HijackInteractive(
+				"-j", pipelineName+"/failing-task",
+				"-s", "my-resource",
+				"--", "sh", "-c",
+				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
+			Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: put"))
+			fmt.Fprintln(stdin, "3")
+			fmt.Fprintln(stdin, "2")
+			Eventually(hijackS).Should(gexec.Exit(0))
+
+			By("hijacking into check container for my-resource")
+			hijackS = flyHelper.Hijack(
+				"-c", pipelineName+"/my-resource",
+				"--", "sh", "-c",
+				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
+			Eventually(hijackS).Should(gexec.Exit(0))
+		})
+
+		Context("when the custom resource type is not privileged", func() {
+			BeforeEach(func() {
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/resource-types.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+					"-y", "privileged=false",
+				)
+			})
+
+			It("should fail running privileged operations in containers", func() {
+				watch := flyHelper.TriggerJob(pipelineName, "failing-task")
+				<-watch.Exited
+				By("hijacking into get container for my-resource")
+				hijackS, stdin := flyHelper.HijackInteractive(
+					"-j", pipelineName+"/failing-task",
+					"-s", "my-resource",
+					"--", "sh", "-c",
+					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
+				Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: get"))
+				fmt.Fprintln(stdin, "1")
+				Eventually(hijackS).Should(gexec.Exit(1))
+
+				By("hijacking into put container for my-resource")
+				hijackS, stdin = flyHelper.HijackInteractive(
+					"-j", pipelineName+"/failing-task",
+					"-s", "my-resource",
+					"--", "sh", "-c",
+					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
+				Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: put"))
+				fmt.Fprintln(stdin, "3")
+				fmt.Fprintln(stdin, "2")
+				Eventually(hijackS).Should(gexec.Exit(1))
+
+				By("hijacking into check container for my-resource")
+				hijackS = flyHelper.Hijack(
+					"-c", pipelineName+"/my-resource",
+					"--", "sh", "-c",
+					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
+				Eventually(hijackS).Should(gexec.Exit(1))
+			})
+		})
+	})
+
 	Context("when resource type named as base resource type", func() {
 		BeforeEach(func() {
 			flyHelper.ConfigurePipeline(
