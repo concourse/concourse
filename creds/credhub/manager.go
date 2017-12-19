f39713e981fef86df95d0d2422d01f95c9e75760
Alex Suraci <suraci.alex@gmail.com>
2017-12-19 14:46:47 -0500

    credhub: add flags for mutual tls, fix ca cert
    fixes concourse/concourse#1873

--- creds/credhub/manager.go
+++ creds/credhub/manager.go
@@ -13,17 +13,30 @@ import (
 )
 
 type CredHubManager struct {
-	URL          string   `long:"url" description:"CredHub server address used to access secrets."`
-	PathPrefix   string   `long:"path-prefix" default:"/concourse" description:"Path under which to namespace credential lookup."`
-	CACerts      []string `long:"ca-cert" description:"Paths to PEM-encoded CA cert files to use to verify the CredHub server SSL cert."`
-	Insecure     bool     `long:"insecure-skip-verify" description:"Enable insecure SSL verification."`
-	ClientId     string   `long:"client-id" description:"Client ID for CredHub authorization."`
-	ClientSecret string   `long:"client-secret" description:"Client secret for CredHub authorization."`
-	caCerts      []string `no-flag:true`
+	URL string `long:"url" description:"CredHub server address used to access secrets."`
+
+	PathPrefix string `long:"path-prefix" default:"/concourse" description:"Path under which to namespace credential lookup."`
+
+	TLS struct {
+		CACerts    []string `long:"ca-cert"              description:"Paths to PEM-encoded CA cert files to use to verify the CredHub server SSL cert."`
+		ClientCert string   `long:"client-cert"          description:"Path to the client certificate for mutual TLS authorization."`
+		ClientKey  string   `long:"client-key"           description:"Path to the client private key for mutual TLS authorization."`
+		Insecure   bool     `long:"insecure-skip-verify" description:"Enable insecure SSL verification."`
+	}
+
+	UAA struct {
+		ClientId     string `long:"client-id"     description:"Client ID for CredHub authorization."`
+		ClientSecret string `long:"client-secret" description:"Client secret for CredHub authorization."`
+	}
 }
 
 func (manager CredHubManager) IsConfigured() bool {
-	return manager.URL != "" || manager.ClientId != "" || manager.ClientSecret != "" || len(manager.CACerts) != 0
+	return manager.URL != "" ||
+		manager.UAA.ClientId != "" ||
+		manager.UAA.ClientSecret != "" ||
+		len(manager.TLS.CACerts) != 0 ||
+		manager.TLS.ClientCert != "" ||
+		manager.TLS.ClientKey != ""
 }
 
 func (manager CredHubManager) Validate() error {
@@ -31,28 +44,11 @@ func (manager CredHubManager) Validate() error {
 	if err != nil {
 		return fmt.Errorf("invalid URL: %s", err)
 	}
+
 	// "foo" will parse without error (as a Path, with an empty Host)
 	// so we'll do a few additional sanity checks that this is a valid URL
 	if parsedUrl.Host == "" || !(parsedUrl.Scheme == "http" || parsedUrl.Scheme == "https") {
-		return fmt.Errorf("invalid URL")
-	}
-
-	if parsedUrl.Scheme == "https" {
-		if len(manager.CACerts) == 0 && !manager.Insecure {
-			return fmt.Errorf("CACerts or insecure needs to be set for secure urls")
-		}
-	}
-
-	if manager.ClientId == "" || manager.ClientSecret == "" {
-		return fmt.Errorf("--credhub-client-id and --credhub-client-secret must be set to use CredHub")
-	}
-
-	for _, cert := range manager.CACerts {
-		contents, err := ioutil.ReadFile(cert)
-		if err != nil {
-			return fmt.Errorf("Could not read CaCert at path %s", cert)
-		}
-		manager.caCerts = append(manager.caCerts, string(contents))
+		return fmt.Errorf("invalid URL (must be http or https)")
 	}
 
 	return nil
@@ -61,11 +57,34 @@ func (manager CredHubManager) Validate() error {
 func (manager CredHubManager) NewVariablesFactory(logger lager.Logger) (creds.VariablesFactory, error) {
 	var options []credhub.Option
 
-	if manager.Insecure {
+	if manager.TLS.Insecure {
 		options = append(options, credhub.SkipTLSValidation(true))
 	}
 
-	options = append(options, credhub.Auth(auth.UaaClientCredentials(manager.ClientId, manager.ClientSecret)))
+	caCerts := []string{}
+	for _, cert := range manager.TLS.CACerts {
+		contents, err := ioutil.ReadFile(cert)
+		if err != nil {
+			return nil, err
+		}
+
+		caCerts = append(caCerts, string(contents))
+	}
+
+	if len(caCerts) > 0 {
+		options = append(options, credhub.CaCerts(caCerts...))
+	}
+
+	if manager.UAA.ClientId != "" && manager.UAA.ClientSecret != "" {
+		options = append(options, credhub.Auth(auth.UaaClientCredentials(
+			manager.UAA.ClientId,
+			manager.UAA.ClientSecret,
+		)))
+	}
+
+	if manager.TLS.ClientCert != "" && manager.TLS.ClientKey != "" {
+		options = append(options, credhub.ClientCert(manager.TLS.ClientCert, manager.TLS.ClientKey))
+	}
 
 	ch, err := credhub.New(manager.URL, options...)
 	if err != nil {
