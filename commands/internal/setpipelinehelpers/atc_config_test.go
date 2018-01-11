584474f3db3ee84471aed8aebbcd82d86d26de2b
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-01-11 13:31:03 +1100

    Use yaml.UnmarshalStrict during strict validation

--- commands/internal/setpipelinehelpers/atc_config_test.go
+++ commands/internal/setpipelinehelpers/atc_config_test.go
@@ -1,6 +1,11 @@
 package setpipelinehelpers_test
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/concourse/atc"
 	. "github.com/concourse/fly/commands/internal/setpipelinehelpers"
 
 	. "github.com/onsi/ginkgo"
@@ -20,6 +25,104 @@ var _ = Describe("ATC Config", func() {
 			It("returns true", func() {
 				Expect(atcConfig.ApplyConfigInteraction()).To(BeTrue())
 			})
+		})
+	})
+
+	Describe("validating", func() {
+		var atcConfig ATCConfig
+		var tmpdir string
+
+		BeforeEach(func() {
+			atcConfig = ATCConfig{}
+
+			var err error
+
+			tmpdir, err = ioutil.TempDir("", "fly-test")
+			Expect(err).NotTo(HaveOccurred())
+
+			err = ioutil.WriteFile(
+				filepath.Join(tmpdir, "good-pipeline.yml"),
+				[]byte(`---
+resource_types:
+- name: foo
+  type: docker-image
+  source:
+    repository: foo/foo
+- name: bar
+  type: docker-image
+  source:
+    repository: bar/bar
+jobs:
+- name: hello-world
+  plan:
+  - task: say-hello
+    config:
+      platform: linux
+      image_resource:
+        type: docker-image
+        source: {repository: ubuntu}
+      run:
+        path: echo
+        args: ["Hello, world!"]
+`),
+				0644,
+			)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = ioutil.WriteFile(
+				filepath.Join(tmpdir, "dupkey-pipeline.yml"),
+				[]byte(`---
+resource_types:
+- name: foo
+  type: docker-image
+  source:
+    repository: foo/foo
+- name: bar
+  type: docker-image
+  source:
+    repository: bar/bar
+jobs:
+- name: hello-world
+  plan:
+  - task: say-hello
+    config:
+      platform: linux
+      image_resource:
+        type: docker-image
+        source: {repository: ubuntu}
+      run:
+        path: echo
+        args: ["Hello, world!"]
+resource_types:
+- name: baz
+  type: docker-image
+  source:
+    repository: baz/baz
+`),
+				0644,
+			)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.RemoveAll(tmpdir)
+		})
+
+		It("validates a good pipeline", func() {
+			err := atcConfig.Validate(atc.PathFlag(filepath.Join(tmpdir, "good-pipeline.yml")), nil, nil, nil, false)
+			Expect(err).To(BeNil())
+		})
+		It("validates a good pipeline with strict", func() {
+			err := atcConfig.Validate(atc.PathFlag(filepath.Join(tmpdir, "good-pipeline.yml")), nil, nil, nil, true)
+			Expect(err).To(BeNil())
+		})
+		It("do not fail validating a pipeline with repeated resource types (probably should but for compat doesn't)", func() {
+			err := atcConfig.Validate(atc.PathFlag(filepath.Join(tmpdir, "dupkey-pipeline.yml")), nil, nil, nil, false)
+			Expect(err).To(BeNil())
+		})
+		It("fail validating a pipeline with repeated resource types with strict", func() {
+			err := atcConfig.Validate(atc.PathFlag(filepath.Join(tmpdir, "dupkey-pipeline.yml")), nil, nil, nil, true)
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
