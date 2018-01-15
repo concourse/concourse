b6e98ab92b075fedc213b9a833b12afac6d08bb3
Alex Suraci <suraci.alex@gmail.com>
2018-01-15 11:12:35 -0500

    Revert "Merge pull request #239 from concourse/get_last_modified_time_query"
    This reverts commit 4661de955a01be729a08ee74d45c265de5bbb1d5, reversing
    changes made to 06d2e32b0784531f389d4beefb4d2c218171f4f3.

--- db/pipeline.go
+++ db/pipeline.go
@@ -1513,17 +1513,27 @@ func (p *pipeline) getLatestModifiedTime() (time.Time, error) {
 			WHEN bi_max > vr_max THEN bi_max
 			ELSE vr_max
 		END
-	FROM (
-		SELECT
-			COALESCE(MAX(bo.modified_time), 'epoch') as bo_max,
-			COALESCE(MAX(bi.modified_time), 'epoch') as bi_max,
-			COALESCE(MAX(vr.modified_time), 'epoch') as vr_max
-		FROM resources r
-		INNER JOIN versioned_resources vr ON r.id = vr.resource_id
-		LEFT OUTER JOIN build_inputs bi ON vr.id = bi.versioned_resource_id
-		LEFT OUTER JOIN build_outputs bo ON vr.id = bo.versioned_resource_id
-		WHERE r.pipeline_id = $1
-	) x
+	FROM
+		(
+			SELECT COALESCE(MAX(bo.modified_time), 'epoch') as bo_max
+			FROM build_outputs bo
+			LEFT OUTER JOIN versioned_resources v ON v.id = bo.versioned_resource_id
+			LEFT OUTER JOIN resources r ON r.id = v.resource_id
+			WHERE r.pipeline_id = $1
+		) bo,
+		(
+			SELECT COALESCE(MAX(bi.modified_time), 'epoch') as bi_max
+			FROM build_inputs bi
+			LEFT OUTER JOIN versioned_resources v ON v.id = bi.versioned_resource_id
+			LEFT OUTER JOIN resources r ON r.id = v.resource_id
+			WHERE r.pipeline_id = $1
+		) bi,
+		(
+			SELECT COALESCE(MAX(vr.modified_time), 'epoch') as vr_max
+			FROM versioned_resources vr
+			LEFT OUTER JOIN resources r ON r.id = vr.resource_id
+			WHERE r.pipeline_id = $1
+		) vr
 	`, p.id).Scan(&maxModifiedTime)
 
 	return maxModifiedTime, err
