5ad255c7f6701e5ed616544c41753e6f54c1d15c
Alex Suraci <asuraci@pivotal.io>
2018-03-08 10:35:03 -0500

    update for new crypto/ssh

--- tsacmd/command.go
+++ tsacmd/command.go
@@ -139,7 +139,11 @@ func (cmd *TSACommand) loadTeamAuthorizedKeys() ([]TeamAuthKeys, error) {
 
 func (cmd *TSACommand) configureSSHServer(sessionAuthTeam *sessionTeam, authorizedKeys []ssh.PublicKey, teamAuthorizedKeys []TeamAuthKeys) (*ssh.ServerConfig, error) {
 	certChecker := &ssh.CertChecker{
-		IsAuthority: func(key ssh.PublicKey) bool {
+		IsUserAuthority: func(key ssh.PublicKey) bool {
+			return false
+		},
+
+		IsHostAuthority: func(key ssh.PublicKey, address string) bool {
 			return false
 		},
 
