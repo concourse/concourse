ebb8b81942efa530b2f12cb4e70ad7905a219686
Topher Bullock <cbullock@pivotal.io>
2018-03-14 11:57:58 -0400

    stricter tls version and cipher suite support
    concourse/concourse#1997

--- atccmd/command.go
+++ atccmd/command.go
@@ -694,8 +694,19 @@ func (cmd *ATCCommand) constructMembers(
 		}
 
 		tlsConfig := &tls.Config{
-			Certificates: []tls.Certificate{cert},
-			NextProtos:   []string{"h2"},
+			Certificates:     []tls.Certificate{cert},
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			},
+			PreferServerCipherSuites: true,
+			NextProtos:               []string{"h2"},
 		}
 
 		members = append(members, grouper.Member{"web-tls", http_server.NewTLSServer(
