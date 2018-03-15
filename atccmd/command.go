1e357e099331ac557c4102b26074a6c561d796a8
Topher Bullock <cbullock@pivotal.io>
2018-03-15 10:37:25 -0400

    only *AES_128_GCM_SHA256 is needed for http2

--- atccmd/command.go
+++ atccmd/command.go
@@ -702,7 +702,6 @@ func (cmd *ATCCommand) constructMembers(
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
 			},
 			PreferServerCipherSuites: true,
