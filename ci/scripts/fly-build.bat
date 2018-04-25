6b0be3a092f0457ddfab57c5f3e44cc82dd3aa7a
Alex Suraci <suraci.alex@gmail.com>
2018-04-25 16:06:26 -0400

    compute sha1 checksums
    concourse/concourse#674

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -6,3 +6,5 @@ set PATH=%CD%\concourse\bin;%PATH%
 set /p FinalVersion=<final-version\version
 
 go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o windows-binary/fly_windows_amd64.exe github.com/concourse/fly
+
+certUtil -hashfile .\windows-binary\fly_windows_amd64.exe SHA1 > .\windows-binary\fly_windows_amd64.exe.sha1
