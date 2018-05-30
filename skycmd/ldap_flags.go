d1921e0df03ddcae9c6deb1572830be59e69b987
Joshua Winters <jwinters@pivotal.io>
2018-05-30 14:33:56 -0400

    improve documentation for ldap flags
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- skycmd/ldap_flags.go
+++ skycmd/ldap_flags.go
@@ -18,32 +18,32 @@ func init() {
 }
 
 type LDAPFlags struct {
-	DisplayName        string    `long:"display-name" description:"Display Name"`
-	Host               string    `long:"host" description:"Host"`
-	BindDN             string    `long:"bind-dn" description:"Bind DN"`
-	BindPW             string    `long:"bind-pw" description:"Bind PW"`
-	InsecureNoSSL      bool      `long:"insecure-no-ssl" description:"Don't use ssl"`
+	DisplayName        string    `long:"display-name" description:"Name to display on login screen"`
+	Host               string    `long:"host" description:"The host and optional port of the LDAP server. If port isn't supplied, it will be guessed based on the TLS configuration. 389 or 636."`
+	BindDN             string    `long:"bind-dn" description:"Bind DN for searching LDAP users and groups. Typically this is a read-only user."`
+	BindPW             string    `long:"bind-pw" description:"Bind Password for the user specified by 'bind-dn'"`
+	InsecureNoSSL      bool      `long:"insecure-no-ssl" description:"Required if LDAP host does not use TLS."`
 	InsecureSkipVerify bool      `long:"insecure-skip-verify" description:"Skip certificate verification"`
 	StartTLS           bool      `long:"start-tls" description:"Start on insecure port, then negotiate TLS"`
 	CACert             flag.File `long:"ca-cert" description:"CA certificate"`
 
 	UserSearch struct {
-		BaseDN    string `long:"user-search-base-dn" description:"Base DN"`
-		Filter    string `long:"user-search-filter" description:"Filter"`
-		Username  string `long:"user-search-username" description:"Username"`
-		Scope     string `long:"user-search-scope" description:"Scope"`
-		IDAttr    string `long:"user-search-id-attr" description:"ID Attr"`
-		EmailAttr string `long:"user-search-email-attr" description:"Email Attr"`
-		NameAttr  string `long:"user-search-name-attr" description:"Name Attr"`
+		BaseDN    string `long:"user-search-base-dn" description:"BaseDN to start the search from. For example 'cn=users,dc=example,dc=com'"`
+		Filter    string `long:"user-search-filter" description:"Optional filter to apply when searching the directory. For example '(objectClass=person)'"`
+		Username  string `long:"user-search-username" description:"Attribute to match against the inputted username. This will be translated and combined with the other filter as '(<attr>=<username>)'."`
+		Scope     string `long:"user-search-scope" description:"Can either be: 'sub' - search the whole sub tree or 'one' - only search one level. Defaults to 'sub'."`
+		IDAttr    string `long:"user-search-id-attr" description:"A mapping of attributes on the user entry to claims. Defaults to 'uid'."`
+		EmailAttr string `long:"user-search-email-attr" description:"A mapping of attributes on the user entry to claims. Defaults to 'mail'."`
+		NameAttr  string `long:"user-search-name-attr" description:"A mapping of attributes on the user entry to claims."`
 	}
 
 	GroupSearch struct {
-		BaseDN    string `long:"group-search-base-dn" description:"Base DN"`
-		Filter    string `long:"group-search-filter" description:"Filter"`
-		Scope     string `long:"group-search-scope" description:"Scope"`
-		UserAttr  string `long:"group-search-user-attr" description:"User Attr"`
-		GroupAttr string `long:"group-search-group-attr" description:"Group Attr"`
-		NameAttr  string `long:"group-search-name-attr" description:"Name Attr"`
+		BaseDN    string `long:"group-search-base-dn" description:"BaseDN to start the search from. For example 'cn=groups,dc=example,dc=com'"`
+		Filter    string `long:"group-search-filter" description:"Optional filter to apply when searching the directory. For example '(objectClass=posixGroup)'"`
+		Scope     string `long:"group-search-scope" description:"Can either be: 'sub' - search the whole sub tree or 'one' - only search one level. Defaults to 'sub'."`
+		UserAttr  string `long:"group-search-user-attr" description:"Adds an additional requirement to the filter that an attribute in the group match the user's attribute value. The exact filter being added is: (<groupAttr>=<userAttr value>)"`
+		GroupAttr string `long:"group-search-group-attr" description:"Adds an additional requirement to the filter that an attribute in the group match the user's attribute value. The exact filter being added is: (<groupAttr>=<userAttr value>)"`
+		NameAttr  string `long:"group-search-name-attr" description:"The attribute of the group that represents its name."`
 	}
 }
 
