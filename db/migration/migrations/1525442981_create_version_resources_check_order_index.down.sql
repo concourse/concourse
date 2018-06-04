f372ab41253c5d6acf53c1b66ae9e7ca0300806e
Joshua Winters <jwinters@pivotal.io>
2018-06-04 11:20:47 -0400

    Revert "Create migration for new BTREE index on check_order column of version_resources table."
    This reverts commit 6bb9911df0f0dc3b05684bb87f4285f8ca758c28.
    
    Signed-off-by: Divya Dadlani <ddadlani@pivotal.io>

--- db/migration/migrations/1525442981_create_version_resources_check_order_index.down.sql
+++
@@ -1,3 +0,0 @@
-BEGIN;
-  DROP INDEX versioned_resources_check_order;
-COMMIT;
--- db/migration/migrations/1525442981_create_version_resources_check_order_index.up.sql
+++
@@ -1,3 +0,0 @@
-BEGIN;
-  CREATE INDEX versioned_resources_check_order ON versioned_resources (check_order DESC);
-COMMIT;
