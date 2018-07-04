bcec1c83ca51f5aae3886bd6ca280746ab74d7f4
Joshua Winters <jwinters@pivotal.io>
2018-07-04 17:20:45 -0400

    Update not authorized message
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- elm/src/NotAuthorized.elm
+++ elm/src/NotAuthorized.elm
@@ -8,5 +8,5 @@ view : Html msg
 view =
     Html.div [ class "not-authorized" ]
         [ Html.div [ class "title" ] [ Html.text "401" ]
-        , Html.div [ class "reason" ] [ Html.text "You are not authorized to view this resource" ]
+        , Html.div [ class "reason" ] [ Html.text "You are not authorized to view that thing you wanted to see." ]
         ]
