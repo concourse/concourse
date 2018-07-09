8406bb52aa4d3fb9ffcf5c3eec3905dbd8163db4
Joshua Winters <jwinters@pivotal.io>
2018-07-09 12:44:25 -0400

    Fix trigger down migrations
    Signed-off-by: Nader Ziada <nziada@pivotal.io>

--- db/migration/migrations/1530823998_create_teams_trigger.down.sql
+++ db/migration/migrations/1530823998_create_teams_trigger.down.sql
@@ -1,10 +1,8 @@
 BEGIN;
-
-  DROP FUNCTION IF EXISTS on_team_delete;
-  DROP FUNCTION IF EXISTS on_team_insert;
-
   DROP TRIGGER IF EXISTS team_build_events_delete_trigger ON teams;
   DROP TRIGGER IF EXISTS team_build_events_insert_trigger ON teams;
 
+  DROP FUNCTION IF EXISTS on_team_delete();
+  DROP FUNCTION IF EXISTS on_team_insert();
 COMMIT;
 
