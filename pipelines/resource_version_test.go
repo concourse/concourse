2c4159ee4b0c77c5d2076dd8c4be643c954edfde
Clara Fu <cfu@pivotal.io>
2018-08-01 12:12:12 -0400

    Added testflight test for pinned resource versions
    concourse/concourse#2439
    
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- pipelines/resource_version_test.go
+++ pipelines/resource_version_test.go
@@ -1,6 +1,9 @@
 package pipelines_test
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/concourse/testflight/gitserver"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,101 +23,203 @@ var _ = Describe("Resource version", func() {
 		originGitServer.Stop()
 	})
 
-	Describe("version: latest", func() {
-		It("only runs builds with latest version", func() {
-			flyHelper.ConfigurePipeline(
-				pipelineName,
-				"-c", "fixtures/simple-trigger.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
-			)
+	Describe("when the version is not pinned on the resource", func() {
+		Describe("version: latest", func() {
+			It("only runs builds with latest version", func() {
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/simple-trigger.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+				)
 
-			guid1 := originGitServer.Commit()
-			watch := flyHelper.Watch(pipelineName, "some-passing-job")
-			Eventually(watch).Should(gbytes.Say(guid1))
+				guid1 := originGitServer.Commit()
+				watch := flyHelper.Watch(pipelineName, "some-passing-job")
+				Eventually(watch).Should(gbytes.Say(guid1))
 
-			flyHelper.PausePipeline(pipelineName)
+				flyHelper.PausePipeline(pipelineName)
 
-			originGitServer.Commit()
-			originGitServer.Commit()
-			guid4 := originGitServer.Commit()
+				originGitServer.Commit()
+				originGitServer.Commit()
+				guid4 := originGitServer.Commit()
 
-			flyHelper.UnpausePipeline(pipelineName)
+				flyHelper.UnpausePipeline(pipelineName)
 
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
-			Eventually(watch).Should(gbytes.Say(guid4))
-			Consistently(func() bool {
-				_, found, err := team.JobBuild(pipelineName, "some-passing-job", "3")
-				Expect(err).NotTo(HaveOccurred())
-				return found
-			}).Should(BeFalse())
+				watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
+				Eventually(watch).Should(gbytes.Say(guid4))
+				Consistently(func() bool {
+					_, found, err := team.JobBuild(pipelineName, "some-passing-job", "3")
+					Expect(err).NotTo(HaveOccurred())
+					return found
+				}).Should(BeFalse())
+			})
+		})
+
+		Describe("version: every", func() {
+			It("runs builds with every version", func() {
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/resource-version-every.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+				)
+
+				guid1 := originGitServer.Commit()
+				watch := flyHelper.Watch(pipelineName, "some-passing-job")
+				Eventually(watch).Should(gbytes.Say(guid1))
+
+				flyHelper.PausePipeline(pipelineName)
+
+				guid2 := originGitServer.Commit()
+				guid3 := originGitServer.Commit()
+				guid4 := originGitServer.Commit()
+
+				flyHelper.UnpausePipeline(pipelineName)
+
+				watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
+				Eventually(watch).Should(gbytes.Say(guid2))
+				watch = flyHelper.Watch(pipelineName, "some-passing-job", "3")
+				Eventually(watch).Should(gbytes.Say(guid3))
+				watch = flyHelper.Watch(pipelineName, "some-passing-job", "4")
+				Eventually(watch).Should(gbytes.Say(guid4))
+			})
+		})
+
+		Describe("version: pinned", func() {
+			It("only runs builds with the pinned version", func() {
+				guid1 := originGitServer.Commit()
+
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/resource-version-every.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+				)
+
+				watch := flyHelper.Watch(pipelineName, "some-passing-job")
+				Eventually(watch).Should(gbytes.Say(guid1))
+
+				flyHelper.PausePipeline(pipelineName)
+
+				originGitServer.Commit()
+				guid3 := originGitServer.Commit()
+				rev := originGitServer.RevParse("master")
+				originGitServer.Commit()
+
+				flyHelper.ReconfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/pinned-version.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+					"-v", "git-resource-version="+rev,
+				)
+
+				flyHelper.UnpausePipeline(pipelineName)
+
+				watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
+				Eventually(watch).Should(gbytes.Say(guid3))
+				Consistently(func() bool {
+					_, found, err := team.JobBuild(pipelineName, "some-passing-job", "3")
+					Expect(err).NotTo(HaveOccurred())
+					return found
+				}).Should(BeFalse())
+			})
 		})
 	})
 
-	Describe("version: every", func() {
-		It("runs builds with every version", func() {
-			flyHelper.ConfigurePipeline(
-				pipelineName,
-				"-c", "fixtures/resource-version-every.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
-			)
+	Describe("when the version is pinned on the resource", func() {
+		Describe("version: latest", func() {
+			It("only runs builds with pinned version", func() {
+				originGitServer.Commit()
+				rev := originGitServer.RevParse("master")
+				guid2 := originGitServer.Commit()
+				rev2 := strings.TrimSpace(originGitServer.RevParse("master"))
+				originGitServer.Commit()
 
-			guid1 := originGitServer.Commit()
-			watch := flyHelper.Watch(pipelineName, "some-passing-job")
-			Eventually(watch).Should(gbytes.Say(guid1))
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/pinned-resource-simple-trigger.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+					"-v", "pinned-resource-version="+rev2,
+				)
+				flyHelper.UnpausePipeline(pipelineName)
 
-			flyHelper.PausePipeline(pipelineName)
+				checkResource := flyHelper.CheckResource("-r", fmt.Sprintf("%s/some-git-resource", pipelineName), "-f", fmt.Sprintf("ref:%s", rev))
+				<-checkResource.Exited
+				Expect(checkResource.ExitCode()).To(Equal(0))
+				Expect(checkResource).To(gbytes.Say("checked 'some-git-resource'"))
 
-			guid2 := originGitServer.Commit()
-			guid3 := originGitServer.Commit()
-			guid4 := originGitServer.Commit()
-
-			flyHelper.UnpausePipeline(pipelineName)
-
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
-			Eventually(watch).Should(gbytes.Say(guid2))
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "3")
-			Eventually(watch).Should(gbytes.Say(guid3))
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "4")
-			Eventually(watch).Should(gbytes.Say(guid4))
+				watch := flyHelper.Watch(pipelineName, "some-passing-job", "1")
+				Eventually(watch).Should(gbytes.Say(guid2))
+				Consistently(func() bool {
+					_, found, err := team.JobBuild(pipelineName, "some-passing-job", "2")
+					Expect(err).NotTo(HaveOccurred())
+					return found
+				}).Should(BeFalse())
+			})
 		})
-	})
 
-	Describe("version: pinned", func() {
-		It("only runs builds with the pinned version", func() {
-			guid1 := originGitServer.Commit()
+		Describe("version: every", func() {
+			It("runs builds with pinned version", func() {
+				originGitServer.Commit()
+				rev := strings.TrimSpace(originGitServer.RevParse("master"))
+				guid2 := originGitServer.Commit()
+				rev2 := strings.TrimSpace(originGitServer.RevParse("master"))
+				originGitServer.Commit()
 
-			flyHelper.ConfigurePipeline(
-				pipelineName,
-				"-c", "fixtures/resource-version-every.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
-			)
+				flyHelper.ConfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/pinned-resource-version-every.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+					"-v", "pinned-resource-version="+rev2,
+				)
 
-			watch := flyHelper.Watch(pipelineName, "some-passing-job")
-			Eventually(watch).Should(gbytes.Say(guid1))
+				flyHelper.UnpausePipeline(pipelineName)
 
-			flyHelper.PausePipeline(pipelineName)
+				checkResource := flyHelper.CheckResource("-r", fmt.Sprintf("%s/some-git-resource", pipelineName), "-f", fmt.Sprintf("ref:%s", rev))
+				<-checkResource.Exited
+				Expect(checkResource.ExitCode()).To(Equal(0))
+				Expect(checkResource).To(gbytes.Say("checked 'some-git-resource'"))
 
-			originGitServer.Commit()
-			guid3 := originGitServer.Commit()
-			rev := originGitServer.RevParse("master")
-			originGitServer.Commit()
+				watch := flyHelper.Watch(pipelineName, "some-passing-job", "1")
+				Eventually(watch).Should(gbytes.Say(guid2))
+				Consistently(func() bool {
+					_, found, err := team.JobBuild(pipelineName, "some-passing-job", "2")
+					Expect(err).NotTo(HaveOccurred())
+					return found
+				}).Should(BeFalse())
+			})
+		})
 
-			flyHelper.ReconfigurePipeline(
-				pipelineName,
-				"-c", "fixtures/pinned-version.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
-				"-v", "git-resource-version="+rev,
-			)
+		Describe("version: pinned", func() {
+			It("only runs builds with the pinned version", func() {
+				originGitServer.Commit()
+				rev := strings.TrimSpace(originGitServer.RevParse("master"))
+				guid2 := originGitServer.Commit()
+				rev2 := strings.TrimSpace(originGitServer.RevParse("master"))
+				originGitServer.Commit()
+				rev3 := strings.TrimSpace(originGitServer.RevParse("master"))
+				originGitServer.Commit()
 
-			flyHelper.UnpausePipeline(pipelineName)
+				flyHelper.ReconfigurePipeline(
+					pipelineName,
+					"-c", "fixtures/pinned-resource-pinned-version.yml",
+					"-v", "origin-git-server="+originGitServer.URI(),
+					"-v", "pinned-resource-version="+rev2,
+					"-v", "git-resource-version="+rev3,
+				)
 
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
-			Eventually(watch).Should(gbytes.Say(guid3))
-			Consistently(func() bool {
-				_, found, err := team.JobBuild(pipelineName, "some-passing-job", "3")
-				Expect(err).NotTo(HaveOccurred())
-				return found
-			}).Should(BeFalse())
+				flyHelper.UnpausePipeline(pipelineName)
+
+				checkResource := flyHelper.CheckResource("-r", fmt.Sprintf("%s/some-git-resource", pipelineName), "-f", fmt.Sprintf("ref:%s", rev))
+				<-checkResource.Exited
+				Expect(checkResource.ExitCode()).To(Equal(0))
+				Expect(checkResource).To(gbytes.Say("checked 'some-git-resource'"))
+
+				watch := flyHelper.Watch(pipelineName, "some-passing-job", "1")
+				Eventually(watch).Should(gbytes.Say(guid2))
+				Consistently(func() bool {
+					_, found, err := team.JobBuild(pipelineName, "some-passing-job", "2")
+					Expect(err).NotTo(HaveOccurred())
+					return found
+				}).Should(BeFalse())
+			})
 		})
 	})
 })
