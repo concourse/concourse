a6030fd2df69a778a793ed82b62c8422dc403fbb
Saman Alvi <salvi@pivotal.io>
2018-08-15 16:30:35 -0400

    Configurable timeout for resource checks
    * tests for resource timeout check
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- pipelines/resource_check_timeout_test.go
+++ pipelines/resource_check_timeout_test.go
@@ -3,6 +3,8 @@ package pipelines_test
 import (
 	"fmt"
 
+	"time"
+
 	"github.com/concourse/testflight/gitserver"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,14 +12,11 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-var checkContents = `#!/bin/sh
-sleep 10000
-`
-
-var _ = FDescribe("A resource check which times out", func() {
+var _ = Describe("A resource check which times out", func() {
 	var originGitServer *gitserver.Server
+	var checkContents string
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		originGitServer = gitserver.Start(client)
 		originGitServer.CommitResource()
 		originGitServer.CommitFileToBranch(checkContents, "rootfs/opt/resource/check", "master")
@@ -30,10 +29,42 @@ var _ = FDescribe("A resource check which times out", func() {
 		)
 	})
 
-	It("times out when checking", func() {
-		watch := flyHelper.CheckResource("-r", fmt.Sprintf("%s/my-resource", pipelineName))
-		<-watch.Exited
-		Expect(watch).To(gbytes.Say("check timed out"))
-		Expect(watch).To(gexec.Exit(1))
+	Context("when check script times out", func() {
+		BeforeEach(func() {
+			checkContents = `#!/bin/sh
+				sleep 50
+				echo 'should not get here' > /tmp/some-random-file
+			`
+		})
+
+		It("prints an error and cancels the job", func() {
+			watch := flyHelper.CheckResource("-r", fmt.Sprintf("%s/my-resource", pipelineName))
+			<-watch.Exited
+			Expect(watch).To(gexec.Exit(1))
+			Expect(watch.Err).To(gbytes.Say("Timed out after 10s while checking for new versions - perhaps increase your resource check timeout?"))
+
+			time.Sleep(40 * time.Second)
+
+			hijack := flyHelper.Hijack("-c", fmt.Sprintf("%s/my-resource", pipelineName), "cat", "/tmp/some-random-file")
+			<-hijack.Exited
+			Expect(hijack).NotTo(gbytes.Say("should not get here"))
+		})
+	})
+
+	Context("when check script finishes before timeout", func() {
+		BeforeEach(func() {
+			checkContents = `#!/bin/sh
+				echo '[{"version":"1"}]'
+			`
+		})
+
+		It("succeeds if the check script returns before the timeout", func() {
+			watch := flyHelper.CheckResource("-r", fmt.Sprintf("%s/my-resource", pipelineName))
+			<-watch.Exited
+			Expect(watch).To(gexec.Exit(0))
+
+			hijack := flyHelper.Hijack("-c", fmt.Sprintf("%s/my-resource", pipelineName), "cat", "/tmp/some-random-file")
+			<-hijack.Exited
+		})
 	})
 })
