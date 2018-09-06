79b57caa4ecef40095e513678fa84cbb87c6e692
Alex Suraci <suraci.alex@gmail.com>
2018-09-06 12:20:40 -0400

    build with packr

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -15,6 +15,9 @@ go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
 .\go-bindata.exe -pkg bindata -o concourse\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
 
+go build -o packr.exe github.com/gobuffalo/packr
+.\packr.exe -i concourse/src/github.com/concourse
+
 go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.WorkerVersion=%WorkerVersion% -X main.WorkerVersion=%WorkerVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
 
 certUtil -hashfile .\binary\concourse_windows_amd64.exe SHA1 > .\binary\concourse_windows_amd64.exe.sha1
