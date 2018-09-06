78b5cccc1700f8f9c15dc44a021fc4ce931710fa
Alex Suraci <suraci.alex@gmail.com>
2018-09-06 12:26:05 -0400

    fix packr cli path

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -15,7 +15,7 @@ go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
 .\go-bindata.exe -pkg bindata -o concourse\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
 
-go build -o packr.exe github.com/gobuffalo/packr
+go build -o packr.exe github.com/gobuffalo/packr/packr
 .\packr.exe -i concourse/src/github.com/concourse
 
 go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.WorkerVersion=%WorkerVersion% -X main.WorkerVersion=%WorkerVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
