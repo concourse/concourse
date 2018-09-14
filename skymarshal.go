59fd55622537e4453bbd7c5bed109cf6ef55e609
Alex Suraci <suraci.alex@gmail.com>
2018-09-14 10:58:47 -0400

    Revert "Always use http for loopback communication because certs"
    This reverts commit b0c80f903ce83584e935ca271ccb3adbf9d8332e.

--- skymarshal.go
+++ skymarshal.go
@@ -64,7 +64,6 @@ func NewServer(config *Config) (*Server, error) {
 		return nil, err
 	}
 
-	internalURL.Scheme = "http"
 	internalURL.Host = config.InternalHost
 
 	skyServer, err := skyserver.NewSkyServer(&skyserver.SkyConfig{
