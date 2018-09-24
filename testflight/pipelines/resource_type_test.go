8e83ad2ff2e35fcc6add761aacd18c2c912d304f
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 16:51:07 -0400

    convert privileged custom type tests to mock

--- testflight/pipelines/resource_type_test.go
+++ testflight/pipelines/resource_type_test.go
@@ -6,7 +6,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Configuring a resource type in a pipeline config", func() {
@@ -40,80 +39,6 @@ var _ = Describe("Configuring a resource type in a pipeline config", func() {
 			<-checkResource.Exited
 			Expect(checkResource.ExitCode()).To(Equal(0))
 			Expect(checkResource).To(gbytes.Say("checked 'my-resource'"))
-		})
-
-		XIt("should be able to run privileged operations in 'check', 'get' and 'put' steps", func() {
-			watch := flyHelper.TriggerJob(pipelineName, "failing-task")
-			<-watch.Exited
-			By("hijacking into get container for my-resource")
-			hijackS, stdin := flyHelper.HijackInteractive(
-				"-j", pipelineName+"/failing-task",
-				"-s", "my-resource",
-				"--", "sh", "-c",
-				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp && exit $?")
-			Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: get"))
-			fmt.Fprintln(stdin, "1")
-			Eventually(hijackS).Should(gexec.Exit(0))
-
-			By("hijacking into put container for my-resource")
-			hijackS, stdin = flyHelper.HijackInteractive(
-				"-j", pipelineName+"/failing-task",
-				"-s", "my-resource",
-				"--", "sh", "-c",
-				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
-			Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: put"))
-			fmt.Fprintln(stdin, "3")
-			fmt.Fprintln(stdin, "2")
-			Eventually(hijackS).Should(gexec.Exit(0))
-
-			By("hijacking into check container for my-resource")
-			hijackS = flyHelper.Hijack(
-				"-c", pipelineName+"/my-resource",
-				"--", "sh", "-c",
-				"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
-			Eventually(hijackS).Should(gexec.Exit(0))
-		})
-
-		XContext("when the custom resource type is not privileged", func() {
-			BeforeEach(func() {
-				flyHelper.ConfigurePipeline(
-					pipelineName,
-					"-c", "fixtures/resource-types.yml",
-					"-y", "privileged=false",
-				)
-			})
-
-			It("should fail running privileged operations in containers", func() {
-				watch := flyHelper.TriggerJob(pipelineName, "failing-task")
-				<-watch.Exited
-				By("hijacking into get container for my-resource")
-				hijackS, stdin := flyHelper.HijackInteractive(
-					"-j", pipelineName+"/failing-task",
-					"-s", "my-resource",
-					"--", "sh", "-c",
-					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
-				Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: get"))
-				fmt.Fprintln(stdin, "1")
-				Eventually(hijackS).Should(gexec.Exit(1))
-
-				By("hijacking into put container for my-resource")
-				hijackS, stdin = flyHelper.HijackInteractive(
-					"-j", pipelineName+"/failing-task",
-					"-s", "my-resource",
-					"--", "sh", "-c",
-					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
-				Eventually(hijackS).Should(gbytes.Say("[1-9]*: build #1, step: my-resource, type: put"))
-				fmt.Fprintln(stdin, "3")
-				fmt.Fprintln(stdin, "2")
-				Eventually(hijackS).Should(gexec.Exit(1))
-
-				By("hijacking into check container for my-resource")
-				hijackS = flyHelper.Hijack(
-					"-c", pipelineName+"/my-resource",
-					"--", "sh", "-c",
-					"mkdir tmp && mount -o size=50m -t tmpfs swap tmp")
-				Eventually(hijackS).Should(gexec.Exit(1))
-			})
 		})
 	})
 
