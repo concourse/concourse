5d3a070951f9ea22ee9f9eefd1b84519e2c8e44b
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 14:06:52 -0400

    switch matrix test to mock resource

--- testflight/pipelines/matrix_test.go
+++ testflight/pipelines/matrix_test.go
@@ -1,38 +1,33 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("A job with a complicated build plan", func() {
-	var originGitServer *gitserver.Server
+	var initialVersion string
 
 	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
+		u, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		initialVersion = u.String()
 
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/matrix.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
+			"-v", "initial_version="+initialVersion,
 		)
 	})
 
-	AfterEach(func() {
-		originGitServer.Stop()
-	})
-
 	It("executes the build plan correctly", func() {
-		By("executing the build when a commit is made")
-		committedGuid := originGitServer.Commit()
-
-		By("propagating data between steps")
-		watch := flyHelper.Watch(pipelineName, "fancy-build-matrix")
-		Eventually(watch).Should(gbytes.Say("passing-unit-1/file passing-unit-2/file " + committedGuid))
-
-		By("failing on aggregates if any branch failed")
-		Eventually(watch).Should(gbytes.Say("failed " + committedGuid))
+		watch := flyHelper.TriggerJob(pipelineName, "fancy-build-matrix")
+		<-watch.Exited
+		Expect(watch.ExitCode()).To(Equal(1)) // expect failure
+		Expect(watch).To(gbytes.Say("passing-unit-1/file passing-unit-2/file " + initialVersion))
+		Expect(watch).To(gbytes.Say("failed aggregate " + initialVersion))
 	})
 })
