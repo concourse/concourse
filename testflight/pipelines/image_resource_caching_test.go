6a97a5b9f39c9f0b98c3aff78ab2dd2f92f1570f
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 10:58:25 -0400

    switch image resource caching test to mock

--- testflight/pipelines/image_resource_caching_test.go
+++ testflight/pipelines/image_resource_caching_test.go
@@ -1,49 +1,51 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("image resource caching", func() {
-	var (
-		originGitServer *gitserver.Server
-	)
+	var version string
 
 	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
-	})
+		u, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
 
-	AfterEach(func() {
-		originGitServer.Stop()
-	})
+		version = u.String()
 
-	It("gets the image resource from the cache based on given params", func() {
-		originGitServer.CommitResource()
-		originGitServer.CommitFileToBranch("initial", "initial", "trigger")
-
-		By("configuring the pipeline")
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/image-resource-with-params.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
+			"-v", "initial_version="+version,
 		)
+	})
 
+	It("gets the image resource from the cache based on given params", func() {
 		By("triggering the resource get with params")
 		watch := flyHelper.TriggerJob(pipelineName, "with-params")
 		<-watch.Exited
-		Expect(watch).To(gbytes.Say("Cloning"))
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).To(gbytes.Say(`fetching.*` + version))
 
 		By("triggering the task with image resource with params")
 		watch = flyHelper.TriggerJob(pipelineName, "image-resource-with-params")
 		<-watch.Exited
-		Expect(watch).ToNot(gbytes.Say("Cloning"))
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).ToNot(gbytes.Say("fetching"))
 
 		By("triggering the task with image resource without params")
 		watch = flyHelper.TriggerJob(pipelineName, "image-resource-without-params")
 		<-watch.Exited
-		Expect(watch).To(gbytes.Say("Cloning"))
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).To(gbytes.Say(`fetching.*` + version))
+
+		By("triggering the resource get without params")
+		watch = flyHelper.TriggerJob(pipelineName, "without-params")
+		<-watch.Exited
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).ToNot(gbytes.Say(`fetching.*` + version))
 	})
 })
