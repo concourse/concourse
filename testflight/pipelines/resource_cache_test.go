5f6e0ce0ea955e4f9620fe66f27527454d80caf4
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 12:16:48 -0400

    convert resource caching test to mock resource

--- testflight/pipelines/resource_cache_test.go
+++ testflight/pipelines/resource_cache_test.go
@@ -1,43 +1,33 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
-var _ = Describe("[#139960779] resource caching", func() {
-	var (
-		originGitServer *gitserver.Server
-	)
+var _ = Describe("Resource caching", func() {
+	It("takes params into account when determining a cache hit", func() {
+		guid, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
 
-	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
-	})
-
-	AfterEach(func() {
-		originGitServer.Stop()
-	})
-
-	It("gets the resource from the cache based on given params", func() {
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/resource-with-params.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
+			"-v", "unique_version="+guid.String(),
 		)
-
-		originGitServer.Commit()
 
 		watch := flyHelper.TriggerJob(pipelineName, "without-params")
 		<-watch.Exited
+		Expect(watch).To(gbytes.Say("fetching.*" + guid.String()))
 
 		watch = flyHelper.TriggerJob(pipelineName, "without-params")
 		<-watch.Exited
-		Expect(watch).ToNot(gbytes.Say("Cloning"))
+		Expect(watch).ToNot(gbytes.Say("fetching"))
 
 		watch = flyHelper.TriggerJob(pipelineName, "with-params")
 		<-watch.Exited
-		Expect(watch).To(gbytes.Say("Cloning"))
+		Expect(watch).To(gbytes.Say("fetching.*" + guid.String()))
 	})
 })
