3e48510205edd1b5e649f708d35260f4a39967aa
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 11:57:39 -0400

    convert serial groups tests to mock resource

--- testflight/pipelines/serial_groups_test.go
+++ testflight/pipelines/serial_groups_test.go
@@ -4,38 +4,28 @@ import (
 	"fmt"
 
 	"github.com/concourse/concourse/atc"
-	"github.com/concourse/concourse/testflight/gitserver"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("serial groups", func() {
-	var originGitServer *gitserver.Server
-
 	Context("when no inputs are available for one resource", func() {
 		BeforeEach(func() {
-			originGitServer = gitserver.Start(client)
-
 			flyHelper.ConfigurePipeline(
 				pipelineName,
 				"-c", "fixtures/serial-groups.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
 			)
-		})
-
-		AfterEach(func() {
-			originGitServer.Stop()
 		})
 
 		It("runs even when another job in the serial group has a pending build", func() {
 			pendingBuild, err := team.CreateJobBuild(pipelineName, "some-pending-job")
 			Expect(err).NotTo(HaveOccurred())
 
-			guid1 := originGitServer.Commit()
-			watch := flyHelper.Watch(pipelineName, "some-passing-job")
-
-			Eventually(watch).Should(gbytes.Say(guid1))
+			watch := flyHelper.TriggerJob(pipelineName, "some-passing-job")
+			<-watch.Exited
+			Expect(watch.ExitCode()).To(Equal(0))
 
 			updatedPendingBuild, found, err := client.Build(fmt.Sprint(pendingBuild.ID))
 			Expect(err).NotTo(HaveOccurred())
@@ -46,49 +36,62 @@ var _ = Describe("serial groups", func() {
 
 	Context("when inputs eventually become available for one resource", func() {
 		BeforeEach(func() {
-			originGitServer = gitserver.Start(client)
-
 			flyHelper.ConfigurePipeline(
 				pipelineName,
 				"-c", "fixtures/serial-groups-inputs-updated.yml",
-				"-v", "origin-git-server="+originGitServer.URI(),
 			)
-		})
-
-		AfterEach(func() {
-			originGitServer.Stop()
 		})
 
 		It("is able to run second job with latest inputs", func() {
 			pendingBuild, err := team.CreateJobBuild(pipelineName, "some-pending-job")
 			Expect(err).NotTo(HaveOccurred())
 
-			By("making a commit master, kicking off some-passing-job, and pending some-pending-job")
-			guid1 := originGitServer.Commit()
-			watch := flyHelper.Watch(pipelineName, "some-passing-job")
-			Eventually(watch).Should(gbytes.Say(guid1))
+			By("making a new version, kicking off some-passing-job, and pending some-pending-job")
+			guid1, err := uuid.NewV4()
+			Expect(err).ToNot(HaveOccurred())
+			found, err := team.CheckResource(pipelineName, "some-resource", atc.Version{"version": guid1.String()})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(found).To(BeTrue())
+
+			watch := flyHelper.TriggerJob(pipelineName, "some-passing-job")
+			<-watch.Exited
+			Expect(watch.ExitCode()).To(Equal(0))
+			Expect(watch).To(gbytes.Say(guid1.String()))
 
 			updatedPendingBuild, found, err := client.Build(fmt.Sprint(pendingBuild.ID))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(found).To(BeTrue())
 			Expect(updatedPendingBuild.Status).To(Equal(string(atc.StatusPending)))
 
-			By("making a commit to master again, kicking off some-passing-job, and pending some-pending-job")
-			guid2 := originGitServer.Commit()
-			watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
-			Eventually(watch).Should(gbytes.Say(guid2))
+			By("making another new version, kicking off some-passing-job, and pending some-pending-job")
+			guid2, err := uuid.NewV4()
+			Expect(err).ToNot(HaveOccurred())
+			found, err = team.CheckResource(pipelineName, "some-resource", atc.Version{"version": guid2.String()})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(found).To(BeTrue())
+
+			watch = flyHelper.TriggerJob(pipelineName, "some-passing-job")
+			<-watch.Exited
+			Expect(watch.ExitCode()).To(Equal(0))
+			Expect(watch).To(gbytes.Say(guid2.String()))
 
 			updatedPendingBuild, found, err = client.Build(fmt.Sprint(pendingBuild.ID))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(found).To(BeTrue())
 			Expect(updatedPendingBuild.Status).To(Equal(string(atc.StatusPending)))
 
-			By("making a commit to other-branch, kicking off some-pending-job which should run with newest resource versions")
-			guid3 := originGitServer.CommitOnBranch("other-branch")
-			watch = flyHelper.Watch(pipelineName, "some-pending-job")
-			Eventually(watch).Should(gbytes.Say(guid3))
+			By("making a version for the other resource, kicking off some-pending-job which should run with newest resource versions")
+			guid3, err := uuid.NewV4()
+			Expect(err).ToNot(HaveOccurred())
+			found, err = team.CheckResource(pipelineName, "other-resource", atc.Version{"version": guid3.String()})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(found).To(BeTrue())
 
-			Eventually(watch).Should(gbytes.Say(guid2))
+			watch = flyHelper.Watch(pipelineName, "some-pending-job")
+			<-watch.Exited
+			Expect(watch.ExitCode()).To(Equal(0))
+			Expect(watch).To(gbytes.Say(guid2.String()))
+			Expect(watch).To(gbytes.Say(guid3.String()))
 
 			getPendingBuildStats := func() string {
 				updatedPendingBuild, found, err = client.Build(fmt.Sprint(pendingBuild.ID))
