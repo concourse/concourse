15c58878671af7a9d59c8a3bf7cc8d72c2c8f801
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 10:37:07 -0400

    switch config params test to mock resource

--- testflight/pipelines/config_params_test.go
+++ testflight/pipelines/config_params_test.go
@@ -1,44 +1,17 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("Configuring a resource in a pipeline config", func() {
-	var gitServer *gitserver.Server
-
 	BeforeEach(func() {
-		gitServer = gitserver.Start(client)
-
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/config_params.yml",
-			"-v", "git-server="+gitServer.URI(),
 		)
-
-		taskFileContents := `---
-platform: linux
-
-image_resource:
-  type: mock
-  source: {mirror_self: true}
-
-params:
-  SOURCE_PARAM: file_source
-
-run:
-  path: sh
-  args: ["-ec", "echo -n 'SOURCE_PARAM is '; printenv SOURCE_PARAM; echo ."]
-`
-		gitServer.WriteFile("some-repo/task.yml", taskFileContents)
-		gitServer.CommitResourceWithFile("task.yml")
-	})
-
-	AfterEach(func() {
-		gitServer.Stop()
 	})
 
 	Context("when specifying file in task config", func() {
