063a7ed141dd471686fd2ba2a0501ffa7c2e8e27
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 11:45:01 -0400

    convert recursive resource type checking to mock

--- testflight/pipelines/custom_resource_check_test.go
+++ testflight/pipelines/custom_resource_check_test.go
@@ -3,33 +3,20 @@ package pipelines_test
 import (
 	"fmt"
 
-	"github.com/concourse/concourse/testflight/gitserver"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("When a resource type depends on another resource type", func() {
-	var originGitServer *gitserver.Server
-
 	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
-		originGitServer.CommitResource()
-		originGitServer.CommitFileToBranch("initial", "initial", "trigger")
-
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/recursive-resource-checking.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
-			"-y", "privileged=false",
 		)
 	})
 
-	AfterEach(func() {
-		originGitServer.Stop()
-	})
-
-	It("can check for resources using a custom type", func() {
+	It("can be checked immediately", func() {
 		checkResource := flyHelper.CheckResource("-r", fmt.Sprintf("%s/recursive-custom-resource", pipelineName))
 		<-checkResource.Exited
 		Expect(checkResource.ExitCode()).To(Equal(0))
