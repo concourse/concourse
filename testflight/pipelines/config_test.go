dbfd40522a5c2c40709e1392fe7a29bbc90248b0
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 11:26:08 -0400

    switch more tests over to mock resource

--- testflight/pipelines/config_test.go
+++ testflight/pipelines/config_test.go
@@ -1,36 +1,22 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Configuring a resource with nested configuration", func() {
-	var gitServer *gitserver.Server
-
 	BeforeEach(func() {
-		gitServer = gitserver.Start(client)
-		gitServer.CommitResource()
-		gitServer.CommitFileToBranch("initial", "initial", "trigger")
-
 		flyHelper.ConfigurePipeline(
 			pipelineName,
-			"-c", "fixtures/config-test.yml",
-			"-v", "git-server="+gitServer.URI(),
+			"-c", "fixtures/nested-config-test.yml",
 		)
-	})
-
-	AfterEach(func() {
-		gitServer.Stop()
 	})
 
 	It("works", func() {
 		watch := flyHelper.TriggerJob(pipelineName, "config-test")
 		<-watch.Exited
-
 		Expect(watch).To(gexec.Exit(0))
 	})
 })
--- testflight/pipelines/resource_reconfigure_test.go
+++ testflight/pipelines/resource_reconfigure_test.go
@@ -1,42 +1,40 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("Reconfiguring a resource", func() {
-	var originGitServer *gitserver.Server
+	It("picks up the new configuration immediately", func() {
+		guid1, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
 
-	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
-	})
+		guid2, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
 
-	AfterEach(func() {
-		originGitServer.Stop()
-	})
-
-	It("creates a new check container with the updated configuration", func() {
 		flyHelper.ConfigurePipeline(
 			pipelineName,
-			"-c", "fixtures/simple-trigger.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
+			"-c", "fixtures/reconfiguring.yml",
+			"-v", "force_version="+guid1.String(),
 		)
 
-		guid1 := originGitServer.Commit()
-		watch := flyHelper.Watch(pipelineName, "some-passing-job", "1")
-		Eventually(watch).Should(gbytes.Say(guid1))
+		watch := flyHelper.TriggerJob(pipelineName, "some-passing-job")
+		<-watch.Exited
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).To(gbytes.Say(guid1.String()))
 
 		flyHelper.ReconfigurePipeline(
 			pipelineName,
-			"-c", "fixtures/simple-trigger-reconfigured.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
+			"-c", "fixtures/reconfiguring.yml",
+			"-v", "force_version="+guid2.String(),
 		)
 
-		guid2 := originGitServer.CommitOnBranch("other")
-		watch = flyHelper.Watch(pipelineName, "some-passing-job", "2")
-		Eventually(watch).Should(gbytes.Say(guid2))
+		watch = flyHelper.TriggerJob(pipelineName, "some-passing-job")
+		<-watch.Exited
+		Expect(watch.ExitCode()).To(Equal(0))
+		Expect(watch).To(gbytes.Say(guid2.String()))
 	})
 })
