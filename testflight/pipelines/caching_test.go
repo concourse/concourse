be99236f75816b8cf8ad2db8e427ac84d1894968
Alex Suraci <suraci.alex@gmail.com>
2018-09-24 12:21:51 -0400

    convert caching test to mock resource

--- testflight/pipelines/caching_test.go
+++ testflight/pipelines/caching_test.go
@@ -1,52 +1,58 @@
 package pipelines_test
 
 import (
-	"github.com/concourse/concourse/testflight/gitserver"
+	"github.com/concourse/concourse/atc"
+	uuid "github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("We shouldn't reclone cached resources", func() {
-	var originGitServer *gitserver.Server
-	var cachedGitServer *gitserver.Server
-
+var _ = Describe("Resource caching", func() {
 	BeforeEach(func() {
-		originGitServer = gitserver.Start(client)
-		cachedGitServer = gitserver.Start(client)
-
 		flyHelper.ConfigurePipeline(
 			pipelineName,
 			"-c", "fixtures/caching.yml",
-			"-v", "origin-git-server="+originGitServer.URI(),
-			"-v", "cached-git-server="+cachedGitServer.URI(),
 		)
-		originGitServer.Commit()
-		cachedGitServer.Commit()
 	})
 
-	AfterEach(func() {
-		originGitServer.Stop()
-		cachedGitServer.Stop()
-	})
+	It("does not fetch if there is nothing new", func() {
+		someResourceV1, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
 
-	It("does not reclone on new commits", func() {
-		By("initially cloning twice")
+		cachedResourceV1, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		found, err := team.CheckResource(pipelineName, "some-resource", atc.Version{"version": someResourceV1.String()})
+		Expect(err).ToNot(HaveOccurred())
+		Expect(found).To(BeTrue())
+
+		found, err = team.CheckResource(pipelineName, "cached-resource", atc.Version{"version": cachedResourceV1.String()})
+		Expect(err).ToNot(HaveOccurred())
+		Expect(found).To(BeTrue())
+
+		By("initially fetching twice")
 		watch := flyHelper.TriggerJob(pipelineName, "some-passing-job")
 		<-watch.Exited
-		Expect(watch).To(gbytes.Say("Cloning into"))
-		Expect(watch).To(gbytes.Say("Cloning into"))
+		Expect(watch).To(gbytes.Say("fetching.*" + someResourceV1.String()))
+		Expect(watch).To(gbytes.Say("fetching.*" + cachedResourceV1.String()))
 		Expect(watch).To(gbytes.Say("succeeded"))
 		Expect(watch).To(gexec.Exit(0))
 
-		originGitServer.Commit()
+		By("coming up with a new version for one resource")
+		someResourceV2, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		found, err = team.CheckResource(pipelineName, "some-resource", atc.Version{"version": someResourceV2.String()})
+		Expect(err).ToNot(HaveOccurred())
+		Expect(found).To(BeTrue())
 
 		By("hitting the cache for the original version and fetching the new one")
 		watch = flyHelper.TriggerJob(pipelineName, "some-passing-job")
 		<-watch.Exited
-		Expect(watch).To(gbytes.Say("Cloning into"))
-		Expect(watch).NotTo(gbytes.Say("Cloning into"))
+		Expect(watch).To(gbytes.Say("fetching.*" + someResourceV2.String()))
+		Expect(watch).NotTo(gbytes.Say("fetching"))
 		Expect(watch).To(gbytes.Say("succeeded"))
 		Expect(watch).To(gexec.Exit(0))
 	})
