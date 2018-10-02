18db1ffc63a493cbcc3382b9727d75a6e7de70dd
Alex Suraci <suraci.alex@gmail.com>
2018-10-02 15:54:15 -0400

    ci: get fly-build-* tasks working
    made final-version an optional input for easier testing with `fly
    execute`

---
+++ ci/tasks/scripts/fly-build.ps1
@@ -0,0 +1,29 @@
+trap {
+  write-error $_
+  exit 1
+}
+
+$env:Path += ";C:\Go\bin;C:\Program Files\Git\cmd"
+
+$env:GOPATH = "$pwd\gopath"
+$env:Path += ";$pwd\gopath\bin"
+
+$ldflags = ""
+if ([System.IO.File]::Exists("final-version\version")) {
+  [string]$FinalVersion = (Get-Content "final-version\version")
+  $ldflags = "-X github.com/concourse/fly/version.Version=$FinalVersion"
+}
+
+Push-Location concourse
+  go build -ldflags "$ldflags" -o fly.exe ./fly
+  mv fly.exe ..\fly-windows
+Pop-Location
+
+Push-Location fly-windows
+  Compress-Archive `
+    -LiteralPath .\fly.exe `
+    -DestinationPath .\fly-windows-amd64.zip
+
+  Get-FileHash -Algorithm SHA1 -LiteralPath .\fly-windows-amd64.zip | `
+    Out-File -Encoding utf8 .\fly-windows-amd64.zip.sha1
+Pop-Location
