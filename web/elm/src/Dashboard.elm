27548767084130ae87a3fc5bbd95b4629c4aef3c
Divya Dadlani <ddadlani@pivotal.io>
2018-10-05 11:26:57 -0400

    web: minor fixes
    - dashboard top bar remains sticky on mobile resolutions
      (concourse/concourse#2434)
    - fix undocumented regression on no pipelines page: the vertical
      centering had stopped working due to CSS changes involved in
      extraneous scrollbar
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- web/elm/src/Dashboard.elm
+++ web/elm/src/Dashboard.elm
@@ -6,6 +6,7 @@ import Concourse.Cli
 import Concourse.Pipeline
 import Concourse.PipelineStatus
 import Concourse.User
+import Css
 import Dashboard.Details as Details
 import Dashboard.Group as Group
 import Dashboard.GroupWithTag as GroupWithTag
@@ -13,9 +14,8 @@ import Dashboard.Pipeline as Pipeline
 import Dashboard.SubState as SubState
 import DashboardHd
 import Dom
-import Html exposing (Html)
-import Html.Attributes exposing (attribute, class, classList, draggable, href, id, src)
-import Html.Attributes.Aria exposing (ariaLabel)
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes exposing (attribute, css, class, classList, draggable, href, id, src)
 import Http
 import Keyboard
 import List.Extra
@@ -390,7 +390,7 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     Html.div [ class "page" ]
-        [ Html.map TopBarMsg (NewTopBar.view model.topBar)
+        [ (Html.map TopBarMsg << Html.fromUnstyled) (NewTopBar.view model.topBar)
         , dashboardView model
         ]
 
@@ -407,14 +407,14 @@ dashboardView model =
                     [ turbulenceView path ]
 
                 Err NoPipelines ->
-                    [ Html.map (always Noop) NoPipeline.view ]
+                    [ Html.div [ class "dashboard-no-content", css [ Css.height (Css.pct 100) ] ] [ (Html.map (always Noop) << Html.fromUnstyled) NoPipeline.view ] ]
 
                 Ok substate ->
-                    pipelinesView substate model.topBar.query ++ [ footerView substate ]
+                    [ Html.div [ class "dashboard-content" ] (pipelinesView substate model.topBar.query ++ [ footerView substate ]) ]
     in
         Html.div
             [ classList [ ( "dashboard", True ), ( "dashboard-hd", model.highDensity ) ] ]
-            [ Html.div [ class "dashboard-content" ] mainContent ]
+            mainContent
 
 
 noResultsView : String -> Html Msg
@@ -467,7 +467,7 @@ toggleView highDensity =
             else
                 Routes.dashboardHdRoute
     in
-        Html.a [ class "toggle-high-density", href route, ariaLabel "Toggle high-density view" ]
+        Html.a [ class "toggle-high-density", href route, attribute "aria-label" "Toggle high-density view" ]
             [ Html.div [ class <| "dashboard-pipeline-icon " ++ hdClass ] [], Html.text "high-density" ]
 
 
@@ -507,11 +507,11 @@ footerView substate =
                         [ Html.text "version: v", substate.teamData |> SubState.apiData |> .version |> Html.text ]
                     , Html.div [ class "concourse-cli" ]
                         [ Html.text "cli: "
-                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "darwin"), ariaLabel "Download OS X CLI" ]
+                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "darwin"), attribute "aria-label" "Download OS X CLI" ]
                             [ Html.i [ class "fa fa-apple" ] [] ]
-                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "windows"), ariaLabel "Download Windows CLI" ]
+                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "windows"), attribute "aria-label" "Download Windows CLI" ]
                             [ Html.i [ class "fa fa-windows" ] [] ]
-                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "linux"), ariaLabel "Download Linux CLI" ]
+                        , Html.a [ href (Concourse.Cli.downloadUrl "amd64" "linux"), attribute "aria-label" "Download Linux CLI" ]
                             [ Html.i [ class "fa fa-linux" ] [] ]
                         ]
                     ]
@@ -585,7 +585,7 @@ pipelinesView substate query =
         if List.isEmpty groupViews then
             [ noResultsView (toString query) ]
         else
-            List.map (Html.map GroupMsg) groupViews
+            List.map (Html.map GroupMsg << Html.fromUnstyled) groupViews
 
 
 handleKeyPressed : Char -> Model -> ( Model, Cmd Msg )
