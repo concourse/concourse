b018d5e4bb4b57b1344675b619d1dd2d0f9f3b12
Mark Huang <mhuang@pivotal.io>
2018-11-22 16:56:40 -0500

    atc: migrate paused resources to a pinned version

--- atc/db/migration/migrations/1538408345_add_api_pinned_version_to_resources.up.sql
+++ atc/db/migration/migrations/1538408345_add_api_pinned_version_to_resources.up.sql
@@ -1,4 +1,16 @@
 BEGIN;
   ALTER TABLE resources
     ADD COLUMN api_pinned_version jsonb;
+
+  WITH latest_resource_config_versions AS (
+    SELECT DISTINCT ON (resource_config_id)
+      resource_config_id, version
+    FROM resource_config_versions
+    ORDER BY resource_config_id, check_order DESC
+  ) UPDATE resources r
+    SET api_pinned_version = v.version
+    FROM latest_resource_config_versions v
+    WHERE r.resource_config_id = v.resource_config_id
+    AND r.paused = TRUE;
+
 COMMIT;
