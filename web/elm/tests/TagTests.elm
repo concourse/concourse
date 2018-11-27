0ddb443b55bdd18d272ea095787085c7d60b0f08
Rui Yang <ryang@pivotal.io>
2018-11-27 15:40:43 -0500

    web: tests for tag styles
    concourse/concourse#2843
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

---
+++ web/elm/tests/TagTests.elm
@@ -0,0 +1,50 @@
+module TagTests exposing (..)
+
+import Dashboard.Group.Tag as Tag
+import Expect exposing (Expectation)
+import Fuzz
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (style)
+
+
+tag : Fuzz.Fuzzer Tag.Tag
+tag =
+    Fuzz.frequency
+        [ ( 1, Fuzz.constant Tag.Owner )
+        , ( 1, Fuzz.constant Tag.Member )
+        , ( 1, Fuzz.constant Tag.Viewer )
+        ]
+
+
+white : String
+white =
+    "#fff"
+
+
+all : Test
+all =
+    describe "dashboard team role pill" <|
+        let
+            hasStyle : List ( String, String ) -> Tag.Tag -> Expectation
+            hasStyle styles =
+                Tag.view False
+                    >> Query.fromHtml
+                    >> Query.has [ style styles ]
+        in
+            [ fuzz tag "has a white border" <|
+                hasStyle [ ( "border", "1px solid " ++ white ) ]
+            , fuzz tag "has very small font" <|
+                hasStyle [ ( "font-size", "0.7em" ) ]
+            , fuzz tag "letters are spaced apart" <|
+                hasStyle [ ( "letter-spacing", "0.2em" ) ]
+            , fuzz tag "has a bit of padding above and below" <|
+                hasStyle
+                    [ ( "padding", "0.5em 0" )
+                    , ( "line-height", "0.9em" )
+                    ]
+            , fuzz tag "has a fixed width of 8 character widths" <|
+                hasStyle [ ( "width", "6em" ) ]
+            , fuzz tag "text is horizontally centered in the white box" <|
+                hasStyle [ ( "text-align", "center" ) ]
+            ]
