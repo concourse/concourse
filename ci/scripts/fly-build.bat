ee8ed17620cbac0b66f461689fa8425d16c92981
Ciro S. Costa <cscosta@pivotal.io>
2019-02-12 13:13:14 -0500

    Bump runc; reference 4.2.3 wip release branch; fix fly windows build
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>
    Co-authored-by: Sameer Vohra <svohra@pivotal.io>

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -7,6 +7,6 @@ set /p FinalVersion=<final-version\version
 
 go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o fly_windows_amd64.exe github.com/concourse/fly
 
-Copy-Item fly_windows_amd64.exe windows-binary/
+powershell.exe Copy-Item fly_windows_amd64.exe windows-binary/
 
 certUtil -hashfile .\windows-binary\fly_windows_amd64.exe SHA1 > .\windows-binary\fly_windows_amd64.exe.sha1
