cc499ba1c156b7b10bd9449cab772aeb1902e9c8
Ciro S. Costa <cscosta@pivotal.io>
2019-02-12 12:17:12 -0500

    Fix windows fly build issue
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>
    Co-authored-by: Sameer Vohra <svohra@pivotal.io>

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -5,6 +5,8 @@ set PATH=%CD%\concourse\bin;%PATH%
 
 set /p FinalVersion=<final-version\version
 
-go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o windows-binary/fly_windows_amd64.exe github.com/concourse/fly
+go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o fly_windows_amd64.exe github.com/concourse/fly
+
+Copy-Item fly_windows_amd64.exe windows-binary/
 
 certUtil -hashfile .\windows-binary\fly_windows_amd64.exe SHA1 > .\windows-binary\fly_windows_amd64.exe.sha1
