a350261d05e90a149d36ac0f04eab319b3d64af2
Topher Bullock <topherbullock@users.noreply.github.com>
2019-02-19 15:55:24 -0500

    improve generate-key command description
    Co-Authored-By: vito <suraci.alex@gmail.com>
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- cmd/concourse/command.go
+++ cmd/concourse/command.go
@@ -19,7 +19,7 @@ type ConcourseCommand struct {
 	LandWorker   land.LandWorkerCommand     `command:"land-worker" description:"Safely drain a worker's assignments for temporary downtime."`
 	RetireWorker retire.RetireWorkerCommand `command:"retire-worker" description:"Safely remove a worker from the cluster permanently."`
 
-	GenerateKey GenerateKeyCommand `command:"generate-key" description:"Generate RSA keys for worker auth."`
+	GenerateKey GenerateKeyCommand `command:"generate-key" description:"Generate RSA key for use with Concourse components."`
 }
 
 func (cmd ConcourseCommand) lessenRequirements(parser *flags.Parser) {
