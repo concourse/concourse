a5b65871b6ea8e4641a385983f36be0b97c001b6
Logan Hendricks <lhendricks88@gmail.com>
2019-02-26 16:09:59 -0700

    general audit log through accessor
    set up general audit logger on accessor handler
    Logs out on a debug level all received api requests with the associated user
    first stage in audit process
    
    Signed-off-by: Logan Hendricks <lhendricks88@gmail.com>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -1300,7 +1300,7 @@ func (cmd *RunCommand) constructAPIHandler(
 			checkWorkerTeamAccessHandlerFactory,
 		),
 		wrappa.NewConcourseVersionWrappa(concourse.Version),
-		wrappa.NewAccessorWrappa(accessFactory),
+		wrappa.NewAccessorWrappa(accessFactory, logger),
 	}
 
 	return api.NewHandler(
