017dbcc08a55af4878e7073c0666227d17c47fc0
Alex Suraci <suraci.alex@gmail.com>
2019-03-10 17:34:48 +0000

    atc: export SaveConfigResponse
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/responses.go
+++ atc/responses.go
@@ -3,3 +3,8 @@ package atc
 type ClearTaskCacheResponse struct {
 	CachesRemoved int64 `json:"caches_removed"`
 }
+
+type SaveConfigResponse struct {
+	Errors   []string        `json:"errors,omitempty"`
+	Warnings []ConfigWarning `json:"warnings,omitempty"`
+}
