ce59639cb98981678a51ffe99f9838176a9e9a6b
Bishoy Youssef <byoussef@pivotal.io>
2019-03-11 17:01:18 -0400

    topgun/k8s add tests for container_limits
    - added tests for container_limits for both ubuntu and cos, ubuntu doesn't seem to be working.
    
    for the issue: concourse/concourse#3102
    
    Signed-off-by: Bishoy Youssef <byoussef@pivotal.io>
    Co-authored-by: Bin Ju <bju@pivotal.io>

---
+++ topgun/k8s/container_limits_test.go
@@ -0,0 +1,111 @@
+package k8s_test
+
+import (
+	"fmt"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"time"
+
+	. "github.com/concourse/concourse/topgun"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Garden Config", func() {
+	var (
+		proxySession        *gexec.Session
+		releaseName         string
+		namespace           string
+		atcEndpoint         string
+		helmDeployTestFlags []string
+	)
+
+	BeforeEach(func() {
+		releaseName = fmt.Sprintf("topgun-gc-%d-%d", GinkgoRandomSeed(), GinkgoParallelNode())
+		namespace = releaseName
+		Run(nil, "kubectl", "create", "namespace", namespace)
+	})
+
+	JustBeforeEach(func() {
+		deployConcourseChart(releaseName, helmDeployTestFlags...)
+
+		waitAllPodsInNamespaceToBeReady(namespace)
+
+		By("Creating the web proxy")
+		proxySession, atcEndpoint = startPortForwarding(namespace, "service/" + releaseName+"-web", "8080")
+
+		By("Logging in")
+		fly.Login("test", "test", atcEndpoint)
+
+		Eventually(func() []Worker {
+			return getRunningWorkers(fly.GetWorkers())
+		}, 2*time.Minute, 10*time.Second).
+			ShouldNot(HaveLen(0))
+	})
+
+	AfterEach(func() {
+		helmDestroy(releaseName)
+		Wait(Start(nil, "kubectl", "delete", "namespace", namespace))
+		Wait(proxySession.Interrupt())
+	})
+
+	Context("passing a config map location to the worker to be used by gdn", func() {
+		BeforeEach(func() {
+			helmDeployTestFlags = []string{
+				`--set=worker.replicas=1`,
+				`--set=concourse.web.defaultTaskCpuLimit=512`,
+				`--set=concourse.web.defaultTaskMemoryLimit=1GB`,
+				"--set=worker.nodeSelector.nodeImage=" + "cos",
+			}
+		})
+
+		It("returns the configure default container limit", func() {
+			//buildSession := spawnFly("execute", "-c", "tasks/tiny.yml")
+			buildSession := fly.Start("execute", "-c", "../tasks/tiny.yml")
+			<-buildSession.Exited
+			Expect(buildSession.ExitCode()).To(Equal(0))
+
+			hijackSession := fly.Start(
+				"hijack",
+				"-b", "1",
+				"--", "sh", "-c",
+				"cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes; cat /sys/fs/cgroup/cpu/cpu.shares",
+			)
+			<-hijackSession.Exited
+
+			Expect(hijackSession.ExitCode()).To(Equal(0))
+			Expect(hijackSession).To(gbytes.Say("1073741824\n512"))
+		})
+	})
+
+	XContext("passing a config map location to the worker to be used by gdn", func() {
+		BeforeEach(func() {
+			helmDeployTestFlags = []string{
+				`--set=worker.replicas=1`,
+				`--set=concourse.web.defaultTaskCpuLimit=512`,
+				`--set=concourse.web.defaultTaskMemoryLimit=1GB`,
+				"--set=worker.nodeSelector.nodeImage=" + "ubuntu",
+			}
+		})
+
+		It("returns the configure default container limit", func() {
+			//buildSession := spawnFly("execute", "-c", "tasks/tiny.yml")
+			buildSession := fly.Start("execute", "-c", "../tasks/tiny.yml")
+			<-buildSession.Exited
+			Expect(buildSession.ExitCode()).To(Equal(0))
+
+			hijackSession := fly.Start(
+				"hijack",
+				"-b", "1",
+				"--", "sh", "-c",
+				"cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes; cat /sys/fs/cgroup/cpu/cpu.shares",
+			)
+			<-hijackSession.Exited
+
+			Expect(hijackSession.ExitCode()).To(Equal(0))
+			Expect(hijackSession).To(gbytes.Say("1073741824\n512"))
+		})
+	})
+
+
+})
