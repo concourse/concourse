78a3db0fe8c2c795ab0f0b68e7c4f5664f90fbb1
Alex Suraci <suraci.alex@gmail.com>
2019-03-22 12:36:06 +0000

    ci: random_name -> random_pet
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- ci/deployments/smoke/smoke.tf
+++ ci/deployments/smoke/smoke.tf
@@ -20,14 +20,14 @@ provider "google" {
 
 data "google_compute_zones" "available" {}
 
-resource "random_name" "smoke" {}
+resource "random_pet" "smoke" {}
 
 resource "google_compute_address" "smoke" {
-  name = "smoke-${random_name.smoke.result}-ip"
+  name = "smoke-${random_pet.smoke.result}-ip"
 }
 
 resource "google_compute_firewall" "bosh-director" {
-  name    = "smoke-${random_name.smoke.result}-allow-http"
+  name    = "smoke-${random_pet.smoke.result}-allow-http"
   network = "default"
 
   allow {
@@ -39,7 +39,7 @@ resource "google_compute_firewall" "bosh-director" {
 }
 
 resource "google_compute_instance" "smoke" {
-  name = "smoke-${random_name.smoke.result}"
+  name = "smoke-${random_pet.smoke.result}"
   machine_type = "custom-8-8192"
   zone = "${data.google_compute_zones.available.names[0]}"
   tags = ["smoke"]
