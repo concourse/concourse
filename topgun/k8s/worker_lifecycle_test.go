526b48ab57ad54a1d56cf85c8076e8b74fd0243f
Taylor Silva <tsilva@pivotal.io>
2019-04-04 14:17:54 -0400

    topgun/k8s: add coverage for worker lifecycle
    Signed-off-by: Taylor Silva <tsilva@pivotal.io>
    Co-authored-by: Bishoy Youssef <byoussef@pivotal.io>

---
+++ topgun/k8s/worker_lifecycle_test.go
@@ -0,0 +1,104 @@
+package k8s_test
+
+import (
+	"time"
+
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+
+	. "github.com/concourse/concourse/topgun"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Worker lifecycle", func() {
+	var (
+		proxySession           *gexec.Session
+		atcEndpoint            string
+		helmDeployTestFlags    []string
+	)
+
+	type Case struct {
+		TerminationGracePeriod    string
+		PipelineYamlFile          string
+		WorkerDisappearingTimeout time.Duration
+		RerunJob                  bool
+	}
+
+	DescribeTable("",
+		func(c Case) {
+			setReleaseNameAndNamespace("wl")
+
+			helmDeployTestFlags = []string{
+				`--set=worker.replicas=1`,
+				`--set=worker.terminationGracePeriodSeconds=` + c.TerminationGracePeriod,
+			}
+			deployConcourseChart(releaseName, helmDeployTestFlags...)
+
+			waitAllPodsInNamespaceToBeReady(namespace)
+
+			By("Creating the web proxy")
+			proxySession, atcEndpoint = startPortForwarding(namespace, "service/"+releaseName+"-web", "8080")
+
+			By("Logging in")
+			fly.Login("test", "test", atcEndpoint)
+
+			Eventually(func() []Worker {
+				return getRunningWorkers(fly.GetWorkers())
+			}, 2*time.Minute, 10*time.Second).
+				ShouldNot(HaveLen(0))
+
+			fly.Run("set-pipeline", "-n", "-c", "../pipelines/"+c.PipelineYamlFile, "-p", "some-pipeline")
+			fly.Run("unpause-pipeline", "-p", "some-pipeline")
+			fly.Run("trigger-job", "-j", "some-pipeline/simple-job")
+
+			Eventually(func() *gbytes.Buffer {
+				startSession := fly.Start("builds", "-j", "some-pipeline/simple-job")
+				<-startSession.Exited
+				return startSession.Out
+			}, 1*time.Minute, 1*time.Second).Should(gbytes.Say(".*started.*"))
+
+			By("deleting the worker pod")
+			Wait(Start(nil, "kubectl", "delete", "pod", "--namespace", namespace, releaseName+"-worker-0", "--wait=false"))
+
+			By("seeing that the worker state is retiring")
+			Eventually(func() string {
+				return fly.GetWorkers()[0].State
+			}, 10*time.Second, 2*time.Second).
+				Should(Equal("retiring"))
+
+			By("seeing that there are no workers")
+			Eventually(func() []Worker {
+				return getRunningWorkers(fly.GetWorkers())
+			}, c.WorkerDisappearingTimeout, 1*time.Second).
+				Should(HaveLen(0))
+
+			By("seeing that workers have been revived")
+			Eventually(func() []Worker {
+				return getRunningWorkers(fly.GetWorkers())
+			}, 2*time.Minute, 10*time.Second).
+				ShouldNot(HaveLen(0))
+
+			if c.RerunJob {
+				By("running the same job again and watching it to completion")
+				fly.Run("trigger-job", "-j", "some-pipeline/simple-job", "-w")
+			}
+			helmDestroy(releaseName)
+			Wait(Start(nil, "kubectl", "delete", "namespace", namespace, "--wait=false"))
+			Wait(proxySession.Interrupt())
+		},
+		Entry("", Case{
+			TerminationGracePeriod:    "600",
+			PipelineYamlFile:          "simple-pipeline.yml",
+			WorkerDisappearingTimeout: 1 * time.Minute,
+			RerunJob:                  true,
+		}),
+		Entry("", Case{
+			TerminationGracePeriod:    "30",
+			PipelineYamlFile:          "task-waiting.yml",
+			WorkerDisappearingTimeout: 40 * time.Second,
+			RerunJob:                  false,
+		}),
+	)
+})
