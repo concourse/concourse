17e281da576a0a23c30a900cc1159401e22eb606
Clara Fu <cfu@pivotal.io>
2019-05-24 13:42:38 -0400

    atc migration: default first_occurrence to false
    ...otherwise this fails for any existing data
    
    Signed-off-by: Clara Fu <cfu@pivotal.io>
    Co-authored-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/db/migration/migrations/1558709402_add_first_occurrence_to_build_resource_config_version_inputs.up.sql
+++ atc/db/migration/migrations/1558709402_add_first_occurrence_to_build_resource_config_version_inputs.up.sql
@@ -1,3 +1,3 @@
 BEGIN;
-  ALTER TABLE build_resource_config_version_inputs ADD COLUMN first_occurrence boolean NOT NULL;
+  ALTER TABLE build_resource_config_version_inputs ADD COLUMN first_occurrence boolean NOT NULL DEFAULT false;
 COMMIT;
