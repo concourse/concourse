6f11b00e019deeea0559f43954150a21be4aa6fa
Clara Fu <cfu@pivotal.io>
2019-05-24 13:47:35 -0400

    Revert "atc migration: default first_occurrence to false"
    not sure if we want to do it this way yet or if we want to actually set
    appropriate data for existing records
    
    This reverts commit 17e281da576a0a23c30a900cc1159401e22eb606.
    
    Co-authored-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/db/migration/migrations/1558709402_add_first_occurrence_to_build_resource_config_version_inputs.up.sql
+++ atc/db/migration/migrations/1558709402_add_first_occurrence_to_build_resource_config_version_inputs.up.sql
@@ -1,3 +1,3 @@
 BEGIN;
-  ALTER TABLE build_resource_config_version_inputs ADD COLUMN first_occurrence boolean NOT NULL DEFAULT false;
+  ALTER TABLE build_resource_config_version_inputs ADD COLUMN first_occurrence boolean NOT NULL;
 COMMIT;
