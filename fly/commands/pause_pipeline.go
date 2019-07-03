b09eee819411ef1798cb0608eef2ec02d3bd9846
Josh Zarrabi <jzarrabi@pivotal.io>
2019-07-03 09:13:15 -0400

    adds '--all' to the pause and unpause commands
    - will pause and unpause all pipleines
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- fly/commands/pause_pipeline.go
+++ fly/commands/pause_pipeline.go
@@ -9,7 +9,8 @@ import (
 )
 
 type PausePipelineCommand struct {
-	Pipeline flaghelpers.PipelineFlag `short:"p"  long:"pipeline" required:"true" description:"Pipeline to pause"`
+	Pipeline flaghelpers.PipelineFlag `short:"p"  long:"pipeline" description:"Pipeline to pause"`
+	All      bool                     `short:"a"  long:"all"      description:"Pause all pipelines"`
 }
 
 func (command *PausePipelineCommand) Validate() error {
@@ -17,12 +18,18 @@ func (command *PausePipelineCommand) Validate() error {
 }
 
 func (command *PausePipelineCommand) Execute(args []string) error {
+	if string(command.Pipeline) == "" && !command.All {
+		displayhelpers.Failf("Either a pipeline name or --all are required")
+	}
+
+	if string(command.Pipeline) != "" && command.All {
+		displayhelpers.Failf("A pipeline and --all can not both be specified")
+	}
+
 	err := command.Validate()
 	if err != nil {
 		return err
 	}
-
-	pipelineName := string(command.Pipeline)
 
 	target, err := rc.LoadTarget(Fly.Target, Fly.Verbose)
 	if err != nil {
@@ -34,15 +41,33 @@ func (command *PausePipelineCommand) Execute(args []string) error {
 		return err
 	}
 
-	found, err := target.Team().PausePipeline(pipelineName)
-	if err != nil {
-		return err
+	var pipelineNames []string
+	if string(command.Pipeline) != "" {
+		pipelineNames = []string{string(command.Pipeline)}
 	}
 
-	if found {
-		fmt.Printf("paused '%s'\n", pipelineName)
-	} else {
-		displayhelpers.Failf("pipeline '%s' not found\n", pipelineName)
+	if command.All {
+		pipelines, err := target.Team().ListPipelines()
+		if err != nil {
+			return err
+		}
+
+		for _, pipeline := range pipelines {
+			pipelineNames = append(pipelineNames, pipeline.Name)
+		}
+	}
+
+	for _, pipelineName := range pipelineNames {
+		found, err := target.Team().PausePipeline(pipelineName)
+		if err != nil {
+			return err
+		}
+
+		if found {
+			fmt.Printf("paused '%s'\n", pipelineName)
+		} else {
+			displayhelpers.Failf("pipeline '%s' not found\n", pipelineName)
+		}
 	}
 
 	return nil
