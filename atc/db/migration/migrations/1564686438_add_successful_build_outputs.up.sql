df0031eddea47744a10c13598240d1c3235bf396
Clara Fu <cfu@pivotal.io>
2019-08-02 14:12:57 -0400

    atc: update gin index
    Updates the GIN index on the successful build outputs table to turn fast
    update on false. This will make postgres update the index each time a
    row is inserted into the table instead of if fast update being on where
    it would've built up a pending list of inserted rows and inserted that
    pending list as a batch update onto the index. This feature is great for
    when the table the index is on does not get new rows often but for our
    case, the successful build outputs table is constantly being inserted
    with new rows so it actually made the performance slower.
    
    [#3602]
    
    Signed-off-by: Clara Fu <cfu@pivotal.io>

--- atc/db/migration/migrations/1564686438_add_successful_build_outputs.up.sql
+++ atc/db/migration/migrations/1564686438_add_successful_build_outputs.up.sql
@@ -7,7 +7,7 @@ BEGIN;
 
   CREATE UNIQUE INDEX successful_build_outputs_build_id_idx ON successful_build_outputs (build_id);
 
-  CREATE INDEX on successful_build_outputs USING GIN (outputs jsonb_path_ops);
+  CREATE INDEX on successful_build_outputs USING GIN (outputs jsonb_path_ops) WITH (FASTUPDATE = false);
 
   CREATE INDEX successful_build_outputs_job_id_idx ON successful_build_outputs (job_id);
 COMMIT;
