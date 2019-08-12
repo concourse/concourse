ac8778051a83c47427f908f36a160ad95f44f9af
Rui Yang <ryang@pivotal.io>
2019-08-13 04:41:20 +0800

    atc: update missing_since in volumes table too
    Signed-off-by: Rui Yang <ryang@pivotal.io>

--- atc/db/migration/migrations/1565366737_update_timestamp_type_containers_volumes_workers.down.sql
+++ atc/db/migration/migrations/1565366737_update_timestamp_type_containers_volumes_workers.down.sql
@@ -2,6 +2,9 @@ BEGIN;
   ALTER TABLE containers
     ALTER COLUMN missing_since TYPE timestamp without time zone;
 
+  ALTER TABLE volumes
+    ALTER COLUMN missing_since TYPE timestamp without time zone;
+
   ALTER TABLE workers
     ALTER COLUMN start_time TYPE integer USING extract(epoch from start_time)::integer;
 COMMIT;
--- atc/db/migration/migrations/1565366737_update_timestamp_type_containers_volumes_workers.up.sql
+++ atc/db/migration/migrations/1565366737_update_timestamp_type_containers_volumes_workers.up.sql
@@ -2,6 +2,9 @@ BEGIN;
   ALTER TABLE containers
     ALTER COLUMN missing_since TYPE timestamp with time zone;
 
+  ALTER TABLE volumes
+    ALTER COLUMN missing_since TYPE timestamp with time zone;
+
   ALTER TABLE workers
     ALTER COLUMN start_time TYPE timestamp with time zone USING to_timestamp(start_time);
 COMMIT;
