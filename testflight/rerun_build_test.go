a303c33cadd33b7ae2c03602d1ce3634ad647aae
Clara Fu <cfu@pivotal.io>
2019-08-14 15:57:13 -0400

    tf: add rerun build test
    [#4268]
    
    Signed-off-by: Clara Fu <cfu@pivotal.io>

---
+++ testflight/rerun_build_test.go
@@ -0,0 +1,77 @@
+package testflight_test
+
+import (
+	uuid "github.com/nu7hatch/gouuid"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+var _ = FDescribe("Rerunning a build", func() {
+	var hash string
+
+	BeforeEach(func() {
+		u, err := uuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		hash = u.String()
+	})
+
+	BeforeEach(func() {
+		setAndUnpausePipeline("fixtures/simple-trigger.yml", "-v", "hash="+hash)
+	})
+
+	Context("when there are two previous builds", func() {
+		BeforeEach(func() {
+			By("running on the initial version")
+			fly("check-resource", "-r", inPipeline("some-resource"), "-f", "version:first-version")
+			watch := waitForBuildAndWatch("some-passing-job")
+			Eventually(watch).Should(gbytes.Say("first-version"))
+
+			By("running again when there's a new version")
+			fly("check-resource", "-r", inPipeline("some-resource"), "-f", "version:second-version")
+			watch = waitForBuildAndWatch("some-passing-job", "2")
+			Eventually(watch).Should(gbytes.Say("second-version"))
+		})
+
+		It("creates a rerun of the first build", func() {
+			By("confirming that there are no rerun builds")
+			build := fly("builds", "-j", inPipeline("some-passing-job"))
+			Expect(build).ToNot(gbytes.Say("1.1"))
+
+			fly("rerun-build", "-j", inPipeline("some-passing-job"), "-b", "1", "-w")
+			watch := waitForBuildAndWatch("some-passing-job")
+			Eventually(watch).Should(gbytes.Say("first-version"))
+
+			By("running again to see if the rerun build appeared")
+			build = fly("builds", "-j", inPipeline("some-passing-job"))
+			Expect(build).To(gbytes.Say("1.1"))
+		})
+
+		Context("when there is a rerun build", func() {
+			BeforeEach(func() {
+				fly("rerun-build", "-j", inPipeline("some-passing-job"), "-b", "1", "-w")
+				watch := waitForBuildAndWatch("some-passing-job")
+				Eventually(watch).Should(gbytes.Say("first-version"))
+
+				By("running again to see if the rerun build appeared")
+				build := fly("builds", "-j", inPipeline("some-passing-job"))
+				Expect(build).To(gbytes.Say("1.1"))
+			})
+
+			It("rerunning a rerun will create a rerun of the first build", func() {
+				By("confirming that there is a rerun build")
+				build := fly("builds", "-j", inPipeline("some-passing-job"))
+				Expect(build).To(gbytes.Say("1.1"))
+
+				fly("rerun-build", "-j", inPipeline("some-passing-job"), "-b", "1.1", "-w")
+				watch := waitForBuildAndWatch("some-passing-job")
+				Eventually(watch).Should(gbytes.Say("first-version"))
+
+				By("running again to see if the rerun build appeared")
+				build = fly("builds", "-j", inPipeline("some-passing-job"))
+				Expect(build).To(gbytes.Say("1.2"))
+			})
+		})
+	})
+})
