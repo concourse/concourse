868eedc7f6368fb0fd6d1f970173086add620bb7
Clara Fu <cfu@pivotal.io>
2019-09-06 17:38:05 -0400

    add more indexes for rerun builds
    Signed-off-by: Clara Fu <cfu@pivotal.io>
    Co-authored-by: Alex Suraci <suraci.alex@gmail.com>

---
+++ atc/db/migration/migrations/1567800389_add_indexes_for_rerun_builds.down.sql
@@ -0,0 +1,7 @@
+BEGIN;
+  DROP INDEX ready_and_scheduled_ordering_with_rerun_builds_idx;
+
+  DROP INDEX succeeded_builds_ordering_with_rerun_builds_idx;
+
+  DROP INDEX successful_build_outputs_ordering_with_rerun_builds_idx;
+COMMIT;
