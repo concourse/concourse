08a5178771f64dc1bfcbbe8580302d912388a1ef
Joshua Winters <jwinters@pivotal.io>
2019-09-23 16:13:45 -0400

    Revert "atc: increase lock mutex timeout to 10s"
    This reverts commit dbe5a7533030245581af4a1b5b7694b569fffde9.
    
    Signed-off-by: Joshua Winters <jwinters@pivotal.io>

--- atc/db/lock/lock.go
+++ atc/db/lock/lock.go
@@ -27,7 +27,7 @@ const (
 
 var ErrLostLock = errors.New("lock was lost while held, possibly due to connection breakage")
 
-const lockTimeout = 10 * time.Second
+const lockTimeout = time.Second
 
 func NewBuildTrackingLockID(buildID int) LockID {
 	return LockID{LockTypeBuildTracking, buildID}
@@ -187,8 +187,6 @@ func (l *lock) Acquire() (bool, error) {
 
 		return true, nil
 	}
-
-	l.logger.Debug("timed-out-acquiring-lock")
 	return false, nil
 }
 
