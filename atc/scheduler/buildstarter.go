e03de0b26f4652fff6d6e3c7e428bd253d9e69c7
Jeff Jun <jjun@pivotal.io>
2019-10-25 11:09:49 -0400

    atc: refactor tryStartNextPendingBuild
    to be shorter, lower cyclomatic complexity
    
    Signed-off-by: Jeff Jun <jjun@pivotal.io>
    Co-authored-by: Jamie Klassen <cklassen@pivotal.io>

--- atc/scheduler/buildstarter.go
+++ atc/scheduler/buildstarter.go
@@ -81,13 +81,7 @@ func (s *buildStarter) tryStartNextPendingBuild(
 	})
 
 	if nextPendingBuild.IsAborted() {
-		logger.Debug("cancel-aborted-pending-build")
-		err := nextPendingBuild.Finish(db.BuildStatusAborted)
-		if err != nil {
-			return false, err
-		}
-
-		return true, nil
+		return finishCancelledBuild(logger, nextPendingBuild)
 	}
 
 	reachedMaxInFlight, err := s.maxInFlightUpdater.UpdateMaxInFlightReached(logger, job, nextPendingBuild.ID())
@@ -98,73 +92,24 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return false, nil
 	}
 
-	if nextPendingBuild.IsManuallyTriggered() {
-		for _, input := range job.Config().Inputs() {
-			resource, found := resources.Lookup(input.Resource)
-
-			if !found {
-				logger.Debug("failed-to-find-resource")
-				return false, nil
-			}
-
-			if resource.CurrentPinnedVersion() != nil {
-				continue
-			}
-
-			if nextPendingBuild.IsNewerThanLastCheckOf(resource) {
-				return false, nil
-			}
-		}
-
-		versions, err := s.pipeline.LoadVersionsDB()
-		if err != nil {
-			logger.Error("failed-to-load-versions-db", err)
-			return false, err
-		}
-
-		_, err = s.inputMapper.SaveNextInputMapping(logger, versions, job, resources)
-		if err != nil {
-			return false, err
-		}
-
-		dbResourceTypes, err := s.pipeline.ResourceTypes()
-		if err != nil {
-			return false, err
-		}
-		resourceTypes = dbResourceTypes.Deserialize()
-	}
-
-	buildInputs, found, err := job.GetNextBuildInputs()
-	if err != nil {
-		logger.Error("failed-to-get-next-build-inputs", err)
+	resourceTypes, prepared, err := s.prepareIfManuallyTriggered(nextPendingBuild, resourceTypes, job, resources, logger)
+	if !prepared {
 		return false, err
 	}
+
+	buildInputs, found, err := getNextBuildInputs(job, logger)
 	if !found {
-		return false, nil
-	}
-
-	pipelinePaused, err := s.pipeline.CheckPaused()
-	if err != nil {
-		logger.Error("failed-to-check-if-pipeline-is-paused", err)
-		return false, err
-	}
-	if pipelinePaused {
-		return false, nil
-	}
-
-	if job.Paused() {
-		return false, nil
-	}
-
-	updated, err := nextPendingBuild.Schedule()
-	if err != nil {
-		logger.Error("failed-to-update-build-to-scheduled", err)
 		return false, err
 	}
 
-	if !updated {
-		logger.Debug("build-already-scheduled")
-		return false, nil
+	paused, err := s.isJobOrPipelinePaused(logger, job)
+	if paused {
+		return false, err
+	}
+
+	scheduled, err := schedule(nextPendingBuild, logger)
+	if !scheduled {
+		return false, err
 	}
 
 	err = nextPendingBuild.UseInputs(buildInputs)
@@ -172,6 +117,54 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return false, err
 	}
 
+	return s.start(job, resources, resourceTypes, buildInputs, nextPendingBuild, logger), nil
+}
+
+func (s *buildStarter) prepareIfManuallyTriggered(
+	nextPendingBuild db.Build,
+	resourceTypes atc.VersionedResourceTypes,
+	job db.Job, resources db.Resources,
+	logger lager.Logger,
+) (atc.VersionedResourceTypes, bool, error) {
+	prepared := true
+	var err error
+	if nextPendingBuild.IsManuallyTriggered() {
+		resourceTypes, prepared, err = s.prepareManuallyTriggeredBuild(job, resources, logger, nextPendingBuild, resourceTypes)
+	}
+	return resourceTypes, prepared, err
+}
+
+func (s *buildStarter) start(
+	job db.Job,
+	resources db.Resources,
+	resourceTypes atc.VersionedResourceTypes,
+	buildInputs []db.BuildInput,
+	nextPendingBuild db.Build,
+	logger lager.Logger,
+) bool {
+	plan, err := s.factory.Create(job.Config(), configs(resources), resourceTypes, buildInputs)
+	if err != nil {
+		// Don't use ErrorBuild because it logs a build event, and this build hasn't started
+		if err = nextPendingBuild.Finish(db.BuildStatusErrored); err != nil {
+			logger.Error("failed-to-mark-build-as-errored", err)
+		}
+		return false
+	}
+	started, err := nextPendingBuild.Start(plan)
+	if err != nil {
+		logger.Error("failed-to-mark-build-as-started", err)
+		return false
+	}
+	if !started {
+		if err = nextPendingBuild.Finish(db.BuildStatusAborted); err != nil {
+			logger.Error("failed-to-mark-build-as-finished", err)
+		}
+		return false
+	}
+	return true
+}
+
+func configs(resources db.Resources) atc.ResourceConfigs {
 	resourceConfigs := atc.ResourceConfigs{}
 	for _, v := range resources {
 		resourceConfigs = append(resourceConfigs, atc.ResourceConfig{
@@ -181,28 +174,107 @@ func (s *buildStarter) tryStartNextPendingBuild(
 			Tags:   v.Tags(),
 		})
 	}
+	return resourceConfigs
+}
 
-	plan, err := s.factory.Create(job.Config(), resourceConfigs, resourceTypes, buildInputs)
+func schedule(nextPendingBuild db.Build, logger lager.Logger) (bool, error) {
+	updated, err := nextPendingBuild.Schedule()
 	if err != nil {
-		// Don't use ErrorBuild because it logs a build event, and this build hasn't started
-		if err = nextPendingBuild.Finish(db.BuildStatusErrored); err != nil {
-			logger.Error("failed-to-mark-build-as-errored", err)
-		}
+		logger.Error("failed-to-update-build-to-scheduled", err)
+		return false, err
+	}
+	if !updated {
+		logger.Debug("build-already-scheduled")
 		return false, nil
 	}
+	return true, nil
+}
 
-	started, err := nextPendingBuild.Start(plan)
+func getNextBuildInputs(job db.Job, logger lager.Logger) ([]db.BuildInput, bool, error) {
+	buildInputs, found, err := job.GetNextBuildInputs()
 	if err != nil {
-		logger.Error("failed-to-mark-build-as-started", err)
-		return false, nil
+		logger.Error("failed-to-get-next-build-inputs", err)
+		return nil, false, err
 	}
+	return buildInputs, found, nil
+}
 
-	if !started {
-		if err = nextPendingBuild.Finish(db.BuildStatusAborted); err != nil {
-			logger.Error("failed-to-mark-build-as-finished", err)
+func (s *buildStarter) isJobOrPipelinePaused(logger lager.Logger, job db.Job) (bool, error) {
+	pipelinePaused, err := s.pipeline.CheckPaused()
+	if err != nil {
+		logger.Error("failed-to-check-if-pipeline-is-paused", err)
+		return true, err
+	}
+	if pipelinePaused {
+		return true, nil
+	}
+	if job.Paused() {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (s *buildStarter) prepareManuallyTriggeredBuild(job db.Job, resources db.Resources, logger lager.Logger, nextPendingBuild db.Build, resourceTypes atc.VersionedResourceTypes, ) (atc.VersionedResourceTypes, bool, error) {
+	if shouldWaitForCheck(job, resources, logger, nextPendingBuild) {
+		return nil, false, nil
+	}
+	err := s.prepareInputMappingForManuallyTriggeredBuild(logger, job, resources)
+	if err != nil {
+		return nil, false, err
+	}
+	resourceTypes, err = s.prepareResourceTypesForManuallyTriggeredBuild(err, resourceTypes)
+	if err != nil {
+		return nil, false, err
+	}
+	return resourceTypes, true, nil
+}
+
+func (s *buildStarter) prepareResourceTypesForManuallyTriggeredBuild(err error, resourceTypes atc.VersionedResourceTypes) (atc.VersionedResourceTypes, error) {
+	dbResourceTypes, err := s.pipeline.ResourceTypes()
+	if err != nil {
+		return nil, err
+	}
+	return dbResourceTypes.Deserialize(), nil
+}
+
+func (s *buildStarter) prepareInputMappingForManuallyTriggeredBuild(logger lager.Logger, job db.Job, resources db.Resources) (error) {
+	versions, err := s.pipeline.LoadVersionsDB()
+	if err != nil {
+		logger.Error("failed-to-load-versions-db", err)
+		return err
+	}
+	_, err = s.inputMapper.SaveNextInputMapping(logger, versions, job, resources)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func shouldWaitForCheck(job db.Job, resources db.Resources, logger lager.Logger, nextPendingBuild db.Build) (bool) {
+	for _, input := range job.Config().Inputs() {
+		resource, found := resources.Lookup(input.Resource)
+
+		if !found {
+			logger.Debug("failed-to-find-resource")
+			return true
 		}
-		return false, nil
-	}
 
+		if resource.CurrentPinnedVersion() != nil {
+			continue
+		}
+
+		if nextPendingBuild.IsNewerThanLastCheckOf(resource) {
+			return true
+		}
+	}
+	return false
+}
+
+func finishCancelledBuild(logger lager.Logger, nextPendingBuild db.Build) (bool, error) {
+	logger.Debug("cancel-aborted-pending-build")
+	err := nextPendingBuild.Finish(db.BuildStatusAborted)
+	if err != nil {
+		return false, err
+	}
 	return true, nil
 }
