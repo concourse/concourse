1817c160441ac2f7fca80813bc245b363d86d9ce
Ciro S. Costa <cscosta@pivotal.io>
2019-11-18 14:08:36 +0000

    worker: add integration tests
    Previously, `backend` was being exercised only with mocks. While that's great
    for the purpose of unit testing our expectations, it does not validate whether
    with a real daemon (containerd) our expectatons match the reality.
    
    The tests added here focus solely on interacting with containerd via the
    backend, using the real containred client implemntation (libcontainerd.client),
    ensuring that we're able to go through the expected workflow:
    
    	1. starting the client & pinging containerd
    
    	2. creating a container
    
    	3. listing containers (getting ours back)
    
    	4. running a process within the container
    
    	5. destroying it
    
    Naturally, these simply fail at the moment :sweat_smile: the backend
    functionality for creating, listing and destroying still needs to be
    implemented.
    
    To run these, make sure you have `containerd` *already running*, and that you
    have access to the socket (by default you need to be uid=0).
    
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

---
+++ worker/backend/integration/integration_test.go
@@ -0,0 +1,77 @@
+package integration_test
+
+import (
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"code.cloudfoundry.org/garden"
+	"github.com/concourse/concourse/worker/backend"
+	"github.com/concourse/concourse/worker/backend/libcontainerd"
+	uuid "github.com/nu7hatch/gouuid"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type BackendSuite struct {
+	suite.Suite
+	*require.Assertions
+
+	backend backend.Backend
+	client  *libcontainerd.Client
+}
+
+func (s *BackendSuite) SetupTest() {
+	namespace := "test" + strconv.FormatInt(time.Now().UnixNano(), 10)
+
+	s.backend = backend.New(
+		libcontainerd.New("/run/containerd/containerd.sock"),
+		namespace,
+	)
+
+	s.NoError(s.backend.Start())
+}
+
+func (s *BackendSuite) TearDownTest() {
+	s.backend.Stop()
+}
+
+func (s *BackendSuite) TestPing() {
+	s.NoError(s.backend.Ping())
+}
+
+func (s *BackendSuite) TestContainerCreation() {
+	handle := mustCreateHandle()
+	rootfs, err := filepath.Abs("testdata/rootfs")
+	s.NoError(err)
+
+	_, err = s.backend.Create(garden.ContainerSpec{
+		Handle:     handle,
+		RootFSPath: "raw://" + rootfs,
+		Privileged: true,
+	})
+	s.NoError(err)
+
+	defer s.backend.Destroy(handle)
+
+	containers, err := s.backend.Containers(nil)
+	s.NoError(err)
+
+	s.Len(containers, 1)
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, &BackendSuite{
+		Assertions: require.New(t),
+	})
+}
+
+func mustCreateHandle() string {
+	u4, err := uuid.NewV4()
+	if err != nil {
+		panic("couldn't create new uuid")
+	}
+
+	return u4.String()
+}
