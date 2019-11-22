6baa3cdac6736fa805fb8e97426187f244662029
Sameer Vohra <svohra@pivotal.io>
2019-11-22 18:42:18 -0500

    add test resource_test for Signature
    Signed-off-by: Krishna Mannem <kmannem@pivotal.io>
    Signed-off-by: Sameer Vohra <vohra.sam@gmail.com>

--- atc/resource/resource_test.go
+++ atc/resource/resource_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/concourse/concourse/atc"
 	. "github.com/concourse/concourse/atc/resource"
 )
 
@@ -11,6 +12,40 @@ var _ = Describe("Resource", func() {
 	Describe("ResourcesDir", func() {
 		It("returns a file path with a prefix", func() {
 			Expect(ResourcesDir("some-prefix")).To(ContainSubstring("some-prefix"))
+		})
+	})
+
+	Describe("Signature", func(){
+		var (
+			resource Resource
+			source atc.Source
+			params atc.Params
+			version atc.Version
+
+			)
+
+		BeforeEach(func(){
+			source = atc.Source{ "some-source-key": "some-source-value"}
+			params = atc.Params{ "some-params-key": "some-params-value"}
+			version = atc.Version{ "some-version-key": "some-version-value"}
+
+			resource = NewResourceFactory().NewResource(source, params, version)
+		})
+
+		It("marshals the source, params and version", func(){
+			actualSignature, err := resource.Signature()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(actualSignature).To(MatchJSON(`{
+			  "source": {
+				"some-source-key": "some-source-value"
+			  },
+			  "params": {
+				"some-params-key": "some-params-value"
+			  },
+			  "version": {
+				"some-version-key": "some-version-value"
+			  }
+			}`))
 		})
 	})
 })
