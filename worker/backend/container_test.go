1c3366a1f8e606e4019c5c55d57c9ba1ac7c6750
Denise Yu <dyu@pivotal.io>
2019-12-19 15:08:57 -0500

    Tidy up tests and godocs
    * Re-order tests to mirror flow of source code
    * Remove specific mentions of Garden from godocs
    * Add assertion for task deletion error case
    
    Signed-off-by: Denise Yu <dyu@pivotal.io>

--- worker/backend/container_test.go
+++ worker/backend/container_test.go
@@ -2,12 +2,15 @@ package backend_test
 
 import (
 	"context"
+	"errors"
+	"syscall"
 	"testing"
+
+	"github.com/containerd/containerd/errdefs"
 
 	"code.cloudfoundry.org/garden"
 	"github.com/concourse/concourse/worker/backend"
 	"github.com/concourse/concourse/worker/backend/libcontainerd/libcontainerdfakes"
-	"github.com/containerd/containerd/errdefs"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -16,7 +19,7 @@ type ContainerSuite struct {
 	suite.Suite
 	*require.Assertions
 
-	backend backend.Backend
+	backend       backend.Backend
 	fakeContainer *libcontainerdfakes.FakeContainer
 
 	backendContainer garden.Container
@@ -27,24 +30,40 @@ func (s *ContainerSuite) SetupTest() {
 	s.backendContainer = backend.NewContainer(context.TODO(), s.fakeContainer)
 }
 
-func(s *ContainerSuite) TestStopUsesSIGTERM() {
+func (s *ContainerSuite) TestStopNonexistentTask() {
+	s.fakeContainer.TaskReturns(nil, errdefs.ErrNotFound)
+	err := s.backendContainer.Stop(true)
+
+	s.NoError(err)
+}
+
+func (s *ContainerSuite) TestStopUsesSIGTERM() {
 	fakeTask := &libcontainerdfakes.FakeTask{}
 	s.fakeContainer.TaskReturns(fakeTask, nil)
 	err := s.backendContainer.Stop(false)
+	_, signal, _ := fakeTask.KillArgsForCall(0)
+
+	s.Equal(signal, syscall.SIGTERM)
 	s.NoError(err)
 }
 
-func(s *ContainerSuite) TestStopUsesSIGKILL() {
+func (s *ContainerSuite) TestStopUsesSIGKILL() {
 	fakeTask := &libcontainerdfakes.FakeTask{}
 	s.fakeContainer.TaskReturns(fakeTask, nil)
 	err := s.backendContainer.Stop(true)
+	_, signal, _ := fakeTask.KillArgsForCall(0)
+
+	s.Equal(signal, syscall.SIGKILL)
 	s.NoError(err)
 }
 
-func (s *ContainerSuite) TestStopWithoutTaskExisted() {
-	s.fakeContainer.TaskReturns(nil, errdefs.ErrNotFound)
-	err := s.backendContainer.Stop(true)
-	s.NoError(err)
+func (s *ContainerSuite) TestStopKillTaskError() {
+	fakeTask := &libcontainerdfakes.FakeTask{}
+	s.fakeContainer.TaskReturns(fakeTask, nil)
+	fakeTask.KillReturns(errors.New("task-kill-error"))
+
+	err := s.backendContainer.Stop(false)
+	s.EqualError(err, "task-kill-error")
 }
 
 func TestSuite(t *testing.T) {
