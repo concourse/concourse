60912593f523261c6ba49ad71d28b043eb6b73c2
Denise Yu <dyu@pivotal.io>
2019-12-23 18:25:11 -0500

    Add TODO for topgun core
    Signed-off-by: Krishna Mannem <kmannem@pivotal.io>
    Signed-off-by: Sameer Vohra <vohra.sam@gmail.com>

--- topgun/core/containers_scoped_to_team_test.go
+++ topgun/core/containers_scoped_to_team_test.go
@@ -9,7 +9,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 )
-
+// TODO-Later Move this to a testflight test as there is no relevant IaaS state
 var _ = Describe("Container scope", func() {
 	Context("when the container is scoped to a team", func() {
 		BeforeEach(func() {
