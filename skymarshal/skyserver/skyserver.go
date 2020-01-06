d88500c5a8fe8bc72516173abead6aba4b3d9ab3
Rui Yang <ryang@pivotal.io>
2020-01-06 12:55:13 -0500

    skymarshal: add tests for callback error handling
    Signed-off-by: Rui Yang <ryang@pivotal.io>

--- skymarshal/skyserver/skyserver.go
+++ skymarshal/skyserver/skyserver.go
@@ -164,7 +164,7 @@ func (s *SkyServer) Callback(w http.ResponseWriter, r *http.Request) {
 	dexToken, err = token.UseIDToken(dexToken)
 	if err != nil {
 		logger.Error("failed-to-convert-id-token", err)
-		http.Error(w, err.Error(), http.StatusUnauthorized)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
