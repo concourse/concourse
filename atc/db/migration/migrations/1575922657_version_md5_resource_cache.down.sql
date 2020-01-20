10aa62b48e8b7506e0de464f3ee5da0f5a96daa8
Clara Fu <cfu@pivotal.io>
2020-01-20 16:35:14 -0500

    atc: use version column in resource caches
    Switch to using the version column in resource caches within the code
    because there can be cases where the resource config version does not
    exist for a resource cache (for example, when it is a fly execute build)
    
    Signed-off-by: Clara Fu <cfu@pivotal.io>
    Co-authored-by: Alex Suraci <suraci.alex@gmail.com>

--- atc/db/migration/migrations/1575922657_version_md5_resource_cache.down.sql
+++ atc/db/migration/migrations/1575922657_version_md5_resource_cache.down.sql
@@ -3,8 +3,6 @@ BEGIN;
 
   ALTER TABLE resource_caches DROP COLUMN version_md5;
 
-  COMMENT ON COLUMN resource_caches.version IS NULL;
-
   CREATE UNIQUE INDEX resource_caches_resource_config_id_version_params_hash_uniq
   ON resource_caches (resource_config_id, md5(version::text), params_hash);
 COMMIT;
