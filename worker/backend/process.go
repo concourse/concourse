334a7e11b4ae7913207890bbdce1e1be6a03eea7
Ciro S. Costa <cscosta@pivotal.io>
2020-02-04 08:21:55 -0500

    containerd: implement process methods
    the previous `backend.Process` implementation was completely no-op.
    
    this means that one couldn't run a process through `garden.Run` and, for
    instance, wait for it to finish, or while `hijack`'ing, have the
    terminal properly resized.
    
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- worker/backend/process.go
+++ worker/backend/process.go
@@ -1,14 +1,72 @@
 package backend
 
 import (
+	"context"
+	"fmt"
+
 	"code.cloudfoundry.org/garden"
+	"github.com/containerd/containerd"
 )
 
-type Process struct{}
+type Process struct {
+	process     containerd.Process
+	exitStatusC <-chan containerd.ExitStatus
+}
+
+func NewProcess(
+	p containerd.Process,
+	ch <-chan containerd.ExitStatus,
+) *Process {
+	return &Process{
+		process:     p,
+		exitStatusC: ch,
+	}
+}
 
 var _ garden.Process = (*Process)(nil)
 
-func (p *Process) ID() (id string)                           { return }
-func (p *Process) Wait() (somethingIDontKnow int, err error) { return }
-func (p *Process) SetTTY(spec garden.TTYSpec) (err error)    { return }
-func (p *Process) Signal(signal garden.Signal) (err error)   { return }
+func (p *Process) ID() string {
+	return p.process.ID()
+}
+
+func (p *Process) Wait() (int, error) {
+	status := <-p.exitStatusC
+	err := status.Error()
+	if err != nil {
+		return 0, fmt.Errorf("waiting for exit status: %w", err)
+	}
+
+	_, err = p.process.Delete(context.Background())
+	if err != nil {
+		return 0, fmt.Errorf("delete process: %w", err)
+	}
+
+	p.process.IO().Wait()
+
+	return int(status.ExitCode()), nil
+}
+
+// SetTTY
+//
+func (p *Process) SetTTY(spec garden.TTYSpec) error {
+	if spec.WindowSize == nil {
+		return nil
+	}
+
+	err := p.process.Resize(context.Background(),
+		uint32(spec.WindowSize.Columns),
+		uint32(spec.WindowSize.Rows),
+	)
+	if err != nil {
+		return fmt.Errorf("resize: %w", err)
+	}
+
+	return nil
+}
+
+// Signal - Not Implemented
+//
+func (p *Process) Signal(signal garden.Signal) (err error) {
+	err = ErrNotImplemented
+	return
+}
