663c7588aa6bb8b28fe47f150f6df1037727c577
Joshua Winters <jwinters@pivotal.io>
2020-02-04 14:33:10 -0500

    atc: backfill tests for missing auth roles
    Signed-off-by: Joshua Winters <jwinters@pivotal.io>
    Co-authored-by: Muntasir Chowdhury <mchowdhury@pivotal.io>

--- atc/api/accessor/accessor_test.go
+++ atc/api/accessor/accessor_test.go
@@ -2,6 +2,7 @@ package accessor_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	"github.com/concourse/concourse/atc"
@@ -166,50 +167,102 @@ var _ = Describe("Accessor", func() {
 					Expect(result).To(BeTrue())
 				})
 			})
-
-			Context("the team has the user configured as a 'member'", func() {
-
-				BeforeEach(func() {
-					fakeTeam1.NameReturns("some-team")
-					fakeTeam1.AuthReturns(atc.TeamAuth{
-						"member": map[string][]string{
-							"users": []string{"some-connector:some-user-id"},
-						},
-					})
-				})
-
-				Context("when the action requires a 'viewer' role", func() {
-					BeforeEach(func() {
-						requiredRole = "viewer"
-					})
-
-					It("returns true", func() {
-						Expect(result).To(BeTrue())
-					})
-				})
-
-				Context("when the action requires a 'member' role", func() {
-					BeforeEach(func() {
-						requiredRole = "member"
-					})
-
-					It("returns true", func() {
-						Expect(result).To(BeTrue())
-					})
-				})
-
-				Context("when the action requires an 'owner' role", func() {
-					BeforeEach(func() {
-						requiredRole = "owner"
-					})
-
-					It("returns false", func() {
-						Expect(result).To(BeFalse())
-					})
-				})
-			})
 		})
 	})
+
+	DescribeTable("IsAuthorized for users",
+		func(requiredRole string, actualRole string, expected bool) {
+
+			verification.HasToken = true
+			verification.IsTokenValid = true
+			verification.Claims = map[string]interface{}{
+				"federated_claims": map[string]interface{}{
+					"connector_id": "some-connector",
+					"user_id":      "some-user-id",
+				},
+			}
+
+			fakeTeam1.NameReturns("some-team")
+			fakeTeam1.AdminReturns(true)
+			fakeTeam1.AuthReturns(atc.TeamAuth{
+				actualRole: map[string][]string{
+					"users": []string{"some-connector:some-user-id"},
+				},
+			})
+
+			access = accessor.NewAccessor(verification, requiredRole, "sub", []string{"system"}, teams)
+			result := access.IsAuthorized("some-team")
+			Expect(expected).Should(Equal(result))
+		},
+
+
+		Entry("viewer attempting viewer action", "viewer", "viewer", true),
+		Entry("pipeline-operator attempting viewer action", "viewer", "pipeline-operator", true),
+		Entry("member attempting viewer action", "viewer", "member", true),
+		Entry("owner attempting viewer action", "viewer", "owner", true),
+
+		Entry("viewer attempting pipeline-operator action", "pipeline-operator", "viewer", false),
+		Entry("pipeline-operator attempting pipeline-operator action", "pipeline-operator", "pipeline-operator", true),
+		Entry("member attempting pipeline-operator action", "pipeline-operator", "member", true),
+		Entry("owner attempting pipeline-operator action", "pipeline-operator", "owner", true),
+
+		Entry("viewer attempting member action", "member", "viewer", false),
+		Entry("pipeline-operator attempting member action", "member", "pipeline-operator", false),
+		Entry("member attempting member action", "member", "member", true),
+		Entry("owner attempting member action", "member", "owner", true),
+
+		Entry("viewer attempting owner action", "owner", "viewer", false),
+		Entry("pipeline-operator attempting owner action", "owner", "pipeline-operator", false),
+		Entry("member attempting owner action", "owner", "member", false),
+		Entry("owner attempting owner action", "owner", "owner", true),
+	)
+
+	DescribeTable("IsAuthorized for groups",
+		func(requiredRole string, actualRole string, expected bool) {
+
+			verification.HasToken = true
+			verification.IsTokenValid = true
+			verification.Claims = map[string]interface{}{
+				"groups": []string{"some-group"},
+				"federated_claims": map[string]interface{}{
+					"connector_id": "some-connector",
+				},
+			}
+
+			fakeTeam1.NameReturns("some-team")
+			fakeTeam1.AdminReturns(true)
+			fakeTeam1.AuthReturns(atc.TeamAuth{
+				actualRole: map[string][]string{
+					"groups": []string{"some-connector:some-group"},
+				},
+			})
+
+			access = accessor.NewAccessor(verification, requiredRole, "sub", []string{"system"}, teams)
+			result := access.IsAuthorized("some-team")
+			Expect(expected).Should(Equal(result))
+		},
+
+
+		Entry("viewer attempting viewer action", "viewer", "viewer", true),
+		Entry("pipeline-operator attempting viewer action", "viewer", "pipeline-operator", true),
+		Entry("member attempting viewer action", "viewer", "member", true),
+		Entry("owner attempting viewer action", "viewer", "owner", true),
+
+		Entry("viewer attempting pipeline-operator action", "pipeline-operator", "viewer", false),
+		Entry("pipeline-operator attempting pipeline-operator action", "pipeline-operator", "pipeline-operator", true),
+		Entry("member attempting pipeline-operator action", "pipeline-operator", "member", true),
+		Entry("owner attempting pipeline-operator action", "pipeline-operator", "owner", true),
+
+		Entry("viewer attempting member action", "member", "viewer", false),
+		Entry("pipeline-operator attempting member action", "member", "pipeline-operator", false),
+		Entry("member attempting member action", "member", "member", true),
+		Entry("owner attempting member action", "member", "owner", true),
+
+		Entry("viewer attempting owner action", "owner", "viewer", false),
+		Entry("pipeline-operator attempting owner action", "owner", "pipeline-operator", false),
+		Entry("member attempting owner action", "owner", "member", false),
+		Entry("owner attempting owner action", "owner", "owner", true),
+	)
 
 	Describe("TeamNames", func() {
 		var result []string
