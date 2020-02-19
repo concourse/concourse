b3b16ebc18a7a6ae6e47476d187945d2b129ef37
Alex Suraci <suraci.alex@gmail.com>
2020-02-19 13:13:19 -0500

    generate metrics based on prometheus sample
    adds a \prometheus-docs plugin function which parses a Prometheus expfmt
    sample and uses it to generate documentation for each metric contained
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- go/docs/plugin.go
+++ go/docs/plugin.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha1"
 	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
 	"time"
 
 	"github.com/blang/semver"
 	"github.com/google/go-github/github"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"golang.org/x/oauth2"
 
 	"github.com/vito/booklit"
@@ -840,6 +844,70 @@ func (p Plugin) ReleaseDate(date string) error {
 	})
 
 	return nil
+}
+
+func (p Plugin) PromethusDocs(sample string) (booklit.Content, error) {
+	parser := new(expfmt.TextParser)
+
+	mfs, err := parser.TextToMetricFamilies(bytes.NewBufferString(sample))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse prometheus sample: %w", err)
+	}
+
+	type metric struct {
+		name   string
+		family *dto.MetricFamily
+	}
+
+	metrics := booklit.Sequence{}
+
+	sorted := []metric{}
+	for metricName, family := range mfs {
+		sorted = append(sorted, metric{
+			name:   metricName,
+			family: family,
+		})
+	}
+
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i].name < sorted[j].name
+	})
+
+	for _, metric := range sorted {
+		metricName := metric.name
+		family := metric.family
+
+		distinctLabels := map[string]bool{}
+
+		labels := booklit.Sequence{}
+		for _, metric := range family.GetMetric() {
+			for _, label := range metric.GetLabel() {
+				labelName := label.GetName()
+
+				if distinctLabels[labelName] {
+					continue
+				}
+
+				distinctLabels[labelName] = true
+
+				labels = append(labels, booklit.String(labelName))
+			}
+		}
+
+		metricType := strings.ToLower(family.GetType().String())
+
+		metrics = append(metrics, booklit.Styled{
+			Style:   "prometheus-metric",
+			Content: booklit.String(family.GetHelp()),
+			Partials: booklit.Partials{
+				"Name":   booklit.String(metricName),
+				"Type":   booklit.String(metricType),
+				"Labels": labels,
+			},
+		})
+	}
+
+	return metrics, nil
 }
 
 type NoIndex struct {
