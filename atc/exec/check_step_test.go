ef039c956e532d58a8ba9ad53a43eba102a3b5ae
Ciro S. Costa <cscosta@pivotal.io>
2020-03-04 12:50:39 -0500

    atc: test args used when calling RunCheckStep
    the previous set of tests were mostly focused on the failure branches,
    lacking verifying that we're creating the structs with what we expect,
    and actually using them when calling RunCheckStep.
    
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- atc/exec/check_step_test.go
+++ atc/exec/check_step_test.go
@@ -3,12 +3,14 @@ package exec_test
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/concourse/concourse/atc"
 	"github.com/concourse/concourse/atc/db"
 	"github.com/concourse/concourse/atc/exec"
 	"github.com/concourse/concourse/atc/exec/execfakes"
 	"github.com/concourse/concourse/atc/resource/resourcefakes"
+	"github.com/concourse/concourse/atc/worker"
 	"github.com/concourse/concourse/atc/worker/workerfakes"
 	"github.com/concourse/concourse/vars/varsfakes"
 
@@ -21,13 +23,16 @@ var _ = Describe("CheckStep", func() {
 	var (
 		fakeRunState        *execfakes.FakeRunState
 		fakeResourceFactory *resourcefakes.FakeResourceFactory
+		fakeResource        *resourcefakes.FakeResource
 		fakePool            *workerfakes.FakePool
 		fakeStrategy        *workerfakes.FakeContainerPlacementStrategy
 		fakeDelegate        *execfakes.FakeCheckDelegate
 		fakeClient          *workerfakes.FakeClient
 
-		checkStep *exec.CheckStep
-		checkPlan atc.CheckPlan
+		stepMetadata      exec.StepMetadata
+		checkStep         *exec.CheckStep
+		checkPlan         atc.CheckPlan
+		containerMetadata db.ContainerMetadata
 
 		err error
 	)
@@ -35,15 +40,19 @@ var _ = Describe("CheckStep", func() {
 	BeforeEach(func() {
 		fakeRunState = new(execfakes.FakeRunState)
 		fakeResourceFactory = new(resourcefakes.FakeResourceFactory)
+		fakeResource = new(resourcefakes.FakeResource)
 		fakePool = new(workerfakes.FakePool)
 		fakeStrategy = new(workerfakes.FakeContainerPlacementStrategy)
 		fakeDelegate = new(execfakes.FakeCheckDelegate)
 		fakeClient = new(workerfakes.FakeClient)
+
+		stepMetadata = exec.StepMetadata{}
+		containerMetadata = db.ContainerMetadata{}
+
+		fakeResourceFactory.NewResourceReturns(fakeResource)
 	})
 
 	JustBeforeEach(func() {
-		containerMetadata := db.ContainerMetadata{}
-		stepMetadata := exec.StepMetadata{}
 		planID := atc.PlanID("some-plan-id")
 
 		checkStep = exec.NewCheckStep(
@@ -139,9 +148,137 @@ var _ = Describe("CheckStep", func() {
 
 	Context("with a resonable configuration", func() {
 		BeforeEach(func() {
-			checkPlan = atc.CheckPlan{
-				Timeout: "10s",
+			resTypes := atc.VersionedResourceTypes{
+				{
+					ResourceType: atc.ResourceType{
+						Source: atc.Source{
+							"foo": "((bar))",
+						},
+					},
+				},
 			}
+
+			checkPlan = atc.CheckPlan{
+				Timeout:                "10s",
+				Type:                   "resource-type",
+				Tags:                   []string{"tag"},
+				VersionedResourceTypes: resTypes,
+			}
+
+			containerMetadata = db.ContainerMetadata{
+				User: "test-user",
+			}
+
+			stepMetadata = exec.StepMetadata{
+				TeamID:             345,
+				ResourceConfigID:   501,
+				BaseResourceTypeID: 502,
+			}
+
+			fakeCredVarsTracker := new(varsfakes.FakeCredVarsTracker)
+			fakeCredVarsTracker.GetReturns("caz", true, nil)
+			fakeDelegate.VariablesReturns(fakeCredVarsTracker)
+		})
+
+		It("uses ResourceConfigCheckSessionOwner", func() {
+			_, _, owner, _, _, _, _, _, _, _ := fakeClient.RunCheckStepArgsForCall(0)
+			expected := db.NewResourceConfigCheckSessionContainerOwner(
+				501,
+				502,
+				db.ContainerOwnerExpiries{Min: 5 * time.Minute, Max: 1 * time.Hour},
+			)
+
+			Expect(owner).To(Equal(expected))
+		})
+
+		Context("uses containerspec", func() {
+			var containerSpec worker.ContainerSpec
+
+			JustBeforeEach(func() {
+				_, _, _, containerSpec, _, _, _, _, _, _ = fakeClient.RunCheckStepArgsForCall(0)
+			})
+
+			It("with certs volume mount", func() {
+				Expect(containerSpec.BindMounts).To(HaveLen(1))
+				mount := containerSpec.BindMounts[0]
+
+				_, ok := mount.(*worker.CertsVolumeMount)
+				Expect(ok).To(BeTrue())
+			})
+
+			It("with imagespec w/ resource type", func() {
+				Expect(containerSpec.ImageSpec).To(Equal(worker.ImageSpec{
+					ResourceType: "resource-type",
+				}))
+			})
+
+			It("with tags set", func() {
+				Expect(containerSpec.Tags).To(ConsistOf("tag"))
+			})
+
+			It("with teamid set", func() {
+				Expect(containerSpec.TeamID).To(Equal(345))
+			})
+
+			It("with env vars", func() {
+				Expect(containerSpec.Env).To(ContainElement("BUILD_TEAM_ID=345"))
+			})
+		})
+
+		Context("uses workerspec", func() {
+			var workerSpec worker.WorkerSpec
+
+			JustBeforeEach(func() {
+				_, _, _, _, workerSpec, _, _, _, _, _ = fakeClient.RunCheckStepArgsForCall(0)
+			})
+
+			It("with resource type", func() {
+				Expect(workerSpec.ResourceType).To(Equal("resource-type"))
+			})
+
+			It("with tags", func() {
+				Expect(workerSpec.Tags).To(ConsistOf("tag"))
+			})
+
+			It("with resource types", func() {
+				Expect(workerSpec.ResourceTypes).To(HaveLen(1))
+				interpolatedResourceType := workerSpec.ResourceTypes[0]
+
+				Expect(interpolatedResourceType.Source).To(Equal(atc.Source{"foo": "caz"}))
+			})
+
+			It("with teamid", func() {
+				Expect(workerSpec.TeamID).To(Equal(345))
+			})
+		})
+
+		It("uses container placement strategy", func() {
+			_, _, _, _, _, strategy, _, _, _, _ := fakeClient.RunCheckStepArgsForCall(0)
+			Expect(strategy).To(Equal(fakeStrategy))
+		})
+
+		It("uses container metadata", func() {
+			_, _, _, _, _, _, metadata, _, _, _ := fakeClient.RunCheckStepArgsForCall(0)
+			Expect(metadata).To(Equal(containerMetadata))
+		})
+
+		It("uses interpolated resource types", func() {
+			_, _, _, _, _, _, _, resourceTypes, _, _ := fakeClient.RunCheckStepArgsForCall(0)
+
+			Expect(resourceTypes).To(HaveLen(1))
+			interpolatedResourceType := resourceTypes[0]
+
+			Expect(interpolatedResourceType.Source).To(Equal(atc.Source{"foo": "caz"}))
+		})
+
+		It("uses the timeout parsed", func() {
+			_, _, _, _, _, _, _, _, timeout, _ := fakeClient.RunCheckStepArgsForCall(0)
+			Expect(timeout).To(Equal(10 * time.Second))
+		})
+
+		It("uses the resource created", func() {
+			_, _, _, _, _, _, _, _, _, resource := fakeClient.RunCheckStepArgsForCall(0)
+			Expect(resource).To(Equal(fakeResource))
 		})
 
 		Context("having RunCheckStep erroring", func() {
@@ -149,8 +286,7 @@ var _ = Describe("CheckStep", func() {
 
 			BeforeEach(func() {
 				expectedErr = errors.New("run-check-step-err")
-
-				fakeClient.RunCheckStepReturns(nil, expectedErr)
+				fakeClient.RunCheckStepReturns(worker.CheckResult{}, expectedErr)
 			})
 
 			It("errors", func() {
