43cbaad3c5b8b01f64f8a53fcd803969e33f7b69
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-03-06 13:52:15 -0500

    web: add fetch jobs endpoint
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- web/elm/src/Api/Endpoints.elm
+++ web/elm/src/Api/Endpoints.elm
@@ -6,20 +6,37 @@ import Url.Builder
 
 type Endpoint
     = Job Concourse.JobIdentifier
+    | Jobs Concourse.PipelineIdentifier
 
 
 toUrl : Endpoint -> String
 toUrl endpoint =
+    let
+        basePath =
+            [ "api", "v1" ]
+    in
     case endpoint of
         Job { teamName, pipelineName, jobName } ->
             Url.Builder.absolute
-                [ "api"
-                , "v1"
-                , "teams"
-                , teamName
-                , "pipelines"
-                , pipelineName
-                , "jobs"
-                , jobName
-                ]
+                (basePath
+                    ++ [ "teams"
+                       , teamName
+                       , "pipelines"
+                       , pipelineName
+                       , "jobs"
+                       , jobName
+                       ]
+                )
+                []
+
+        Jobs { teamName, pipelineName } ->
+            Url.Builder.absolute
+                (basePath
+                    ++ [ "teams"
+                       , teamName
+                       , "pipelines"
+                       , pipelineName
+                       , "jobs"
+                       ]
+                )
                 []
