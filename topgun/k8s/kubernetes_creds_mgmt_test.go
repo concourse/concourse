de6d1912bcc9fc1c033358567df96e88b6fdefa7
Jamie Klassen <cklassen@pivotal.io>
2020-04-23 19:57:33 -0400

    topgun/k8s: fix duration format
    Now that we're using stricter env var parsing, these syntax errors are causing
    k8s-topgun failures. In fact, this means that the properties were never getting
    parsed properly, and therefore that the cache duration was effectively the
    default at the binary level, which, at the time of writing, is 1 minute.
    
    Let's keep an eye out for whether this change increases the running time of
    k8s-topgun.
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- topgun/k8s/kubernetes_creds_mgmt_test.go
+++ topgun/k8s/kubernetes_creds_mgmt_test.go
@@ -60,7 +60,7 @@ var _ = Describe("Kubernetes credential management", func() {
 		var cachingSetup = func() {
 			deployConcourseChart(releaseName, "--set=worker.replicas=1",
 				"--set=concourse.web.secretCacheEnabled=true",
-				"--set=concourse.web.secretCacheDuration=600",
+				"--set=concourse.web.secretCacheDuration=600s",
 			)
 		}
 
@@ -70,7 +70,7 @@ var _ = Describe("Kubernetes credential management", func() {
 
 			deployConcourseChart(releaseName, "--set=worker.replicas=1",
 				"--set=concourse.web.secretCacheEnabled=true",
-				"--set=concourse.web.secretCacheDuration=600",
+				"--set=concourse.web.secretCacheDuration=600s",
 				"--set=concourse.web.kubernetes.createTeamNamespaces=false",
 			)
 		}
