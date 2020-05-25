8f2668a649fdd79a18cfc669d87b680c33a21453
Zoe Tian <ztian@pivotal.io>
2020-05-25 15:04:09 -0400

    tracing: behaviour: configure syncer => provider
    concourse/concourse#5654
    
    Now, consumers of the tracing package must supply a trace.Provider
    rather than an export.SpanSyncer -- this involves a bit more boilerplate
    for the existing configurations, so we exposed a TraceProvider helper
    method. The flexibility of providing the entire Provider rather than
    simply the Exporter will make testing easiser.
    
    Signed-off-by: Zoe Tian <ztian@pivotal.io>
    Co-authored-by: Jamie Klassen <cklassen@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -16,6 +16,8 @@ import (
 	"strings"
 	"time"
 
+	export "go.opentelemetry.io/otel/sdk/export/trace"
+
 	"code.cloudfoundry.org/clock"
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagerctx"
