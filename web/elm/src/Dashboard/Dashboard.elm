6bfc52e9c8244a90f63911a36e9efcc9883c68bf
Jamie Klassen <jklassen@vmware.com>
2020-05-29 10:56:03 -0400

    web: behaviour: fresh data over stale
    Signed-off-by: Jamie Klassen <jklassen@vmware.com>

--- web/elm/src/Dashboard/Dashboard.elm
+++ web/elm/src/Dashboard/Dashboard.elm
@@ -460,14 +460,19 @@ handleDeliveryBody delivery ( model, effects ) =
             ( model, effects ++ [ GetViewportOf Dashboard AlwaysShow ] )
 
         CachedPipelinesReceived (Ok pipelines) ->
-            let
-                newPipelines =
-                    pipelines
-                        |> List.map (toDashboardPipeline True (model.jobsError == Just Disabled))
-                        |> Just
-            in
-            if newPipelines |> pipelinesChangedFrom model.pipelines then
-                ( { model | pipelines = newPipelines }, effects )
+            if model.pipelines == Nothing then
+                ( { model
+                    | pipelines =
+                        pipelines
+                            |> List.map
+                                (toDashboardPipeline
+                                    True
+                                    (model.jobsError == Just Disabled)
+                                )
+                            |> Just
+                  }
+                , effects
+                )
 
             else
                 ( model, effects )
