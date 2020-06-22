fe36aa393645ca9ed9b54ac0b9fc38abee4c2ee9
toby lorne <toby@toby.codes>
2020-06-23 00:05:51 +0100

    testflight: set-pipeline uses fixture file
    Using ((vars)) means we don't have to write a temporary pipeline file
    
    We can also use the suite helpers like setAndUnpausePipeline and take
    advantage of the auto-generated random pipeline name
    
    Using the autogenerated pipeline name means "second-sp" doesn't make
    sense, so rename that as well
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- testflight/set-pipeline-step_test.go
+++ testflight/set-pipeline-step_test.go
@@ -1,89 +1,29 @@
 package testflight_test
 
 import (
-	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 )
 
 var _ = Describe("set-pipeline Step", func() {
 	const (
-		pipelineName       = "first-sp"
-		secondPipelineName = "second-sp"
+		createdPipelineName = "created-pipeline"
 	)
 
-	const pipelineContent = `---
-resources:
-- name: some-resource
-  type: mock
-  source:
-    create_files:
-      pipeline.yml: |
-        ---
-        jobs:
-        - name: normal-job
-          public: true
-          plan:
-          - task: a-task
-            config:
-              platform: linux
-              image_resource:
-                type: mock
-                source: {mirror_self: true}
-              run:
-                path: echo
-                args: ["hello world"]
-      name.yml: |
-        ---
-        name: somebody
-
-jobs:
-- name: sp
-  public: true
-  plan:
-    - get: some-resource
-    - set_pipeline: %s
-      team: ((team_name))
-      file: some-resource/pipeline.yml
-      var_files:
-      - some-resource/name.yml
-      vars:
-        greetings: hello`
-
 	var (
-		fixture          string
 		currentTeamName  string
 		currentFlyTarget string
 	)
 
-	BeforeEach(func() {
-		fixture = filepath.Join(tmp, "fixture")
-
-		err := os.MkdirAll(fixture, 0755)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
 	JustBeforeEach(func() {
-		err := ioutil.WriteFile(
-			filepath.Join(fixture, pipelineName+".yml"),
-			[]byte(fmt.Sprintf(pipelineContent, secondPipelineName)),
-			0755,
-		)
-		Expect(err).NotTo(HaveOccurred())
-
 		withFlyTarget(currentFlyTarget, func() {
-			fly(
-				"set-pipeline", "-n",
-				"-p", pipelineName,
-				"-c", fixture+"/"+pipelineName+".yml",
+			setAndUnpausePipeline(
+				"fixtures/set-pipeline.yml",
 				"-v", "team_name="+currentTeamName,
+				"-v", "pipeline_name="+createdPipelineName,
 			)
-			fly("unpause-pipeline", "-p", pipelineName)
 		})
 	})
 
@@ -100,23 +40,23 @@ jobs:
 		})
 
 		AfterEach(func() {
-			fly("destroy-pipeline", "-n", "-p", secondPipelineName)
+			fly("destroy-pipeline", "-n", "-p", createdPipelineName)
 		})
 
 		It("sets the other pipeline", func() {
 			By("second pipeline should initially not exist")
-			execS := spawnFly("get-pipeline", "-p", secondPipelineName)
+			execS := spawnFly("get-pipeline", "-p", createdPipelineName)
 			<-execS.Exited
 			Expect(execS).To(gexec.Exit(1))
 			Expect(execS.Err).To(gbytes.Say("pipeline not found"))
 
 			By("set-pipeline step should succeed")
 			execS = fly("trigger-job", "-w", "-j", pipelineName+"/sp")
-			Expect(execS.Out).To(gbytes.Say("setting pipeline: second-sp"))
+			Expect(execS.Out).To(gbytes.Say("setting pipeline: created-pipeline"))
 			Expect(execS.Out).To(gbytes.Say("done"))
 
 			By("should trigger the second pipeline job successfully")
-			execS = fly("trigger-job", "-w", "-j", secondPipelineName+"/normal-job")
+			execS = fly("trigger-job", "-w", "-j", createdPipelineName+"/normal-job")
 			Expect(execS.Out).To(gbytes.Say("hello world"))
 		})
 	})
@@ -130,7 +70,7 @@ jobs:
 		It("sets the other pipeline", func() {
 			By("second pipeline should initially not exist")
 			withFlyTarget(testflightFlyTarget, func() {
-				execS := spawnFly("get-pipeline", "-p", secondPipelineName)
+				execS := spawnFly("get-pipeline", "-p", createdPipelineName)
 				<-execS.Exited
 				Expect(execS).To(gexec.Exit(1))
 				Expect(execS.Err).To(gbytes.Say("pipeline not found"))
@@ -139,20 +79,20 @@ jobs:
 			By("set-pipeline step should succeed")
 			withFlyTarget(adminFlyTarget, func() {
 				execS := fly("trigger-job", "-w", "-j", pipelineName+"/sp")
-				Expect(execS.Out).To(gbytes.Say("setting pipeline: second-sp"))
+				Expect(execS.Out).To(gbytes.Say("setting pipeline: created-pipeline"))
 				Expect(execS.Out).To(gbytes.Say("done"))
 			})
 
 			By("should trigger the second pipeline job successfully")
 			withFlyTarget(testflightFlyTarget, func() {
-				execS := fly("trigger-job", "-w", "-j", secondPipelineName+"/normal-job")
+				execS := fly("trigger-job", "-w", "-j", createdPipelineName+"/normal-job")
 				Expect(execS.Out).To(gbytes.Say("hello world"))
 			})
 		})
 
 		AfterEach(func() {
 			withFlyTarget(testflightFlyTarget, func() {
-				fly("destroy-pipeline", "-n", "-p", secondPipelineName)
+				fly("destroy-pipeline", "-n", "-p", createdPipelineName)
 			})
 		})
 	})
@@ -166,7 +106,7 @@ jobs:
 		It("fails to set the other pipeline", func() {
 			By("second pipeline should initially not exist")
 			withFlyTarget(adminFlyTarget, func() {
-				execS := spawnFly("get-pipeline", "-p", secondPipelineName)
+				execS := spawnFly("get-pipeline", "-p", createdPipelineName)
 				<-execS.Exited
 				Expect(execS).To(gexec.Exit(1))
 				Expect(execS.Err).To(gbytes.Say("pipeline not found"))
@@ -183,7 +123,7 @@ jobs:
 
 			By("second pipeline should still not exist")
 			withFlyTarget(adminFlyTarget, func() {
-				execS := spawnFly("get-pipeline", "-p", secondPipelineName)
+				execS := spawnFly("get-pipeline", "-p", createdPipelineName)
 				<-execS.Exited
 				Expect(execS).To(gexec.Exit(1))
 				Expect(execS.Err).To(gbytes.Say("pipeline not found"))
