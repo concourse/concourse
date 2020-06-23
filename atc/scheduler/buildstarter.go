surendranaidu/concourse-1

atc: structure: convert scheduler to new Steps
Signed-off-by: Alex Suraci <asuraci@pivotal.io>

atc/scheduler/buildstarter.go
@@ -27,7 +27,7 @@ type BuildStarter interface {
 //go:generate counterfeiter . BuildPlanner
 
 type BuildPlanner interface {
-	Create(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	Create(atc.StepConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 }
 
 type Build interface {
@@ -235,7 +235,7 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return startResults{}, fmt.Errorf("config: %w", err)
 	}
 
-	plan, err := s.planner.Create(config.PlanConfig(), resourceConfigs, resourceTypes.Deserialize(), buildInputs)
+	plan, err := s.planner.Create(config.StepConfig(), resourceConfigs, resourceTypes.Deserialize(), buildInputs)
 	if err != nil {
 		logger.Error("failed-to-create-build-plan", err)
 

atc/scheduler/schedulerfakes/fake_build_planner.go
@@ -10,10 +10,10 @@ import (
 )
 
 type FakeBuildPlanner struct {
-	CreateStub        func(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	CreateStub        func(atc.StepConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 	createMutex       sync.RWMutex
 	createArgsForCall []struct {
-		arg1 atc.PlanConfig
+		arg1 atc.StepConfig
 		arg2 atc.ResourceConfigs
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -30,7 +30,7 @@ type FakeBuildPlanner struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeBuildPlanner) Create(arg1 atc.PlanConfig, arg2 atc.ResourceConfigs, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
+func (fake *FakeBuildPlanner) Create(arg1 atc.StepConfig, arg2 atc.ResourceConfigs, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
 	var arg4Copy []db.BuildInput
 	if arg4 != nil {
 		arg4Copy = make([]db.BuildInput, len(arg4))
@@ -39,7 +39,7 @@ func (fake *FakeBuildPlanner) Create(arg1 atc.PlanConfig, arg2 atc.ResourceConfi
 	fake.createMutex.Lock()
 	ret, specificReturn := fake.createReturnsOnCall[len(fake.createArgsForCall)]
 	fake.createArgsForCall = append(fake.createArgsForCall, struct {
-		arg1 atc.PlanConfig
+		arg1 atc.StepConfig
 		arg2 atc.ResourceConfigs
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -62,13 +62,13 @@ func (fake *FakeBuildPlanner) CreateCallCount() int {
 	return len(fake.createArgsForCall)
 }
 
-func (fake *FakeBuildPlanner) CreateCalls(stub func(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
+func (fake *FakeBuildPlanner) CreateCalls(stub func(atc.StepConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
 	fake.createMutex.Lock()
 	defer fake.createMutex.Unlock()
 	fake.CreateStub = stub
 }
 
-func (fake *FakeBuildPlanner) CreateArgsForCall(i int) (atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) {
+func (fake *FakeBuildPlanner) CreateArgsForCall(i int) (atc.StepConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) {
 	fake.createMutex.RLock()
 	defer fake.createMutex.RUnlock()
 	argsForCall := fake.createArgsForCall[i]

concourse/concourse

atc/scheduler/buildstarter.go
@@ -23,7 +23,7 @@ type BuildStarter interface {
 //go:generate counterfeiter . BuildPlanner
 
 type BuildPlanner interface {
-	Create(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	Create(atc.StepConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 }
 
 type Build interface {
@@ -198,7 +198,7 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return startResults{}, fmt.Errorf("config: %w", err)
 	}
 
-	plan, err := s.planner.Create(config.PlanConfig(), job.Resources, job.ResourceTypes, buildInputs)
+	plan, err := s.planner.Create(config.StepConfig(), job.Resources, job.ResourceTypes, buildInputs)
 	if err != nil {
 		logger.Error("failed-to-create-build-plan", err)
 

atc/scheduler/schedulerfakes/fake_build_planner.go
@@ -10,10 +10,10 @@ import (
 )
 
 type FakeBuildPlanner struct {
-	CreateStub        func(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	CreateStub        func(atc.StepConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 	createMutex       sync.RWMutex
 	createArgsForCall []struct {
-		arg1 atc.PlanConfig
+		arg1 atc.StepConfig
 		arg2 db.SchedulerResources
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -30,7 +30,7 @@ type FakeBuildPlanner struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeBuildPlanner) Create(arg1 atc.PlanConfig, arg2 db.SchedulerResources, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
+func (fake *FakeBuildPlanner) Create(arg1 atc.StepConfig, arg2 db.SchedulerResources, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
 	var arg4Copy []db.BuildInput
 	if arg4 != nil {
 		arg4Copy = make([]db.BuildInput, len(arg4))
@@ -39,7 +39,7 @@ func (fake *FakeBuildPlanner) Create(arg1 atc.PlanConfig, arg2 db.SchedulerResou
 	fake.createMutex.Lock()
 	ret, specificReturn := fake.createReturnsOnCall[len(fake.createArgsForCall)]
 	fake.createArgsForCall = append(fake.createArgsForCall, struct {
-		arg1 atc.PlanConfig
+		arg1 atc.StepConfig
 		arg2 db.SchedulerResources
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -62,13 +62,13 @@ func (fake *FakeBuildPlanner) CreateCallCount() int {
 	return len(fake.createArgsForCall)
 }
 
-func (fake *FakeBuildPlanner) CreateCalls(stub func(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
+func (fake *FakeBuildPlanner) CreateCalls(stub func(atc.StepConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
 	fake.createMutex.Lock()
 	defer fake.createMutex.Unlock()
 	fake.CreateStub = stub
 }
 
-func (fake *FakeBuildPlanner) CreateArgsForCall(i int) (atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) {
+func (fake *FakeBuildPlanner) CreateArgsForCall(i int) (atc.StepConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) {
 	fake.createMutex.RLock()
 	defer fake.createMutex.RUnlock()
 	argsForCall := fake.createArgsForCall[i]