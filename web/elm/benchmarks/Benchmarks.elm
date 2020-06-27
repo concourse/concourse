b34892d0e5205a8fd825711b86ec39cb861875fa
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-06-27 01:00:29 -0400

    fix yarn benchmark
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- web/elm/benchmarks/Benchmarks.elm
+++ web/elm/benchmarks/Benchmarks.elm
@@ -843,9 +843,10 @@ steps =
         }
 
 
-sampleJob : String -> List String -> Concourse.Job
-sampleJob name passed =
-    { name = name
+sampleJob : Int -> String -> List String -> Concourse.Job
+sampleJob id name passed =
+    { id = id
+    , name = name
     , pipelineName = "pipeline"
     , teamName = "team"
     , nextBuild = Nothing
@@ -867,11 +868,11 @@ sampleJob name passed =
 
 sampleJobs : List Concourse.Job
 sampleJobs =
-    [ sampleJob "job1" []
-    , sampleJob "job2a" [ "job1" ]
-    , sampleJob "job2b" [ "job1" ]
-    , sampleJob "job3" [ "job2a" ]
-    , sampleJob "job4" [ "job3" ]
+    [ sampleJob 0 "job1" []
+    , sampleJob 1 "job2a" [ "job1" ]
+    , sampleJob 2 "job2b" [ "job1" ]
+    , sampleJob 3 "job3" [ "job2a" ]
+    , sampleJob 4 "job4" [ "job3" ]
     ]
 
 
@@ -968,7 +969,8 @@ jobByName jobs job =
             a
 
         Nothing ->
-            { name = ""
+            { id = 0
+            , name = ""
             , pipelineName = ""
             , teamName = ""
             , nextBuild = Nothing
