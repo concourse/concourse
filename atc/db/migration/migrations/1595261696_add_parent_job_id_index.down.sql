23efce7c8b32cbd8bc0f247fc3f9d1ac9d0347a1
Taylor Silva <tsilva@pivotal.io>
2020-07-29 09:45:49 -0400

    atc/migrations: remove parent_build_id index
    We discovered that it will likely never be used because a single job
    probably won't have enough child pipelines to fill more than a few
    database pages.
    
    Signed-off-by: Taylor Silva <tsilva@pivotal.io>

--- atc/db/migration/migrations/1595261696_add_parent_job_id_index.down.sql
+++ atc/db/migration/migrations/1595261696_add_parent_job_id_index.down.sql
@@ -1,4 +1,3 @@
 BEGIN;
   DROP INDEX parent_job_id;
-  DROP INDEX parent_build_id;
 COMMIT;
--- atc/db/migration/migrations/1595261696_add_parent_job_id_index.up.sql
+++ atc/db/migration/migrations/1595261696_add_parent_job_id_index.up.sql
@@ -1,4 +1,3 @@
 BEGIN;
   CREATE INDEX parent_job_id ON pipelines USING btree (parent_job_id);
-  CREATE INDEX parent_build_id ON pipelines USING btree (parent_build_id);
 COMMIT;
