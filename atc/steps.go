c09110b19fd3e6143ef83dd00540ca4aa706094e
Taylor Silva <tsilva@pivotal.io>
2020-07-29 10:11:13 -0400

    atc/steps: update comment about StepWrapper's
    Words are hard.
    
    This comment should help clarify what is expected of the Wrap() function
    in this interface. Someone can also look at existing StepWrapper's like
    RetryStep.
    
    Signed-off-by: Taylor Silva <tsilva@pivotal.io>

--- atc/steps.go
+++ atc/steps.go
@@ -172,8 +172,8 @@ type StepWrapper interface {
 	// Wrap is called during (Step).UnmarshalJSON whenever an 'inner' step is
 	// parsed.
 	//
-	// Modifier step types should implement this by assigning a `json:"-"` tag to
-	// an internal field of type StepConfig in their struct.
+	// Modifier step types should implement this function by assigning to an
+	// internal field, that has a `json:"-"` tag, the passed in StepConfig.
 	Wrap(StepConfig)
 
 	// Unwrap is called during (Step).MarshalJSON and must return the wrapped
