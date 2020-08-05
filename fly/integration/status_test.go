dcb06e66cb746990bde03e37fa7339f66266f5a8
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-08-05 09:11:48 -0400

    fly: behaviour: status command works with opaque token
    concourse/concourse#5897
    
    In the PR linked above, I had missed some changes to fly that expect the
    token to be a JWT. This commit fixes the status command
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- fly/integration/status_test.go
+++ fly/integration/status_test.go
@@ -2,6 +2,7 @@ package integration_test
 
 import (
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -29,34 +30,18 @@ var _ = Describe("status Command", func() {
 
 		flyFixtureData := []byte(`
 targets:
-  another-test:
+  with-token:
     api: ` + atcServer.URL() + `
     team: test
     token:
       type: Bearer
-      value: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNDU4MzUyNDcwIiwiaXNBZG1pbiI6Im5vcGUiLCJ0ZWFtSUQiOjEsInRlYW1OYW1lIjoibWFpbiJ9.v04hbwIFdMNjp6BCpz2jvOYNpAeBY8pio6hlXQizLAM
-  bad-test:
+      value: some-nice-opaque-access-token
+  without-token:
     api: https://example.com/another-test
-    team: test
-    token:
-      type: Bearer
-      value: bad-token
-  loggedout-test:
-    api: https://example.com/loggedout-test
     team: test
     token:
       type: ""
       value: ""
-  invalid-test:
-    api: https://example.com/invalid-test
-    team: test
-    token:
-  expired-test:
-    api: https://example.com/expired-test
-    team: test
-    token:
-      type: Bearer
-      value: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjc3JmIjoiNGZlOGM1Y2VhYjFjNWFiMzE5MzMzNmQ2YThkOTE3OTJmZTA0YmEzZjg5Y2IwZDg0MTkzN2I2MzkzYTdhYTg2MyIsImV4cCI6MTUyMTMwODk2MCwiaXNBZG1pbiI6dHJ1ZSwidGVhbU5hbWUiOiJtYWluIn0.oyb-2CPLnXy-7S-9FWWlx106KI5Xpd6B5XIrFOvcG1yyh5nrGpM4NfgaW7ugN4zzi2mSFGawRlkulzgAZ4RxAEdTOnlSXvVZO3vD70sMlrp_LX-lYaqJ7XXVXNKvKE_74YGZY414TYVy2IxL-4Qf7pbb0uGDky03jQFxkWVSUiD5iLwaqpvxpHTEuVNoZc9a8YNiOdETvqnt50drsmxpkblM60DrWuDVPifOfTrooSMxULnl3pYXDsTPZbrc6QVLA_Hpi7wWCNEZbAojTQ3taIwzp7BBAuxUNcVMpJKy3Um5oMHcibe1R0PsZ0J49PbLSclZfhJ7wjHBc7FQEKTZzQ
 `)
 
 		err = ioutil.WriteFile(flyrc, flyFixtureData, 0600)
@@ -99,7 +84,7 @@ targets:
 			})
 
 			It("command exist with 0", func() {
-				flyCmd = exec.Command(flyPath, "-t", "another-test", "status")
+				flyCmd = exec.Command(flyPath, "-t", "with-token", "status")
 				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).NotTo(HaveOccurred())
 
@@ -110,19 +95,19 @@ targets:
 			})
 		})
 
-		Context("when target is saved with valid token but is unauthorized on server", func() {
+		Context("when target is saved with a token that is rejected by the server", func() {
 			BeforeEach(func() {
 				atcServer.Reset()
 				atcServer.AppendHandlers(
 					ghttp.CombineHandlers(
 						ghttp.VerifyRequest("GET", "/api/v1/user"),
-						ghttp.RespondWith(401, nil),
+						ghttp.RespondWith(http.StatusUnauthorized, nil),
 					),
 				)
 			})
 
-			It("command exist with 1", func() {
-				flyCmd = exec.Command(flyPath, "-t", "another-test", "status")
+			It("the command fails", func() {
+				flyCmd = exec.Command(flyPath, "-t", "with-token", "status")
 				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).NotTo(HaveOccurred())
 
@@ -130,72 +115,20 @@ targets:
 				Expect(sess.ExitCode()).To(Equal(1))
 
 				Expect(sess.Err).To(gbytes.Say(`please login again`))
-				Expect(sess.Err).To(gbytes.Say(`token validation failed with error : not authorized`))
+				Expect(sess.Err).To(gbytes.Say(`token validation failed with error: not authorized`))
 			})
 		})
 
 		Context("when target is saved with invalid token", func() {
-			It("command exist with 1", func() {
-				flyCmd = exec.Command(flyPath, "-t", "bad-test", "status")
+			It("the command fails", func() {
+				flyCmd = exec.Command(flyPath, "-t", "without-token", "status")
 				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).NotTo(HaveOccurred())
 
 				<-sess.Exited
 				Expect(sess.ExitCode()).To(Equal(1))
 
-				Expect(sess.Err).To(gbytes.Say(`please login again`))
-				Expect(sess.Err).To(gbytes.Say(`token validation failed with error : token contains an invalid number of segments`))
-			})
-		})
-
-		Context("when target is saved with expired token", func() {
-			It("command exist with 1", func() {
-				flyCmd = exec.Command(flyPath, "-t", "expired-test", "status")
-				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				<-sess.Exited
-				Expect(sess.ExitCode()).To(Equal(1))
-
-				Expect(sess.Err).To(gbytes.Say(`please login again`))
-				Expect(sess.Err).To(gbytes.Say(`token validation failed with error : Token is expired`))
-			})
-		})
-
-		Context("when target is logged out", func() {
-			It("command exist with 1 and log out msg", func() {
-				flyCmd = exec.Command(flyPath, "-t", "loggedout-test", "status")
-				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				<-sess.Exited
-				Expect(sess.ExitCode()).To(Equal(1))
 				Expect(sess.Err).To(gbytes.Say(`logged out`))
-			})
-		})
-
-		Context("when invalid token is in target", func() {
-			It("command exist with 1", func() {
-				flyCmd = exec.Command(flyPath, "-t", "invalid-test", "status")
-				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				<-sess.Exited
-				Expect(sess.ExitCode()).To(Equal(1))
-				Expect(sess.Err).To(gbytes.Say(`logged out`))
-			})
-		})
-
-		Context("when unknown target is used", func() {
-			It("command exist with 1", func() {
-				flyCmd = exec.Command(flyPath, "-t", "unknown-test", "status")
-				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).NotTo(HaveOccurred())
-
-				<-sess.Exited
-				Expect(sess.ExitCode()).To(Equal(1))
-
-				Expect(sess.Err).To(gbytes.Say(`unknown target: unknown-test`))
 			})
 		})
 	})
