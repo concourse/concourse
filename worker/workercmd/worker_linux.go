49b70b2513f43d265bfb8011438ee567cbba7607
Muntasir Chowdhury <mchowdhury@pivotal.io>
2020-09-14 17:36:07 -0400

    add default for containerd max-containers
    Limiting this to 250 similar to Guardian to avoid
    stampeding herd problem. With stress testing in the future
    this can be updated to a more accurate value.
    
    issue#5985
    
    Signed-off-by: Muntasir Chowdhury <mchowdhury@pivotal.io>

--- worker/workercmd/worker_linux.go
+++ worker/workercmd/worker_linux.go
@@ -47,7 +47,7 @@ type ContainerdRuntime struct {
 	DNS                DNSConfig `group:"DNS Proxy Configuration" namespace:"dns-proxy"`
 	DNSServers         []string  `long:"dns-server" description:"DNS server IP address to use instead of automatically determined servers. Can be specified multiple times."`
 	RestrictedNetworks []string  `long:"restricted-network" description:"Network ranges to which traffic from containers will be restricted. Can be specified multiple times."`
-	MaxContainers      int       `long:"max-containers" default:"0" description:"Max container capacity. 0 means no limit."`
+	MaxContainers      int       `long:"max-containers" default:"250" description:"Max container capacity. 0 means no limit."`
 	NetworkPool        string    `long:"network-pool" default:"10.80.0.0/16" description:"Network range to use for dynamically allocated container subnets."`
 }
 
