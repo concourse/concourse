62a7acd6b6108668e0f4687d8527d9e0dc4365fd
Alex Suraci <suraci.alex@gmail.com>
2020-10-01 18:16:59 -0400

    worker: skip integration tests when non-root
    this just makes it a bit smoother to run unit tests recursively on a
    Linux dev machine
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- worker/integration/suite_test.go
+++ worker/integration/suite_test.go
@@ -1,13 +1,25 @@
 package integration_test
 
 import (
+	"os/user"
+	"testing"
+
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 func TestSuite(t *testing.T) {
+	req := require.New(t)
+
+	user, err := user.Current()
+	req.NoError(err)
+
+	if user.Uid != "0" {
+		t.Skip("must be run as root")
+		return
+	}
+
 	suite.Run(t, &WorkerRunnerSuite{
-		Assertions: require.New(t),
+		Assertions: req,
 	})
 }
