7f42e2fd5e291db955970eb676ca21ad5baccc3c
Alex Suraci <suraci.alex@gmail.com>
2020-10-03 17:32:19 -0400

    fly: behavior: show check status for resources
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- fly/commands/resources.go
+++ fly/commands/resources.go
@@ -43,27 +43,40 @@ func (command *ResourcesCommand) Execute([]string) error {
 		return nil
 	}
 
-	headers = []string{"name", "type", "pinned"}
+	headers = []string{"name", "type", "pinned", "status"}
 	table := ui.Table{Headers: ui.TableRow{}}
 	for _, h := range headers {
 		table.Headers = append(table.Headers, ui.TableCell{Contents: h, Color: color.New(color.Bold)})
 	}
 
-	for _, p := range resources {
-		row := ui.TableRow{}
-		row = append(row, ui.TableCell{Contents: p.Name})
-		row = append(row, ui.TableCell{Contents: p.Type})
-
+	for _, resource := range resources {
 		var pinnedColumn ui.TableCell
-		if p.PinnedVersion != nil {
-			pinnedColumn.Contents = ui.PresentVersion(p.PinnedVersion)
+		if resource.PinnedVersion != nil {
+			pinnedColumn.Contents = ui.PresentVersion(resource.PinnedVersion)
 		} else {
 			pinnedColumn.Contents = "n/a"
 		}
 
-		row = append(row, pinnedColumn)
+		var statusColumn ui.TableCell
+		if resource.FailingToCheck {
+			if resource.CheckError != "" {
+				statusColumn.Contents = resource.CheckError
+				statusColumn.Color = ui.FailedColor
+			} else if resource.CheckSetupError != "" {
+				statusColumn.Contents = resource.CheckSetupError
+				statusColumn.Color = ui.ErroredColor
+			}
+		} else {
+			statusColumn.Contents = "ok"
+			statusColumn.Color = ui.SucceededColor
+		}
 
-		table.Data = append(table.Data, row)
+		table.Data = append(table.Data, ui.TableRow{
+			ui.TableCell{Contents: resource.Name},
+			ui.TableCell{Contents: resource.Type},
+			pinnedColumn,
+			statusColumn,
+		})
 	}
 
 	return table.Render(os.Stdout, Fly.PrintTableHeaders)
