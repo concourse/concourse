e733e8f8aa5823790e1f0b71082d79d40bcea0c6
Rui Yang <ruiya@vmware.com>
2020-10-22 12:57:32 -0400

    skymarshal: dex: bump dex module to v0.2.0
    so it could get latest from upstream dex and the release of
    concourse/dex in the future could be picked up by dependabot
    
    Signed-off-by: Rui Yang <ruiya@vmware.com>

--- skymarshal/skycmd/oidc_flags.go
+++ skymarshal/skycmd/oidc_flags.go
@@ -65,18 +65,21 @@ func (flag *OIDCFlags) Serialize(redirectURI string) ([]byte, error) {
 		caCerts = append(caCerts, file.Path())
 	}
 
-	return json.Marshal(oidc.Config{
+	config := oidc.Config{
 		Issuer:             flag.Issuer,
 		ClientID:           flag.ClientID,
 		ClientSecret:       flag.ClientSecret,
 		Scopes:             flag.Scopes,
-		GroupsKey:          flag.GroupsKey,
 		UserNameKey:        flag.UserNameKey,
 		HostedDomains:      flag.HostedDomains,
 		RootCAs:            caCerts,
 		InsecureSkipVerify: flag.InsecureSkipVerify,
 		RedirectURI:        redirectURI,
-	})
+	}
+
+	config.ClaimMapping.GroupsKey = flag.GroupsKey
+
+	return json.Marshal(config)
 }
 
 type OIDCTeamFlags struct {
