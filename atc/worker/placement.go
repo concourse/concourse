a67348b02d3065e9980b6634f9d2c58636bcce31
Chao Li <chaol@vmware.com>
2020-12-03 15:21:43 +0800

    atc(feat): add new container placement strategies: limit-max-containers and limit-max-volumes.
    Signed-off-by: Chao Li <chaol@vmware.com>

--- atc/worker/placement.go
+++ atc/worker/placement.go
@@ -12,8 +12,10 @@ import (
 )
 
 type ContainerPlacementStrategyOptions struct {
-	ContainerPlacementStrategy []string `long:"container-placement-strategy" default:"volume-locality" choice:"volume-locality" choice:"random" choice:"fewest-build-containers" choice:"limit-active-tasks" description:"Method by which a worker is selected during container placement. If multiple methods are specified, they will be applied in order. Random strategy should only be used alone."`
-	MaxActiveTasksPerWorker    int      `long:"max-active-tasks-per-worker" default:"0" description:"Maximum allowed number of active build tasks per worker. Has effect only when used with limit-active-tasks placement strategy. 0 means no limit."`
+	ContainerPlacementStrategy   []string `long:"container-placement-strategy" default:"volume-locality" choice:"volume-locality" choice:"random" choice:"fewest-build-containers" choice:"limit-active-tasks" choice:"limit-active-containers" choice:"limit-active-volumes" description:"Method by which a worker is selected during container placement. If multiple methods are specified, they will be applied in order. Random strategy should only be used alone."`
+	MaxActiveTasksPerWorker      int      `long:"max-active-tasks-per-worker" default:"0" description:"Maximum allowed number of active build tasks per worker. Has effect only when used with limit-active-tasks placement strategy. 0 means no limit."`
+	MaxActiveContainersPerWorker int      `long:"max-active-containers-per-worker" default:"0" description:"Maximum allowed number of active containers per worker. Has effect only when used with limit-active-containers placement strategy. 0 means no limit."`
+	MaxActiveVolumesPerWorker    int      `long:"max-active-volumes-per-worker" default:"0" description:"Maximum allowed number of active volumes per worker. Has effect only when used with limit-active-volumes placement strategy. 0 means no limit."`
 }
 
 type ContainerPlacementStrategy interface {
@@ -46,6 +48,16 @@ func NewContainerPlacementStrategy(opts ContainerPlacementStrategyOptions) (*con
 				return nil, errors.New("max-active-tasks-per-worker must be greater or equal than 0")
 			}
 			cps.nodes = append(cps.nodes, newLimitActiveTasksPlacementStrategy(opts.MaxActiveTasksPerWorker))
+		case "limit-active-containers":
+			if opts.MaxActiveContainersPerWorker < 0 {
+				return nil, errors.New("max-active-containers-per-worker must be greater or equal than 0")
+			}
+			cps.nodes = append(cps.nodes, newLimitActiveContainersPlacementStrategy(opts.MaxActiveContainersPerWorker))
+		case "limit-active-volumes":
+			if opts.MaxActiveVolumesPerWorker < 0 {
+				return nil, errors.New("max-active-volumes-per-worker must be greater or equal than 0")
+			}
+			cps.nodes = append(cps.nodes, newLimitActiveVolumesPlacementStrategy(opts.MaxActiveVolumesPerWorker))
 		case "volume-locality":
 			cps.nodes = append(cps.nodes, newVolumeLocalityPlacementStrategyNode())
 		default:
@@ -168,7 +180,7 @@ func (strategy *LimitActiveTasksPlacementStrategyNode) Choose(logger lager.Logge
 	for _, w := range workers {
 		activeTasks, err := w.ActiveTasks()
 		if err != nil {
-			logger.Error("Cannot retrive active tasks on worker. Skipping.", err)
+			logger.Error("Cannot retrieve active tasks on worker. Skipping.", err)
 			continue
 		}
 
@@ -189,4 +201,56 @@ func (strategy *LimitActiveTasksPlacementStrategyNode) Choose(logger lager.Logge
 
 func (strategy *LimitActiveTasksPlacementStrategyNode) ModifiesActiveTasks() bool {
 	return true
+}
+
+type LimitActiveContainersPlacementStrategyNode struct {
+	maxContainers int
+}
+
+func newLimitActiveContainersPlacementStrategy(maxContainers int) ContainerPlacementStrategyChainNode {
+	return &LimitActiveContainersPlacementStrategyNode{
+		maxContainers: maxContainers,
+	}
+}
+
+func (strategy *LimitActiveContainersPlacementStrategyNode) Choose(logger lager.Logger, workers []Worker, spec ContainerSpec) ([]Worker, error) {
+	candidates := []Worker{}
+
+	for _, w := range workers {
+		if w.ActiveContainers() <= strategy.maxContainers {
+			candidates = append(candidates, w)
+		}
+	}
+
+	return candidates, nil
+}
+
+func (strategy *LimitActiveContainersPlacementStrategyNode) ModifiesActiveTasks() bool {
+	return false
+}
+
+type LimitActiveVolumesPlacementStrategyNode struct {
+	maxVolumes int
+}
+
+func newLimitActiveVolumesPlacementStrategy(maxVolumes int) ContainerPlacementStrategyChainNode {
+	return &LimitActiveVolumesPlacementStrategyNode{
+		maxVolumes: maxVolumes,
+	}
+}
+
+func (strategy *LimitActiveVolumesPlacementStrategyNode) Choose(logger lager.Logger, workers []Worker, spec ContainerSpec) ([]Worker, error) {
+	candidates := []Worker{}
+
+	for _, w := range workers {
+		if w.ActiveVolumes() <= strategy.maxVolumes {
+			candidates = append(candidates, w)
+		}
+	}
+
+	return candidates, nil
+}
+
+func (strategy *LimitActiveVolumesPlacementStrategyNode) ModifiesActiveTasks() bool {
+	return false
 }
