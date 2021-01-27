dfd5490c89cc44bd1a8bf15841c4bd10c9fd5833
kirillbilchenko <kirya7@gmail.com>
2021-01-27 16:07:46 +0100

    Add additional parameter to oidc SkipEmailVerified to translate it to dex
    Signed-off-by: kirillbilchenko <kirya7@gmail.com>

--- skymarshal/skycmd/oidc_flags.go
+++ skymarshal/skycmd/oidc_flags.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/concourse/dex/connector/oidc"
 	"github.com/concourse/flag"
-	multierror "github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-multierror"
 )
 
 func init() {
@@ -28,7 +28,8 @@ type OIDCFlags struct {
 	HostedDomains      []string    `long:"hosted-domains" description:"List of whitelisted domains when using Google, only users from a listed domain will be allowed to log in"`
 	CACerts            []flag.File `long:"ca-cert" description:"CA Certificate"`
 	InsecureSkipVerify bool        `long:"skip-ssl-validation" description:"Skip SSL validation"`
-  DisableGroups bool      `long:"disable-groups" description:"Disable OIDC groups claims"`
+	DisableGroups      bool        `long:"disable-groups" description:"Disable OIDC groups claims"`
+	SkipEmailVerified  bool        `long:"skip-email-verified" description: "Override email_verified field"`
 }
 
 func (flag *OIDCFlags) Name() string {
@@ -67,16 +68,17 @@ func (flag *OIDCFlags) Serialize(redirectURI string) ([]byte, error) {
 	}
 
 	config := oidc.Config{
-		Issuer:             flag.Issuer,
-		ClientID:           flag.ClientID,
-		ClientSecret:       flag.ClientSecret,
-		Scopes:             flag.Scopes,
-		UserNameKey:        flag.UserNameKey,
-		HostedDomains:      flag.HostedDomains,
-		RootCAs:            caCerts,
-		InsecureSkipVerify: flag.InsecureSkipVerify,
-		RedirectURI:        redirectURI,
-    InsecureEnableGroups: ! flag.DisableGroups,
+		Issuer:                    flag.Issuer,
+		ClientID:                  flag.ClientID,
+		ClientSecret:              flag.ClientSecret,
+		Scopes:                    flag.Scopes,
+		UserNameKey:               flag.UserNameKey,
+		HostedDomains:             flag.HostedDomains,
+		RootCAs:                   caCerts,
+		InsecureSkipVerify:        flag.InsecureSkipVerify,
+		RedirectURI:               redirectURI,
+		InsecureEnableGroups:      !flag.DisableGroups,
+		InsecureSkipEmailVerified: flag.SkipEmailVerified,
 	}
 
 	config.ClaimMapping.GroupsKey = flag.GroupsKey
