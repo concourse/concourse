1e4521039f9bb510b44acf4eee833f61a5bb2330
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-02-04 10:23:14 -0500

    don't drop build_image_resource_caches_job_build_id
    This reverts a dropped index from concourse/concourse#5993. Since this
    was never released, it shouldn't be a problem modifying the migration -
    we'll just need to be careful when we deploy CI/Hush House
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/db/migration/migrations/1609958558_add_job_id_to_build_image_resource_caches.down.sql
+++ atc/db/migration/migrations/1609958558_add_job_id_to_build_image_resource_caches.down.sql
@@ -1,6 +1,4 @@
 BEGIN;
   ALTER TABLE build_image_resource_caches
     DROP COLUMN IF EXISTS job_id;
-
-  CREATE INDEX build_image_resource_caches_build_id ON build_image_resource_caches USING btree (build_id);
 COMMIT;
