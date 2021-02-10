961416a1524911b136872278e26873a16ec24ea8
Bohan Chen <bochen@pivotal.io>
2021-02-10 17:41:05 -0500

    atc: wip planning atc.Plan structure
    - statically generate nested build plans for fetching the image for a
    get/put step
    - reference the imagespec and version of a step from the outputs of a
    previous check/get step
    
    TODO:
    - need to watch out for taskconfig file
    
    [#6274]
    
    Signed-off-by: Bohan Chen <bochen@pivotal.io>
    Co-authored-by: Clara Fu <cfu@pivotal.io>

--- atc/config.go
+++ atc/config.go
@@ -201,6 +201,8 @@ type ResourceType struct {
 	CheckEvery *CheckEvery `json:"check_every,omitempty"`
 	Tags       Tags        `json:"tags,omitempty"`
 	Params     Params      `json:"params,omitempty"`
+
+	Parent *ResourceType `json:"parent_resource_type,omitempty"`
 }
 
 type Prototype struct {
