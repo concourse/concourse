ac5e2c6526b1677b7a85f64d6920a989c88e798c
Clara Fu <fclara@vmware.com>
2021-03-26 10:35:47 -0400

    convert on delete no action to on delete cascade
    When the foreign key constraint was added, I forgot to add a ON DELETE
    CASCADE to the constraint. Without it, it will automatically do an ON
    DELETE NO ACTION which will restrict the parent from being deleted if a
    child row references it. This caused a bug where we were unable to
    delete a pipeline because there were build image resource cache rows
    that were owned by a job within the pipeline they were trying to delete.
    By adding an ON DELETE CASCADE, deleting the pipeline will delete the
    job which will now delete any build image resource caches that were for
    that job.
    
    [#6577]
    
    Signed-off-by: Clara Fu <fclara@vmware.com>

---
+++ atc/db/migration/migrations/1616768782_add_job_id_on_delete_cascade_to_build_image_caches.down.sql
@@ -0,0 +1,5 @@
+BEGIN;
+  ALTER TABLE build_image_resource_caches
+    DROP CONSTRAINT build_image_resource_caches_job_id_fkey,
+    ADD CONSTRAINT build_image_resource_caches_job_id_fkey FOREIGN KEY (job_id) REFERENCES jobs(id);
+COMMIT;
