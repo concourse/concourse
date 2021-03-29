84f5cecb275403665598293393300f3908b4a472
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-29 17:19:10 -0400

    S: Use teamHandleFactory for ordering API
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>
    Co-authored-by: Esteban Foronda <eforonda@vmware.com>

--- atc/api/pipelineserver/order_within_group.go
+++ atc/api/pipelineserver/order_within_group.go
@@ -11,47 +11,35 @@ import (
 	"github.com/concourse/concourse/atc/db"
 )
 
-func (s *Server) OrderPipelinesWithinGroup(w http.ResponseWriter, r *http.Request) {
-	logger := s.logger.Session("order-pipelines-within-group")
+func (s *Server) OrderPipelinesWithinGroup(team db.Team) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := s.logger.Session("order-pipelines-within-group")
 
-	var instanceVars []atc.InstanceVars
-	if err := json.NewDecoder(r.Body).Decode(&instanceVars); err != nil {
-		logger.Error("invalid-json", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	teamName := r.FormValue(":team_name")
-	team, found, err := s.teamFactory.FindTeam(teamName)
-	if err != nil {
-		logger.Error("failed-to-get-team", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if !found {
-		logger.Info("team-not-found")
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	groupName := r.FormValue(":pipeline_name")
-
-	err = team.OrderPipelinesWithinGroup(groupName, instanceVars)
-	if err != nil {
-		logger.Error("failed-to-order-pipelines", err, lager.Data{
-			"pipeline_name": groupName,
-			"instance_vars": instanceVars,
-		})
-		var errNotFound db.ErrPipelineNotFound
-		if errors.As(err, &errNotFound) {
+		var instanceVars []atc.InstanceVars
+		if err := json.NewDecoder(r.Body).Decode(&instanceVars); err != nil {
+			logger.Error("invalid-json", err)
 			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, err.Error())
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
-		return
-	}
 
-	w.WriteHeader(http.StatusOK)
+		groupName := r.FormValue(":pipeline_name")
+
+		err := team.OrderPipelinesWithinGroup(groupName, instanceVars)
+		if err != nil {
+			logger.Error("failed-to-order-pipelines", err, lager.Data{
+				"pipeline_name": groupName,
+				"instance_vars": instanceVars,
+			})
+			var errNotFound db.ErrPipelineNotFound
+			if errors.As(err, &errNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintln(w, err.Error())
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
 }
