6fa41769911aaa30ed15f3b17070118a23382b9a
Clara Fu <cfu@pivotal.io>
2021-03-29 15:58:11 -0400

    Apply suggestions from code review
    Co-authored-by: Rui Yang <ruiy@outlook.com>

--- atc/db/migration/migrations/1616768782_add_job_id_on_delete_cascade_to_build_image_caches.down.sql
+++ atc/db/migration/migrations/1616768782_add_job_id_on_delete_cascade_to_build_image_caches.down.sql
@@ -1,4 +1,3 @@
   ALTER TABLE build_image_resource_caches
     DROP CONSTRAINT build_image_resource_caches_job_id_fkey,
     ADD CONSTRAINT build_image_resource_caches_job_id_fkey FOREIGN KEY (job_id) REFERENCES jobs(id);
-COMMIT;
--- atc/db/migration/migrations/1616768782_add_job_id_on_delete_cascade_to_build_image_caches.up.sql
+++ atc/db/migration/migrations/1616768782_add_job_id_on_delete_cascade_to_build_image_caches.up.sql
@@ -1,5 +1,3 @@
-BEGIN;
   ALTER TABLE build_image_resource_caches
     DROP CONSTRAINT build_image_resource_caches_job_id_fkey,
     ADD CONSTRAINT build_image_resource_caches_job_id_fkey FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE;
-COMMIT;
