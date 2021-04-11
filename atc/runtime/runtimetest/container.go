33a7040f260911e86026d4f8ffbce7e33b5570ba
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-04-11 10:18:38 -0400

    S: refactor runtimetest.{Container,Process}
    the way it was setup before was a bit awkward and only checked the ID in
    the spec, rather than the entire spec. by splitting up processes into
    definitions and running processes, we can also catch issues like calling
    Attach before Run, or processes not being called
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/runtime/runtimetest/container.go
+++ atc/runtime/runtimetest/container.go
@@ -3,74 +3,85 @@ package runtimetest
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	"github.com/concourse/concourse/atc/db"
 	"github.com/concourse/concourse/atc/db/dbfakes"
 	"github.com/concourse/concourse/atc/runtime"
 )
 
+type ProcessDefinition struct {
+	Spec runtime.ProcessSpec
+	Stub ProcessStub
+}
+
 type Container struct {
-	Processes    map[string]ProcessStub
+	ProcessDefs  []ProcessDefinition
+	Processes    []*Process
 	Props        map[string]string
 	DBContainer_ *dbfakes.FakeCreatedContainer
 }
 
-func NewContainer() Container {
+func NewContainer() *Container {
 	dbContainer := new(dbfakes.FakeCreatedContainer)
-	return Container{
-		Processes:    make(map[string]ProcessStub),
+	return &Container{
 		Props:        make(map[string]string),
 		DBContainer_: dbContainer,
 	}
 }
 
-func (c Container) WithProcess(spec runtime.ProcessSpec, stub ProcessStub) Container {
-	stubs := cloneProcs(c.Processes)
-	stubs[spec.ID] = stub
-	return Container{
-		Processes: stubs,
-		Props:     cloneProps(c.Props),
+func (c Container) WithProcess(spec runtime.ProcessSpec, stub ProcessStub) *Container {
+	c2 := c
+	c2.ProcessDefs = make([]ProcessDefinition, len(c.ProcessDefs)+1)
+	copy(c2.ProcessDefs, c.ProcessDefs)
+	c2.ProcessDefs[len(c2.ProcessDefs)-1] = ProcessDefinition{
+		Spec: spec,
+		Stub: stub,
 	}
+	return &c2
 }
 
-func (c Container) Run(ctx context.Context, spec runtime.ProcessSpec, io runtime.ProcessIO) (runtime.Process, error) {
-	p, ok := c.Processes[spec.ID]
-	if !ok {
-		return nil, fmt.Errorf("must setup a ProcessStub for process %q (%+v)", spec.ID, spec)
+func (c *Container) Run(ctx context.Context, spec runtime.ProcessSpec, io runtime.ProcessIO) (runtime.Process, error) {
+	for i, pd := range c.ProcessDefs {
+		if reflect.DeepEqual(pd.Spec, spec) {
+			// remove current ProcessDefinition
+			c.ProcessDefs = append(c.ProcessDefs[:i], c.ProcessDefs[i+1:]...)
+
+			// setup a new process
+			p := &Process{Spec: pd.Spec, ProcessStub: pd.Stub}
+			p.addIO(io)
+			c.Processes = append(c.Processes, p)
+			return p, nil
+		}
 	}
-	return &Process{id: spec.ID, ProcessStub: p, TTY: spec.TTY, io: io}, nil
+	return nil, fmt.Errorf("must setup a ProcessStub for process %q (%+v)", spec.ID, spec)
 }
 
 func (c Container) Attach(ctx context.Context, id string, io runtime.ProcessIO) (runtime.Process, error) {
-	p, ok := c.Processes[id]
-	if !ok {
-		return nil, fmt.Errorf("must setup a ProcessStub for process %q", id)
+	for _, p := range c.Processes {
+		if p.Spec.ID == id {
+			if !p.Attachable {
+				return nil, fmt.Errorf("cannot attach to process %q because Attachable was not set to true in the ProcessStub", id)
+			}
+			p.addIO(io)
+			return p, nil
+		}
 	}
-	if p.Attachable {
-		return &Process{ProcessStub: p, io: io}, nil
-	}
-	return nil, fmt.Errorf("cannot attach to process %q because Attachable was not set to true in the ProcessStub", id)
+	return nil, fmt.Errorf("must setup a ProcessStub for process %q", id)
 }
 
 func (c Container) Properties() (map[string]string, error) {
 	return c.Props, nil
 }
 
-func (c Container) SetProperty(name string, value string) error {
+func (c *Container) SetProperty(name string, value string) error {
+	c.Props = cloneProps(c.Props)
 	c.Props[name] = value
 	return nil
 }
 
 func (c Container) DBContainer() db.CreatedContainer {
 	return c.DBContainer_
-}
-
-func cloneProcs(m map[string]ProcessStub) map[string]ProcessStub {
-	m2 := make(map[string]ProcessStub, len(m))
-	for k, v := range m {
-		m2[k] = v
-	}
-	return m2
 }
 
 func cloneProps(m map[string]string) map[string]string {
--- atc/runtime/runtimetest/process.go
+++ atc/runtime/runtimetest/process.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"sync"
 
 	"github.com/concourse/concourse/atc/runtime"
 )
@@ -19,13 +21,14 @@ type ProcessStub struct {
 
 type Process struct {
 	ProcessStub
-	id  string
-	TTY *runtime.TTYSpec
-	io  runtime.ProcessIO
+	Spec runtime.ProcessSpec
+
+	mtx sync.Mutex
+	io  []runtime.ProcessIO
 }
 
 func (p Process) ID() string {
-	return p.id
+	return p.Spec.ID
 }
 
 func (p *Process) Wait(ctx context.Context) (runtime.ProcessResult, error) {
@@ -33,16 +36,47 @@ func (p *Process) Wait(ctx context.Context) (runtime.ProcessResult, error) {
 		return runtime.ProcessResult{}, errors.New(p.Err)
 	}
 	if p.Stderr != "" {
-		fmt.Fprint(p.io.Stderr, p.Stderr)
+		fmt.Fprint(p.stderr(), p.Stderr)
 	}
 	if p.ExitStatus != 0 {
 		return runtime.ProcessResult{ExitStatus: p.ExitStatus}, nil
 	}
-	json.NewEncoder(p.io.Stdout).Encode(p.Output)
+	json.NewEncoder(p.stdout()).Encode(p.Output)
 	return runtime.ProcessResult{ExitStatus: 0}, nil
 }
 
 func (p *Process) SetTTY(tty runtime.TTYSpec) error {
-	p.TTY = &tty
+	p.Spec.TTY = &tty
 	return nil
+}
+
+func (p *Process) addIO(io runtime.ProcessIO) {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+
+	p.io = append(p.io, io)
+}
+
+func (p *Process) stdout() io.Writer {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+
+	var writers []io.Writer
+	for _, io := range p.io {
+		writers = append(writers, io.Stdout)
+	}
+
+	return io.MultiWriter(writers...)
+}
+
+func (p *Process) stderr() io.Writer {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+
+	var writers []io.Writer
+	for _, io := range p.io {
+		writers = append(writers, io.Stderr)
+	}
+
+	return io.MultiWriter(writers...)
 }
