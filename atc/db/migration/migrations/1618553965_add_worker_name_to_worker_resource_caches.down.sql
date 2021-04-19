6c7e53ff0135c25137f68172e2e1fc6ff4632898
Chao Li <chaol@vmware.com>
2021-04-19 14:01:22 +0800

    fix integration test.
    Signed-off-by: Chao Li <chaol@vmware.com>

--- atc/db/migration/migrations/1618553965_add_worker_name_to_worker_resource_caches.down.sql
+++ atc/db/migration/migrations/1618553965_add_worker_name_to_worker_resource_caches.down.sql
@@ -1,7 +1,7 @@
+DROP INDEX worker_resource_caches_uniq;
+
 ALTER TABLE worker_resource_caches
     DROP COLUMN worker_name;
-
-DROP INDEX worker_resource_caches_uniq;
 
 CREATE UNIQUE INDEX worker_resource_caches_uniq
     ON worker_resource_caches (resource_cache_id, worker_base_resource_type_id);
