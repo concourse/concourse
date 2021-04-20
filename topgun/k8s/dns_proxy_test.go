718f0fdaf45c6fcb8e5559c4c962e904f2d16481
Esteban Foronda <eforonda@vmware.com>
2021-04-20 16:55:03 -0400

    re-enable k8s dns proxy test
    this time with more documentation so we know what's going on and how to
    debug it.
    
    also modified the task so that it prints out slightly more useful
    information. by printing out the etc/resolv.conf, we can see exactly
    which dns server it's going to hit and reason about the test failure a
    bit more.
    
    Signed-off-by: Esteban Foronda <eforonda@vmware.com>
    Co-authored-by: Bohan Chen <bochen@pivotal.io>

--- topgun/k8s/dns_proxy_test.go
+++ topgun/k8s/dns_proxy_test.go
@@ -52,12 +52,13 @@ var _ = Describe("DNS Resolution", func() {
 			args = append(args, `--set-string=concourse.worker.containerd.dnsProxyEnable=`+dnsProxyEnable)
 			if dnsServer != "" {
 				args = append(args,
-					`--set=worker.env[0].name=CONCOURSE_CONTAINERD_DNS_SERVER`,
-					`--set=worker.env[0].value=`+dnsServer)
+					`--set=concourse.worker.containerd.dnsServers=['`+dnsServer+`']`,
+				)
 			}
 		case runtime == guardianRuntime:
 			args = append(args, `--set-string=concourse.worker.garden.dnsProxyEnable=`+dnsProxyEnable)
 			if dnsServer != "" {
+				// garden flags aren't explicityly defined in the chart, so add them as env vars directly
 				args = append(args,
 					`--set=worker.env[0].name=CONCOURSE_GARDEN_DNS_SERVER`,
 					`--set=worker.env[0].value=`+dnsServer)
@@ -86,39 +87,43 @@ var _ = Describe("DNS Resolution", func() {
 
 				Expect(sess.ExitCode()).To(BeZero())
 			},
+			// local proxy will pass through requests to the k8s native dns
 			Entry("Proxy Enabled, with full service name", Case{
 				enableDnsProxy:  "true",
 				addressFunction: fullAddress,
 				shouldWork:      true,
 			}),
+			// the short address is expanded into the full addresses with the help of
+			// the `search` configuration in the resolv.conf. But our implementation
+			// of the proxy server kinda just ignores this configuration. So short
+			// addresses won't ever work
 			Entry("Proxy Enabled, with short service name", Case{
 				enableDnsProxy:  "true",
 				addressFunction: shortAddress,
 				shouldWork:      false,
 			}),
+			// no dns proxy = the super powerful k8s native dns
 			Entry("Proxy Disabled, with full service name", Case{
 				enableDnsProxy:  "false",
 				addressFunction: fullAddress,
 				shouldWork:      true,
 			}),
+			// no dns proxy = the super powerful k8s native dns
 			Entry("Proxy Disabled, with short service name", Case{
 				enableDnsProxy:  "false",
 				addressFunction: shortAddress,
 				shouldWork:      true,
 			}),
+			// the public 8.8.8.8 server won't be able to resolve k8s addresses
 			Entry("Adding extra dns server, with Proxy Disabled and full address", Case{
 				enableDnsProxy:  "false",
 				dnsServer:       "8.8.8.8",
 				addressFunction: fullAddress,
 				shouldWork:      false,
 			}),
-			// Skipping this test as it frequently fails - it asserts that the
-			// lookup of an internal k8s address should fail when 8.8.8.8 is
-			// provided as the DNS server, but when we generate the
-			// /etc/resolv.conf in containers, we add both 8.8.8.8 AND the
-			// local DNS proxy as nameservers, so it seems like the DNS
-			// resolution should work.
-			XEntry("Adding extra dns server, with Proxy Enabled and full address", Case{
+			// dns requests hits the 8.8.8.8 server first, as long as it doesn't time
+			// out, it should fail to resolve and end the search chain, skipping the proxy
+			Entry("Adding extra dns server, with Proxy Enabled and full address", Case{
 				enableDnsProxy:  "true",
 				dnsServer:       "8.8.8.8",
 				addressFunction: fullAddress,
