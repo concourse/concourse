d1fe20cfcb577f8b795935f87bf4d2fb4cfecde4
Taylor Silva <tsilva@pivotal.io>
2021-05-28 15:09:59 -0400

    Move test from integration to testflight
    Signed-off-by: Taylor Silva <tsilva@pivotal.io>

---
+++ testflight/build_events_test.go
@@ -0,0 +1,33 @@
+package testflight_test
+
+import (
+	"encoding/json"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Build Events", func() {
+	It("across steps log errors from sub-steps", func() {
+		setAndUnpausePipeline("fixtures/erroring_pipeline.yml")
+		sess := spawnFly("trigger-job", "-j", inPipeline("across-step"), "-w")
+		wait(sess, true)
+
+		sess = spawnFly("builds", "-j", inPipeline("across-step"), "--json")
+		wait(sess, false)
+
+		buildsRaw := sess.Out.Contents()
+		builds := []struct {
+			ApiUrl string `json:"api_url"`
+		}{}
+		json.Unmarshal(buildsRaw, &builds)
+
+		sess = spawnFly("curl", builds[0].ApiUrl+"/events", "--", "--max-time", "5")
+		wait(sess, true)
+
+		buildEvents := sess.Out.Contents()
+		errEvents := strings.Count(string(buildEvents), `"event":"error"`)
+		Expect(errEvents).To(Equal(2))
+	})
+})
