c26ca78df8335b9851b84edc8e4fb75de7411f5a
Muntasir Chowdhury <mchowdhury@pivotal.io>
2021-06-01 16:17:19 -0400

    worker: set containerd as the default runtime
    Setting the default value of --runtime to containerd. No longer need to set the
    value explicitly in the docker-compose.yml.
    
    concourse/concourse#7106
    
    Signed-off-by: Muntasir Chowdhury <mchowdhury@pivotal.io>

--- worker/workercmd/worker_linux.go
+++ worker/workercmd/worker_linux.go
@@ -25,7 +25,7 @@ type Certs struct {
 }
 
 type RuntimeConfiguration struct {
-	Runtime string `long:"runtime" default:"guardian" choice:"guardian" choice:"containerd" choice:"houdini" description:"Runtime to use with the worker. Please note that Houdini is insecure and doesn't run 'tasks' in containers."`
+	Runtime string `long:"runtime" default:"containerd" choice:"guardian" choice:"containerd" choice:"houdini" description:"Runtime to use with the worker. Please note that Houdini is insecure and doesn't run 'tasks' in containers."`
 }
 
 type GuardianRuntime struct {
