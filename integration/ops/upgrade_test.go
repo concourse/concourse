795dccbb72448a98445199a95798b56360600976
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-06-11 10:56:41 -0400

    use fixed name for upgrade/downgrade test
    the upgrade/downgrade tests could previously stall out because the
    worker name changed, but the ATC hadn't realized that the old worker was
    stalled before it tried to schedule a task on it, and then gets stuck
    retrying communication with either garden or baggageclaim (not sure
    which) on the missing worker.
    
    this is a bug with the suite that currently exists on master - e.g. see
    https://ci.concourse-ci.org/teams/main/pipelines/concourse/jobs/integration/builds/247#L60c1769f:451
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- integration/ops/upgrade_test.go
+++ integration/ops/upgrade_test.go
@@ -10,7 +10,7 @@ import (
 func TestUpgrade(t *testing.T) {
 	t.Parallel()
 
-	latestDC := dctest.Init(t, "../docker-compose.yml", "overrides/latest.yml")
+	latestDC := dctest.Init(t, "../docker-compose.yml", "overrides/named.yml", "overrides/latest.yml")
 
 	t.Run("deploy latest", func(t *testing.T) {
 		latestDC.Run(t, "up", "-d")
@@ -19,7 +19,7 @@ func TestUpgrade(t *testing.T) {
 	fly := flytest.Init(t, latestDC)
 	setupUpgradeDowngrade(t, fly)
 
-	devDC := dctest.Init(t, "../docker-compose.yml")
+	devDC := dctest.Init(t, "../docker-compose.yml", "overrides/named.yml")
 
 	t.Run("upgrade to dev", func(t *testing.T) {
 		devDC.Run(t, "up", "-d")
