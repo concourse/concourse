62f310fdb46314726b709cbf32f97a351176e7e4
Rishabh Jain <rishabh.jain.cs14@gmail.com>
2021-06-14 20:04:37 +0530

    fly: add --team flag to checklist
    Signed-off-by: Rishabh Jain <rishabh.jain.cs14@gmail.com>

--- fly/integration/checklist_test.go
+++ fly/integration/checklist_test.go
@@ -145,5 +145,59 @@ job-2: concourse.check %s main some-pipeline/branch:master job-2
 				})
 			})
 		})
+
+		Context("when a team name is specified alongwith pipeline name", func() {
+			JustBeforeEach(func() {
+				atcServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v1/teams/other-team"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, atc.Team{
+							Name: "other-team",
+						}),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v1/teams/other-team/pipelines/some-pipeline/config"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, atc.ConfigResponse{Config: config}, http.Header{atc.ConfigVersionHeader: {"42"}}),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("PUT", "/api/v1/teams/other-team/pipelines/some-pipeline/config"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, atc.ConfigResponse{Config: config}, http.Header{atc.ConfigVersionHeader: {"42"}}),
+					),
+				)
+			})
+
+			Context("when there are no groups", func() {
+				BeforeEach(func() {
+					config = atc.Config{
+						Jobs: atc.JobConfigs{
+							{
+								Name: "job-1",
+							},
+							{
+								Name: "job-2",
+							},
+						},
+					}
+
+				})
+
+				It("prints the config as yaml to stdout, and uses the pipeline name as header", func() {
+					flyCmd := exec.Command(flyPath, "-t", targetName, "checklist", "-p", "some-pipeline/branch:master", "--team", "other-team")
+
+					sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
+					Expect(err).NotTo(HaveOccurred())
+
+					<-sess.Exited
+					Expect(sess.ExitCode()).To(Equal(0))
+
+					Expect(string(sess.Out.Contents())).To(Equal(fmt.Sprintf(
+						`#- some-pipeline/branch:master
+job-1: concourse.check %s other-team some-pipeline/branch:master job-1
+job-2: concourse.check %s other-team some-pipeline/branch:master job-2
+
+`, atcServer.URL(), atcServer.URL())))
+				})
+			})
+		})
 	})
 })
