81077624022061a94798831b0168bc725adb8b97
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-06-17 17:53:45 -0400

    make query parsing more robust
    this adds an query extraction method to reduce the likelihood of false
    positives by treating CTEs, UNIONs, and subqueries as separate when
    cointing the number of JOINs
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/postgresrunner/join_limit_conn.go
+++ atc/postgresrunner/join_limit_conn.go
@@ -12,13 +12,13 @@ import (
 )
 
 func validateJoinLimit(query string) error {
-	// Each subquery, split by a UNION or a UNION ALL, can have up to 8 JOINs
-	subQueries := strings.Split(query, "UNION")
-	for _, subQuery := range subQueries {
-		numJoins := strings.Count(strings.ToUpper(subQuery), "JOIN")
+	// Each subquery can have up to 8 explicit JOINs
+	individualQueries := ExtractQueries(query)
+	for _, q := range individualQueries {
+		numJoins := strings.Count(strings.ToUpper(q), "JOIN")
 		if numJoins > 8 {
-			errMsg := "the following query has too many JOINs\n\n" + subQuery
-			if len(subQueries) > 1 {
+			errMsg := "the following query has too many JOINs\n\n" + q
+			if len(individualQueries) > 1 {
 				errMsg += "\n\nit is part of this full query:\n\n" + query
 			}
 			errMsg += fmt.Sprintf("\n\njoin_collapse_limit defaults to 8 JOINs, while this query has %d. exceeding the limit can result in really slow queries", numJoins)
@@ -27,6 +27,52 @@ func validateJoinLimit(query string) error {
 		}
 	}
 	return nil
+}
+
+// ExtractQueries is an helper to extract a list of queries from a top-level
+// query. In particular, it separates any CTE's, UNIONs, and subqueries from
+// the main query, since they are each evaluated separately when calculating
+// the number of joins.
+func ExtractQueries(query string) []string {
+	var queries []string
+
+	var recurse func(int) int
+	recurse = func(i int) int {
+		start := i
+		curQuery := ""
+		for i < len(query) {
+			if query[i] == ')' {
+				break
+			}
+			if query[i] == '(' {
+				curQuery += query[start:i] + "(...)"
+				start = recurse(i+1) + 1
+				i = start
+			}
+			i += 1
+		}
+		if start < len(query) {
+			curQuery += query[start:i]
+		}
+		j := 0
+		for j < len(curQuery) {
+			unionIndex := strings.Index(strings.ToUpper(curQuery[j:]), "UNION")
+			if unionIndex < 0 {
+				unionIndex = len(curQuery)
+			} else {
+				unionIndex += j
+			}
+			queries = append(queries, strings.TrimSpace(curQuery[j:unionIndex]))
+			j = unionIndex + 5
+		}
+		return i
+	}
+
+	i := 0
+	for i < len(query) {
+		i = recurse(i)
+	}
+	return queries
 }
 
 type joinLimitValidatorConn struct {
---
+++ atc/postgresrunner/join_limit_conn_test.go
@@ -0,0 +1,37 @@
+package postgresrunner_test
+
+import (
+	"github.com/concourse/concourse/atc/postgresrunner"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ExtractQueries", func() {
+	It("parses queries out of a top-level query", func() {
+		query := `WITH abc AS (
+  SELECT def FROM (SELECT 1)
+), ghi AS (
+  SELECT blah
+  UNION
+  SELECT bloo
+)
+SELECT who, cares
+FROM something
+JOIN other ON col1 = (SELECT other_col FROM something)
+	union
+SELECT something_else FROM a_table`
+
+		Expect(postgresrunner.ExtractQueries(query)).To(ConsistOf(
+			`WITH abc AS (...), ghi AS (...)
+SELECT who, cares
+FROM something
+JOIN other ON col1 = (...)`,
+			`SELECT something_else FROM a_table`,
+			`SELECT other_col FROM something`,
+			`SELECT def FROM (...)`,
+			`SELECT blah`,
+			`SELECT bloo`,
+			`SELECT 1`,
+		))
+	})
+})
