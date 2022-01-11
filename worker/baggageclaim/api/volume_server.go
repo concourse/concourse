f947e345a489ff44ad564c73057915502ac60d2e
Evan <chaol@vmware.com>
2022-01-11 08:16:13 +0800

    fixed typo in code comment.
    Signed-off-by: Evan <chaol@vmware.com>

--- worker/baggageclaim/api/volume_server.go
+++ worker/baggageclaim/api/volume_server.go
@@ -613,9 +613,9 @@ func (vs *VolumeServer) StreamP2pOut(w http.ResponseWriter, req *http.Request) {
 		}
 	}(doneChan)
 
-	// Send a white space to cline as an indicator of in-progress every 10 seconds.
-	// After source worker finishes sending volume ot dest worker, send either "ok"
-	// for success or an error message to client.
+	// Send a white space to client as an indicator of in-progress every 10 seconds.
+	// After source worker finishes sending volume ot dest worker, send client "ok"
+	// for success or an error message.
 	tick := time.NewTicker(10 * time.Second)
 	for {
 		select {
