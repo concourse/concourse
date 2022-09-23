c52952325b7f23db45a76715e6fff097417cf767
Jonathan Ryan <jonathan_ryan@comcast.com>
2022-09-23 10:45:09 -0400

    WIP

--- atc/task.go
+++ atc/task.go
@@ -174,8 +174,8 @@ type TaskServiceSpecificConfig struct {
 }
 
 type TaskServiceConfig struct {
-	TaskConfig                TaskConfig
-	TaskServiceSpecificConfig TaskServiceSpecificConfig
+	TaskConfig                *TaskConfig
+	TaskServiceSpecificConfig *TaskServiceSpecificConfig
 }
 
 type TaskServicePlan struct {
