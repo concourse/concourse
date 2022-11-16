c9c1c120f1b1ce78784556be9d465b915a762635
Rui Yang <ruiya@vmware.com>
2022-11-16 18:15:00 -0500

    remove rerun_of int->bigint migrations
    Signed-off-by: Rui Yang <ruiya@vmware.com>

--- atc/db/migration/migrations/1653924132_int_to_bigint.up.sql
+++ atc/db/migration/migrations/1653924132_int_to_bigint.up.sql
@@ -3,9 +3,6 @@ ALTER SEQUENCE resource_config_versions_id_seq AS bigint;
 ALTER TABLE build_comments
     ALTER COLUMN build_id TYPE bigint;
 
-ALTER TABLE builds
-    ALTER COLUMN rerun_of TYPE bigint;
-
 ALTER TABLE successful_build_outputs
     ALTER COLUMN rerun_of TYPE bigint;
 
