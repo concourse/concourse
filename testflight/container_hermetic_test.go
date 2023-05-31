6a122250d3f0b2c110d89ef1ac4d04caf27c193a
Mark Stokan <stokanm@vmware.com>
2023-05-31 16:07:45 -0700

    use wget in hermetic test
    Co-authored-by: Mark Stokan <stokanm@vmware.com>
    Co-authored-by: Rui Yang <ruiya@vmware.com>
    Signed-off-by: Mark Stokan <stokanm@vmware.com>

--- testflight/container_hermetic_test.go
+++ testflight/container_hermetic_test.go
@@ -17,11 +17,11 @@ var _ = Describe("A job with a task that has hermetic set to true", func() {
 
 		if config.Runtime == "containerd" {
 			By("containerd runtime it should failed on establish network connection")
-			Expect(watch).To(gbytes.Say("failure resolving"))
-			Expect(watch).To(gexec.Exit(100)) // can't apt update
+			Expect(watch).To(gbytes.Say("failed: Network is unreachable"))
+			Expect(watch).To(gexec.Exit(4)) // can't apt update
 		} else {
-			By("guardian runtime it should success on curl")
-			Expect(watch).To(gbytes.Say("200"))
+			By("guardian runtime it should success establish network connection")
+			Expect(watch).To(gbytes.Say("200 OK"))
 		}
 	})
 })
