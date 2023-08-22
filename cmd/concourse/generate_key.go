19341a66b3cb00b76945735a463aaea29ee23357
Bernhard Schuster <bernhard@ahoi.io>
2023-08-22 18:09:34 +0200

    feat/cli: allow to extract the default seccomp profile
    Signed-off-by: Bernhard Schuster <bernhard@ahoi.io>

--- cmd/concourse/generate_key.go
+++ cmd/concourse/generate_key.go
@@ -4,9 +4,13 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	bespec "github.com/concourse/concourse/worker/runtime/spec"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -73,4 +77,28 @@ func (cmd *GenerateKeyCommand) Execute(args []string) error {
 	}
 
 	return nil
+}
+
+type ExtractInternalConfigCommand struct {
+	FilePath string `short:"f"  long:"filename"  required:"true"  description:"File path where the key shall be created. When generating ssh keys, the public key will be stored in a file with the same name but with '.pub' appended."`
+
+	Seccomp bool `long:"seccomp" required:"false"  description:"Extract the default builtin seccomp filter"`
+}
+
+func (cmd *ExtractInternalConfigCommand) Execute(args []string) error {
+	var dest = cmd.FilePath
+	if cmd.Seccomp {
+		seccompfilter := bespec.GetDefaultSeccompProfile()
+		bytes, err := json.Marshal(seccompfilter)
+		if err != nil {
+			return fmt.Errorf("failed to serialize key file: %s", err)
+		}
+		err = ioutil.WriteFile(dest, bytes, 0644)
+		if err != nil {
+			return fmt.Errorf("failed to write json to file: %s @ %s", err, dest)
+		}
+		return nil
+	} else {
+		return fmt.Errorf("Nothing to extract, use one of the optional flags for the subcommand")
+	}
 }
