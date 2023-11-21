6da6b104a00e6b219077a195a3036f00682e6eb2
Jonathan Giroux <giroux.jo@gmail.com>
2023-11-21 13:00:45 +0100

    feat(fly): add `background` option to `execute` command
    Signed-off-by: Jonathan Giroux <giroux.jo@gmail.com>

--- fly/commands/execute.go
+++ fly/commands/execute.go
@@ -24,6 +24,7 @@ import (
 )
 
 type ExecuteCommand struct {
+	Background     bool                               `short:"b" long:"background"                            description:"Create the build and exit, i.e. neither watch logs nor retrieve outputs."`
 	TaskConfig     atc.PathFlag                       `short:"c" long:"config" required:"true"                description:"The task config to execute"`
 	Privileged     bool                               `short:"p" long:"privileged"                            description:"Run the task with full privileges"`
 	IncludeIgnored bool                               `          long:"include-ignored"                       description:"Including .gitignored paths. Disregards .gitignore entries and uploads everything"`
@@ -85,6 +86,10 @@ func (command *ExecuteCommand) Execute(args []string) error {
 		return err
 	}
 
+	if len(outputs) > 0 && command.Background {
+		return fmt.Errorf("background execution cannot withstand outputs")
+	}
+
 	plan, err := executehelpers.CreateBuildPlan(
 		planFactory,
 		target,
@@ -128,6 +133,11 @@ func (command *ExecuteCommand) Execute(args []string) error {
 	}
 
 	fmt.Printf("executing build %d at %s\n", build.ID, clientURL.ResolveReference(buildURL))
+
+	if command.Background {
+		os.Exit(0)
+		return nil
+	}
 
 	terminate := make(chan os.Signal, 1)
 
