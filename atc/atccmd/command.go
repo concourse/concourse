6695297230a5e8f1331e8a961594a9e2874559b9
Mathias Bogaert <mathias.bogaert@gmail.com>
2025-02-04 09:20:43 +0000

    Add support for Strict-Transport-Security header
    Enables setting CONCOURSE_STRICT_TRANSPORT_SECURITY.
    
    Signed-off-by: Mathias Bogaert <mathias.bogaert@gmail.com>
    Co-authored-by: Taylor Silva <dev@taydev.net>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -180,11 +180,12 @@ type RunCommand struct {
 	} `group:"Policy Checking"`
 
 	Server struct {
-		XFrameOptions         string `long:"x-frame-options" default:"deny" description:"The value to set for the X-Frame-Options header."`
-		ContentSecurityPolicy string `long:"content-security-policy" default:"frame-ancestors 'none'" description:"The value to set for the Content-Security-Policy header."`
-		ClusterName           string `long:"cluster-name" description:"A name for this Concourse cluster, to be displayed on the dashboard page."`
-		ClientID              string `long:"client-id" default:"concourse-web" description:"Client ID to use for login flow"`
-		ClientSecret          string `long:"client-secret" required:"true" description:"Client secret to use for login flow"`
+		XFrameOptions           string `long:"x-frame-options" default:"deny" description:"The value to set for the X-Frame-Options header."`
+		ContentSecurityPolicy   string `long:"content-security-policy" default:"frame-ancestors 'none'" description:"The value to set for the Content-Security-Policy header."`
+		StrictTransportSecurity string `long:"strict-transport-security" description:"The value to set for the Strict-Transport-Security header."`
+		ClusterName             string `long:"cluster-name" description:"A name for this Concourse cluster, to be displayed on the dashboard page."`
+		ClientID                string `long:"client-id" default:"concourse-web" description:"Client ID to use for login flow"`
+		ClientSecret            string `long:"client-secret" required:"true" description:"Client secret to use for login flow"`
 	} `group:"Web Server"`
 
 	LogDBQueries   bool `long:"log-db-queries" description:"Log database queries."`
@@ -1816,8 +1817,9 @@ func (cmd *RunCommand) constructHTTPHandler(
 		Logger: logger,
 
 		Handler: wrappa.SecurityHandler{
-			XFrameOptions:         cmd.Server.XFrameOptions,
-			ContentSecurityPolicy: cmd.Server.ContentSecurityPolicy,
+			XFrameOptions:           cmd.Server.XFrameOptions,
+			ContentSecurityPolicy:   cmd.Server.ContentSecurityPolicy,
+			StrictTransportSecurity: cmd.Server.StrictTransportSecurity,
 
 			// proxy Authorization header to/from auth cookie,
 			// to support auth from JS (EventSource) and custom JWT auth
