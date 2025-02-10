a86ca27e2bc981d1dac7be8bfcef42d164aaaceb
Mathias Bogaert <mathias.bogaert@gmail.com>
2025-02-10 19:44:48 +0000

    Update to latest inconsolata
    from https://github.com/googlefonts/Inconsolata/releases.
    
    * Move from ttf to woff2.
    * Add content type.
    * Rename manifest.json to manifest.webmanifest and add content type.
    
    Signed-off-by: Mathias Bogaert <mathias.bogaert@gmail.com>
    Co-authored-by: Taylor Silva <dev@taydev.net>

--- web/public_handler.go
+++ web/public_handler.go
@@ -2,10 +2,14 @@ package web
 
 import (
 	"io/fs"
+	"mime"
 	"net/http"
 )
 
 func PublicHandler(publicFS fs.FS) http.Handler {
+	mime.AddExtensionType(".woff2", "font/woff2")
+	mime.AddExtensionType(".webmanifest", "application/manifest+json")
+
 	return CacheNearlyForever(
 		http.StripPrefix(
 			"/public/",
