99414ca53b5eba239ccb8d94c695b8ef24ceb6ee
Mathias Bogaert <mathias.bogaert@gmail.com>
2025-03-01 08:26:28 +0000

    Fix: prevent potential race condition in Tracker.IterateInterpolatedCreds
    Ensure the read lock is properly released using defer to guarantee thread safety
    when iterating over credentials. This prevents a possible race condition if
    multiple goroutines access the Tracker concurrently.
    
    nit: modernize code by using any instead of interface{}
    
    Signed-off-by: Mathias Bogaert <mathias.bogaert@gmail.com>
    Co-authored-by: Taylor Silva <dev@taydev.net>

--- vars/tracker.go
+++ vars/tracker.go
@@ -24,7 +24,7 @@ func NewTracker(on bool) *Tracker {
 	}
 }
 
-func (t *Tracker) Track(varRef Reference, val interface{}) {
+func (t *Tracker) Track(varRef Reference, val any) {
 	if !t.Enabled {
 		return
 	}
@@ -35,16 +35,16 @@ func (t *Tracker) Track(varRef Reference, val interface{}) {
 	t.track(varRef, val)
 }
 
-func (t *Tracker) track(varRef Reference, val interface{}) {
+func (t *Tracker) track(varRef Reference, val any) {
 	switch v := val.(type) {
-	case map[interface{}]interface{}:
+	case map[any]any:
 		for kk, vv := range v {
 			t.track(Reference{
 				Path:   varRef.Path,
 				Fields: append(varRef.Fields, kk.(string)),
 			}, vv)
 		}
-	case map[string]interface{}:
+	case map[string]any:
 		for kk, vv := range v {
 			t.track(Reference{
 				Path:   varRef.Path,
@@ -62,10 +62,10 @@ func (t *Tracker) track(varRef Reference, val interface{}) {
 
 func (t *Tracker) IterateInterpolatedCreds(iter TrackedVarsIterator) {
 	t.lock.RLock()
+	defer t.lock.RUnlock()
 	for k, v := range t.interpolatedCreds {
 		iter.YieldCred(k, v)
 	}
-	t.lock.RUnlock()
 }
 
 type CredVarsTracker struct {
@@ -73,7 +73,7 @@ type CredVarsTracker struct {
 	CredVars Variables
 }
 
-func (t *CredVarsTracker) Get(ref Reference) (interface{}, bool, error) {
+func (t *CredVarsTracker) Get(ref Reference) (any, bool, error) {
 	val, found, err := t.CredVars.Get(ref)
 	if found {
 		t.Tracker.Track(ref, val)
