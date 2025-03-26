8169c3492bc3098670ceaca41f4c8599d39019fc
Mathias Bogaert <mathias.bogaert@gmail.com>
2025-03-26 10:06:18 +0000

    refactor: optimize Counter and Gauge with atomic.Int64 - Update to use atomic.Int64 instead of function-based atomic operations - Maintain backward compatibility with existing behavior
    This change modernizes the code to use Go 1.19+ atomic types while
    improving performance in high-concurrency scenarios.
    
    Signed-off-by: Mathias Bogaert <mathias.bogaert@gmail.com>

--- atc/metric/counter.go
+++ atc/metric/counter.go
@@ -3,18 +3,18 @@ package metric
 import "sync/atomic"
 
 type Counter struct {
-	cur int64
+	cur atomic.Int64
 }
 
 func (m *Counter) Inc() {
-	atomic.AddInt64((*int64)(&m.cur), 1)
+	m.cur.Add(1)
 }
 
 func (m *Counter) IncDelta(delta int) {
-	atomic.AddInt64((*int64)(&m.cur), int64(delta))
+	m.cur.Add(int64(delta))
 }
 
 func (m *Counter) Delta() float64 {
-	cur := atomic.SwapInt64((*int64)(&m.cur), 0)
+	cur := m.cur.Swap(0)
 	return float64(cur)
 }
