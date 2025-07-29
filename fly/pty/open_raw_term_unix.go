1492afb5791dabf4b30dd35ff179af887753ef78
Taylor Silva <dev@taydev.net>
2025-07-29 13:45:32 -0400

    remove all usage of deprecated golang.org/x/crypto/ssh/terminal
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- fly/pty/open_raw_term_unix.go
+++ fly/pty/open_raw_term_unix.go
@@ -6,17 +6,17 @@ package pty
 import (
 	"os"
 
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 
-	"github.com/pkg/term"
+	pkgterm "github.com/pkg/term"
 )
 
 func IsTerminal() bool {
-	return terminal.IsTerminal(int(os.Stdin.Fd()))
+	return term.IsTerminal(int(os.Stdin.Fd()))
 }
 
 func OpenRawTerm() (Term, error) {
-	t, err := term.Open(os.Stdin.Name(), term.RawMode)
+	t, err := pkgterm.Open(os.Stdin.Name(), pkgterm.RawMode)
 	if err != nil {
 		return nil, err
 	}
