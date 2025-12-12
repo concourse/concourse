779719535b8332cb382223ad0cbe85a18b9d6ce0
Taylor Silva <dev@taydev.net>
2025-12-12 17:22:37 -0500

    ensure robocopy doesn't retry on errors for 347 days
    which is an insane set of default values to have.
    
    Also made sure to bubble up any errors from robocopy.
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- worker/baggageclaim/volume/copy/copy_windows.go
+++ worker/baggageclaim/volume/copy/copy_windows.go
@@ -1,11 +1,23 @@
 package copy
 
 import (
+	"errors"
 	"os/exec"
 )
 
 func Cp(followSymlinks bool, src, dest string) error {
-	args := []string{"/e", "/nfl", "/ndl", "/mt"}
+	args := []string{
+		"/e",   // copy subdir's even if they're empty
+		"/nfl", // don't log ever file copied
+		"/ndl", // don't log ever dir copied
+		"/mt",  // do multi-thread copying, defaults to 8 threads
+
+		// retrying has a default value of 1 million with 30s waits, which is
+		// 347 days of retrying. Let's override that.
+		"/r:5", // retry any failed copy up to 5 times
+		"/w:5", // wait 5 seconds between each retry
+	}
+
 	if !followSymlinks {
 		args = append(args, "/sl")
 	}
@@ -24,7 +36,7 @@ func robocopy(args ...string) error {
 		// 1 means that files were copied successfully. Google for additional error codes.
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			if exitErr.ExitCode() > 1 {
-				return err
+				return errors.Join(err, errors.New(string(exitErr.Stderr)))
 			}
 		} else {
 			return err
