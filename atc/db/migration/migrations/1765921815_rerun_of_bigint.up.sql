2254550df5cdbab572328d154379a4f992027245
Taylor Silva <dev@taydev.net>
2025-12-16 18:03:56 -0500

    add migration to add bigint version of rerun_of column
    Signed-off-by: Taylor Silva <dev@taydev.net>

---
+++ atc/db/migration/migrations/1765921815_rerun_of_bigint.up.sql
@@ -0,0 +1,18 @@
+
+ALTER TABLE builds RENAME COLUMN "rerun_of" TO "rerun_of_old";
+
+ALTER TABLE builds ADD COLUMN "rerun_of" bigint REFERENCES builds (id) ON DELETE CASCADE;
+
+ALTER INDEX rerun_of_builds_idx RENAME TO rerun_of_old_builds_idx;
+CREATE INDEX rerun_of_builds_idx ON builds (rerun_of);
+
+ALTER INDEX succeeded_builds_ordering_with_rerun_builds_idx RENAME TO succeeded_builds_ordering_with_rerun_old_builds_idx;
+CREATE INDEX succeeded_builds_ordering_with_rerun_builds_idx ON builds (job_id, rerun_of, COALESCE(rerun_of, id) DESC, id DESC) WHERE status = 'succeeded';
+
+ALTER INDEX order_builds_by_rerun_of_or_id_idx RENAME TO order_builds_by_rerun_of_old_or_id_idx;
+CREATE INDEX order_builds_by_rerun_of_or_id_idx ON builds((COALESCE(rerun_of, id)) DESC, id DESC);
+
+ALTER INDEX order_job_builds_by_rerun_of_or_id_idx RENAME TO order_job_builds_by_rerun_of_old_or_id_idx;
+CREATE INDEX order_job_builds_by_rerun_of_or_id_idx
+  ON builds (job_id, COALESCE(rerun_of, id) DESC, id DESC)
+  WHERE job_id IS NOT NULL;
