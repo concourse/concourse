ec4630aa74acfe9a518feb0a2bad414ba40fb5c4
Taylor Silva <dev@taydev.net>
2026-02-18 13:20:52 -0500

    go fix ./go-concourse
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- go-concourse/concourse/internal/policy_warn.go
+++ go-concourse/concourse/internal/policy_warn.go
@@ -14,10 +14,11 @@ func showPolicyCheckWarningIfHas(response *http.Response) {
 	}
 
 	parts := strings.Split(warn, " * ")
-	warnText := fmt.Sprintf("\x1b[1;33m%s\x1b[0m\n", parts[0])
+	var warnText strings.Builder
+	warnText.WriteString(fmt.Sprintf("\x1b[1;33m%s\x1b[0m\n", parts[0]))
 	for i := 1; i < len(parts); i++ {
-		warnText += fmt.Sprintf("\x1b[1;33m * %s\x1b[0m\n", parts[i])
+		warnText.WriteString(fmt.Sprintf("\x1b[1;33m * %s\x1b[0m\n", parts[i]))
 	}
-	warnText += fmt.Sprintln("\x1b[33mWARNING: unblocking from the policy check failure for soft enforcement\x1b[0m")
-	fmt.Fprintln(os.Stderr, warnText)
+	warnText.WriteString(fmt.Sprintln("\x1b[33mWARNING: unblocking from the policy check failure for soft enforcement\x1b[0m"))
+	fmt.Fprintln(os.Stderr, warnText.String())
 }
