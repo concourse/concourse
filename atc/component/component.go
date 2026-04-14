ac82bbb2c494b7c77699db67b571af8bed3d9f9d
Taylor Silva <dev@taydev.net>
2026-04-14 13:27:02 -0400

    replace mockey with counterfeiter
    This was the only place we were using mockery. We use counterfeiter
    everywhere else
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- atc/component/component.go
+++ atc/component/component.go
@@ -1,5 +1,8 @@
 package component
 
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
+
+//counterfeiter:generate . Component
 type Component interface {
 	Name() string
 	Paused() bool
