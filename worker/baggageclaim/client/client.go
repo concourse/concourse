1783adff92e33a76b7a55edbcb3e9d52f7931c2f
Taylor Silva <dev@taydev.net>
2026-06-02 12:01:16 -0400

    nit: remove wrapper around baggageclaim.Client in client package
    was only referenced in the test suites and didn't expand on the
    interface. Added an interface check since maybe that was the original
    intention of adding that wrapper.
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- worker/baggageclaim/client/client.go
+++ worker/baggageclaim/client/client.go
@@ -26,9 +26,7 @@ import (
 
 var ErrVolumeDeletion = errors.New("failed-to-delete-volume")
 
-type Client interface {
-	baggageclaim.Client
-}
+var _ baggageclaim.Client = (*client)(nil)
 
 type client struct {
 	requestGenerator *rata.RequestGenerator
@@ -39,7 +37,7 @@ type client struct {
 	givenHttpClient *http.Client
 }
 
-func New(apiURL string, nestedRoundTripper http.RoundTripper) Client {
+func New(apiURL string, nestedRoundTripper http.RoundTripper) baggageclaim.Client {
 	return &client{
 		requestGenerator: rata.NewRequestGenerator(apiURL, baggageclaim.Routes),
 
@@ -49,7 +47,7 @@ func New(apiURL string, nestedRoundTripper http.RoundTripper) Client {
 	}
 }
 
-func NewWithHTTPClient(apiURL string, httpClient *http.Client) Client {
+func NewWithHTTPClient(apiURL string, httpClient *http.Client) baggageclaim.Client {
 	return &client{
 		givenHttpClient:  httpClient,
 		requestGenerator: rata.NewRequestGenerator(apiURL, baggageclaim.Routes),
