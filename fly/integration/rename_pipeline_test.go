0e69102927b50c6ffb9215bcc79356549ef72926
BG <bguttmann@anynines.com>
2026-09-24 14:25:11 +0200

    Implement changes to FLY CLI Changes

--- fly/integration/rename_pipeline_test.go
+++ fly/integration/rename_pipeline_test.go
@@ -51,8 +51,8 @@ var _ = Describe("rename-pipeline", func() {
 			})
 		})
 
-		Context("when specifying a new pipeline name with a '/' character in it", func() {
-			It("fails and says '/' characters are not allowed", func() {
+		Context("when the new-name flag has an invalid instance vars format", func() {
+			It("fails and prints an invalid flag error", func() {
 				flyCmd := exec.Command(flyPath, "-t", targetName, "rename-pipeline", "-o", "some-pipeline", "-n", "forbidden/pipelinename")
 
 				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
@@ -61,13 +61,13 @@ var _ = Describe("rename-pipeline", func() {
 				<-sess.Exited
 				Expect(sess.ExitCode()).To(Equal(1))
 
-				Expect(sess.Err).To(gbytes.Say("error: new pipeline name cannot contain '/'"))
+				Expect(sess.Err).To(gbytes.Say("error: invalid argument for flag `" + osFlag("n", "new-name")))
 			})
 		})
 
-		Context("when the pipeline flag is invalid", func() {
-			It("fails and print invalid flag error", func() {
-				flyCmd := exec.Command(flyPath, "-t", targetName, "rename-pipeline", "-o", "pipeline/branch:master", "-n", "some-new-name")
+		Context("when the old-name flag has an invalid instance vars format", func() {
+			It("fails and prints an invalid flag error", func() {
+				flyCmd := exec.Command(flyPath, "-t", targetName, "rename-pipeline", "-o", "pipeline/badformat", "-n", "some-new-name")
 
 				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).NotTo(HaveOccurred())
@@ -75,7 +75,7 @@ var _ = Describe("rename-pipeline", func() {
 				<-sess.Exited
 				Expect(sess.ExitCode()).To(Equal(1))
 
-				Expect(sess.Err).To(gbytes.Say("error: old pipeline name cannot contain '/'"))
+				Expect(sess.Err).To(gbytes.Say("error: invalid argument for flag `" + osFlag("o", "old-name")))
 			})
 		})
 
@@ -169,6 +169,54 @@ var _ = Describe("rename-pipeline", func() {
 			Eventually(sess).Should(gexec.Exit(0))
 			Expect(sess.Out).To(gbytes.Say(fmt.Sprintf("pipeline successfully renamed to '%s'", newName)))
 
+		})
+	})
+
+	Context("when instance vars are involved", func() {
+		queryParams := "vars.branch=%22master%22"
+
+		Context("when renaming an instanced pipeline to another instanced pipeline", func() {
+			BeforeEach(func() {
+				atcServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("PUT", "/api/v1/teams/main/pipelines/some-pipeline/rename", queryParams),
+						ghttp.VerifyJSON(`{"name":"new-pipeline","instance_vars":{"branch":"develop"}}`),
+						ghttp.RespondWith(http.StatusNoContent, ""),
+					),
+				)
+			})
+
+			It("sends the old ref as query params and the new ref in the request body", func() {
+				flyCmd := exec.Command(flyPath, "-t", targetName, "rename-pipeline", "-o", "some-pipeline/branch:master", "-n", "new-pipeline/branch:develop")
+
+				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(sess).Should(gexec.Exit(0))
+				Expect(sess.Out).To(gbytes.Say("pipeline successfully renamed to 'new-pipeline/branch:develop'"))
+			})
+		})
+
+		Context("when transitioning from a non-instanced pipeline to an instanced pipeline", func() {
+			BeforeEach(func() {
+				atcServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("PUT", "/api/v1/teams/main/pipelines/some-pipeline/rename"),
+						ghttp.VerifyJSON(`{"name":"some-pipeline","instance_vars":{"branch":"develop"}}`),
+						ghttp.RespondWith(http.StatusNoContent, ""),
+					),
+				)
+			})
+
+			It("succeeds", func() {
+				flyCmd := exec.Command(flyPath, "-t", targetName, "rename-pipeline", "-o", "some-pipeline", "-n", "some-pipeline/branch:develop")
+
+				sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(sess).Should(gexec.Exit(0))
+				Expect(sess.Out).To(gbytes.Say("pipeline successfully renamed to 'some-pipeline/branch:develop'"))
+			})
 		})
 	})
 })
